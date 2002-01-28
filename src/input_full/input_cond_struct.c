51739816d111d06bf036e5d4a61507b7dc77b875
Michael Gee <gee@lnm.mw.tum.de>
2002-01-28 15:30:48 +0000

    structural condition splitted into neumann and dirichlet, see Gid problemtype as well
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@69 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/input_full/input_cond_struct.c
+++ src/input_full/input_cond_struct.c
@@ -33,7 +33,8 @@ dstrc_enter("inp_cond_nodal_struct");
 /*------------------------------------------ init the condition pointer */
 for (i=0; i<field->numnp; i++) field->node[i].c==NULL;
 /*------------------------------------------------- read the conditions */
-frfind("--STRUCT NODAL CONDITIONS");
+/*------------------------------------------- read dirichlet conditions */
+frfind("--STRUCT NODAL DIRICH CONDITIONS");
 frread();
 while(strncmp(allfiles.actplace,"------",6)!=0)
 {
@@ -70,14 +71,8 @@ while(strncmp(allfiles.actplace,"------",6)!=0)
       frint("CURVE",&(actnode->c->curve),&ierr);
       if (ierr!=1) dserror("cannot read COND_STRUCT_NODE from file");
    }
-/*------------- read whether its a neum condition or a dirich condition */    
+/*----------------------------------------------- init temporary arrays */   
    for (i=0; i<MAXDOFPERNODE; i++) { itmp[i]=0; dtmp[i]=0.0; }
-   frchk("DN",&ierr);
-   if (!ierr) dserror("Cannot read structural nodal conditions");
-   colpointer = strstr(allfiles.actplace,"DN");
-   colpointer++;
-   colpointer++;
-   colpointer++;
 /*------------------------------------------------ read the ONOFF flags */   
    frint_n("ONOFF" ,&(itmp[0]),genprob.numdf,&ierr);
    if (!ierr) dserror("Cannot read structural nodal conditions");
@@ -89,53 +84,97 @@ while(strncmp(allfiles.actplace,"------",6)!=0)
    {
       if (itmp[i]) /*--------------------- the i'th flag is switched on */
       {
-         if (strncmp(colpointer,"Neum",4)==0) /* Neumann type */
+         actnode->c->isdirich=1;
+         if (actnode->c->dirich_onoff.a.iv==NULL)
          {
-            actnode->c->isneum=1;
-            if (actnode->c->neum_onoff.a.iv==NULL)
-            {
-               amdef("neum1",&(actnode->c->neum_onoff),genprob.numdf,1,"IV");
-               amzero(&(actnode->c->neum_onoff));
-               amdef("neum2",&(actnode->c->neum_val)  ,genprob.numdf,1,"DV");
-               amzero(&(actnode->c->neum_val));
-            }
-            actnode->c->neum_onoff.a.iv[i]=itmp[i];
-            actnode->c->neum_val.a.dv[i] += dtmp[i];
-            colpointer=colpointer+5;
-            continue;
+            amdef("dirich1",&(actnode->c->dirich_onoff),genprob.numdf,1,"IV");
+            amzero(&(actnode->c->dirich_onoff));
+            amdef("dirich2",&(actnode->c->dirich_val)  ,genprob.numdf,1,"DV");
+            amzero(&(actnode->c->dirich_val));
          }
-         if (strncmp(colpointer,"Dirich",6)==0) /* Dirichlet type */
-         {
-            actnode->c->isdirich=1;
-            if (actnode->c->dirich_onoff.a.iv==NULL)
-            {
-               amdef("dirich1",&(actnode->c->dirich_onoff),genprob.numdf,1,"IV");
-               amzero(&(actnode->c->dirich_onoff));
-               amdef("dirich2",&(actnode->c->dirich_val)  ,genprob.numdf,1,"DV");
-               amzero(&(actnode->c->dirich_val));
-            }
-            actnode->c->dirich_onoff.a.iv[i]=itmp[i];
-            actnode->c->dirich_val.a.dv[i] += dtmp[i];
-            colpointer=colpointer+7;
-            continue;
-         }
-      }
-      else /*---------------------------- the i'th flag is switched off */
-      {
-         if (strncmp(colpointer,"Neum",4)==0) 
-         {
-            colpointer=colpointer+5;
-            continue;
-         }
-         if (strncmp(colpointer,"Dirich",6)==0) 
-         {
-            colpointer=colpointer+7;
-            continue;
-         }
+         actnode->c->dirich_onoff.a.iv[i]=itmp[i];
+         actnode->c->dirich_val.a.dv[i] += dtmp[i];
+         continue;
       }
    }
    frread();/* read next line of file */
-}
+}/* end of reading dirichlet conditions */
+/*----------------------------- rewind file and read neumann conditions */
+frrewind();
+
+
+
+
+
+
+
+/*--------------------------------------------- read neumann conditions */
+frfind("--STRUCT NODAL NEUM CONDITIONS");
+frread();
+while(strncmp(allfiles.actplace,"------",6)!=0)
+{
+   nodeid = strtol(allfiles.actplace,&colpointer,10);
+   nodeid--;
+   for (j=0; j<field->numnp; j++)
+   {
+      if (field->node[j].Id == nodeid) 
+      {
+         actnode = &(field->node[j]);
+         break;
+      }
+   }
+   if (actnode->c==NULL)
+   {
+      actnode->c=(COND_NODE*)calloc(1,sizeof(COND_NODE));
+      if (actnode->c==NULL) dserror("Allocation of COND_NODE failed"); 
+      actnode->c->isneum=0;
+      actnode->c->isdirich=0;
+      actnode->c->iscoupled=0;
+      actnode->c->neum_onoff.a.iv=NULL;
+      actnode->c->neum_val.a.iv=NULL;
+      actnode->c->dirich_onoff.a.iv=NULL;
+      actnode->c->dirich_val.a.iv=NULL;
+      actnode->c->couple.a.iv=NULL;
+   }
+/*--------------------------------------------------- read curve number */   
+/* be carefull, at the moment only one curve possible (in GID 5!)       */
+   frchar("CURVE",buffer,&ierr);
+   if (ierr!=1) dserror("cannot read COND_STRUCT_NODE from file");
+   if (strncmp(buffer,"none",4)==0) actnode->c->curve=0;
+   else
+   {
+      frint("CURVE",&(actnode->c->curve),&ierr);
+      if (ierr!=1) dserror("cannot read COND_STRUCT_NODE from file");
+   }
+/*----------------------------------------------- init temporary arrays */   
+   for (i=0; i<MAXDOFPERNODE; i++) { itmp[i]=0; dtmp[i]=0.0; }
+/*------------------------------------------------ read the ONOFF flags */   
+   frint_n("ONOFF" ,&(itmp[0]),genprob.numdf,&ierr);
+   if (!ierr) dserror("Cannot read structural nodal conditions");
+/*----------------------------------------------------- read the values */   
+   frdouble_n("VAL",&(dtmp[0]),genprob.numdf,&ierr);
+   if (!ierr) dserror("Cannot read structural nodal conditions");
+/*------------------------- now loop through the types of the condition */   
+   for (i=0; i<genprob.numdf; i++)
+   {
+      if (itmp[i]) /*--------------------- the i'th flag is switched on */
+      {
+         actnode->c->isneum=1;
+         if (actnode->c->neum_onoff.a.iv==NULL)
+         {
+            amdef("neum1",&(actnode->c->neum_onoff),genprob.numdf,1,"IV");
+            amzero(&(actnode->c->neum_onoff));
+            amdef("neum2",&(actnode->c->neum_val)  ,genprob.numdf,1,"DV");
+            amzero(&(actnode->c->neum_val));
+         }
+         actnode->c->neum_onoff.a.iv[i]=itmp[i];
+         actnode->c->neum_val.a.dv[i] += dtmp[i];
+         continue;
+      }
+   }
+   frread();/* read next line of file */
+}/* end of reading neumann conditions */
+frrewind();
 /*----------------------------------------------------------------------*/
 #ifdef DEBUG 
 dstrc_exit();
