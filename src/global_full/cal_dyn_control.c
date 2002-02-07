deee20ca3d75a19aa4303a3efbe9a9ee38d15e88
Michael Gee <gee@lnm.mw.tum.de>
2002-02-07 09:46:07 +0000

    added global variables
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@103 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/global_full/cal_dyn_control.c
+++ src/global_full/cal_dyn_control.c
@@ -1,5 +1,11 @@
 #include "../headers/standardtypes.h"
 /*----------------------------------------------------------------------*
+ |                                                       m.gee 06/01    |
+ | general problem data                                                 |
+ | global variable GENPROB genprob is defined in global_control.c       |
+ *----------------------------------------------------------------------*/
+extern struct _GENPROB     genprob;
+/*----------------------------------------------------------------------*
  |  routine to control dynamic execution                 m.gee 5/01     |
  *----------------------------------------------------------------------*/
 void caldyn()
