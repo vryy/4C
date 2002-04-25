f8b5d132a5835df1dd8e82db5a7d2de2f3f5ea05
Michael Gee <gee@lnm.mw.tum.de>
2002-04-25 09:21:25 +0000

    deleted minor bug
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@203 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/shell8/s8_loccoordnode.c
+++ src/shell8/s8_loccoordnode.c
@@ -4,7 +4,7 @@
 /*----------------------------------------------------------------------*
  | local coordinates of nodal points                      m.gee 6/01    |
  *----------------------------------------------------------------------*/
-double s8_local_coord_node(int node, int flag, ELEMENT_TYP typ)
+double s8_local_coord_node(int node, int flag, enum _DIS_TYP typ)
 {
 double     coord;
 const double node489[9][2] = {{1.0,1.0},{-1.0,1.0},{-1.0,-1.0},{1.0,-1.0},
