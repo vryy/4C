80be652a33d017de20cf9db510de239b4139f7a7
Michael Gee <gee@lnm.mw.tum.de>
2002-04-29 15:18:18 +0000

    working batch script for jobs on cuss
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@208 060e22d8-9d1c-0410-868a-e3a950d63d95

--- mpi_cuss_job.sh
+++ mpi_cuss_job.sh
@@ -43,14 +43,14 @@
 
 # give the job a name
 
-#$ -N hello
+#$ -N cca
 
 # setup resource requests like number of CPUs, memory, ...
 # eg 512M of virtual memory and 1 minute of CPU time, and 2-8 parallel CPUs 
 
 #$ -l h_vmem=512M
-#$ -l h_cpu=00:01:00
-#$ -pe mpi 2-8
+#$ -l h_cpu=00:05:00
+#$ -pe mpi 1-2
 
 # the following is necessary for parallel jobs 
 #$ -l cre=true
@@ -90,7 +90,7 @@
 # ----------------------------------------------------------------------
 # the real work starts here
 
-echo '...the real work starts here' `/usr/bin/date`
+echo '...the real work starts here' `/users/stgt/<name>/ccarat/cca.exe`
 
 # make special 'computer center extensions' available and setup environment
 
@@ -99,30 +99,31 @@ echo '...after ". /etc/bash_profile"'
 
 
 # make the script abort on any error
-set -e
+# set -e
 
 
 # if you need special packages use the same 'option' commands as
 # interactively; you can get a list by 'options'
 # eg for Gaussian 98
-
 # --- uncommented since not neeeded here --- option g98
 
 
 # you may want to create a working diretory for this job
 # in the local scratch area
 #
-# WORKBASE=/work/$USER
-# WORKDIR=$JOB_NAME.$JOB_ID
-# cd $WORKBASE
-# mkdir $WORKDIR
-# cd $WORKDIR
+WORKBASE=/work/$USER
+WORKDIR=$JOB_NAME.$JOB_ID
+cd $WORKBASE
+mkdir $WORKDIR
+cd $WORKDIR
 
 
 # if you need files from the submit host to be copied to the
 # local scratch area on the execution host add something like
 #
 # rcp$COD_O_HOST:<original_path>/<original_name> $WORKBASE/$WORKDIR
+rcp $COD_O_HOST:/users/stgt/<name>/ccarat/dyn_shell_lind.dat $WORKBASE/$WORKDIR
+rcp $COD_O_HOST:/users/stgt/<name>/ccarat/cca.exe $WORKBASE/$WORKDIR
 #
 # be aware that by default the batch system changes the current
 # directory to the one from which the job was submitted
@@ -133,23 +134,23 @@ set -e
 # ok, start the real work (my app here is just 'date')
 # (output gets discarded because we redirected stdout to /dev/null)
 
-echo '...preparing the parallel application start' `/usr/bin/date`
+echo '...preparing the parallel application start' `/users/stgt/<name>/ccarat/cca.exe`
 echo '...this job is running with' NSLOTS=$NSLOTS , $TMPDIR/machines:
 cat $TMPDIR/machines
 echo 'EOF' 
  
-echo '...starting the application' `/usr/bin/date`
+echo '...starting the application' `/users/stgt/<name>/ccarat/cca.exe`
  
 # the following command starts your MPI-executable in parallel 
 #                       --------------------------------------       
  
-$CODINE_ROOT/mpi/MPRUN -np $NSLOTS -Mf $TMPDIR/machines ./hello 
+$CODINE_ROOT/mpi/MPRUN -np $NSLOTS -Mf $TMPDIR/machines ./cca.exe dyn_shell_lind.dat testout
 #                                                       ------- 
  
 # you may substitute "hello" by your executable and you may add additional
 # arguments, but you must not change the other parts of this command. 
  
-echo '...end of the application' `/usr/bin/date`
+echo '...end of the application' `/users/stgt/<name>/ccarat/cca.exe`
 
 
 # do cleanup if required
@@ -159,4 +160,4 @@ echo '...end of the application' `/usr/bin/date`
 # cd $WORKBASE
 # rm -rf $WORKBASE/$WORKDIR
 
-echo '...end of clean up' `/usr/bin/date`
+echo '...end of clean up' `/users/stgt/gee/ccarat/cca.exe`
