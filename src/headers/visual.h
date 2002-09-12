02125d6c65b16471faf4a9fbd52df9ee7e4d66e3
Unknown Genk <genk@lnm.mw.tum.de>
2002-09-12 08:37:26 +0000

    header file for visualisation
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@356 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/headers/visual.h
@@ -0,0 +1,16 @@
+/*----------------------------------------------------------------------*
+ | control structure                                      m.gee 5/02    |
+ | for nonlinear structural dynamics                                    |           
+ *----------------------------------------------------------------------*/
+typedef struct _VISUAL_FLUID
+{
+int                          step;     
+
+long int                     time;
+
+long int                     handle_of_node_handles;
+int                          node_fdim;
+int                          node_sdim;
+long int                    *node_handles;
+
+} VISUAL_FLUID;
