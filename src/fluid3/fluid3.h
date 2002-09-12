a282a257d539536ad9a72c4a99583808a19c94ff
Unknown Genk <genk@lnm.mw.tum.de>
2002-09-12 08:49:18 +0000

    fluid3 parameters added
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@366 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/fluid3/fluid3.h
+++ src/fluid3/fluid3.h
@@ -1,18 +1,53 @@
-/*----------------------------------------------------------------------*
- | fluid3                                                 m.gee 8/00    |
- *----------------------------------------------------------------------*/
+/*!---------------------------------------------------------------------
+\file
+\brief The 3D fluid element
+
+------------------------------------------------------------------------*/
+/*!---------------------------------------------------------------------
+\brief fluid2                                                 
+
+<pre>                                                         genk 03/02 
+
+In this structure all variables used for element evaluation by the 3D
+fluid element fluid2 are stored.
+
+</pre>
+
+--------------------------------------------------------------------------*/
 typedef struct _FLUID3
 {
-int                nGP[3];   /* number of gaussian points in rst directions */
-int                nGP_tri;  /* number of gaussian points for triangle elements */
+int                ntyp;     /*!< flag for element type: 1=quad; 2=tri    */
+int                nGP[3];   /*!< number of gaussian points in rs direct. */
+int                is_ale;   /*!< flag whether there is ale to me or not  */
+struct _ELEMENT   *my_ale;   /*!< pointer to my ale ele, otherwise NULL   */
 
-int                is_ale;   /* flag whether there is ale to me or not */
-struct _ELEMENT   *my_ale;   /* pointer to my ale element, otherwise NULL */
+/*--------------------------------------------------- stabilisation flags */
+int                istabi;   /*!< stabilasation: 0=no; 1=yes		  */
+int                iadvec;   /*!< adevction stab.: 0=no; 1=yes  	  */
+int                ipres;    /*!< pressure stab.: 0=no; 1=yes		  */
+int                ivisc;    /*!< diffusion stab.: 0=no; 1=GLS-; 2=GLS+   */
+int                icont;    /*!< continuity stab.: 0=no; 1=yes 	  */
+int                istapa;   /*!< version of stab. parameter		  */
+int                istapc;   /*!< flag for stab parameter calculation	  */
+int                mk;       /*!< 0=mk fixed 1=min(1/3,2*C); -1 mk=1/3    */
+int                ihele[3]; /*!< x/y/z length-def. for vel/pres/cont stab*/
+int                ninths;   /*!< number of integ. points for streamlength*/
+
+/*---------------------------------------------------- stabilisation norm */
+int                norm_p;   /*!< p-norm: p+1<=infinity; 0=Max.norm       */
+
+/*----------------------------------------------- stabilisation constants */
+double             clamb;
+
+/*------------------------------------ statiblisation control information */
+int                istrle;   /*!< has streamlength to be computed	  */
+int                ivol ;    /*!< calculation of area length		  */
+int                iduring;  /*!< calculation during int.-pt.loop	  */
+int                itau[3];  /*!< has diagonals etc. to be computed	  */
+int                idiaxy;   /*!< flags for tau_? calculation 
+			     	  (-1: before; 1: during		  */
+
+/*--------------------------------- element sizes for stability parameter */
+double             hk[3];    /*!< vel/pres/cont                           */
 } FLUID3;
 
-
-
-/*----------------------------------------------------------------------*
- |  f3_inpele.c                                          m.gee 11/01    |
- *----------------------------------------------------------------------*/
-void f3inp(ELEMENT *ele);
