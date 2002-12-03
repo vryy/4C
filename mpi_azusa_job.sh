f9a916ef64e36e2510a0fbe762bfc05384bed7a9
Michael Gee <gee@lnm.mw.tum.de>
2002-12-03 16:04:55 +0000

    *** empty log message ***
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@467 060e22d8-9d1c-0410-868a-e3a950d63d95

--- mpi_azusa_job.sh
+++ mpi_azusa_job.sh
@@ -2,16 +2,16 @@
 #PBS -N ca_test
 #PBS -l nodes=1:ppn=2,cput=0:30:00,mem=500mb
 
-
-WORKDIR=$SCRDIR/gee
-cp ./cca_ita1_debg.exe $WORKDIR/gee/.
-cp ./dyn_shell_dirich.dat $WORKDIR/gee/.
-cd $WORKDIR
+HOMEDIR=$HOME/<myname>/ccarat
+SCRATCHDIR=$SCRDIR/gee
+cp $HOMEDIR/<exe> $SCRATCHDIR/.
+cp $HOMEDIR/<inputfile> $SCRATCHDIR/.
+cd $SCRATCHDIR
 #
 date
 echo  "start application:"
 #get_input job
-#mpirun -np 2 cca_ita1_debg.exe dyn_shell_dirich.dat xxx
+/opt/NECmpi/bin/mpirun -np 2 <exe> <inputfile xxx > xxx.stdout
 #save_result data
 echo "job finished"
 date
