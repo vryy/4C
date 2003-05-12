98dcf242f5211dc385f98d2004cb4b5470202c09
Unknown Genk <genk@lnm.mw.tum.de>
2003-05-12 15:06:08 +0000

    created: Aitken's iteration scheme for FSI
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@620 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/fsi_full/fsi_aitken.c
@@ -0,0 +1,129 @@
+/*!----------------------------------------------------------------------
+\file
+\brief AITKEN iteration
+
+*----------------------------------------------------------------------*/
+/*! 
+\addtogroup FSI
+*//*! @{ (documentation module open)*/
+#include "../headers/standardtypes.h"
+#include "../headers/solution_mlpcg.h"
+#include "../headers/solution.h"
+#include "fsi_prototypes.h"    
+#ifdef D_FSI
+static double done = 1.0E20;
+/*----------------------------------------------------------------------*
+ |                                                       m.gee 06/01    |
+ | ranks and communicators                                              |
+ | This structure struct _PAR par; is defined in main_ccarat.c
+ *----------------------------------------------------------------------*/
+ extern struct _PAR   par;  
+/*!---------------------------------------------------------------------*
+\brief compute relaxation parameter via AITKEN iteration
+
+<pre>                                                         genk 01/03
+
+RELAX(i) = 1 - NU(i) (NU(i) at time (n+1))
+NU(i) = N(i-1) + [NU(i-1) - 1)] * TOP/DEN
+TOP = [DELTAd(i) - DETLAd(i+1)] * DELTAd(i+1)
+DEN = [DELTAd(i) - DETLAd(i+1)]^2
+DELTAd(i) = d(i-1) - d~(i)
+d(i-1) ... relaxad interface displacements of last iteration step
+d~(i)  ... unrelaxed interface displacements of this iteration step
+
+see also Dissertation of D.P.MOK, chapter 6.4 
+		     
+</pre>
+
+\param *structfield   FIELD	     (i)   structural field
+\param *fsidyn 	      FSI_DYNAMIC    (i)   
+\param  itnum         int            (i)   actual iteration step
+\return void     
+
+------------------------------------------------------------------------*/
+void fsi_aitken(  
+                 FIELD          *structfield, 
+                 FSI_DYNAMIC    *fsidyn, 
+		 int             itnum
+	       )
+{
+int            i,j;           /* some counters                          */
+int            numdf;         /* number of nodal dofs                   */
+int            dof;           /* actual dof                             */
+static int     numnp_total;   /* total number of nodes                  */
+static int     numdf_total;   /* total number of coupled dofs           */
+static int    *sid;           /* flags for structural interface dofs    */
+static double  nu;            /* actual AITKEN factor                   */
+double         top=ZERO;      
+double         den=ZERO;
+double         del2;				
+double       **sol_mf;        /* multifield nodal solution array        */
+static ARRAY   del_a;      
+static double *del;           /* DELTAd(i)                              */
+NODE          *actsnode;      /* actual structural node                 */
+
+#ifdef DEBUG 
+dstrc_enter("fsi_aitken");
+#endif
+
+if (itnum==0)
+{
+   if (fsidyn->step==1)   
+   { 
+      nu          = ZERO; 
+      numnp_total = structfield->dis[0].numnp;
+      sid         = fsidyn->sid.a.iv;
+      numdf_total = fsidyn->sid.fdim;
+      del         = amdef("del",&del_a,numdf_total,1,"DV");
+      aminit(&del_a,&done);
+   }
+   else 
+      aminit(&del_a,&done);  
+}
+
+/*----------------------- the solution history sol_mf of the structfield:
+- sol_mf[0][j] holds the latest struct-displacements
+- sol_mf[1][j] holds the (relaxed) displacements of the last iteration step
+
+/*---------------------------------------------------------- loop nodes */
+for (i=0;i<numnp_total;i++)
+{
+   actsnode  = &(structfield->dis[0].node[i]);
+   numdf = actsnode->numdf; 
+   sol_mf = actsnode->sol_mf.a.da;
+   /*--------------------------------- loop dofs and check for coupling */
+   for (j=0;j<numdf;j++)
+   {
+      dof = actsnode->dof[j];      
+      dsassert(dof<numdf_total,"dofnumber not valid!\n");
+      if (sid[dof]==0) continue;
+      del2     = del[dof]; 
+      del[dof] = sol_mf[1][j] - sol_mf[0][j];
+      del2     = del2 - del[dof];
+      top     += del2*del[dof];
+      den     += del2*del2;        
+   } /* end of loop over dofs */   
+} /* end of loop over nodes */
+
+nu = nu + (nu - ONE)*top/den;
+
+/*---------------------------------------------------------- finalising */
+fsidyn->relax = ONE - nu;
+/*------------------------------------------------- output to the screen */
+if (par.myrank==0)
+{
+printf("\n");
+printf("AITKEN ITERATION: RELAX = %.5lf\n",fsidyn->relax);
+printf("\n");
+}
+
+#ifdef DEBUG 
+dstrc_exit();
+#endif
+return;
+} /* end of fsi_aitken */
+
+#endif
+
+/*! @} (documentation module close)*/
+
