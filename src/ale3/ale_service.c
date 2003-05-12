04c0eceea21a0f760cbf78628c5caa1ce3aa6c3c
Unknown Genk <genk@lnm.mw.tum.de>
2003-05-12 15:37:36 +0000

    created: service functions
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@661 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/ale3/ale_service.c
@@ -0,0 +1,50 @@
+/*!----------------------------------------------------------------------
+\file
+\brief init ale field
+
+*----------------------------------------------------------------------*/
+#ifdef D_ALE
+#include "../headers/standardtypes.h"
+/*! 
+\addtogroup Ale 
+*//*! @{ (documentation module open)*/
+/*!----------------------------------------------------------------------
+\brief 
+
+<pre>                                                         genk 03/03 
+
+</pre>
+
+\return void                                               
+
+*----------------------------------------------------------------------*/
+void ale_init(
+		    FIELD *actfield
+             )     
+{
+int i;
+int numnp_total;
+NODE *actnode;
+
+#ifdef DEBUG 
+dstrc_enter("ale_init");
+#endif  
+
+/*---------------------------------------------------- set some values */
+numnp_total  = actfield->dis[0].numnp;
+/*-------------------------------- allocate space for solution history */
+for (i=0;i<numnp_total;i++)
+{
+      actnode=&(actfield->dis[0].node[i]);
+      amredef(&(actnode->sol_increment),2,actnode->sol_increment.sdim,"DA");
+      amzero(&(actnode->sol_increment));
+}
+
+/*----------------------------------------------------------------------*/
+#ifdef DEBUG 
+dstrc_exit();
+#endif
+return;
+} /* end of ale_init*/ 
+#endif
+/*! @} (documentation module close)*/
