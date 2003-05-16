aa13a5ff25b2d5b046c7193a10d16a1b92025604
Unknown Neumann <neumann@lnm.mw.tum.de>
2003-05-16 13:27:07 +0000

    documentation added
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@680 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/output/out_gid_solfsi.c
+++ src/output/out_gid_solfsi.c
@@ -1,4 +1,15 @@
+/*!----------------------------------------------------------------------
+  \file
+  \brief controls output of fsi for gid
+
+  ------------------------------------------------------------------------*/
+/*! 
+  \addtogroup FSI
+  *//*! @{ (documentation module open)*/
+#ifdef D_FSI
 #include "../headers/standardtypes.h"
+#include "../headers/solution_mlpcg.h"
+#include "../headers/solution.h"
 #include "gid.h"
 #include "../shell8/shell8.h"
 #include "../shell9/shell9.h"
@@ -7,255 +18,279 @@
 #include "../ale2/ale2.h"
 #include "../ale3/ale3.h"
 /*----------------------------------------------------------------------*
- |                                                       m.gee 06/01    |
- | vector of numfld FIELDs, defined in global_control.c                 |
+  |                                                       m.gee 06/01    |
+  | vector of numfld FIELDs, defined in global_control.c                 |
  *----------------------------------------------------------------------*/
 extern struct _FIELD      *field;
 /*----------------------------------------------------------------------*
- |                                                       m.gee 06/01    |
- | general problem data                                                 |
- | global variable GENPROB genprob is defined in global_control.c       |
+  |                                                       m.gee 06/01    |
+  | general problem data                                                 |
+  | global variable GENPROB genprob is defined in global_control.c       |
  *----------------------------------------------------------------------*/
 extern struct _GENPROB     genprob;
 /*!----------------------------------------------------------------------
-\brief file pointers
+  \brief file pointers
 
-<pre>                                                         m.gee 8/00
-This structure struct _FILES allfiles is defined in input_control_global.c
-and the type is in standardtypes.h                                                  
-It holds all file pointers and some variables needed for the FRSYSTEM
-</pre>
-*----------------------------------------------------------------------*/
+  <pre>                                                         m.gee 8/00
+  This structure struct _FILES allfiles is defined in input_control_global.c
+  and the type is in standardtypes.h                                                  
+  It holds all file pointers and some variables needed for the FRSYSTEM
+  </pre>
+ *----------------------------------------------------------------------*/
 extern struct _FILES  allfiles;
 /*----------------------------------------------------------------------*
- |                                                       m.gee 06/01    |
- | pointer to allocate dynamic variables if needed                      |
- | dedfined in global_control.c                                         |
- | ALLDYNA               *alldyn;                                       |
+  |                                                       m.gee 06/01    |
+  | pointer to allocate dynamic variables if needed                      |
+  | dedfined in global_control.c                                         |
+  | ALLDYNA               *alldyn;                                       |
  *----------------------------------------------------------------------*/
 extern ALLDYNA      *alldyn;   
 /*----------------------------------------------------------------------*
- |                                                          mn 06/02    |
- | structure of flags to control output                                 |
- | defined in out_global.c                                              |
+  |                                                          mn 06/02    |
+  | structure of flags to control output                                 |
+  | defined in out_global.c                                              |
  *----------------------------------------------------------------------*/
- extern struct _IO_FLAGS     ioflags;
+extern struct _IO_FLAGS     ioflags;
 /*!----------------------------------------------------------------------
-\brief ranks and communicators
+  \brief ranks and communicators
 
-<pre>                                                         m.gee 8/00
-This structure struct _PAR par; is defined in main_ccarat.c
-and the type is in partition.h                                                  
-</pre>
+  <pre>                                                         m.gee 8/00
+  This structure struct _PAR par; is defined in main_ccarat.c
+  and the type is in partition.h                                                  
+  </pre>
 
-*----------------------------------------------------------------------*/
- extern struct _PAR   par;                      
+ *----------------------------------------------------------------------*/
+extern struct _PAR   par;                      
 
 
 
 /*----------------------------------------------------------------------*
- |  routine to write solution of a step to GID           m.gee 12/01    |
+  |  routine to write solution of a step to GID           m.gee 12/01    |
  *----------------------------------------------------------------------*/
-void out_gid_sol_fsi(FIELD *fluid, FIELD *structure)
+extern struct _CURVE *curve;
+/*!---------------------------------------------------------------------                                         
+  \brief routine to control output og fsi to gid
+
+  <pre>                                                           mn 05/03
+
+  This function writes the output of the current timestep into the
+  output-file for gid.  The output includes:
+  - fluid displacements (mesh deformation)
+  - fluid velocities
+  - fluid pressure
+  - structure displacements
+  - structure stresses
+  - structure velocities    (not yet implemented) 
+  - structure accelerations (not yet implemented)
+  </pre>
+
+  \param *fluidfield    FIELD   (i)     the fluid field to output
+  \param *structfield   FIELD   (i)     the structure field to output
+
+  \return void
+
+  ------------------------------------------------------------------------*/
+void out_gid_sol_fsi(FIELD *fluidfield, FIELD *structfield)
 {
-INT           i,j;
+  INT           i;
 
-static FLUID_DYNAMIC  *fdyn;
-static FSI_DYNAMIC    *fsidyn;
-static STRUCT_DYNAMIC *sdyn;
+  static FLUID_DYNAMIC  *fdyn;
+  static FSI_DYNAMIC    *fsidyn;
+  static STRUCT_DYNAMIC *sdyn;
 
-INTRA        *actintras;
-INTRA        *actintraf;
+  INTRA        *actintras;
+  INTRA        *actintraf;
 
-FILE         *out = allfiles.gidres;
+  FILE         *out = allfiles.gidres;
 
-NODE         *actnode;
-NODE         *actanode;
+  NODE         *actnode;
+  NODE         *actanode;
 
-char         *resulttype;
-char         *resultplace;
-char         *rangetable;
-INT           ncomponent;
-char         *componentnames[18];
+  char         *resulttype;
+  char         *resultplace;
+  char         *rangetable;
+  INT           ncomponent;
+  char         *componentnames[18];
 
-char          sign='"';
+  char          sign='"';
 
 
 #ifdef DEBUG 
-dstrc_enter("out_gid_sol_fsi");
+  dstrc_enter("out_gid_sol_fsi");
 #endif
-/*----------------------------------------------------------------------*/
-sdyn= alldyn[0].sdyn;
-fdyn= alldyn[1].fdyn;
-fsidyn= alldyn[3].fsidyn;
-/*----------------------------------------------------------------------*/
+  /*----------------------------------------------------------------------*/
+  sdyn= alldyn[0].sdyn;
+  fdyn= alldyn[1].fdyn;
+  fsidyn= alldyn[3].fsidyn;
+  /*----------------------------------------------------------------------*/
 #ifdef PARALLEL 
-actintraf    = &(par.intra[genprob.numff]);
-actintras    = &(par.intra[genprob.numsf]);
+  actintraf    = &(par.intra[genprob.numff]);
+  actintras    = &(par.intra[genprob.numsf]);
 #else
-actintraf   = (INTRA*)CCACALLOC(1,sizeof(INTRA));
-if (!actintraf) dserror("Allocation of INTRA failed");
-actintraf->intra_fieldtyp = fluid;
-actintraf->intra_rank     = 0;
-actintraf->intra_nprocs   = 1;
-actintras   = (INTRA*)CCACALLOC(1,sizeof(INTRA));
-if (!actintras) dserror("Allocation of INTRA failed");
-actintras->intra_fieldtyp = structure;
-actintras->intra_rank     = 0;
-actintras->intra_nprocs   = 1;
+  actintraf   = (INTRA*)CCACALLOC(1,sizeof(INTRA));
+  if (!actintraf) dserror("Allocation of INTRA failed");
+  actintraf->intra_fieldtyp = fluid;
+  actintraf->intra_rank     = 0;
+  actintraf->intra_nprocs   = 1;
+  actintras   = (INTRA*)CCACALLOC(1,sizeof(INTRA));
+  if (!actintras) dserror("Allocation of INTRA failed");
+  actintras->intra_fieldtyp = structure;
+  actintras->intra_rank     = 0;
+  actintras->intra_nprocs   = 1;
 #endif
 
 
-if ( sdyn->step != fdyn->step)
-  dserror("Something is wrong!!");
+  if ( sdyn->step != fdyn->step)
+    dserror("Something is wrong!!");
 
-fprintf(out,"#-------------------------------------------------------------------------------\n");
-fprintf(out,"# Converged Solution of timestep %d\n",fdyn->step);
-fprintf(out,"#-------------------------------------------------------------------------------\n");
+  fprintf(out,"#-------------------------------------------------------------------------------\n");
+  fprintf(out,"# Converged Solution of timestep %d\n",fdyn->step);
+  fprintf(out,"#-------------------------------------------------------------------------------\n");
 
-/* ------------- write displacements of structure field */
-/*----------------------------------------------------------------------*/
-if (ioflags.struct_disp_gid==1||ioflags.fluid_sol_gid==1)
-{
-   resulttype        = "VECTOR";
-   resultplace       = "ONNODES";
-   rangetable        = "standard_structure";
-   ncomponent        = genprob.ndim;
-   componentnames[0] = "x-displ";
-   componentnames[1] = "y-displ";
-   componentnames[2] = "z-displ";
-   /*-------------------------------------------------------------------*/
-   fprintf(out,"#-------------------------------------------------------------------------------\n");
-   fprintf(out,"# RESULT DISPLACEMENTS on FIELD FSI\n");
-   fprintf(out,"#-------------------------------------------------------------------------------\n");
-   fprintf(out,"RESULT %cdisplacement%c %cpcarat%c %d %s %s\n",
-                                                             sign,sign,
-                                                             sign,sign,
-                                                             sdyn->step,
-                                                             resulttype,
-                                                             resultplace
-                                                             );
-   fprintf(out,"RESULTRANGESTABLE %c%s%c\n",
-                                            sign,rangetable,sign
-                                            );
-   /*-------------------------------------------------------------------*/
-   switch (genprob.ndim)
-   {
-     case 3:
-       fprintf(out,"COMPONENTNAMES %c%s%c,%c%s%c,%c%s%c\n",
-                                                       sign,componentnames[0],sign,
-                                                       sign,componentnames[1],sign,
-                                                       sign,componentnames[2],sign
-                                                       );
-     break;
-     case 2:
-       fprintf(out,"COMPONENTNAMES %c%s%c,%c%s%c\n",
-                                                       sign,componentnames[0],sign,
-                                                       sign,componentnames[1],sign
-                                                       );
-     break;
-     default:
-       dserror("Unknown numer of dimensions");
-     break;
-   }
-   /*-------------------------------------------------------------------*/
-   fprintf(out,"VALUES\n");
-}
-if (ioflags.struct_disp_gid==1)
-{
-   for (i=0; i<structure->dis[0].numnp; i++)
-   {
-      actnode = &(structure->dis[0].node[i]);
+  /* ------------- write displacements of structure field */
+  /*----------------------------------------------------------------------*/
+  if (ioflags.struct_disp_gid==1||ioflags.fluid_sol_gid==1)
+  {
+    resulttype        = "VECTOR";
+    resultplace       = "ONNODES";
+    rangetable        = "standard_structure";
+    ncomponent        = genprob.ndim;
+    componentnames[0] = "x-displ";
+    componentnames[1] = "y-displ";
+    componentnames[2] = "z-displ";
+    /*-------------------------------------------------------------------*/
+    fprintf(out,"#-------------------------------------------------------------------------------\n");
+    fprintf(out,"# RESULT DISPLACEMENTS on FIELD FSI\n");
+    fprintf(out,"#-------------------------------------------------------------------------------\n");
+    fprintf(out,"RESULT %cdisplacement%c %cpcarat%c %d %s %s\n",
+        sign,sign,
+        sign,sign,
+        sdyn->step,
+        resulttype,
+        resultplace
+        );
+    fprintf(out,"RESULTRANGESTABLE %c%s%c\n",
+        sign,rangetable,sign
+        );
+    /*-------------------------------------------------------------------*/
+    switch (genprob.ndim)
+    {
+      case 3:
+        fprintf(out,"COMPONENTNAMES %c%s%c,%c%s%c,%c%s%c\n",
+            sign,componentnames[0],sign,
+            sign,componentnames[1],sign,
+            sign,componentnames[2],sign
+            );
+        break;
+      case 2:
+        fprintf(out,"COMPONENTNAMES %c%s%c,%c%s%c\n",
+            sign,componentnames[0],sign,
+            sign,componentnames[1],sign
+            );
+        break;
+      default:
+        dserror("Unknown numer of dimensions");
+        break;
+    }
+    /*-------------------------------------------------------------------*/
+    fprintf(out,"VALUES\n");
+  }
+  if (ioflags.struct_disp_gid==1)
+  {
+    for (i=0; i<structfield->dis[0].numnp; i++)
+    {
+      actnode = &(structfield->dis[0].node[i]);
       switch (genprob.ndim)
       {
-	case 3:
-        fprintf(out," %6d %18.5E %18.5E %18.5E\n",
-                                                   actnode->Id+1,
-                                                   actnode->sol.a.da[0][0],
-                                                   actnode->sol.a.da[0][1],
-                                                   actnode->sol.a.da[0][2]
-                                                   );
-	break;
-	case 2:
-        fprintf(out," %6d %18.5E %18.5E \n",
-                                                   actnode->Id+1,
-                                                   actnode->sol.a.da[0][0],
-                                                   actnode->sol.a.da[0][1]
-                                                   );
-        break;
-	default:
-	  dserror("Unknown number of dimensions");
-        break;
+        case 3:
+          fprintf(out," %6d %18.5E %18.5E %18.5E\n",
+              actnode->Id+1,
+              actnode->sol.a.da[0][0],
+              actnode->sol.a.da[0][1],
+              actnode->sol.a.da[0][2]
+              );
+          break;
+        case 2:
+          fprintf(out," %6d %18.5E %18.5E \n",
+              actnode->Id+1,
+              actnode->sol.a.da[0][0],
+              actnode->sol.a.da[0][1]
+              );
+          break;
+        default:
+          dserror("Unknown number of dimensions");
+          break;
       }
-   }
+    }
 
 
-/* write displacements of fluid field */
-/*-------------------------------------------------------------------*/
-}
-if (ioflags.fluid_sol_gid==1)
-{
-     for (i=0; i<fluid->dis[0].numnp; i++)
-     {
-       actnode = &(fluid->dis[0].node[i]);
-       actanode = (fluid->dis[0].node[i].gnode->mfcpnode[genprob.numaf]);
-       switch (genprob.ndim)
-       {
-         case 3:
-           fprintf(out," %6d %18.5E %18.5E %18.5E\n",
-               actnode->Id+1,
-               actanode->sol.a.da[fsidyn->actpos-1][0],
-               actanode->sol.a.da[fsidyn->actpos-1][1],
-               actanode->sol.a.da[fsidyn->actpos-1][2]
-               );
-           break;
-         case 2:
-           fprintf(out," %6d %18.5E %18.5E \n",
-               actnode->Id+1,
-               actanode->sol.a.da[fsidyn->actpos-1][0],
-               actanode->sol.a.da[fsidyn->actpos-1][1]
-               );
-           break;
-         default:
-           dserror("Unknown number of dimensions");
-           break;
-       }
-     }
-}
-if (ioflags.struct_disp_gid==1||ioflags.fluid_sol_gid==1)
-{
-   fprintf(out,"END VALUES\n");
-}
-/*-------------------------------------------------------------------*/
+    /* write displacements of fluid field */
+    /*-------------------------------------------------------------------*/
+  }
+  if (ioflags.fluid_sol_gid==1)
+  {
+    for (i=0; i<fluidfield->dis[0].numnp; i++)
+    {
+      actnode = &(fluidfield->dis[0].node[i]);
+      actanode = (fluidfield->dis[0].node[i].gnode->mfcpnode[genprob.numaf]);
+      switch (genprob.ndim)
+      {
+        case 3:
+          fprintf(out," %6d %18.5E %18.5E %18.5E\n",
+              actnode->Id+1,
+              actanode->sol.a.da[fsidyn->actpos-1][0],
+              actanode->sol.a.da[fsidyn->actpos-1][1],
+              actanode->sol.a.da[fsidyn->actpos-1][2]
+              );
+          break;
+        case 2:
+          fprintf(out," %6d %18.5E %18.5E \n",
+              actnode->Id+1,
+              actanode->sol.a.da[fsidyn->actpos-1][0],
+              actanode->sol.a.da[fsidyn->actpos-1][1]
+              );
+          break;
+        default:
+          dserror("Unknown number of dimensions");
+          break;
+      }
+    }
+  }
+  if (ioflags.struct_disp_gid==1||ioflags.fluid_sol_gid==1)
+  {
+    fprintf(out,"END VALUES\n");
+  }
+  /*-------------------------------------------------------------------*/
 
 
 
-/* write velocities and pressure of fluid field */
-if (ioflags.fluid_sol_gid==1) 
-{
-  out_gid_sol("velocity",fluid,actintraf,fdyn->step,fsidyn->actpos-1);
-  out_gid_sol("pressure",fluid,actintraf,fdyn->step,fsidyn->actpos-1);
-}
+  /* write velocities and pressure of fluid field */
+  if (ioflags.fluid_sol_gid==1) 
+  {
+    out_gid_sol("velocity",fluidfield,actintraf,fdyn->step,fsidyn->actpos-1);
+    out_gid_sol("pressure",fluidfield,actintraf,fdyn->step,fsidyn->actpos-1);
+  }
 
 
-/* write velocities, accelerations and stresses of structure field */
-if (ioflags.struct_disp_gid==1)
-{
-  out_gid_sol("velocities",structure,actintras,sdyn->step,1);
-  out_gid_sol("accelerations",structure,actintras,sdyn->step,2);
-}
-if (ioflags.struct_stress_gid==1)
-  out_gid_sol("stress"      ,structure,actintras,sdyn->step,0);
+  /* write velocities, accelerations and stresses of structure field */
+  if (ioflags.struct_disp_gid==1)
+  {
+    out_gid_sol("velocities",structfield,actintras,sdyn->step,1);
+    out_gid_sol("accelerations",structfield,actintras,sdyn->step,2);
+  }
+  if (ioflags.struct_stress_gid==1)
+    out_gid_sol("stress"      ,structfield,actintras,sdyn->step,0);
 
 
-/*----------------------------------------------------------------------*/
-fflush(out);
-/*----------------------------------------------------------------------*/
+  /*----------------------------------------------------------------------*/
+  fflush(out);
+  /*----------------------------------------------------------------------*/
 #ifdef DEBUG 
-dstrc_exit();
+  dstrc_exit();
 #endif
-return;
+  return;
 } /* end of out_gid_sol_fsi */
 
+#endif
 
-
+/*! @} (documentation module close)*/
