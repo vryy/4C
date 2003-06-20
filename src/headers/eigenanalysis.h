16e8a661af618f8d819706ae13529f7f4f25499b
Unknown Lipka <lipka@lnm.mw.tum.de>
2003-06-20 10:14:50 +0000

    header file for eigenvalue solution added
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@768 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/headers/eigenanalysis.h
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------*
+ |                                                          al 08/02    |
+ | general eigensolution-variables                                      |
+ *----------------------------------------------------------------------*/
+typedef struct _ALLEIG                 
+{
+EIG_SOL_TYPE       soltyp;    /* eigenvalue solution process                        */
+INT                sturm ;    /* perform sturm sequence check  0=no                 */
+INT                subtyp;    /* subspace: 1 = jacobi rotation 2 = qz-algorithm     */
+INT                ifsh;      /* flag for shift                                     */
+INT                ilmp;      /* use lumped mass matrix (0=consistent)              */
+INT                range;     /* search in a range                                  */
+INT                numvec;    /* number of iteration vectors                        */
+INT                nroot;     /* number of requested eigenvalues to be converged    */
+INT                itemax;    /* maximum number of iterations                       */
+INT                ifctr;     /* 1 = output to console 2 = print iteration on file  */ 
+DOUBLE             toleig;    /* tolerance to be used in convergence check          */
+DOUBLE             shift;     /* relative shift (removed after iteration)           */
+DOUBLE             boulo;     /* lower boundary                                     */
+DOUBLE             bouup;     /* upper boundary                                     */
+} ALLEIG;
+
