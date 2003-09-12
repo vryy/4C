1e8a0e6c54e668ca80cd41f7be60b566841130d0
Unknown Hartmann <hartmann@lnm.mw.tum.de>
2003-09-12 09:06:37 +0000

    unnecessary variables removed bug fixing for EAS
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@905 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/shell9/s9_vthv.c
+++ src/shell9/s9_vthv.c
@@ -23,14 +23,13 @@ due to EAS.
 </pre>
 \param  DOUBLE   **gmkovc   (o)  kovariant metric at specified location in shell body (cur. config.)
 \param  DOUBLE   **gmkonc   (o)  kontravariant metric at specified location in shell body (cur. config.)
-\param  DOUBLE    *epsh     (i)  strains from EAS (E_tilde)
+\param  DOUBLE    *epsh     (i)  strains from EAS (E_tilde) of this kin. layer
 \param  DOUBLE    *detc     (o)  determinant of gkovc (cur. config.)
 \param  DOUBLE     e3       (i)  zeta_3_L (within one material layer)
 \param  DOUBLE     h        (i)  total thickness of this element
 \param  DOUBLE    *klayhgt  (i)  hight of kin layer in % of total thickness of shell 
 \param  DOUBLE    *mlayhgt  (i)  hight of mat layer in % of adjacent kin layer 
 \param  INT        num_klay (i)  number of kin layers to this element  
-\param  INT        num_mlay (i)  number of mat layers to this kin layer 
 \param  INT        klay     (i)  actual kin layer 
 \param  INT        mlay     (i)  actual mat layer of this kin layer 
 
@@ -48,15 +47,14 @@ void s9_vthv(DOUBLE **gmkovc,
              DOUBLE  *klayhgt,   /* hight of kin layer in % of total thickness of shell */
              DOUBLE  *mlayhgt,   /* hight of mat layer in % of adjacent kin layer */
              INT      num_klay,  /* number of kin layers to this element */
-             INT      num_mlay,  /* number of mat layers to this kin layer */
              INT      klay,      /* actual kin layer */
              INT      mlay,      /* actual mat layer of this kin layer */
              DOUBLE   condfac)
 {
-INT i,jlay;
+INT i;
 DOUBLE deltah, h_mlay, h_kl;
 DOUBLE zeta_kl,zeta,det_dummy;
-
+/*----------------------------------------------------------------------*/
 #ifdef DEBUG 
 dstrc_enter("s9_vthv");
 #endif
@@ -71,21 +69,17 @@ for (i=0; i<=mlay; i++)                        /* sum of the absolute hights of 
 }
 zeta_kl = -1. + (-deltah*(1.-e3)+2.*h_mlay)/h_kl;  /* equals the theta3 value of the act. kinematic layer, see equation (5.45) in Dis. Braun */
 /*----------------------------------------------------------------------*/
-/*--- loop over all kinematic layers to get the continuity coeficients (Pic. 7.4 on p. 116 in Dis. Braun)-> old s9mmtr ------*/
-for (jlay=0; jlay<num_klay; jlay++)
-{
-   zeta = zeta_kl;
-   zeta = s9con(zeta,num_klay,klay,jlay,condfac);
-   if (zeta != 0.0)  /*---- interpolation -------- g1,g2 (kov.) -------*/
-   {
-     gmkovc[0][0] = gmkovc[0][0] + 2.0 * (epsh[0]+zeta*epsh[6]);
-     gmkovc[1][0] = gmkovc[1][0] +       (epsh[1]+zeta*epsh[7]);
-     gmkovc[2][0] = gmkovc[2][0] +       (epsh[3]+zeta*epsh[9]);
-     gmkovc[1][1] = gmkovc[1][1] + 2.0 * (epsh[2]+zeta*epsh[8]);
-     gmkovc[2][1] = gmkovc[2][1] +       (epsh[4]+zeta*epsh[10]);
-     gmkovc[2][2] = gmkovc[2][2] + 2.0 * (epsh[5]+zeta*epsh[11]);
-   }
-} /*================================ end loop over all layers ========*/
+
+zeta = zeta_kl;
+zeta = s9con(zeta,num_klay,klay,klay,condfac);
+
+  gmkovc[0][0] = gmkovc[0][0] + 2.0 * (epsh[0]+zeta*epsh[6])    ;
+  gmkovc[1][0] = gmkovc[1][0] +       (epsh[1]+zeta*epsh[7])    ;
+  gmkovc[2][0] = gmkovc[2][0] +       (epsh[3]+zeta*epsh[9])    ;
+  gmkovc[1][1] = gmkovc[1][1] + 2.0 * (epsh[2]+zeta*epsh[8])    ;
+  gmkovc[2][1] = gmkovc[2][1] +       (epsh[4]+zeta*epsh[10])   ;
+  gmkovc[2][2] = gmkovc[2][2] + 2.0 * (epsh[5]+zeta*epsh[11])   ;
+
 gmkovc[0][2] = gmkovc[2][0];
 gmkovc[1][2] = gmkovc[2][1];
 gmkovc[0][1] = gmkovc[1][0];
