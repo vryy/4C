bfd2fc18da9ec7d4e1bc56fc17a5a33cb77a9142
Unknown Hartmann <hartmann@lnm.mw.tum.de>
2003-09-12 09:11:32 +0000

    unnecessary variables removed new materials added (mises/hoff) -> WA
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@908 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/shell9/s9_init.c
+++ src/shell9/s9_init.c
@@ -13,6 +13,18 @@
 *//*! @{ (documentation module open)*/
 
 /*!----------------------------------------------------------------------
+\brief vector of multilayer material law
+
+<pre>                                                            sh 10/02
+This structure struct _MULTIMAT  *multimat is defined in global_control.c
+and the type is in materials.h                                                  
+It holds all information about the layered section data
+</pre>
+*----------------------------------------------------------------------*/
+extern struct _MULTIMAT  *multimat;
+
+
+/*!----------------------------------------------------------------------
 \brief initialize the element                                      
 
 <pre>                     m.gee 6/01              modified by    sh 10/02
@@ -31,17 +43,22 @@ memory for different arrays is allocated ("forces", "stresses").
 void s9init(FIELD *actfield)
 {
 INT          i,j,k;
+INT          size_j;
 ELEMENT     *actele;
 NODE        *actnode;
-S9_DATA      data;
-/*DOUBLE     **a3ref;*/
-INT          numele;
+
+INT          num_mlay,ml;     /*number of material layers to a kinematic layer*/
+INT          num_klay,kl;     /*number of kineamtic layers to this element*/
+INT          sumlay;          /*total number of layers to this element*/
+INT          actlay;          /*actual layers */
+MULTIMAT    *actmultimat;                            /* material of actual material layer */
+INT          wa_flag;         /*flag if allocation of Working Array is necessary*/
 INT          numa3;
 DOUBLE       a3[3];
 ARRAY        collaverdir_a;
 DOUBLE     **collaverdir;
 DOUBLE       h2;            /*thickness of actele -> evaluation of shared director of thickness of adjacent elements differ from each other*/
-
+/*----------------------------------------------------------------------*/
 #ifdef DEBUG 
 dstrc_enter("s9init");
 #endif
@@ -53,10 +70,95 @@ for (i=0; i<actfield->dis[0].numele; i++)
    /*---------------------------------------- init directors of element */
    s9a3(actele);
    /*--------------- allocate the space for forces -> stress resultants */
-/*   am4def("forces",&(actele->e.s9->forces),1,18,MAXGAUSS,0,"D3");
+/*   am4def("forces",&(actele->e.s9->forces),1,18,MAXGAUSS,0,"D3");*/
    /*------------------------- allocate the space for physical stresses */
-   am4def("stresses",&(actele->e.s9->stresses),1,6,MAXGAUSS,0,"D3");
+   am4def("stresses",&(actele->e.s9->stresses),1,6,MAXNODESTRESS_SHELL9,0,"D3");
    /*amdef("energy",&(actele->e.s9->energy),MAXGAUSS,6,"DA");*/
+
+
+  /*-------- init working array for each material layer if necessary ---*/
+  num_klay = actele->e.s9->num_klay;
+  /*check if there is a nonlinear material that needs a working array*/
+  wa_flag = 0;
+  sumlay = 0;
+  for (kl=0; kl<num_klay; kl++) /*loop all kinematic layers*/
+  {
+     num_mlay = actele->e.s9->kinlay[kl].num_mlay;
+     sumlay  += num_mlay; /*count total number of layers*/
+     for (ml=0; ml<num_mlay; ml++) /*loop all material layers*/
+     {
+       actmultimat = &(multimat[actele->e.s9->kinlay[kl].mmatID[ml]-1]);
+       if (actmultimat->mattyp == m_pl_mises  ||
+           actmultimat->mattyp == m_pl_hoff )     wa_flag = 1;
+     }/*end loop material layers*/
+  }/*end loop kinematic layers*/
+  
+  /*--- allocate memory for wa if wa_flag == 1 ; for each layer ----------*/
+  if (wa_flag == 1)
+  {
+     actele->e.s9->elewa = (S9_ELE_WA*)CCACALLOC(sumlay,sizeof(S9_ELE_WA));
+
+     actlay = 0;
+     for (kl=0; kl<num_klay; kl++) /*loop all kinematic layers*/
+     {
+        num_mlay = actele->e.s9->kinlay[kl].num_mlay;
+        for (ml=0; ml<num_mlay; ml++) /*loop all material layers*/
+        {
+          actmultimat = &(multimat[actele->e.s9->kinlay[kl].mmatID[ml]-1]);
+          /*-------------- init working array for plasticity -------------*/
+          if (actmultimat->mattyp == m_pl_mises ||
+              actmultimat->mattyp == m_pl_hoff )
+          {
+             size_j = actele->e.s9->nGP[0]*actele->e.s9->nGP[1]*actele->e.s9->nGP[2];
+             actele->e.s9->elewa[actlay].ipwa = (S9_IP_WA*)CCACALLOC(size_j,sizeof(S9_IP_WA));
+
+             /* mises ... */
+             if (actmultimat->mattyp == m_pl_mises)
+             {
+                for (k=0; k<size_j; k++)/*initialize for every gausspoint*/
+                {
+                  actele->e.s9->elewa[actlay].ipwa[k].epstn = 0.;
+                  actele->e.s9->elewa[actlay].ipwa[k].yip   = -1;
+ 
+                  for (j=0; j<6; j++)
+                  {
+                    actele->e.s9->elewa[actlay].ipwa[k].sig[j] = 0.;
+                    actele->e.s9->elewa[actlay].ipwa[k].eps[j] = 0.;
+                    actele->e.s9->elewa[actlay].ipwa[k].qn[j]  = 0.;
+                  }
+                }
+             }/*end mises ... */
+
+             /* hoff ... */
+             if (actmultimat->mattyp == m_pl_hoff)
+             {
+                for (k=0; k<size_j; k++)/*initialize for every gausspoint*/
+                {
+                  actele->e.s9->elewa[actlay].ipwa[k].dhard = 0.;
+                  actele->e.s9->elewa[actlay].ipwa[k].yip   = -1;
+ 
+                  for (j=0; j<6; j++)
+                  {
+                    actele->e.s9->elewa[actlay].ipwa[k].sig[j]    = 0.;
+                    actele->e.s9->elewa[actlay].ipwa[k].eps[j]    = 0.;
+                    actele->e.s9->elewa[actlay].ipwa[k].dkappa[j] = 0.;
+                    actele->e.s9->elewa[actlay].ipwa[k].gamma[j]  = 0.;
+                  }
+                  for (j=0; j<9; j++)
+                  {
+                    actele->e.s9->elewa[actlay].ipwa[k].rkappa[j] = 0.;
+                  }
+                }
+             }/*end hoff ... */
+
+          }/*end plasticity */
+
+          actlay += 1;
+
+        }/*end loop material layers*/
+    }/*end loop kinematic layers*/
+  }/*end wa_flag ==1 */
+
 }
 /* --------------------- now do modification of directors bischoff style */
  /*------------------------------ allocate space for directors at a node */
