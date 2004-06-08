cbb13043b631da68c45024ef3316e781be9452b5
Unknown Neumann <neumann@lnm.mw.tum.de>
2004-06-08 07:46:55 +0000

    added new warnings for too large values of MAXNOD etc.
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@1402 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/pss_full/pss_ds.c
+++ src/pss_full/pss_ds.c
@@ -674,9 +674,13 @@ warning	= 1	when trinangular ale elements are monitored with wrong
 		for a distyp it	has not been implemented for
 	= 4	min J quality criterion for ale elements is called for a
 		distyp it has not been implemented for 
-        = 5     ?? 
-        = 6     ??
-        = 7     your new warning issue
+        = 5     Function used for points outside the range 0 < xi < 1 
+        = 6     Function used for points not on the defined line
+        = 7     the value givne for MAXNOD is too large
+        = 8     the value givne for MAXELE is too large
+        = 9     the value givne for MAXDOFPERNODE is too large
+        = 10    the value givne for MAXGAUSS is too large
+        = 11    your new warning issue
 
 </pre>
 
@@ -699,6 +703,10 @@ static INT ale_quality_ca;
 static INT ale_quality_Je;
 static INT funct_range;     /* warning 5 */
 static INT funct_line;      /* warning 6 */
+static INT max_nod;             /* warning 7 */
+static INT max_ele;             /* warning 8 */
+static INT max_dofpernode;      /* warning 9 */
+static INT max_gauss;           /* warning 10 */
 /* DEFINE your new warning flag here!!! */
 
 FILE   *err = allfiles.out_err;
@@ -719,6 +727,10 @@ switch (task)
     ale_quality_Je = 0;
     funct_range = 0;
     funct_line  = 0;
+    max_nod        = 0;
+    max_ele        = 0 ;
+    max_dofpernode = 0;
+    max_gauss      = 0;
     /* INITIALISE your new warning here!!! */
   break;
   /*------------------------------------------------- create warning ---*/
@@ -736,6 +748,14 @@ switch (task)
       funct_range++;
     else if (warning == 6)
       funct_line++;
+    else if (warning == 7)
+      max_nod++;
+    else if (warning == 8)
+      max_ele++;
+    else if (warning == 9)
+      max_dofpernode++;
+    else if (warning ==10)
+      max_gauss++;
     /* COUNT your new warning here!!! */
   break;
   /*------------------------------------------------- write warnings ---*/
@@ -798,6 +818,30 @@ switch (task)
       {
         printf("\n WARNING PROC %i: Function used for points not on the defined line!!\n", par.myrank);
         fprintf(err,"\n WARNING PROC %i: Function used for points not on the defined line!!\n", par.myrank);
+      }
+
+      if (max_nod)
+      {
+        printf("\n WARNING PROC %i: The given value for MAXNOD was too large!! This will reduce the performance!!\n", par.myrank);
+        fprintf(err,"\n WARNING PROC %i: The given value for MAXNOD was too large!! This will reduce the performance!!\n", par.myrank);
+      }
+
+      if (max_ele)
+      {
+        printf("\n WARNING PROC %i: The given value for MAXELE was too large!! This will reduce the performance!!\n", par.myrank);
+        fprintf(err,"\n WARNING PROC %i: The given value for MAXELE was too large!! This will reduce the performance!!\n", par.myrank);
+      }
+
+      if (max_dofpernode)
+      {
+        printf("\n WARNING PROC %i: The given value for MAXDOFPERNODE was too large!! This will reduce the performance!!\n", par.myrank);
+        fprintf(err,"\n WARNING PROC %i: The given value for MAXDOFPERNODE was too large!! This will reduce the performance!!\n", par.myrank);
+      }
+
+      if (max_gauss)
+      {
+        printf("\n WARNING PROC %i: The given value for MAXGAUSS was too large!! This will reduce the performance!!\n", par.myrank);
+        fprintf(err,"\n WARNING PROC %i: The given value for MAXGAUSS was too large!! This will reduce the performance!!\n", par.myrank);
       }
       /* ADD more warning messages here!!! */
   #ifdef PARALLEL
