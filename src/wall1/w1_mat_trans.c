76cac43010aa458bd111037854715da78417ee80
Unknown Hartmann <hartmann@lnm.mw.tum.de>
2004-07-19 13:18:46 +0000

    bug fixing of condensation of 3D material law to plane stress
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@1535 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/wall1/w1_mat_trans.c
+++ src/wall1/w1_mat_trans.c
@@ -25,10 +25,29 @@ Maintainer: Andrea Hund
 \addtogroup WALL1 
 *//*! @{ (documentation module open)*/
 
-/*-----------------------------------------------------------------------|
-|      topic: blowing up plane stress/strain conditions           sh 7/02|
-|             to 3D --> 3D-Material Law                                  |
-|-----------------------------------------------------------------------*/
+/*!----------------------------------------------------------------------
+\brief blowing up plane stress/strain conditions                                       
+
+<pre>                                sh 7/02 modified by         sh 03/04
+This routine performs the blowing up of plane stress/strain conditions
+for a 3D-material formulation
+</pre>
+\param  DOUBLE       ym           (i)  youngs modulus
+\param  DOUBLE       pv           (i)  poissons ratio
+\param  ELEMENT     *ele          (i)  element array
+\param  WALL_TYPE    wtype        (i)  wtype=plane_stress/plane_strain
+\param  DOUBLE     **bop          (i)  B-Operator
+\param  DOUBLE      *gop          (i)  for incomp. modes
+\param  DOUBLE      *alpha        (i)  for incomp. modes
+\param  INT          ip           (i)  actual integration point
+\param  DOUBLE       strain[4]    (o)  strains (extended from) [11,22,12,33]
+
+\warning  NOTE: everything in here is with sorting [11,22,12,33]
+\return void                                               
+\sa calling: ---; called by: w1_mat_plast_mises_3D()   [w1_mat_plast_mises3D.c]
+                             w1_mat_plast_epc3D()      [w1_mat_plast_epc3D.c]
+
+*----------------------------------------------------------------------*/
 void w1mat_trans_up (DOUBLE     ym,
                      DOUBLE     pv,
                      ELEMENT   *ele,                                        
@@ -37,21 +56,11 @@ void w1mat_trans_up (DOUBLE     ym,
                      DOUBLE    *gop,
                      DOUBLE    *alpha,
                      INT        ip,
-                     DOUBLE    *stress,    /*actuel stress condensed        */
-                     DOUBLE    *stress3D,  /*actuel stress condensed [6]    */
-                     DOUBLE    *strain3D,  /*strains to be calculated [6]   */
-                     DOUBLE    *sig3D,     /*stresses from last update [6]  */ 
-                     DOUBLE    *eps3D,     /*strains from last update [6]   */
-                     DOUBLE    *qn3D,      /*backstress vektor [6]          */
-                     INT        newval)    /*controls evaluation of new stresses*/           
+                     DOUBLE     strain[4]) 
 {
 /*----------------------------------------------------------------------*/
-INT i,j;
+INT i;
 DOUBLE disd[5];
-DOUBLE sig[4];      /*stresses from last update -> WA*/
-DOUBLE eps[4];      /*strains from last update -> WA*/
-DOUBLE strain[4];   /*actual strains from displacements*/
-DOUBLE qn[4];       /*backstress vektor from last update -> WA*/
 DOUBLE sigi[4];     /*stress from last iteration step (not condensed*/
 DOUBLE epsi[4];     /*strains from last iteration step (with e33)*/
 DOUBLE di[4];       /*components d41,d42,d43,d44 of th const. tensor from
@@ -70,19 +79,7 @@ dstrc_enter("w1mat_trans_up");
   w1_disd (ele,bop,gop,alpha,wtype,disd) ;                  
 /*------------------------------------- get actual strains -> strain ---*/
   w1_eps (disd,local_wtype,strain);
-/*----------------------------- get old values -> sig, eps,epstn,yip ---*/
-  for (i=0; i<4; i++)
-  {
-    sig[i] = ele->e.w1->elewa[0].ipwa[ip].sig[i];
-    eps[i] = ele->e.w1->elewa[0].ipwa[ip].eps[i];
-    qn[i]  = ele->e.w1->elewa[0].ipwa[ip].qn[i];
-  }
 
-  if(newval==1)
-  {
-    for (i=0; i<4; i++)  stress[i] = sig[i];
-    goto end;
-  }
 
 /*extend the strain vector by eps_zz if plane_stress */  
 if(wtype==plane_stress)      
@@ -93,69 +90,13 @@ if(wtype==plane_stress)
     epsi[i]   = ele->e.w1->elewa[0].ipwa[ip].epsi[i];
     di[  i]   = ele->e.w1->elewa[0].ipwa[ip].di[  i];
   }
-  if (fabs(di[3]) - 0.0001 < 0.)
+  if (fabs(di[3]) - 0.0001 < 0.)  /*get elastic constitutive matrix DI*/
   {
     w1iwadi (ym, pv, di);
   }
   w1de33 (sigi,epsi,di,strain);
 } 
 
-/*-- change row 3<->4 and enlarge to 6 entries -> 3D ---------*/
-/*-- Sort: 11[0],22[1],33[2],12[3],23[4],13[5]  3D  (brick) --*/
-/*--       11[0],22[1],12[2],33[3]                  (wall)  --*/
-
-for (i=0; i<4; i++)
-{
-  sig3D[i]    = sig[i];
-  strain3D[i] = strain[i];
-  eps3D[i]    = eps[i];
-  qn3D[i]     = qn[i];
-}
-
-for (i=4; i<6; i++)
-{
-  sig3D[i]    = 0.;
-  strain3D[i] = 0.;
-  eps3D[i]    = 0.;
-  qn3D[i]     = 0.;
-}
-
-w1_vec_switch(sig3D,2,3);      
-w1_vec_switch(strain3D,2,3);      
-w1_vec_switch(eps3D,2,3);      
-w1_vec_switch(qn3D,2,3);      
-
-
-/*sig3D[0] = sig[0];    
-sig3D[1] = sig[1];
-sig3D[2] = sig[3];
-sig3D[3] = sig[2];
-sig3D[4] = 0.0;
-sig3D[5] = 0.0;
-
-strain3D[0] = strain[0];    
-strain3D[1] = strain[1];
-strain3D[2] = strain[3];
-strain3D[3] = strain[2];
-strain3D[4] = 0.0;
-strain3D[5] = 0.0;
-
-eps3D[0] = eps[0];    
-eps3D[1] = eps[1];
-eps3D[2] = eps[3];
-eps3D[3] = eps[2];
-eps3D[4] = 0.0;
-eps3D[5] = 0.0;
-
-qn3D[0] = qn[0];    
-qn3D[1] = qn[1];
-qn3D[2] = qn[3];
-qn3D[3] = qn[2];
-qn3D[4] = 0.0;
-qn3D[5] = 0.0;*/
-
-/*----------------------------------------------------------------------*/
-end:
 /*----------------------------------------------------------------------*/
 #ifdef DEBUG 
 dstrc_exit();
@@ -165,28 +106,48 @@ dstrc_exit();
 } /* end of w1mat_trans_up */
 
 
-/*-----------------------------------------------------------------------|
-|      topic: kondense 3D conditions                              sh 7/02|
-|             to plane stress/strain conditions                          |
-|-----------------------------------------------------------------------*/
-void w1mat_trans_down (DOUBLE   **d, /*current material matrix 3D -> 2D */
+/*!----------------------------------------------------------------------
+\brief kondense 3D conditions to plane stress/strain conditions                                   
+
+<pre>                                sh 7/02 modified by         sh 03/04
+This routine condenses stresses and D from 3D to 
+plane stress/strain conditions
+</pre>
+\param  DOUBLE     **d           (i/o) to be condensed
+\param  ELEMENT     *ele          (i)  element array
+\param  WALL_TYPE    wtype        (i)  wtype=plane_stress/plane_strain
+\param  INT          ip           (i)  actual integration point
+\param  INT          yipc         (i)  flag for storing in Ele-WA
+\param  DOUBLE      *stressc      (o)  condensed stresses [11,22,12,33]
+\param  DOUBLE      *sig          (i)  sig from last update [11,22,12,33]
+\param  DOUBLE      *eps          (i)  eps from last update [11,22,12,33]
+\param  DOUBLE      *stress       (i)  stress [11,22,12,33] to be condensed
+\param  DOUBLE      *strain       (i)  strain  [11,22,12,33]
+\param  DOUBLE      *qn           (i)  qn [11,22,12,33] to be condensed
+
+\warning NOTE: everything in here is with sorting [11,22,12,33]
+\return void                                               
+\sa calling: ---; called by: w1_mat_plast_mises_3D()   [w1_mat_plast_mises3D.c]
+                             w1_mat_plast_epc3D()      [w1_mat_plast_epc3D.c]
+
+*----------------------------------------------------------------------*/
+void w1mat_trans_down (DOUBLE   **d, 
                        ELEMENT   *ele,
                        WALL_TYPE  wtype,
                        INT        ip,
                        INT        yipc,
-                       DOUBLE    *stressc, /*condensed*/
+                       DOUBLE    *stressc, 
                        DOUBLE    *sig,
                        DOUBLE    *eps,
                        DOUBLE    *stress,
-                       DOUBLE    *strain,  /*actual strain [4]          */
+                       DOUBLE    *strain,  
                        DOUBLE    *qn)
 {
 /*----------------------------------------------------------------------*/
-INT i,j;
+INT i;
 DOUBLE tau[4];
 DOUBLE tauc[4];
 DOUBLE qnc[4];
-DOUBLE di[4];
 /*----------------------------------------------------------------------*/
 #ifdef DEBUG 
 dstrc_enter("w1mat_trans_down");
@@ -199,20 +160,11 @@ for (i=0; i<4; i++)
   tauc[i]   = 0.0;
   qnc[i]    = 0.0;
 }
-/*--------Sorting from [11,22,33,12,23,13] -> [11,22,12,33 | 23,13]  ---*/
-/*------------    switch columns and row position 3<->4           ------*/
-  for (i=0; i<6; i++) tau[i]  = stress[i] - qn[i];
+/*----------------------------------------------------------------------*/
+  for (i=0; i<4; i++) tau[i]  = stress[i] - qn[i];
+  for (i=0; i<4; i++) tauc[i] = tau[i] ;
+  for (i=0; i<4; i++) qnc[i]  = qn[i];
 
-  w1_vec_switch(tau,2,3);      
-  w1_vec_switch(stress,2,3);      
-  w1_vec_switch(strain,2,3);      
-  w1_vec_switch(qn,2,3);      
-  w1_vec_switch(sig,2,3);      
-  w1_vec_switch(eps,2,3);      
-  w1_matrix_switch(d,2,3,6);      
-
-  for (i=0; i<6; i++) tauc[i] = tau[i] ;
-  for (i=0; i<6; i++) qnc[i]  = qn[i];
 /******************************************/
 if (yipc>0)     /*stresses are available from last update*/
 {
@@ -226,6 +178,10 @@ if (yipc>0)     /*stresses are available from last update*/
      strain[i] = eps[i];
     }
     w1concep (d);
+    for (i=0; i<4; i++)
+    {
+      strain[i] = eps[i];
+    }
   }  
 }
 else /*(yipc < 0)*/
@@ -236,7 +192,7 @@ else /*(yipc < 0)*/
     w1consig (d,qn,qnc);
     for (i=0; i<4; i++)
     {
-     ele->e.w1->elewa[0].ipwa[ip].sigi[i] = tau[i];
+     ele->e.w1->elewa[0].ipwa[ip].sigi[i] = stress[i];
      ele->e.w1->elewa[0].ipwa[ip].epsi[i] = strain[i];
      ele->e.w1->elewa[0].ipwa[ip].di[  i] = d[i][3];
     }
@@ -246,7 +202,6 @@ else /*(yipc < 0)*/
 
 for (i=0 ; i<4; i++)
 {
-  stress[ i] = tau[ i] + qn[ i];
   stressc[i] = tauc[i] + qnc[i];
 }  
 
@@ -301,7 +256,7 @@ void w1_matrix_switch(DOUBLE **mat,   /* matrix do be modified          */
                       INT l)          /* length of row/column of matrix */
 {
 DOUBLE help;
-INT i,j;
+INT i;
 /*----------------------------------------------------------------------*/
 #ifdef DEBUG 
 dstrc_enter("w1_matrix_switch");
