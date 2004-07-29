2f7565008fff4baeb859c550f4de045ec81cabe9
Unknown Genk <genk@lnm.mw.tum.de>
2004-07-29 06:10:00 +0000

    warnings and unused flags (nic) removed
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@1573 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/fluid2/f2_calgalmat.c
+++ src/fluid2/f2_calgalmat.c
@@ -41,7 +41,6 @@ static FLUID_DYNAMIC *fdyn;
 In this routine the galerkin part of matrix Kvv is calculated:
 
 EULER/ALE:
-
     /
    |  2 * nue * eps(v) : eps(u)   d_omega
   /
@@ -109,7 +108,7 @@ void f2_calkvv( ELEMENT         *ele,
  |   icol - column number in element matrix                             |
  |   irn  - row node: number of node considered for matrix-row          |
  |   icn  - column node: number of node considered for matrix column    |  
-/*----------------------------------------------------------------------*/
+ *----------------------------------------------------------------------*/
 INT     irow, icol,irn,icn;
 DOUBLE  c,aux;
 
@@ -149,23 +148,20 @@ for (icn=0;icn<iel;icn++)
    |  v * u_old * grad(u)     d_omega
   /
  *----------------------------------------------------------------------*/
-if(fdyn->nic != 0) /* evaluate for Newton- and fixed-point-like-iteration */
+icol=0;
+for (icn=0;icn<iel;icn++)
 {
-   icol=0;
-   for (icn=0;icn<iel;icn++)
+   irow=0;  
+   for (irn=0;irn<iel;irn++)
    {
-      irow=0;  
-      for (irn=0;irn<iel;irn++)
-      {
-         aux = (velint[0]*derxy[0][icn] \
-               +velint[1]*derxy[1][icn])*funct[irn]*fac;
-	 estif[irow][icol]     += aux;
-         estif[irow+1][icol+1] += aux;
-         irow += 2;	 
-      } /* end loop over irn */
-      icol += 2;
-   } /* end loop over icn */
-} /* endif (fdyn->nic != 0) */
+      aux = (velint[0]*derxy[0][icn] \
+            +velint[1]*derxy[1][icn])*funct[irn]*fac;
+      estif[irow][icol]     += aux;
+      estif[irow+1][icol+1] += aux;
+      irow += 2;      
+   } /* end loop over irn */
+   icol += 2;
+} /* end loop over icn */
 
 /*----------------------------------------------------------------------*
    Calculate full Galerkin part of matrix Nr(u):
@@ -182,11 +178,11 @@ if (fdyn->nir != 0) /* evaluate for Newton iteraton */
       for (irn=0;irn<iel;irn++)
       {
          aux = funct[irn]*funct[icn]*fac;
-	 estif[irow][icol]     += aux*vderxy[0][0];
-	 estif[irow+1][icol]   += aux*vderxy[1][0];
-	 estif[irow+1][icol+1] += aux*vderxy[1][1];
-	 estif[irow][icol+1]   += aux*vderxy[0][1];
-	 irow += 2;
+         estif[irow][icol]     += aux*vderxy[0][0];
+         estif[irow+1][icol]   += aux*vderxy[1][0];
+         estif[irow+1][icol+1] += aux*vderxy[1][1];
+         estif[irow][icol+1]   += aux*vderxy[0][1];
+         irow += 2;
       } /* end loop over irn */
       icol += 2;
    } /* end loop over icn */
@@ -202,33 +198,28 @@ if (fdyn->nir != 0) /* evaluate for Newton iteraton */
     - for implicit free surface this term is nonlinear (Nc), since u_G is 
       unknown at the free surface. So it depends on the nonlinear iteration
       scheme if this term has to be taken into account:
-         - fixpoint: NO
 	 - Newton: YES
 	 - fixpoint-like: YES
  *----------------------------------------------------------------------*/
 if(ele->e.f2->is_ale !=0) /* evaluate only for ALE */
 {
-   if(ele->e.f2->fs_on!=2 || (fdyn->nic!=0 && ele->e.f2->fs_on==2))
+   dsassert(gridvint!=NULL,"no grid velocity calculated!\n");
+   icol=0;
+   for (icn=0;icn<iel;icn++)
    {
-      dsassert(gridvint!=NULL,"no grid velocity calculated!\n");
-      icol=0;
-      for (icn=0;icn<iel;icn++)
+      irow=0;  
+      for (irn=0;irn<iel;irn++)
       {
-         irow=0;  
-         for (irn=0;irn<iel;irn++)
-         {
-            aux = (gridvint[0]*derxy[0][icn] \
-                  +gridvint[1]*derxy[1][icn])*funct[irn]*fac;
-	    estif[irow][icol]     -= aux;
-            estif[irow+1][icol+1] -= aux;
-            irow += 2;	 
-         } /* end loop over irn */
-         icol += 2;
-      } /* end loop over icn */
-   } /* endif (fdyn->nic != 0) */
-   else
-      dserror("implicit free surface for fixpoint iteration not implemented yet!");
-} /* endif (is_ale !=0)
+         aux = (gridvint[0]*derxy[0][icn] \
+               +gridvint[1]*derxy[1][icn])*funct[irn]*fac;
+         estif[irow][icol]     -= aux;
+         estif[irow+1][icol+1] -= aux;
+         irow += 2;   
+      } /* end loop over irn */
+      icol += 2;
+   } /* end loop over icn */
+} /* endif (is_ale !=0) */
+
 /*----------------------------------------------------------------------*/
 #ifdef DEBUG 
 dstrc_exit();
@@ -259,20 +250,20 @@ NOTE: there's only one elestif
       --> Kpv is stored in estif[((2*iel)..(3*iel-1)][0..(2*iel-1)]
       
 </pre>
-\param **estif     DOUBLE	   (i/o)  ele stiffness matrix
-\param  *funct     DOUBLE	   (i)    nat. shape funcs
-\param **derxy     DOUBLE	   (i)    global coord. deriv.
-\param   fac       DOUBLE	   (i)    weighting factor
-\param   iel       INT  	   (i)    number of nodes of act. ele
+\param **estif     DOUBLE     (i/o)  ele stiffness matrix
+\param  *funct     DOUBLE     (i)    nat. shape funcs
+\param **derxy     DOUBLE     (i)    global coord. deriv.
+\param   fac       DOUBLE     (i)    weighting factor
+\param   iel       INT        (i)    number of nodes of act. ele
 \return void                                                                       
 
 ------------------------------------------------------------------------*/
 void f2_calkvp(
-		DOUBLE         **estif,   
-		DOUBLE          *funct,  
-		DOUBLE         **derxy,  
-		DOUBLE           fac,    
-		INT              iel      	     
+               DOUBLE         **estif,   
+               DOUBLE          *funct,  
+               DOUBLE         **derxy,  
+               DOUBLE           fac,    
+               INT              iel      	     
               )
 {
 /*----------------------------------------------------------------------*
@@ -283,7 +274,7 @@ void f2_calkvp(
  |   ird  - row dim.: number of spatial dimension at row node           |  
  |   posc - since there's only one full element stiffness matrix the    |
  |          column number has to be changed!                            |
-/*----------------------------------------------------------------------*/
+ *----------------------------------------------------------------------*/
 INT     irow, icol,irn,ird;  
 INT     posc;
 DOUBLE  aux;
@@ -305,16 +296,16 @@ dstrc_enter("f2_calkvp");
  *----------------------------------------------------------------------*/
 for (icol=0;icol<iel;icol++)
 {
-  irow=-1;
-  posc = icol + 2*iel;
-  for (irn=0;irn<iel;irn++)
-  {
-     for(ird=0;ird<2;ird++)
-     {      
-	aux = funct[icol]*derxy[ird][irn]*fac;
-	irow++;
-	estif[irow][posc] -= aux;
-	estif[posc][irow] -= aux;		
+   irow=-1;
+   posc = icol + 2*iel;
+   for (irn=0;irn<iel;irn++)
+   {
+      for(ird=0;ird<2;ird++)
+      {      
+         aux = funct[icol]*derxy[ird][irn]*fac;
+         irow++;
+         estif[irow][posc] -= aux;
+         estif[posc][irow] -= aux;		
      } /* end loop over ird */
   } /* end loop over irn */
 } /* end loop over icol */
@@ -344,20 +335,20 @@ NOTE: there's only one elestif
       --> Kvg is stored in estif[0..(2*iel-1)](3*iel)..(5*iel-1)]
       
 </pre>
-\param **estif     DOUBLE	   (i/o)  ele stiffness matrix
-\param **vderxy    DOUBLE	   (i)    global vel derivatives
-\param  *funct     DOUBLE	   (i)    nat. shape funcs
-\param   fac 	   DOUBLE	   (i)    weighting factor	           
-\param   iel	   INT  	   (i)	  number of nodes of act. ele
+\param **estif     DOUBLE           (i/o)  ele stiffness matrix
+\param **vderxy    DOUBLE           (i)    global vel derivatives
+\param  *funct     DOUBLE           (i)    nat. shape funcs
+\param   fac       DOUBLE           (i)    weighting factor	           
+\param   iel       INT              (i)    number of nodes of act. ele
 \return void                                                                       
 
 ------------------------------------------------------------------------*/
 void f2_calkvg( 
-		DOUBLE         **estif,   
-		DOUBLE         **vderxy, 
-		DOUBLE          *funct,  
-		DOUBLE           fac,    
-		INT              iel     
+               DOUBLE         **estif,   
+               DOUBLE         **vderxy, 
+               DOUBLE          *funct,  
+               DOUBLE           fac,    
+               INT              iel     
               )
 {
 /*----------------------------------------------------------------------*
@@ -366,7 +357,7 @@ void f2_calkvg(
  |   icol - column number in element matrix                             |
  |   irn  - row node: number of node considered for matrix-row          |
  |   icn  - column node: number of node considered for matrix column    |  
-/*----------------------------------------------------------------------*/
+ *----------------------------------------------------------------------*/
 INT     irow, icol,irn,icn;
 DOUBLE  aux;
 
@@ -394,11 +385,11 @@ if (fdyn->nir != 0) /* evaluate for Newton iteraton */
       for (irn=0;irn<iel;irn++)
       {
          aux = funct[irn]*funct[icn]*fac;
-	 estif[irow][icol]     -= aux*vderxy[0][0];
-	 estif[irow+1][icol]   -= aux*vderxy[1][0];
-	 estif[irow+1][icol+1] -= aux*vderxy[1][1];
-	 estif[irow][icol+1]   -= aux*vderxy[0][1];
-	 irow += 2;
+         estif[irow][icol]     -= aux*vderxy[0][0];
+         estif[irow+1][icol]   -= aux*vderxy[1][0];
+         estif[irow+1][icol+1] -= aux*vderxy[1][1];
+         estif[irow][icol+1]   -= aux*vderxy[0][1];
+         irow += 2;
       } /* end loop over irn */
       icol += 2;
    } /* end loop over icn */
@@ -437,10 +428,10 @@ NOTE: there's only one elemass
 
 ------------------------------------------------------------------------*/
 void f2_calmvv(
-		DOUBLE         **emass,  
-		DOUBLE          *funct, 
-		DOUBLE           fac,   
-		INT              iel    
+               DOUBLE         **emass,  
+               DOUBLE          *funct, 
+               DOUBLE           fac,   
+               INT              iel    
               )
 {
 /*----------------------------------------------------------------------*
@@ -449,10 +440,9 @@ void f2_calmvv(
  |   icol - column number in element matrix                             |
  |   irn  - row node: number of node considered for matrix-row          |
  |   icn  - column node: number of node considered for matrix column    |  
-/*----------------------------------------------------------------------*/
+ *----------------------------------------------------------------------*/
 INT     irow, icol,irn,icn;  
 INT     nvdfe;             /* number of velocity dofs of actual element */
-INT     debug=0;
 DOUBLE  aux;
 
 #ifdef DEBUG 
@@ -508,22 +498,22 @@ In this routine the galerkin part of matrix Kgg is calculated:
 NOTE: there's only one estif 			      
       
 </pre>
-\param **estif     DOUBLE	   (i/o)  ele stiffness matrix
-\param  *funct     DOUBLE	   (i)    nat. shape funcs at edge
-\param   fac       DOUBLE	   (i)    weighting factor
-\param   iel       INT   	   (i)    number of nodes of act. element
-\param  *iedgnod   INT             (i)    edge nodes
-\param   ngnode    INT             (i)    number of nodes of act. edge
+\param **estif     DOUBLE     (i/o)  ele stiffness matrix
+\param  *funct     DOUBLE     (i)    nat. shape funcs at edge
+\param   fac       DOUBLE     (i)    weighting factor
+\param   iel       INT        (i)    number of nodes of act. element
+\param  *iedgnod   INT        (i)    edge nodes
+\param   ngnode    INT        (i)    number of nodes of act. edge
 \return void                                                                       
 
 ------------------------------------------------------------------------*/
 void f2_calkgedge(
-	   	   DOUBLE         **estif,  
-		   DOUBLE          *funct, 
-		   DOUBLE           fac,
-		   INT             *iedgnod,    		      
-		   INT              iel,
-		   INT              ngnode
+                  DOUBLE         **estif,  
+                  DOUBLE          *funct, 
+                  DOUBLE           fac,
+                  INT             *iedgnod,    		      
+                  INT              iel,
+                  INT              ngnode
                 )
 {
 /*----------------------------------------------------------------------*
@@ -532,7 +522,7 @@ void f2_calkgedge(
  |   icol - column number in element matrix                             |
  |   irn  - row node: number of node considered for matrix-row          |
  |   icn  - column node: number of node considered for matrix column    |  
-/*----------------------------------------------------------------------*/
+ *----------------------------------------------------------------------*/
 INT     irow,icolv,icolg,irn,icn;  
 INT     nd;                 
 DOUBLE  aux;
@@ -577,6 +567,198 @@ dstrc_exit();
 return;
 } /* end of f2_calkgedge */
 
+
+/*!---------------------------------------------------------------------
+\brief evaluate galerkin part of Kgg
+
+<pre>                                                         genk 01/03
+  
+NOTE: there's only one estif 			      
+      
+</pre>
+\param **estif     DOUBLE     (i/o)  ele stiffness matrix
+\param  *funct     DOUBLE     (i)    nat. shape funcs at edge
+\param   fac       DOUBLE     (i)    weighting factor
+\param   iel       INT        (i)    number of nodes of act. element
+\param  *iedgnod   INT        (i)    edge nodes
+\param   ngnode    INT        (i)    number of nodes of act. edge
+\return void                                                                       
+
+------------------------------------------------------------------------*/
+void f2_calgfskgg(
+                  DOUBLE         **estif,  
+                  DOUBLE          *funct, 
+                  DOUBLE         **deriv,
+                  DOUBLE         **xjm,
+                  DOUBLE           det,
+                  DOUBLE           fac,
+                  INT              iel
+                  
+                )
+{
+INT inode, node_start;
+INT nd;
+INT icol,irow;
+INT i,j,k,l,m;
+DOUBLE bop[3][2*MAXNOD_F2];
+DOUBLE xji[2][2];
+DOUBLE d[3][3];
+DOUBLE db[3];
+DOUBLE e1,e2,e3;
+DOUBLE dum;
+
+#ifdef DEBUG 
+dstrc_enter("f2_calgfskgg");
+#endif		
+
+nd = 2*iel;
+
+/*---------------------------------------------------------- initialise */
+for (i=0;i<iel*2;i++)
+{
+   bop[0][i]=ZERO;
+   bop[1][i]=ZERO;
+   bop[2][i]=ZERO;
+}
+
+/*---------------------------------------------- inverse of jacobian ---*/
+xji[0][0] = xjm[1][1]/ det;
+xji[0][1] =-xjm[0][1]/ det;
+xji[1][0] =-xjm[1][0]/ det;
+xji[1][1] = xjm[0][0]/ det;
+
+/*----------------------------- get operator b of global derivatives ---*/
+for (inode=0; inode<iel; inode++)
+{
+  node_start = inode*2;
+  
+  bop[0][node_start+0] += xji[0][0] * deriv[0][inode] 
+                       +  xji[0][1] * deriv[1][inode];                             
+  bop[1][node_start+1] += xji[1][0] * deriv[0][inode]                             
+                       +  xji[1][1] * deriv[1][inode];                             
+  bop[2][node_start+1] = bop[0][node_start+0];                                                     
+  bop[2][node_start+0] = bop[1][node_start+1];                                                     
+} /* end of loop over nodes */
+
+/*---------------------------------------- get ficticous material data */
+e1=ONE;
+e2=ZERO;
+e3=e1/TWO;
+
+d[0][0]=e1;
+d[0][1]=e2;
+d[0][2]=ZERO;
+d[1][0]=e2;
+d[1][1]=e1;
+d[1][2]=ZERO;
+d[2][0]=ZERO;
+d[2][1]=ZERO;
+d[2][2]=e3;
+
+/*---------------------------------------- get element stiffness matrix */
+icol = iel * NUMDOF_FLUID2;
+for (j=0; j<nd; j++)
+{
+   for (k=0; k<3; k++)
+   {
+      db[k] = ZERO ;                                                              
+      for (l=0; l<3; l++)
+      {
+         db[k] += d[k][l]*bop[l][j]*fac ;
+      }
+   }
+   irow = iel * NUMDOF_FLUID2;
+   for (i=0; i<nd; i+=2)
+   {
+      dum = ZERO ;                                                                
+      for (m=0; m<3; m++)
+      {
+         dum += bop[m][i]*db[m] ;
+      }
+      estif[irow+i][icol] += dum ;
+   }
+   icol++;
+}
+
+/*----------------------------------------------------------------------*/ 
+#ifdef DEBUG 
+dstrc_exit();
+#endif
+
+return;
+} /* end of f2_calkgedge */
+
+/*!---------------------------------------------------------------------
+\brief evaluate galerkin part of Kgg
+
+<pre>                                                         genk 01/03
+  
+NOTE: there's only one estif 			      
+      
+</pre>
+\param **estif     DOUBLE     (i/o)  ele stiffness matrix
+\param  *funct     DOUBLE     (i)    nat. shape funcs at edge
+\param   fac       DOUBLE     (i)    weighting factor
+\param   iel       INT        (i)    number of nodes of act. element
+\param  *iedgnod   INT        (i)    edge nodes
+\param   ngnode    INT        (i)    number of nodes of act. edge
+\return void                                                                       
+
+------------------------------------------------------------------------*/
+void f2_calgfskgedge(
+                     DOUBLE         **estif,  
+                     DOUBLE          *funct, 
+                     DOUBLE          *vnint,
+                     DOUBLE           fac,
+                     INT             *iedgnod,
+                     INT              iel,                     
+                     INT              ngnode 
+                  
+                  )
+{
+INT     nd;                 
+INT     irow, icolv,icolg;
+INT     irn,icn;
+DOUBLE  aux;
+
+#ifdef DEBUG 
+dstrc_enter("f2_calgfskgedge");
+#endif		
+
+nd = NUMDOF_FLUID2*iel;
+
+/*----------------------------------------------------------------------*
+   Calculate full Galerkin part of matrix Kgv and Kgg:
+      /
+     |  w * n * u    d_gamma_FS
+    /
+
+     /
+  - |  w * n * u_G    d_gamma_FS
+   /
+ *----------------------------------------------------------------------*/
+for(icn=0;icn<ngnode;icn++)
+{
+   icolv = NUM_F2_VELDOF*iedgnod[icn];
+   icolg = icolv+nd;
+   for(irn=0;irn<ngnode;irn++)
+   {
+      irow = nd+1+NUM_F2_VELDOF*iedgnod[irn];
+      aux = funct[icn]*funct[irn]*fac;
+      estif[irow][icolv]     += aux*vnint[0];
+      estif[irow][icolv+1]   += aux*vnint[1];
+      estif[irow][icolg]     -= aux*vnint[0];
+      estif[irow][icolg+1]   -= aux*vnint[1];
+   } /* end loop over irow */
+} /* end loop over icol */
+
+/*----------------------------------------------------------------------*/ 
+#ifdef DEBUG 
+dstrc_exit();
+#endif
+
+return;
+} /* end of f2_calgfskgedge */
 
 #endif
 /*! @} (documentation module close)*/
