d9ca8f24444368802f7eecc7d0b3da2909d44ee8
Unknown Neumann <neumann@lnm.mw.tum.de>
2004-11-05 13:37:50 +0000

    looping algorithm for the test script
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@1885 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ testing/loop_all.sh
@@ -0,0 +1,122 @@
+# loop this list
+for file in $liste; do
+
+  # write time and filename to the srceen
+  number=`expr $number + 1`
+  echo ' '
+  echo '========================================'
+  echo 'Testing ' $file '   ('$number'/'$total')'
+
+
+  file_ohne=`echo $file | sed -e 's/Input\///g'`
+  inputfile=$file
+
+  # create Makefile
+  echo
+  echo '  Making Makefile ...'
+  echo "  NODEPS=$NODEPS ./configure $configfile $definefile"
+
+  . ./scripts/extract-defines.sh
+  . ./scripts/setup-variables.sh
+  . ./scripts/setup-libraries.sh
+  . ./scripts/setup-objects.sh
+  . ./scripts/build-define-header.sh
+  . ./scripts/build-makefile.sh
+
+  exe=$PROGRAMNAME
+
+  # remove executable
+  rm -f $exe
+
+  # get the start-time in seconds:
+  h=`date +"%H"`
+  m=`date +"%M"`
+  s=`date +"%S"`
+  s_time=`expr $s + 60 \* $m + 60 \* 60 \* $h`
+
+  # make the executable
+  echo
+  echo '  Making executable ...'
+  make -f $makefile clean 2>&1 | cat > make.log
+  make -f $makefile 2>&1 | cat >> make.log
+
+  # get the end-time in seconds:
+  h=`date +"%H"`
+  m=`date +"%M"`
+  s=`date +"%S"`
+  e_time=`expr $s + 60 \* $m + 60 \* 60 \* $h`
+  time=`expr $e_time - $s_time`
+  mins=`expr $time / 60`
+  secs=`expr $time % 60`
+  if [ $secs -le 9 ]; then
+    echo '    '$mins':0'$secs' mins:secs'
+  else
+    echo '    '$mins':'$secs' mins:secs'
+  fi
+
+
+  # check the success of the make
+  # (look for the executable)
+  if [ -a $exe ]; then
+    echo '    OK'
+    pass1=`expr $pass1 + 1`
+    chmod 755 $exe
+  else
+    echo '    Failed!!'
+
+    # save the log on failture
+    mv make.log $file_ohne.make.log
+
+    fail1=`expr $fail1 + 1`
+    continue
+  fi
+
+
+  # get the start-time in seconds:
+  h=`date +"%H"`
+  m=`date +"%M"`
+  s=`date +"%S"`
+  s_time=`expr $s + 60 \* $m + 60 \* 60 \* $h`
+
+  if [ "x$PARALLEL" = "xno" ]; then
+    # run the executable with the inputfile
+    echo
+    echo '  Running Input-file...'
+    ./$exe $file test_out_ >test.tmp
+  else
+    # parallel run
+    echo
+    echo '  Running Input-file in parallel...'
+    mpirun -np 2 ./$exe $file test_out_ >test.tmp
+  fi
+
+  # get the end-time in seconds:
+  h=`date +"%H"`
+  m=`date +"%M"`
+  s=`date +"%S"`
+  e_time=`expr $s + 60 \* $m + 60 \* 60 \* $h`
+  time=`expr $e_time - $s_time`
+  mins=`expr $time / 60`
+  secs=`expr $time % 60`
+  if [ $secs -le 9 ]; then
+    echo '    '$mins':0'$secs' mins:secs'
+  else
+    echo '    '$mins':'$secs' mins:secs'
+  fi
+
+  # evaluate the output
+  # (look for the word 'normally' in the output
+  count=`cat test.tmp | grep -c "normally"`
+  if [ $count -ge 1 ]; then
+    echo '    OK'
+    pass2=`expr $pass2 + 1`
+  else
+    echo '    Failed!!'
+    fail2=`expr $fail2 + 1`
+    # copy screen output to $file_ohne.scr
+    cp test.tmp $file_ohne.scr
+    continue
+  fi
+
+done
+# end of the loop over all input files
