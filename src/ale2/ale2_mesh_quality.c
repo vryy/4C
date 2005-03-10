4a69bb8d258bfba69c2a51c4cb74be9f04fbef40
Christiane Foerster <foerster@lnm.mw.tum.de>
2005-03-10 08:54:04 +0000

    some slight change in data structure
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@1960 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/ale2/ale2_mesh_quality.c
+++ src/ale2/ale2_mesh_quality.c
@@ -28,7 +28,7 @@ such that 1 - element with for rectangular corners
           0 - failure element with one corner with 180 degree angle
 
 </pre>
-\param **xyz  DOUBLE    (i)   elemental coordinates
+\param xyz[2][MAXNOD]  DOUBLE    (i)   elemental coordinates
 
 \warning There is nothing special to this routine
 \return DOUBLE corner angle
@@ -36,7 +36,7 @@ such that 1 - element with for rectangular corners
              called by: ale2_statik_ke(), ale2_statik_ke_test2()
 
 *----------------------------------------------------------------------*/
-DOUBLE ale2_corner_angle(DOUBLE **xyz)
+DOUBLE ale2_corner_angle(DOUBLE xyz[2][MAXNOD])
 {
 
 DOUBLE edge[4];     /* lengths of the element edges */
@@ -51,14 +51,14 @@ DOUBLE max;         /* maximal angular distortion */
 dstrc_enter("ale2_corner_angle");
 #endif
 /*--------------------------------------------- x- and y-differences ---*/
-delta_x[0] = xyz[0][0]-xyz[1][0];   /* line 0-1 */
-delta_y[0] = xyz[0][1]-xyz[1][1];
-delta_x[1] = xyz[1][0]-xyz[2][0];   /* line 1-2 */
-delta_y[1] = xyz[1][1]-xyz[2][1];
-delta_x[2] = xyz[2][0]-xyz[3][0];   /* line 2-3 */
-delta_y[2] = xyz[2][1]-xyz[3][1];
-delta_x[3] = xyz[3][0]-xyz[0][0];   /* line 3-0 */
-delta_y[3] = xyz[3][1]-xyz[0][1];
+delta_x[0] = xyz[0][0]-xyz[0][1];   /* line 0-1 */
+delta_y[0] = xyz[1][0]-xyz[1][1];
+delta_x[1] = xyz[0][1]-xyz[0][2];   /* line 1-2 */
+delta_y[1] = xyz[1][1]-xyz[1][2];
+delta_x[2] = xyz[0][2]-xyz[0][3];   /* line 2-3 */
+delta_y[2] = xyz[1][2]-xyz[1][3];
+delta_x[3] = xyz[0][3]-xyz[0][0];   /* line 3-0 */
+delta_y[3] = xyz[1][3]-xyz[1][0];
 /*------------------------------------------- evaluate element edges ---*/
 edge[0] = sqrt( delta_x[0] * delta_x[0]
               + delta_y[0] * delta_y[0] ); /*line 0-1*/
@@ -101,7 +101,7 @@ such that 1 - element with for corners of 60 degree angle
           0 - failure element with one corner with 180 degree angle
 
 </pre>
-\param **xyz  DOUBLE    (i)   elemental coordinates
+\param xyz  DOUBLE    (i)   elemental coordinates
 
 \warning There is nothing special to this routine
 \return DOUBLE corner angle
@@ -109,7 +109,7 @@ such that 1 - element with for corners of 60 degree angle
              called by: ale2_statik_ke(), ale2_statik_ke_test2()
 
 *----------------------------------------------------------------------*/
-DOUBLE ale2_corner_angle_tria(DOUBLE **xyz)
+DOUBLE ale2_corner_angle_tria(DOUBLE xyz[2][MAXNOD])
 {
 
 DOUBLE edge[3];     /* lengths of the element edges */
@@ -124,12 +124,12 @@ DOUBLE max;         /* maximal angular distortion */
 dstrc_enter("ale2_corner_angle_tria");
 #endif
 /*--------------------------------------------- x- and y-differences ---*/
-delta_x[0] = xyz[0][0]-xyz[1][0];   /* line 0-1 */
-delta_y[0] = xyz[0][1]-xyz[1][1];
-delta_x[1] = xyz[1][0]-xyz[2][0];   /* line 1-2 */
-delta_y[1] = xyz[1][1]-xyz[2][1];
-delta_x[2] = xyz[2][0]-xyz[0][0];   /* line 2-0 */
-delta_y[2] = xyz[2][1]-xyz[0][1];
+delta_x[0] = xyz[0][0]-xyz[0][1];   /* line 0-1 */
+delta_y[0] = xyz[1][0]-xyz[1][1];
+delta_x[1] = xyz[0][1]-xyz[0][2];   /* line 1-2 */
+delta_y[1] = xyz[1][1]-xyz[1][2];
+delta_x[2] = xyz[0][2]-xyz[0][0];   /* line 2-0 */
+delta_y[2] = xyz[1][2]-xyz[1][0];
 /*------------------------------------------- evaluate element edges ---*/
 edge[0] = sqrt( delta_x[0] * delta_x[0]
               + delta_y[0] * delta_y[0] ); /*line 0-1*/
@@ -169,7 +169,7 @@ for a square element and 0 represents an element that is degenerated to a
 triangle.
 
 </pre>
-\param **xyz  DOUBLE    (i)   elemental coordinates
+\param xyz[2][MAXNOD]  DOUBLE    (i)   elemental coordinates
 
 \warning There is nothing special to this routine
 \return void
@@ -177,7 +177,7 @@ triangle.
              called by: ale2_statik_ke(), ale2_statik_ke_test2()
 
 *----------------------------------------------------------------------*/
-DOUBLE ale2_aspect_ratio(DOUBLE **xyz)
+DOUBLE ale2_aspect_ratio(DOUBLE xyz[2][MAXNOD])
 {
 INT i;             /* a counter */
 
@@ -194,18 +194,18 @@ DOUBLE diag[2];    /* lengths of diagonals*/
 dstrc_enter("ale2_aspect_ratio");
 #endif
 /*-------------------------------------------------- squared lengths ---*/
-sqed[0] = (xyz[0][0]-xyz[1][0])*(xyz[0][0]-xyz[1][0])
-         +(xyz[0][1]-xyz[1][1])*(xyz[0][1]-xyz[1][1]); /* line 0-1 squared */
-sqed[1] = (xyz[1][0]-xyz[2][0])*(xyz[1][0]-xyz[2][0])
-         +(xyz[1][1]-xyz[2][1])*(xyz[1][1]-xyz[2][1]); /* line 1-2 squared */
-sqed[2] = (xyz[2][0]-xyz[3][0])*(xyz[2][0]-xyz[3][0])
-         +(xyz[2][1]-xyz[3][1])*(xyz[2][1]-xyz[3][1]); /* line 2-3 squared */
-sqed[3] = (xyz[3][0]-xyz[0][0])*(xyz[3][0]-xyz[0][0])
-         +(xyz[3][1]-xyz[0][1])*(xyz[3][1]-xyz[0][1]); /* line 3-0 squared */
-sqdg[0] = (xyz[2][0]-xyz[0][0])*(xyz[2][0]-xyz[0][0])
-         +(xyz[2][1]-xyz[0][1])*(xyz[2][1]-xyz[0][1]); /* diag 2-0 squared */
-sqdg[1] = (xyz[1][0]-xyz[3][0])*(xyz[1][0]-xyz[3][0])
-         +(xyz[1][1]-xyz[3][1])*(xyz[1][1]-xyz[3][1]); /* diag 1-3 squared */
+sqed[0] = (xyz[0][0]-xyz[0][1])*(xyz[0][0]-xyz[0][1])
+         +(xyz[1][0]-xyz[1][1])*(xyz[1][0]-xyz[1][1]); /* line 0-1 squared */
+sqed[1] = (xyz[0][1]-xyz[0][2])*(xyz[0][1]-xyz[0][2])
+         +(xyz[1][1]-xyz[1][2])*(xyz[1][1]-xyz[1][2]); /* line 1-2 squared */
+sqed[2] = (xyz[0][2]-xyz[0][3])*(xyz[0][2]-xyz[0][3])
+         +(xyz[1][2]-xyz[1][3])*(xyz[1][2]-xyz[1][3]); /* line 2-3 squared */
+sqed[3] = (xyz[0][3]-xyz[0][0])*(xyz[0][3]-xyz[0][0])
+         +(xyz[1][3]-xyz[1][0])*(xyz[1][3]-xyz[1][0]); /* line 3-0 squared */
+sqdg[0] = (xyz[0][2]-xyz[0][0])*(xyz[0][2]-xyz[0][0])
+         +(xyz[1][2]-xyz[1][0])*(xyz[1][2]-xyz[1][0]); /* diag 2-0 squared */
+sqdg[1] = (xyz[0][1]-xyz[0][3])*(xyz[0][1]-xyz[0][3])
+         +(xyz[1][1]-xyz[1][3])*(xyz[1][1]-xyz[1][3]); /* diag 1-3 squared */
 /*---------------------------------------------------------- lengths ---*/
 for (i=0; i<4; i++) edge[i] = sqrt(sqed[i]);
 for (i=0; i<2; i++) diag[i] = sqrt(sqdg[i]);
@@ -244,7 +244,7 @@ Normalisation such that 1 stands for a regular triangle element and
 0 represents an element that is degenerated to a line.
 
 </pre>
-\param **xyz  DOUBLE    (i)   elemental coordinates
+\param xyz[2][MAXNOD]  DOUBLE    (i)   elemental coordinates
 
 \warning There is nothing special to this routine
 \return void
@@ -252,7 +252,7 @@ Normalisation such that 1 stands for a regular triangle element and
              called by: ale2_statik_ke(), ale2_statik_ke_test2()
 
 *----------------------------------------------------------------------*/
-DOUBLE ale2_aspect_ratio_tria(DOUBLE **xyz)
+DOUBLE ale2_aspect_ratio_tria(DOUBLE xyz[2][MAXNOD])
 {
 INT i;             /* a counter */
 
@@ -265,12 +265,12 @@ DOUBLE edge[3];    /* lengths of edges */
 dstrc_enter("ale2_aspect_ratio");
 #endif
 /*-------------------------------------------------- squared lengths ---*/
-sqed[0] = (xyz[0][0]-xyz[1][0])*(xyz[0][0]-xyz[1][0])
-         +(xyz[0][1]-xyz[1][1])*(xyz[0][1]-xyz[1][1]); /* line 0-1 squared */
-sqed[1] = (xyz[1][0]-xyz[2][0])*(xyz[1][0]-xyz[2][0])
-         +(xyz[1][1]-xyz[2][1])*(xyz[1][1]-xyz[2][1]); /* line 1-2 squared */
-sqed[2] = (xyz[2][0]-xyz[0][0])*(xyz[2][0]-xyz[0][0])
-         +(xyz[2][1]-xyz[0][1])*(xyz[2][1]-xyz[0][1]); /* diag 2-0 squared */
+sqed[0] = (xyz[0][0]-xyz[0][1])*(xyz[0][0]-xyz[0][1])
+         +(xyz[1][0]-xyz[1][1])*(xyz[1][0]-xyz[1][1]); /* line 0-1 squared */
+sqed[1] = (xyz[0][1]-xyz[0][2])*(xyz[0][1]-xyz[0][2])
+         +(xyz[1][1]-xyz[1][2])*(xyz[1][1]-xyz[1][2]); /* line 1-2 squared */
+sqed[2] = (xyz[0][2]-xyz[0][0])*(xyz[0][2]-xyz[0][0])
+         +(xyz[1][2]-xyz[1][0])*(xyz[1][2]-xyz[1][0]); /* diag 2-0 squared */
 /*---------------------------------------------------------- lengths ---*/
 for (i=0; i<3; i++) edge[i] = sqrt(sqed[i]);
 /*----------------------------------------------------- aspect ratio ---*/
@@ -294,16 +294,16 @@ This routine writes the appropriate element quality to the ale element in
 order to prepare the element quality statistics
 
 </pre>
-\param  *ele       ELEMENT   (i)   actual element
-\param   quality   INT       (i)   flag, which quality to write
-\param **xyz       DOUBLE    (i)   elemental coordinates
-\param   min_detF  DOUBLE    (i)   minimal Jacobian determinant
+\param  *ele               ELEMENT   (i)   actual element
+\param   quality           INT       (i)   flag, which quality to write
+\param   xyz[2][MAXNOD]    DOUBLE    (i)   elemental coordinates
+\param   min_detF          DOUBLE    (i)   minimal Jacobian determinant
 
 \warning There is nothing special to this routine
 \return void
 \sa calling: ale2_aspect_ratio(), ale2_aspect_ratio_tria(),
              ale2_corner_angle(), ale2_corner_angle_tria(),
-	     ale2_el_area();
+	     area_lin_2d();
              called by: ale2_static_ke_stiff(), ale2_static_ke_prestress(),
 	     ale2_static_ke_step2(), ale2_static_ke_spring(),
 	     ale2_static_ke_laplace()
@@ -311,7 +311,7 @@ order to prepare the element quality statistics
 *----------------------------------------------------------------------*/
 void write_element_quality(ELEMENT  *ele,
                            INT       quality,
-			   DOUBLE  **xyz,
+			   DOUBLE    xyz[2][MAXNOD],
 			   DOUBLE    min_detF)
 {
 DOUBLE el_area;            /* area of the actual element */
@@ -326,6 +326,7 @@ else if (quality == 1)  /* case aspect ratio */
    switch (ele->distyp)
    {
       case quad4:
+      case quad9:
          ele->e.ale2->quality = ale2_aspect_ratio(xyz);
 	 break;
       case tri3:
@@ -353,7 +354,9 @@ else if (quality == 3)  /* case (normalised) min. Jacobian determinant */
    switch (ele->distyp)
    {
        case quad4:
-          el_area = ale2_el_area(xyz);
+       case quad8:
+       case quad9:
+          el_area = area_lin_2d(ele,xyz);
 	  ele->e.ale2->quality = min_detF * 4.0/el_area;
 	  break;
        case tri3:
