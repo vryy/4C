77020efdd33221338aa140545af8996201195da0
Unknown Neumann <neumann@lnm.mw.tum.de>
2005-03-10 17:20:51 +0000

    cosmetic change
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2056 060e22d8-9d1c-0410-868a-e3a950d63d95

--- scripts/extract-defines.sh
+++ scripts/extract-defines.sh
@@ -1,5 +1,5 @@
 
-# read the necessary defines from the input file and 
+# read the necessary defines from the input file and
 # write them to $definefile
 awk '/necessary defines:/ { for (i=4; i<=NF; i++) print $i }' "$inputfile" > "$definefile"
 awk '/#define / {print $3 "=" $4}' "$inputfile" | sed -e 's/(//g' -e 's/)//g' >> "$definefile"
