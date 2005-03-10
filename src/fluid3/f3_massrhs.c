039828e4a951090d77895347492768a63d275d25
Christiane Foerster <foerster@lnm.mw.tum.de>
2005-03-10 10:03:01 +0000

    volume body forces and possible free surface effects introduced
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2000 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/fluid3/f3_massrhs.c
+++ src/fluid3/f3_massrhs.c
@@ -1,7 +1,6 @@
 /*!----------------------------------------------------------------------
 \file
-\brief routine to evaluate iteration rhs on element base resulting from
-mass and acceleration in generalised alpha time integration
+\brief routine to evaluate mass right hand side
 
 <pre>
 Maintainer: Christiane Foerster
@@ -19,37 +18,59 @@ Maintainer: Christiane Foerster
 #include "fluid3_prototypes.h"
 #include "fluid3.h"
 #include "../fluid_full/fluid_prototypes.h"
+/*----------------------------------------------------------------------*
+ |                                                       m.gee 06/01    |
+ | pointer to allocate dynamic variables if needed                      |
+ | dedfined in global_control.c                                         |
+ | ALLDYNA               *alldyn;                                       |
+ *----------------------------------------------------------------------*/
+extern ALLDYNA      *alldyn;
+/*----------------------------------------------------------------------*
+ |                                                       m.gee 06/01    |
+ | general problem data                                                 |
+ | global variable GENPROB genprob is defined in global_control.c       |
+ *----------------------------------------------------------------------*/
+extern struct _GENPROB     genprob;
 
 /*!---------------------------------------------------------------------
-\brief calculates mass-acceleration-part of the rhs vector for
-       generalised-alpha time integration
+\brief calculates mass-rhs of history values
 
 <pre>                                                        chfoe 11/03
+                                    free surface included    chfoe 02/05
 
-This routine performs the multiplication emass*acc(n) and writes its
-negative result on the elemental iteration force vector. This gives a
-part of the rhs required in the generalised alpha time integration scheme
-for fluids.
+This routine performs the multiplication emass*(hist+deadload) and writes 
+its result on the elemental iteration force vector. This replaces the 
+time right hand side of WAW.
 
 </pre>
 \param	 *ele		ELEMENT		(i)	actual element
 \param	**emass		DOUBLE		(i)	element mass matrix
-\param  **eaccn		DOUBLE		(i)	element nodal accelerations
+\param  **hist		DOUBLE		(i)	elemental nodal history data
+\param   *edeadng       DOUBLE          (i)     elemental body force at t=n+1
 \param 	 *eiforce	DOUBLE	        (o)	element iteration force vec
+\param   *hasext        INT             (i)     flag, if there's external load
 
-\warning This routine has not yet been tested!!!
+\warning This routine sets *hasext to true in order to ensure assembly!
 
 \return void
 \sa calling:
              called by: f3_calele()
 
 ------------------------------------------------------------------------*/
-void f3_massrhs(ELEMENT *ele, DOUBLE **emass, DOUBLE **eaccn, DOUBLE *eiforce)
+void f3_massrhs(ELEMENT *ele, 
+                DOUBLE **emass, 
+                DOUBLE **hist,  
+                DOUBLE  *edeadng, 
+                DOUBLE  *eiforce,
+                INT     *hasext)
 {
 INT 	i,j;		/* counters					*/
 INT 	iel;		/* element node number				*/
 INT	dim=3;		/* dimension, number of velocity-dofs		*/
 INT	dofpern=dim+1;	/* degrees of freedom per node			*/
+INT     row[MAXNOD_F3]; /* index field for free surface elements        */
+DOUBLE  timefac;        /* factor from time integration                 */
+FLUID_DYNAMIC   *fdyn;  /* pointer to fluid dynamic variables           */
 
 /*----------------------------------------------------------------------*/
 #ifdef DEBUG
@@ -57,26 +78,74 @@ dstrc_enter("f3_massrhs");
 #endif
 
 /*------------------------------------------------- set initial data ---*/
-iel=ele->numnp;
+iel     = ele->numnp;
+fdyn    = alldyn[genprob.numff].fdyn;
+timefac = fdyn->thsl;
 
-/*------------------------------------------- perform multiplication ---*/
-for (i=0; i<iel; i++)
-{
-   for (j=0; j<iel; j++)
+/*----------------------------------------------------------------------*
+ |       compute "external" Force Vector (b)                            |
+ |  dead load may vary over time, but stays constant over               |
+ |  the whole domain --> no nodal dependence                            |
+ *----------------------------------------------------------------------*/
+/*--------------------- add body force load to right hand side terms ---*/
+if(*hasext)
+   for(i=0; i<iel; i++)
    {
-      eiforce[i*dofpern]   += (emass[i*dim][j*dim]   * eaccn[0][j]
-                             + emass[i*dim][j*dim+1] * eaccn[1][j]
-                             + emass[i*dim][j*dim+2] * eaccn[2][j]);
-      eiforce[i*dofpern+1] += (emass[i*dim+1][j*dim]   * eaccn[0][j]
-                             + emass[i*dim+1][j*dim+1] * eaccn[1][j]
-                             + emass[i*dim+1][j*dim+2] * eaccn[2][j]);
-      eiforce[i*dofpern+2] += (emass[i*dim+2][j*dim]   * eaccn[0][j]
-                             + emass[i*dim+2][j*dim+1] * eaccn[1][j]
-                             + emass[i*dim+2][j*dim+2] * eaccn[2][j]);
-      eiforce[i*dofpern+3] += (emass[iel*dim+i][j*dim]   * eaccn[0][j]
-                             + emass[iel*dim+i][j*dim+1] * eaccn[1][j]
-                             + emass[iel*dim+i][j*dim+2] * eaccn[2][j]);
+      hist[0][i] += timefac * edeadng[0];
+      hist[1][i] += timefac * edeadng[1];
+      hist[2][i] += timefac * edeadng[2];
    }
+else *hasext = 1; /* this assures rhs assembly! */
+
+switch(ele->e.f3->fs_on)
+{
+case 0: case 1: case 3:
+   /*------------------------------------------- perform multiplication ---*/
+   for (i=0; i<iel; i++)
+   {
+      for (j=0; j<iel; j++)
+      {
+         eiforce[i*dofpern]   += (emass[i*dim][j*dim]   * hist[0][j]
+                                + emass[i*dim][j*dim+1] * hist[1][j]
+                                + emass[i*dim][j*dim+2] * hist[2][j]);
+         eiforce[i*dofpern+1] += (emass[i*dim+1][j*dim]   * hist[0][j]
+                                + emass[i*dim+1][j*dim+1] * hist[1][j]
+                                + emass[i*dim+1][j*dim+2] * hist[2][j]);
+         eiforce[i*dofpern+2] += (emass[i*dim+2][j*dim]   * hist[0][j]
+                                + emass[i*dim+2][j*dim+1] * hist[1][j]
+                                + emass[i*dim+2][j*dim+2] * hist[2][j]);
+         eiforce[i*dofpern+3] += (emass[iel*dim+i][j*dim]   * hist[0][j]
+                                + emass[iel*dim+i][j*dim+1] * hist[1][j]
+                                + emass[iel*dim+i][j*dim+2] * hist[2][j]);
+      }
+   }
+
+break;
+case 2: /* element has free surface contribution */
+   row[0] = 0;
+   for (i=0; i<iel; i++) row[i+1] = row[i]+ele->node[i]->numdf;
+   /*---------------------------------------- perform multiplication ---*/
+   for (i=0; i<iel; i++)
+   {
+      for (j=0; j<iel; j++)
+      {
+         eiforce[row[i]]   += (emass[i*dim][j*dim]   * hist[0][j]
+                             + emass[i*dim][j*dim+1] * hist[1][j]
+                             + emass[i*dim][j*dim+2] * hist[2][j]);
+         eiforce[row[i]+1] += (emass[i*dim+1][j*dim]   * hist[0][j]
+                             + emass[i*dim+1][j*dim+1] * hist[1][j]
+                             + emass[i*dim+1][j*dim+2] * hist[2][j]);
+         eiforce[row[i]+2] += (emass[i*dim+2][j*dim]   * hist[0][j]
+                             + emass[i*dim+2][j*dim+1] * hist[1][j]
+                             + emass[i*dim+2][j*dim+2] * hist[2][j]);
+         eiforce[row[i]+3] += (emass[iel*dim+i][j*dim]   * hist[0][j]
+                             + emass[iel*dim+i][j*dim+1] * hist[1][j]
+                             + emass[iel*dim+i][j*dim+2] * hist[2][j]);
+      }
+   }
+break;
+default:
+   dserror("parameter fs_on out of range!\n");
 }
 
 /*----------------------------------------------------------------------*/
