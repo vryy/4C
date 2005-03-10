76de4bea6a576040ec0ab6cc6704a78080ef6184
Christiane Foerster <foerster@lnm.mw.tum.de>
2005-03-10 10:52:45 +0000

    position flags for fluid nodal sol_increment field introduced
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2023 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/fluid_full/inp_fluid_start_data.c
+++ src/fluid_full/inp_fluid_start_data.c
@@ -34,6 +34,19 @@ extern struct _MATERIAL  *mat;
  | variables needed for parallel comp.                    m.gee 8/00    |
  *----------------------------------------------------------------------*/
 extern struct _PAR   par;
+/*!----------------------------------------------------------------------
+\brief positions of physical values in node arrays
+
+<pre>                                                        chfoe 11/04
+
+This structure contains the positions of the various fluid solutions 
+within the nodal array of sol_increment.a.da[ipos][dim].
+
+extern variable defined in fluid_service.c
+</pre>
+
+------------------------------------------------------------------------*/
+extern struct _FLUID_POSITION ipos;
 /*!---------------------------------------------------------------------
 \brief input from file 'fluid_start-data'
 
@@ -252,25 +265,30 @@ for (i=0;i<numnp;i++)
    actnode=&(actfield->dis[0].node[i]);
    num = actnode->Id_loc;
    actnode->sol.a.da[0][0] = velx[num];
-   actnode->sol_increment.a.da[1][0] = velx[num];
-   actnode->sol_increment.a.da[3][0] = velx[num];
+   actnode->sol_increment.a.da[ipos.velnm][0] = velx[num];
+   actnode->sol_increment.a.da[ipos.veln][0] = velx[num];
+   actnode->sol_increment.a.da[ipos.velp][0] = velx[num];
    actnode->sol.a.da[0][1] = vely[num];
-   actnode->sol_increment.a.da[1][1] = vely[num];
-   actnode->sol_increment.a.da[3][1] = vely[num];
+   actnode->sol_increment.a.da[ipos.velnm][1] = vely[num];
+   actnode->sol_increment.a.da[ipos.veln][1] = vely[num];
+   actnode->sol_increment.a.da[ipos.velp][1] = vely[num];
    if(numdf==4)
    {
       actnode->sol.a.da[0][2] = velz[num];
-      actnode->sol_increment.a.da[1][2] = velz[num];
-      actnode->sol_increment.a.da[3][2] = velz[num];
+      actnode->sol_increment.a.da[ipos.velnm][2] = velz[num];
+      actnode->sol_increment.a.da[ipos.veln][2] = velz[num];
+      actnode->sol_increment.a.da[ipos.velp][2] = velz[num];
       actnode->sol.a.da[0][3] = pre[num]/dens;
-      actnode->sol_increment.a.da[1][3] = pre[num]/dens;
-      actnode->sol_increment.a.da[3][3] = pre[num]/dens;
+      actnode->sol_increment.a.da[ipos.velnm][3] = pre[num]/dens;
+      actnode->sol_increment.a.da[ipos.veln][3] = pre[num]/dens;
+      actnode->sol_increment.a.da[ipos.velp][3] = pre[num]/dens;
    }
    else
    {
       actnode->sol.a.da[0][2] = pre[num];
-      actnode->sol_increment.a.da[1][2] = pre[num]/dens;
-      actnode->sol_increment.a.da[3][2] = pre[num]/dens;
+      actnode->sol_increment.a.da[ipos.velnm][2] = pre[num]/dens;
+      actnode->sol_increment.a.da[ipos.veln][2] = pre[num]/dens;
+      actnode->sol_increment.a.da[ipos.velp][2] = pre[num]/dens;
    }
 }
 /*--------------------------------------------- delete temporary arrays */
