4874782d79710e276af8fc1859e970efee385722
Christiane Foerster <foerster@lnm.mw.tum.de>
2005-03-10 10:10:23 +0000

    body forces included in mass rhs
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2005 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/fluid3_fast/f3f_massrhs.f
+++ src/fluid3_fast/f3f_massrhs.f
@@ -1,8 +1,7 @@
 C--------------------------------------------------------------------------
 C
 C \file
-C \brief routine to evaluate iteration rhs on element base resulting from
-C        mass and acceleration in generalised alpha time integration
+C \brief routine to evaluate rhs on element base
 C
 C
 C <pre>
@@ -20,13 +19,13 @@ C
 C \brief calculates mass-acceleration-part of the rhs vector for
 C        generalised-alpha time integration
 C
-C This routine performs the multiplication emass*acc(n) and writes its
-C negative result on the elemental iteration force vector. This gives a
-C part of the rhs required in the generalised alpha time integration scheme
-C for fluids.
+C This routine performs the multiplication emass*hist and writes its
+C result on the elemental rhs force vector. This gives the right hand
+C side for fluids.
 C
 C \param emass()()()     real*8   (o) element mass matrix
-C \param eaccn()()()     real*8   (i) nodal accelarations
+C \param ehist()()()     real*8   (i) nodal accelarations
+C \param edeadng()()     real*8   (i) body force (constant in space)
 C \param eiforce()()     real*8   (i) element iteration force
 C \param sizevec(6)      integer  (i) some sizes
 C
@@ -36,15 +35,31 @@ C \author mn
 C \date   10/04
 C
 C--------------------------------------------------------------------------
-      subroutine f3fmassrhs(emass, eaccn, eiforce, sizevec)
+      subroutine f3fmassrhs(emass,ehist,edeadng,eiforce,hasext,
+     &                      facl,sizevec)
 
       integer sizevec(6)
+      integer hasext(sizevec(4))
       real*8 emass(sizevec(4),sizevec(3),sizevec(3))
-      real*8 eaccn(sizevec(4),sizevec(1),3)
+      real*8 ehist(sizevec(4),sizevec(1),3)
       real*8 eiforce(sizevec(4),sizevec(3))
+      real*8 edeadng(sizevec(4),3)
+      real*8 facl
 
       integer i,j,k
       integer dim
+      
+      do i=1,sizevec(5)
+        if(hasext(i).ne.0) then
+          do j=1,sizevec(2)       ! count over nodes
+            ehist(i,j,1) = ehist(i,j,1) + facl * edeadng(i,1)
+            ehist(i,j,2) = ehist(i,j,2) + facl * edeadng(i,2)
+            ehist(i,j,3) = ehist(i,j,3) + facl * edeadng(i,3)
+          enddo
+        else 
+          hasext(i) = 1 ! this assures rhs assembly
+        end if
+      enddo
 
 c     perform multiplication
       dim=4
@@ -53,21 +68,21 @@ c     perform multiplication
         do j=1,sizevec(2)
           do k=1,sizevec(5)
             eiforce(k,(i-1)*dim+1) = eiforce(k,(i-1)*dim+1)
-     &              +(emass(k,(j-1)*dim+1,(i-1)*dim+1) * eaccn(k,j,1)
-     &              + emass(k,(j-1)*dim+2,(i-1)*dim+1) * eaccn(k,j,2)
-     &              + emass(k,(j-1)*dim+3,(i-1)*dim+1) * eaccn(k,j,3))
+     &              +(emass(k,(j-1)*dim+1,(i-1)*dim+1) * ehist(k,j,1)
+     &              + emass(k,(j-1)*dim+2,(i-1)*dim+1) * ehist(k,j,2)
+     &              + emass(k,(j-1)*dim+3,(i-1)*dim+1) * ehist(k,j,3))
             eiforce(k,(i-1)*dim+2) = eiforce(k,(i-1)*dim+2)
-     &              +(emass(k,(j-1)*dim+1,(i-1)*dim+2) * eaccn(k,j,1)
-     &              + emass(k,(j-1)*dim+2,(i-1)*dim+2) * eaccn(k,j,2)
-     &              + emass(k,(j-1)*dim+3,(i-1)*dim+2) * eaccn(k,j,3))
+     &              +(emass(k,(j-1)*dim+1,(i-1)*dim+2) * ehist(k,j,1)
+     &              + emass(k,(j-1)*dim+2,(i-1)*dim+2) * ehist(k,j,2)
+     &              + emass(k,(j-1)*dim+3,(i-1)*dim+2) * ehist(k,j,3))
             eiforce(k,(i-1)*dim+3) = eiforce(k,(i-1)*dim+3)
-     &              +(emass(k,(j-1)*dim+1,(i-1)*dim+3) * eaccn(k,j,1)
-     &              + emass(k,(j-1)*dim+2,(i-1)*dim+3) * eaccn(k,j,2)
-     &              + emass(k,(j-1)*dim+3,(i-1)*dim+3 )* eaccn(k,j,3))
+     &              +(emass(k,(j-1)*dim+1,(i-1)*dim+3) * ehist(k,j,1)
+     &              + emass(k,(j-1)*dim+2,(i-1)*dim+3) * ehist(k,j,2)
+     &              + emass(k,(j-1)*dim+3,(i-1)*dim+3 )* ehist(k,j,3))
             eiforce(k,(i-1)*dim+4) = eiforce(k,(i-1)*dim+4)
-     &              +(emass(k,(j-1)*dim+1,(i-1)*dim+4) * eaccn(k,j,1)
-     &              + emass(k,(j-1)*dim+2,(i-1)*dim+4) * eaccn(k,j,2)
-     &              + emass(k,(j-1)*dim+3,(i-1)*dim+4) * eaccn(k,j,3))
+     &              +(emass(k,(j-1)*dim+1,(i-1)*dim+4) * ehist(k,j,1)
+     &              + emass(k,(j-1)*dim+2,(i-1)*dim+4) * ehist(k,j,2)
+     &              + emass(k,(j-1)*dim+3,(i-1)*dim+4) * ehist(k,j,3))
 
           enddo
         enddo
