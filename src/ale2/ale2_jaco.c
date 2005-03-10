d966a6e27138eeb475be7886780f27b0e2717590
Christiane Foerster <foerster@lnm.mw.tum.de>
2005-03-10 08:53:20 +0000

    some change in data structure
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@1959 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/ale2/ale2_jaco.c
+++ src/ale2/ale2_jaco.c
@@ -2,8 +2,7 @@
 \file
 \brief contains the routine 'ale2_jaco' which calculates the Jacobian
 matrix for a 2d ale element and the routine 'ale2_min_jaco' which searches
-for the smalles Jacobian determinant of a quad4-element as well as
-'ale2_el_area'
+for the smalles Jacobian determinant of a quad4-element
 
 <pre>
 Maintainer: Christiane Foerster
@@ -24,16 +23,16 @@ Maintainer: Christiane Foerster
 /*!----------------------------------------------------------------------
 \brief  calculate the Jacobian matrix
 
-<pre>                                                              mn 06/02
+<pre>                                                           chfoe 11/02
 This routine calculates the Jacobian matrix  at a point r,s for
 a 2D ale element.
 
 </pre>
-\param *deriv   DOUBLE     (i)   derivatives of the shape functions
-\param **xjm    DOUBLE     (o)   the Jacobian matrix
-\param *det     DOUBLE     (i)   determinant of the Jacobian matrix
-\param *ele     ELEMENT    (i)   the element
-\param iel      INT        (i)   number of nodes of the element
+\param *deriv     DOUBLE     (i)   derivatives of the shape functions
+\param **xjm      DOUBLE     (o)   the Jacobian matrix
+\param *det       DOUBLE     (i)   determinant of the Jacobian matrix
+\param  xyz[2][4] DOUBLE     (i)   element coordinates
+\param  iel       INT        (i)   number of nodes of the element
 
 \warning There is nothing special to this routine
 \return void
@@ -43,7 +42,7 @@ a 2D ale element.
 void ale2_jaco(DOUBLE    **deriv,
                DOUBLE    **xjm,
                DOUBLE     *det,
-               DOUBLE    **xyz,
+               DOUBLE      xyz[2][MAXNOD],
                INT         iel)
 {
 /*----------------------------------------------------------------------*/
@@ -59,10 +58,10 @@ dstrc_enter("ale2_jaco");
 
    for (k=0; k<iel; k++)
    {
-     xjm[0][0] += deriv[0][k] * xyz[k][0] ;
-     xjm[0][1] += deriv[0][k] * xyz[k][1] ;
-     xjm[1][0] += deriv[1][k] * xyz[k][0] ;
-     xjm[1][1] += deriv[1][k] * xyz[k][1] ;
+     xjm[0][0] += deriv[0][k] * xyz[0][k] ;
+     xjm[0][1] += deriv[0][k] * xyz[1][k] ;
+     xjm[1][0] += deriv[1][k] * xyz[0][k] ;
+     xjm[1][1] += deriv[1][k] * xyz[1][k] ;
    }
 /*------------------------------------------ determinant of jacobian ---*/
      *det = xjm[0][0]* xjm[1][1] - xjm[1][0]* xjm[0][1];
@@ -78,16 +77,20 @@ return;
 /*!----------------------------------------------------------------------
 \brief  routine looks for the minimal Jacobian determinant
 
-<pre>                                                              ck 01/03
+<pre>                                                           chfoe 01/03
 This routine searches for the minimal Jacobian determinant of a 4 noded
 quad element by evaluating the Jacobian at all nodal points and looking
 for the smallest. The Jacobian of linear triangles is constant and hence
 minimal everywhere. The search for the minimal Jacobian determinant of
 and higher order elements are not implemented.
 
+Routine is also used for evaluating the minimal Jacobian of higher order 
+elements assuming that the sides remain stright and the side nodes are
+placed in equal distances.
+
 </pre>
 \param   distyp    enum _DIS_TYP (i)   distyp of actual element
-\param **xyz       DOUBLE        (i)   elemental coordinates
+\param   xyz[2][9] DOUBLE        (i)   elemental coordinates
 \param  *min_detF  DOUBLE        (o)   minimal Jacobian determinant
 
 \warning There is nothing special to this routine.
@@ -97,7 +100,7 @@ and higher order elements are not implemented.
              called by: ale2_statik_ke(), ale2_statik_ke_test2()
 
 *----------------------------------------------------------------------*/
-void ale2_min_jaco(enum _DIS_TYP distyp, DOUBLE **xyz, DOUBLE *min_detF)
+void ale2_min_jaco(enum _DIS_TYP distyp, DOUBLE xyz[2][MAXNOD], DOUBLE *min_detF)
 {
 DOUBLE           detF[4];          /* Jacobian determinant at nodes */
 
@@ -108,15 +111,17 @@ dstrc_enter("ale2_min_jaco");
 switch (distyp)
 {
    case quad4:
+   case quad8:
+   case quad9:
       /*--------------------- evaluate Jacobian determinant at nodes ---*/
-      detF[0] = 0.25 * ( (xyz[0][0]-xyz[1][0]) * (xyz[0][1]-xyz[3][1])
-                       - (xyz[0][1]-xyz[1][1]) * (xyz[0][0]-xyz[3][0]) );
-      detF[1] = 0.25 * ( (xyz[0][0]-xyz[1][0]) * (xyz[1][1]-xyz[2][1])
-                       - (xyz[0][1]-xyz[1][1]) * (xyz[1][0]-xyz[2][0]) );
-      detF[2] = 0.25 * ( (xyz[3][0]-xyz[2][0]) * (xyz[1][1]-xyz[2][1])
-                       - (xyz[3][1]-xyz[2][1]) * (xyz[1][0]-xyz[2][0]) );
-      detF[3] = 0.25 * ( (xyz[3][0]-xyz[2][0]) * (xyz[0][1]-xyz[3][1])
-                       - (xyz[3][1]-xyz[2][1]) * (xyz[0][0]-xyz[3][0]) );
+      detF[0] = 0.25 * ( (xyz[0][0]-xyz[0][1]) * (xyz[1][0]-xyz[1][3])
+                       - (xyz[1][0]-xyz[1][1]) * (xyz[0][0]-xyz[0][3]) );
+      detF[1] = 0.25 * ( (xyz[0][0]-xyz[0][1]) * (xyz[1][1]-xyz[1][2])
+                       - (xyz[1][0]-xyz[1][1]) * (xyz[0][1]-xyz[0][2]) );
+      detF[2] = 0.25 * ( (xyz[0][3]-xyz[0][2]) * (xyz[1][1]-xyz[1][2])
+                       - (xyz[1][3]-xyz[1][2]) * (xyz[0][1]-xyz[0][2]) );
+      detF[3] = 0.25 * ( (xyz[0][3]-xyz[0][2]) * (xyz[1][0]-xyz[1][3])
+                       - (xyz[1][3]-xyz[1][2]) * (xyz[0][0]-xyz[0][3]) );
 
       /*------------------------------------------------- check sign ---*/
       if (detF[0] <= 0.0) dserror("Negative JACOBIAN ");
@@ -130,8 +135,8 @@ switch (distyp)
       /*----------------------------------------------------------------*/
       break;
    case tri3:
-      *min_detF = (-xyz[0][0]+xyz[1][0]) * (-xyz[0][1]+xyz[2][1])
-                - (-xyz[0][0]+xyz[2][0]) * (-xyz[0][1]+xyz[1][1]);
+      *min_detF = (-xyz[0][0]+xyz[0][1]) * (-xyz[1][0]+xyz[1][2])
+                - (-xyz[0][0]+xyz[0][2]) * (-xyz[1][0]+xyz[1][1]);
       if (*min_detF <= 0.0) dserror("Negative JACOBIAN ");
       break;
    default:
