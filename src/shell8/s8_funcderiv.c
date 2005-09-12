affed99e3f20bb9319e65820618bd8f0376fcfba
Unknown Neumann <neumann@lnm.mw.tum.de>
2005-09-12 06:50:19 +0000

    added shape functions for quad8 elements
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2202 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/shell8/s8_funcderiv.c
+++ src/shell8/s8_funcderiv.c
@@ -17,107 +17,154 @@ Maintainer: Michael Gee
 /*----------------------------------------------------------------------*
  | shape functions and derivatives                            m.gee 6/01|
  *----------------------------------------------------------------------*/
-void s8_funct_deriv(DOUBLE     *funct,
-                    DOUBLE    **deriv,
-                    DOUBLE      r,
-                    DOUBLE      s,
-                    DIS_TYP     typ,
-                    INT         option)
+void s8_funct_deriv(
+    DOUBLE     *funct,
+    DOUBLE    **deriv,
+    DOUBLE      r,
+    DOUBLE      s,
+    DIS_TYP     typ,
+    INT         option)
 {
-const DOUBLE   q12 = 1.0/2.0;
-const DOUBLE   q14 = 1.0/4.0;
-const DOUBLE   q16 = 1.0/6.0;
-DOUBLE         rr,ss,rp,rm,sp,sm,r2,s2;
-DOUBLE         rh,sh,rs,rhp,rhm,shp,shm;
+
+  const DOUBLE   q12 = 1.0/2.0;
+  const DOUBLE   q14 = 1.0/4.0;
+  const DOUBLE   q16 = 1.0/6.0;
+  DOUBLE         rr,ss,rp,rm,sp,sm,r2,s2;
+  DOUBLE         rh,sh,rs,rhp,rhm,shp,shm;
+
 #ifdef DEBUG
-dstrc_enter("s8_funct_deriv");
+  dstrc_enter("s8_funct_deriv");
 #endif
-/*----------------------------------------------------------------------*/
-/* if option ==0 only funtion evaluation, if option==1 also derivatives */
-/*----------------------------------------------------------------------*/
-rr = r*r;
-ss = s*s;
-rp = 1.0+r;
-rm = 1.0-r;
-sp = 1.0+s;
-sm = 1.0-s;
-r2 = 1.0-rr;
-s2 = 1.0-ss;
-switch(typ)
-{
-/*------------------------------------------------ rectangular elements */
-case quad4:/*------------------------- linear rectangular interpolation */
-   funct[0] = q14*rp*sp;
-   funct[1] = q14*rm*sp;
-   funct[2] = q14*rm*sm;
-   funct[3] = q14*rp*sm;
-   if (option==1)
-   {
-      deriv[0][0]= q14*sp;
-      deriv[0][1]=-q14*sp;
-      deriv[0][2]=-q14*sm;
-      deriv[0][3]= q14*sm;
-      deriv[1][0]= q14*rp;
-      deriv[1][1]= q14*rm;
-      deriv[1][2]=-q14*rm;
-      deriv[1][3]=-q14*rp;
-   }
-break;
-case quad8:
-break;
-case quad9:/*---------------- quadratic interpolation with central node */
-   rh  = q12*r;
-   sh  = q12*s;
-   rs  = rh*sh;
-   rhp = r+q12;
-   rhm = r-q12;
-   shp = s+q12;
-   shm = s-q12;
-   funct[0] = rs*rp*sp;
-   funct[1] =-rs*rm*sp;
-   funct[2] = rs*rm*sm;
-   funct[3] =-rs*rp*sm;
-   funct[4] = sh*sp*r2;
-   funct[5] =-rh*rm*s2;
-   funct[6] =-sh*sm*r2;
-   funct[7] = rh*rp*s2;
-   funct[8] = r2*s2;
-   if (option==1)
-   {
-      deriv[0][0]= rhp*sh*sp;
-      deriv[0][1]= rhm*sh*sp;
-      deriv[0][2]=-rhm*sh*sm;
-      deriv[0][3]=-rhp*sh*sm;
-      deriv[0][4]=-2.0*r*sh*sp;
-      deriv[0][5]= rhm*s2;
-      deriv[0][6]= 2.0*r*sh*sm;
-      deriv[0][7]= rhp*s2;
-      deriv[0][8]=-2.0*r*s2;
-      deriv[1][0]= shp*rh*rp;
-      deriv[1][1]=-shp*rh*rm;
-      deriv[1][2]=-shm*rh*rm;
-      deriv[1][3]= shm*rh*rp;
-      deriv[1][4]= shp*r2;
-      deriv[1][5]= 2.0*s*rh*rm;
-      deriv[1][6]= shm*r2;
-      deriv[1][7]=-2.0*s*rh*rp;
-      deriv[1][8]=-2.0*s*r2;
-   }
-break;
-/*------------------------------------------------- triangular elements */
-case tri3:
-break;
-case tri6:
-break;
-default:
-   dserror("unknown typ of interpolation");
-break;
-} /* end of switch typ */
-/*----------------------------------------------------------------------*/
+
+
+  /*----------------------------------------------------------------------*/
+  /* if option ==0 only funtion evaluation, if option==1 also derivatives */
+  /*----------------------------------------------------------------------*/
+
+
+  rr = r*r;
+  ss = s*s;
+  rp = 1.0+r;
+  rm = 1.0-r;
+  sp = 1.0+s;
+  sm = 1.0-s;
+  r2 = 1.0-rr;
+  s2 = 1.0-ss;
+
+
+  switch(typ)
+  {
+    /*------------------------------------------------ rectangular elements */
+    case quad4:/*------------------------- linear rectangular interpolation */
+      funct[0] = q14*rp*sp;
+      funct[1] = q14*rm*sp;
+      funct[2] = q14*rm*sm;
+      funct[3] = q14*rp*sm;
+      if (option==1)
+      {
+        deriv[0][0]= q14*sp;
+        deriv[0][1]=-q14*sp;
+        deriv[0][2]=-q14*sm;
+        deriv[0][3]= q14*sm;
+        deriv[1][0]= q14*rp;
+        deriv[1][1]= q14*rm;
+        deriv[1][2]=-q14*rm;
+        deriv[1][3]=-q14*rp;
+      }
+      break;
+
+
+    case quad8:
+      funct[0] = -q14*(1-r)*(1-s)*(1+r+s);
+      funct[1] = -q14*(1+r)*(1-s)*(1-r+s);
+      funct[2] = -q14*(1+r)*(1+s)*(1-r-s);
+      funct[3] = -q14*(1-r)*(1+s)*(1+r-s);
+      funct[4] =  q12*(1-r*r)*(1-s);
+      funct[5] =  q12*(1+r)*(1-s*s);
+      funct[6] =  q12*(1-r*r)*(1+s);
+      funct[7] =  q12*(1-r)*(1-s*s);
+      if (option==1)
+      {
+        deriv[0][0]=  q14*(1-s)*(2*r+s);
+        deriv[0][1]=  q14*(1-s)*(2*r-s);
+        deriv[0][2]=  q14*(1+s)*(2*r+s);
+        deriv[0][3]=  q14*(1+s)*(2*r-s);
+        deriv[0][4]= -r*(1-s);
+        deriv[0][5]=  q12*(1-s*s);
+        deriv[0][6]= -r*(1+s);
+        deriv[0][7]=  q12*(1-s*s);
+
+        deriv[1][0]=  q14*(1-r)*(r+2*s);
+        deriv[1][1]=  q14*(1+r)*(-r+2*s);
+        deriv[1][2]=  q14*(1+r)*(r+2*s);
+        deriv[1][3]=  q14*(1-r)*(-r+2*s);
+        deriv[1][4]= -q12*(1-r*r);
+        deriv[1][5]= -s*(1+r);
+        deriv[1][6]=  q12*(1-r*r);
+        deriv[1][7]= -s*(1-r);
+      }
+      break;
+    case quad9:/*---------------- quadratic interpolation with central node */
+      rh  = q12*r;
+      sh  = q12*s;
+      rs  = rh*sh;
+      rhp = r+q12;
+      rhm = r-q12;
+      shp = s+q12;
+      shm = s-q12;
+      funct[0] = rs*rp*sp;
+      funct[1] =-rs*rm*sp;
+      funct[2] = rs*rm*sm;
+      funct[3] =-rs*rp*sm;
+      funct[4] = sh*sp*r2;
+      funct[5] =-rh*rm*s2;
+      funct[6] =-sh*sm*r2;
+      funct[7] = rh*rp*s2;
+      funct[8] = r2*s2;
+      if (option==1)
+      {
+        deriv[0][0]= rhp*sh*sp;
+        deriv[0][1]= rhm*sh*sp;
+        deriv[0][2]=-rhm*sh*sm;
+        deriv[0][3]=-rhp*sh*sm;
+        deriv[0][4]=-2.0*r*sh*sp;
+        deriv[0][5]= rhm*s2;
+        deriv[0][6]= 2.0*r*sh*sm;
+        deriv[0][7]= rhp*s2;
+        deriv[0][8]=-2.0*r*s2;
+        deriv[1][0]= shp*rh*rp;
+        deriv[1][1]=-shp*rh*rm;
+        deriv[1][2]=-shm*rh*rm;
+        deriv[1][3]= shm*rh*rp;
+        deriv[1][4]= shp*r2;
+        deriv[1][5]= 2.0*s*rh*rm;
+        deriv[1][6]= shm*r2;
+        deriv[1][7]=-2.0*s*rh*rp;
+        deriv[1][8]=-2.0*s*r2;
+      }
+      break;
+
+
+    /* triangular elements */
+    case tri3:
+      dserror("tri3 not possible for shell8!!");
+      break;
+
+    case tri6:
+      dserror("tri3 not possible for shell8!!");
+      break;
+
+    default:
+      dserror("unknown typ of interpolation");
+      break;
+  } /* end of switch typ */
+
+
 #ifdef DEBUG
-dstrc_exit();
+  dstrc_exit();
 #endif
-return;
+
+  return;
 } /* end of s8_funct_deriv */
 #endif
 
--- src/shell8/s8_intg.c
+++ src/shell8/s8_intg.c
@@ -28,144 +28,151 @@ dstrc_enter("s8intg");
 /*----------------------------------------------------------------------*/
 if (option==0)
 {
-       switch(ele->e.s8->nGP[2])/*---------------- thickness direction t */
-       {
-          case 2:
-             b   = 1.0/3.0;
-             b   = sqrt(b);
-             wgt = 1.0;
-             data->xgpt[0] = -b;
-             data->xgpt[1] =  b;
-             data->xgpt[2] =  0.0;
-             data->wgtt[0] =  wgt;
-             data->wgtt[1] =  wgt;
-             data->wgtt[2] =  0.0;
-          break;
-          default:
-              dserror("unknown number of GP points");
-          break;
-       }
-   if (ele->distyp == quad4 || /*---------------- quadrilateral elements */
-       ele->distyp == quad8 ||
-       ele->distyp == quad9 )
-   {
-       switch(ele->e.s8->nGP[0])/* direction r */
-       {
-       case 1:
-          data->xgpr[0] = 0.0;
-          data->xgpr[1] = 0.0;
-          data->xgpr[2] = 0.0;
-          data->wgtr[0] = 2.0;
-          data->wgtr[1] = 0.0;
-          data->wgtr[2] = 0.0;
-       break;
-       case 2:
-          b   = 1.0/3.0;
-          b   = sqrt(b);
-          wgt = 1.0;
-          data->xgpr[0] = -b;
-          data->xgpr[1] =  b;
-          data->xgpr[2] =  0.0;
-          data->wgtr[0] =  wgt;
-          data->wgtr[1] =  wgt;
-          data->wgtr[2] =  0.0;
-       break;
-       case 3:
-          b    = 3.0/5.0;
-          b    = sqrt(b);
-          wgt  = 5.0/9.0;
-          wgt0 = 8.0/9.0;
-          data->xgpr[0] = -b;
-          data->xgpr[1] =  0.0;
-          data->xgpr[2] =  b;
-          data->wgtr[0] =  wgt;
-          data->wgtr[1] =  wgt0;
-          data->wgtr[2] =  wgt;
-       break;
-       default:
-          dserror("unknown number of GP points");
-       break;
-       }
-       switch(ele->e.s8->nGP[1])/* direction s */
-       {
-       case 1:
-          data->xgps[0] = 0.0;
-          data->xgps[1] = 0.0;
-          data->xgps[2] = 0.0;
-          data->wgts[0] = 2.0;
-          data->wgts[1] = 0.0;
-          data->wgts[2] = 0.0;
-       break;
-       case 2:
-          b   = 1.0/3.0;
-          b   = sqrt(b);
-          wgt = 1.0;
-          data->xgps[0] = -b;
-          data->xgps[1] =  b;
-          data->xgps[2] =  0.0;
-          data->wgts[0] =  wgt;
-          data->wgts[1] =  wgt;
-          data->wgts[2] =  0.0;
-       break;
-       case 3:
-          b    = 3.0/5.0;
-          b    = sqrt(b);
-          wgt  = 5.0/9.0;
-          wgt0 = 8.0/9.0;
-          data->xgps[0] = -b;
-          data->xgps[1] =  0.0;
-          data->xgps[2] =  b;
-          data->wgts[0] =  wgt;
-          data->wgts[1] =  wgt0;
-          data->wgts[2] =  wgt;
-       break;
-       default:
-          dserror("unknown number of GP points");
-       break;
-       }
-    }
-    else if (ele->distyp == tri3 || /*-------------- triangular elements */
-             ele->distyp == tri6 )
+
+  switch(ele->e.s8->nGP[2])/*---------------- thickness direction t */
+  {
+    case 2:
+      b   = 1.0/3.0;
+      b   = sqrt(b);
+      wgt = 1.0;
+      data->xgpt[0] = -b;
+      data->xgpt[1] =  b;
+      data->xgpt[2] =  0.0;
+      data->wgtt[0] =  wgt;
+      data->wgtt[1] =  wgt;
+      data->wgtt[2] =  0.0;
+      break;
+    default:
+      dserror("unknown number of GP points");
+      break;
+  }  /* switch(ele->e.s8->nGP[2]) */
+
+
+  if (ele->distyp == quad4 || /*---------------- quadrilateral elements */
+      ele->distyp == quad8 ||
+      ele->distyp == quad9 )
+  {
+
+    switch(ele->e.s8->nGP[0])/* direction r */
     {
-       switch(ele->e.s8->nGP_tri)
-       {
-       case 1:
-          b   = 1.0/3,0;
-          wgt = 1.0/2.0;
-          data->xgpr[0] =  b;
-          data->xgpr[1] =  0.0;
-          data->xgpr[2] =  0.0;
-          data->xgps[0] =  b;
-          data->xgps[1] =  0.0;
-          data->xgps[2] =  0.0;
-          data->wgtr[0] =  wgt;
-          data->wgtr[1] =  0.0;
-          data->wgtr[2] =  0.0;
-          data->wgts[0] =  wgt;
-          data->wgts[1] =  0.0;
-          data->wgts[2] =  0.0;
-       break;
-       case 3:
-          b   = 1.0/2.0;
-          wgt = 1.0/6.0;
-          data->xgpr[0] =  b;
-          data->xgpr[1] =  b;
-          data->xgpr[2] =  0.0;
-          data->xgps[0] =  0.0;
-          data->xgps[1] =  b;
-          data->xgps[2] =  b;
-          data->wgtr[0] =  wgt;
-          data->wgtr[1] =  wgt;
-          data->wgtr[2] =  wgt;
-          data->wgts[0] =  wgt;
-          data->wgts[1] =  wgt;
-          data->wgts[2] =  wgt;
-       break;
-       default:
-             dserror("unknown number of GP points");
-       break;
-       }
+      case 1:
+        data->xgpr[0] = 0.0;
+        data->xgpr[1] = 0.0;
+        data->xgpr[2] = 0.0;
+        data->wgtr[0] = 2.0;
+        data->wgtr[1] = 0.0;
+        data->wgtr[2] = 0.0;
+        break;
+      case 2:
+        b   = 1.0/3.0;
+        b   = sqrt(b);
+        wgt = 1.0;
+        data->xgpr[0] = -b;
+        data->xgpr[1] =  b;
+        data->xgpr[2] =  0.0;
+        data->wgtr[0] =  wgt;
+        data->wgtr[1] =  wgt;
+        data->wgtr[2] =  0.0;
+        break;
+      case 3:
+        b    = 3.0/5.0;
+        b    = sqrt(b);
+        wgt  = 5.0/9.0;
+        wgt0 = 8.0/9.0;
+        data->xgpr[0] = -b;
+        data->xgpr[1] =  0.0;
+        data->xgpr[2] =  b;
+        data->wgtr[0] =  wgt;
+        data->wgtr[1] =  wgt0;
+        data->wgtr[2] =  wgt;
+        break;
+      default:
+        dserror("unknown number of GP points");
+        break;
     }
+
+    switch(ele->e.s8->nGP[1])/* direction s */
+    {
+      case 1:
+        data->xgps[0] = 0.0;
+        data->xgps[1] = 0.0;
+        data->xgps[2] = 0.0;
+        data->wgts[0] = 2.0;
+        data->wgts[1] = 0.0;
+        data->wgts[2] = 0.0;
+        break;
+      case 2:
+        b   = 1.0/3.0;
+        b   = sqrt(b);
+        wgt = 1.0;
+        data->xgps[0] = -b;
+        data->xgps[1] =  b;
+        data->xgps[2] =  0.0;
+        data->wgts[0] =  wgt;
+        data->wgts[1] =  wgt;
+        data->wgts[2] =  0.0;
+        break;
+      case 3:
+        b    = 3.0/5.0;
+        b    = sqrt(b);
+        wgt  = 5.0/9.0;
+        wgt0 = 8.0/9.0;
+        data->xgps[0] = -b;
+        data->xgps[1] =  0.0;
+        data->xgps[2] =  b;
+        data->wgts[0] =  wgt;
+        data->wgts[1] =  wgt0;
+        data->wgts[2] =  wgt;
+        break;
+      default:
+        dserror("unknown number of GP points");
+        break;
+    }
+  }
+
+  else if (ele->distyp == tri3 || /*-------------- triangular elements */
+      ele->distyp == tri6 )
+  {
+
+    switch(ele->e.s8->nGP_tri)
+    {
+      case 1:
+        b   = 1.0/3,0;
+        wgt = 1.0/2.0;
+        data->xgpr[0] =  b;
+        data->xgpr[1] =  0.0;
+        data->xgpr[2] =  0.0;
+        data->xgps[0] =  b;
+        data->xgps[1] =  0.0;
+        data->xgps[2] =  0.0;
+        data->wgtr[0] =  wgt;
+        data->wgtr[1] =  0.0;
+        data->wgtr[2] =  0.0;
+        data->wgts[0] =  wgt;
+        data->wgts[1] =  0.0;
+        data->wgts[2] =  0.0;
+        break;
+      case 3:
+        b   = 1.0/2.0;
+        wgt = 1.0/6.0;
+        data->xgpr[0] =  b;
+        data->xgpr[1] =  b;
+        data->xgpr[2] =  0.0;
+        data->xgps[0] =  0.0;
+        data->xgps[1] =  b;
+        data->xgps[2] =  b;
+        data->wgtr[0] =  wgt;
+        data->wgtr[1] =  wgt;
+        data->wgtr[2] =  wgt;
+        data->wgts[0] =  wgt;
+        data->wgts[1] =  wgt;
+        data->wgts[2] =  wgt;
+        break;
+      default:
+        dserror("unknown number of GP points");
+        break;
+    }
+  }
 }
 /*----------------------------------------------------------------------*/
 #ifdef DEBUG
