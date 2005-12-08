45dbbafbd673fabf71b6d5cfe68417ca05d1e011
Unknown Neumann <neumann@lnm.mw.tum.de>
2005-12-08 08:40:01 +0000

    introduced the calculation on an arbitrary discretization in many parts of the code:   dis[0]      ->  dis[disnum]   dis[kk]     ->  dis[disnum]   dis[actdis] ->  dis[disnum]
    removed compiler warnings and beautified the code
    
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2255 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/headers/container.h
+++ src/headers/container.h
@@ -1,11 +1,11 @@
 /*!----------------------------------------------------------------------
-\brief file pointers
+  \brief file pointers
 
-<pre>
+  <pre>
 Maintainer: Malte Neumann
-            neumann@statik.uni-stuttgart.de
-            http://www.uni-stuttgart.de/ibs/members/neumann/
-            0711 - 685-6121
+neumann@statik.uni-stuttgart.de
+http://www.uni-stuttgart.de/ibs/members/neumann/
+0711 - 685-6121
 </pre>
 
 <pre>                                                         m.gee 10/02
@@ -13,88 +13,88 @@ This Container is used to transport variables from the 'steuerroutinen'
 to the 'Elementroutinen'
 </pre>
 
-*----------------------------------------------------------------------*/
+ *----------------------------------------------------------------------*/
 typedef struct _CONTAINER
 {
-/*------------------------------------------------------------- file I/O */
-enum _FIELDTYP fieldtyp;     /*!< typ of field */
-INT            handsize;     /*!< has to do with restart */
-long int      *handles;      /*!< has to do with restart */
-DOUBLE        *dvec;         /*!< global redundant vector passed to elements */
-DOUBLE        *dirich;       /*!< ? */
-INT            global_numeq; /*!< size of dvec */
-DOUBLE        *dirichfacs;   /*!< factors for rhs-entries due to prescribed displacements */
-INT            isdyn;        /*!< flag for dynamic or static calculation */
-                             /*!< isdyn = 0 for static calculation */
-                             /*!< isdyn = 1 for dynamic calculation */
-DOUBLE         ekin;         /*!< kinetic energy, calculated at element level */
-INT            kintyp;       /*!< kinematic to be used */
-                             /*!< kintyp = 0: linear kinematic */
-                             /*!< kintyp = 1: updated lagrange */
-                             /*!< kintyp = 2: total lagrange */
-INT            kstep;        /*!< time in increment step we are in */
-INT            actndis;      /*!< which discretisation we have */
-INT            inherit;
-INT            point_neum;
 
-INT            quality;      /*!< element quality measure */
+  enum _FIELDTYP fieldtyp;     /*!< typ of field */
+  INT            handsize;     /*!< has to do with restart */
+  long int      *handles;      /*!< has to do with restart */
+  DOUBLE        *dvec;         /*!< global redundant vector passed to elements */
+  DOUBLE        *dirich;       /*!< ? */
+  INT            global_numeq; /*!< size of dvec */
+  DOUBLE        *dirichfacs;   /*!< factors for rhs-entries due to prescribed displacements */
+  INT            isdyn;        /*!< flag for dynamic or static calculation
+                                 isdyn = 0 for static calculation
+                                 isdyn = 1 for dynamic calculation */
+  DOUBLE         ekin;         /*!< kinetic energy, calculated at element level */
+  INT            kintyp;       /*!< kinematic to be used
+                                 kintyp = 0: linear kinematic
+                                 kintyp = 1: updated lagrange
+                                 kintyp = 2: total lagrange */
+  INT            kstep;        /*!< time in increment step we are in */
+  INT            disnum;       /*!< which discretisation we have */
+  INT            inherit;
+  INT            point_neum;
+
+  INT            quality;      /*!< element quality measure */
 
 #ifdef D_ALE
-DOUBLE         min, max;     /*<! scaling parameters for ale two_step */
-DOUBLE         min_stiff;
-DOUBLE         max_stiff;
+  DOUBLE         min, max;     /*<! scaling parameters for ale two_step */
+  DOUBLE         min_stiff;
+  DOUBLE         max_stiff;
 #endif
 
-INT            pos;          /*<! sol_increment[pos] contains dbc in ale */
-INT            coupl_typ;    /*!< conforming or non-conf. discretization */
-DOUBLE         relax_param;  /*!< the relaxation parameter omega */
+  INT            pos;          /*<! sol_increment[pos] contains dbc in ale */
+  INT            coupl_typ;    /*!< conforming or non-conf. discretization */
+  DOUBLE         relax_param;  /*!< the relaxation parameter omega */
 
 
 #ifdef D_FLUID               /* ab hier fuer fluid */
-DOUBLE        *frhs;
-DOUBLE        *liftdrag;
-DOUBLE        *fidrichrhs;   /*!< for storing the pressure rhs values */
-DOUBLE        *ftimerhs_pro;
-INT            nii;
+  DOUBLE        *frhs;
+  DOUBLE        *liftdrag;
+  DOUBLE        *fidrichrhs;   /*!< for storing the pressure rhs values */
+  DOUBLE        *ftimerhs_pro;
+  INT            nii;
 
 
-INT            error_norm;   /*!< error norm for error calculation
-                                  = 0 ... infinity norm
-                                  = 1 ... L1 norm
-                                  = 2 ... L2 norm */
-DOUBLE         vel_error;    /*!< vel error in the above given norm */
-DOUBLE         pre_error;    /*!< pre error in the above given norm */
+  INT            error_norm;   /*!< error norm for error calculation
+                                 = 0 ... infinity norm
+                                 = 1 ... L1 norm
+                                 = 2 ... L2 norm */
+  DOUBLE         vel_error;    /*!< vel error in the above given norm */
+  DOUBLE         pre_error;    /*!< pre error in the above given norm */
 
-DOUBLE         vel_norm;     /*!< above given norm of the anal. sol */
-DOUBLE         pre_norm;     /*!< above given norm of the anal. sol */
+  DOUBLE         vel_norm;     /*!< above given norm of the anal. sol */
+  DOUBLE         pre_norm;     /*!< above given norm of the anal. sol */
 
 
 #ifdef D_FLUID2_PRO
-struct _DBCSR *gradmatrix;   /*!< gradient matrix Projection Method */
-struct _DBCSR *lumpedmass;   /*!< gradient matrix Projection Method */
-DOUBLE        *fvelrhs2;     /*!< pointer to second right hand side */
+  struct _DBCSR *gradmatrix;   /*!< gradient matrix Projection Method */
+  struct _DBCSR *lumpedmass;   /*!< gradient matrix Projection Method */
+  DOUBLE        *fvelrhs2;     /*!< pointer to second right hand side */
 #endif
 
-INT            turbu;
+  INT            turbu;
 
 #ifdef D_FLUID2TU
-INT            niturbu_pro;
-INT            niturbu_n;
-DOUBLE        *ftimerhs;     /* used within turbulence (only??) */
+  INT            niturbu_pro;
+  INT            niturbu_n;
+  DOUBLE        *ftimerhs;     /* used within turbulence (only??) */
 #endif
 
-enum _FLUID_STRESS str;
-INT           *iedgnod;
-INT            ngnode;
-INT            is_relax;      /*!< flag, if calculation is for relaxation parameter */
-                              /*!< is_relax = 0 -> fluid results are read from sol_increment[3][i] */
-                              /*!< is_relax = 1 -> fluid results are read from sol_increment[7][i] */
+  enum _FLUID_STRESS str;
+  INT           *iedgnod;
+  INT            ngnode;
+  INT            is_relax;      /*!< flag, if calculation is for relaxation parameter */
+  /*!< is_relax = 0 -> fluid results are read from sol_increment[3][i] */
+  /*!< is_relax = 1 -> fluid results are read from sol_increment[7][i] */
 
 #endif /* ifdef D_FLUID */
 
 #ifdef D_OPTIM                /* include optimization code to ccarat        */
-DOUBLE         getvalue ;     /*!< optimization */
-DOUBLE        *getvector;     /*!< optimization */
+  DOUBLE         getvalue ;     /*!< optimization */
+  DOUBLE        *getvector;     /*!< optimization */
 #endif                        /* stop including optimization code to ccarat */
 
 } CONTAINER;
