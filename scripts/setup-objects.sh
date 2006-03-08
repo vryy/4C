5e1c1530344b85fee7eaf4fe3e01a1e72743e5c7
Unknown Neumann <neumann@lnm.mw.tum.de>
2006-03-08 08:33:36 +0000

    allow blanks at the beginning of a line in the definefile
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2315 060e22d8-9d1c-0410-868a-e3a950d63d95

--- scripts/setup-objects.sh
+++ scripts/setup-objects.sh
@@ -13,27 +13,27 @@ OBJECTS="\$(OBJS_MAIN) \$(OBJS_GLOBAL) \$(OBJS_SOLVER) \$(OBJS_PSS) \$(OBJS_INPU
 \$(OBJS_PAR) \$(OBJS_MATH) \$(OBJS_OUTPUT) \$(OBJS_FORTRAN) \$(OBJS_VISUAL) \$(OBJS_STRUCTURE) \$(OBJS_IO)"
 
 # ALE
-if grep '^D_ALE' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_ALE' "$definefile" 2>&1 > /dev/null ; then
     OBJECTS="$OBJECTS \$(OBJS_ALE)"
 fi
 
 # AXISHELL
-if grep '^D_AXISHELL' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_AXISHELL' "$definefile" 2>&1 > /dev/null ; then
     OBJECTS="$OBJECTS \$(OBJS_AXISHELL)"
 fi
 
 # BEAM3
-if grep '^D_BEAM3' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_BEAM3' "$definefile" 2>&1 > /dev/null ; then
     OBJECTS="$OBJECTS \$(OBJS_BEAM3)"
 fi
 
 # BRICK1
-if grep '^D_BRICK1' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_BRICK1' "$definefile" 2>&1 > /dev/null ; then
     OBJECTS="$OBJECTS \$(OBJS_BRICK1)"
 fi
 
 # FLUID2
-if grep '^D_FLUID2' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_FLUID2' "$definefile" 2>&1 > /dev/null ; then
     if [ "x$OBJ_FLUID" = "x0" ] ; then
         OBJECTS="$OBJECTS \$(OBJS_FLUID2) \$(OBJS_FLUID)"
         DEFINES="$DEFINES -DD_FLUID"
@@ -44,7 +44,7 @@ if grep '^D_FLUID2' "$definefile" 2>&1 > /dev/null ; then
 fi
 
 # FLUID2_ML
-if grep '^FLUID2_ML' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*FLUID2_ML' "$definefile" 2>&1 > /dev/null ; then
     if [ "x$OBJ_FLUID" = "x0" ] ; then
         OBJECTS="$OBJECTS \$(OBJS_F2ML) \$(OBJS_FLUID)"
         DEFINES="$DEFINES -DD_FLUID"
@@ -55,7 +55,7 @@ if grep '^FLUID2_ML' "$definefile" 2>&1 > /dev/null ; then
 fi
 
 # FLUID3
-if grep '^D_FLUID3' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_FLUID3' "$definefile" 2>&1 > /dev/null ; then
     if [ "x$OBJ_FLUID" = "x0" ] ; then
         OBJECTS="$OBJECTS \$(OBJS_FLUID3) \$(OBJS_FLUID)"
         DEFINES="$DEFINES -DD_FLUID"
@@ -66,7 +66,7 @@ if grep '^D_FLUID3' "$definefile" 2>&1 > /dev/null ; then
 fi
 
 # FLUID3_FAST
-if grep '^D_FLUID3_F' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_FLUID3_F' "$definefile" 2>&1 > /dev/null ; then
     if [ "x$OBJ_FLUID" = "x0" ] ; then
         OBJECTS="$OBJECTS \$(OBJS_F3F) \$(OBJS_F3F_F) \$(OBJS_FLUID)"
         OBJ_FLUID=1
@@ -76,7 +76,7 @@ if grep '^D_FLUID3_F' "$definefile" 2>&1 > /dev/null ; then
 fi
 
 # FLUID3_ML
-if grep '^FLUID3_ML' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*FLUID3_ML' "$definefile" 2>&1 > /dev/null ; then
     if [ "x$OBJ_FLUID" = "x0" ] ; then
         OBJECTS="$OBJECTS \$(OBJS_F3ML) \$(OBJS_FLUID)"
         DEFINES="$DEFINES -DD_FLUID"
@@ -87,7 +87,7 @@ if grep '^FLUID3_ML' "$definefile" 2>&1 > /dev/null ; then
 fi
 
 # FLUID2_PRO
-if grep '^D_FLUID2_PRO' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_FLUID2_PRO' "$definefile" 2>&1 > /dev/null ; then
     if [ "x$OBJ_FLUID" = "x0" ] ; then
         OBJECTS="$OBJECTS \$(OBJS_F2PRO) \$(OBJS_FLUID)"
         DEFINES="$DEFINES -DD_FLUID"
@@ -98,7 +98,7 @@ if grep '^D_FLUID2_PRO' "$definefile" 2>&1 > /dev/null ; then
 fi
 
 # FLUID3_PRO
-if grep '^D_FLUID3_PRO' "$definefile" 3>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_FLUID3_PRO' "$definefile" 3>&1 > /dev/null ; then
     if [ "x$OBJ_FLUID" = "x0" ] ; then
         OBJECTS="$OBJECTS \$(OBJS_F3PRO) \$(OBJS_FLUID)"
         DEFINES="$DEFINES -DD_FLUID"
@@ -109,35 +109,35 @@ if grep '^D_FLUID3_PRO' "$definefile" 3>&1 > /dev/null ; then
 fi
 
 # SHELL8
-if grep '^D_SHELL8' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_SHELL8' "$definefile" 2>&1 > /dev/null ; then
     OBJECTS="$OBJECTS \$(OBJS_SHELL8)"
 fi
 
 # SHELL9
-if grep '^D_SHELL9' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_SHELL9' "$definefile" 2>&1 > /dev/null ; then
     OBJECTS="$OBJECTS \$(OBJS_SHELL9)"
 fi
 
 # WALL1
-if grep '^D_WALL1' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_WALL1' "$definefile" 2>&1 > /dev/null ; then
     OBJECTS="$OBJECTS \$(OBJS_WALL1)"
     OBJ_W1=1
 fi
 
 # INTERF
-if grep '^D_INTERF' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_INTERF' "$definefile" 2>&1 > /dev/null ; then
     OBJECTS="$OBJECTS \$(OBJS_IF)"
     OBJ_IF=1
 fi
 
 # WALLGE
-if grep '^D_WALLGE' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_WALLGE' "$definefile" 2>&1 > /dev/null ; then
     OBJECTS="$OBJECTS \$(OBJS_WALLGE)"
     OBJ_WGE=1
 fi
 
 # MLSTRUCT
-if grep '^D_MLSTRUCT' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_MLSTRUCT' "$definefile" 2>&1 > /dev/null ; then
     if [ "x$OBJ_W1" = "x1" -a "x$OBJ_WGE" = "x1" -a "x$OBJ_IF" = "x1" ] ; then
         OBJECTS="$OBJECTS \$(OBJS_S2ML)"
     fi
@@ -172,27 +172,27 @@ if grep '^D_MLSTRUCT' "$definefile" 2>&1 > /dev/null ; then
 fi
 
 # FSI
-if grep '^D_FSI' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_FSI' "$definefile" 2>&1 > /dev/null ; then
     OBJECTS="$OBJECTS \$(OBJS_FSI)"
 fi
 
 # SSI
-if grep '^D_SSI' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_SSI' "$definefile" 2>&1 > /dev/null ; then
     OBJECTS="$OBJECTS \$(OBJS_SSI)"
 fi
 
 # OPTIM
-if grep '^D_OPTIM' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_OPTIM' "$definefile" 2>&1 > /dev/null ; then
     OBJECTS="$OBJECTS \$(OBJS_OPT)"
 fi
 
 # MAT
-if grep '^D_MAT' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*D_MAT' "$definefile" 2>&1 > /dev/null ; then
     OBJECTS="$OBJECTS \$(OBJS_MAT)"
 fi
 
 # NURBS
-if grep '^NURBS' "$definefile" 2>&1 > /dev/null ; then
+if grep '^[[:blank:]]*NURBS' "$definefile" 2>&1 > /dev/null ; then
   if [ "x$NURBS_LIB" = "x" ] ; then
     echo $0: Warning: NURBS_PACKAGE not available on this platform.
     echo $0: Warning: Using straight interpolation for subdivision!!
