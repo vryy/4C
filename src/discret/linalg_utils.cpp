b4da67cb93d79757dd7fb31514709158b7b469cd
Michael Gee <gee@lnm.mw.tum.de>
2006-12-19 09:52:47 +0000

    assembly and sparse matrix addition
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2634 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/discret/linalg_utils.cpp
+++ src/discret/linalg_utils.cpp
@@ -15,6 +15,8 @@ Maintainer: Michael Gee
 
 #include "linalg_utils.H"
 #include "drt_dserror.H"
+#include "EpetraExt_Transpose_RowMatrix.h"
+#include "Epetra_SerialDenseSolver.h"
 
 
 /*----------------------------------------------------------------------*
@@ -81,6 +83,142 @@ void LINALG::Export(const Epetra_Vector& source, Epetra_Vector& target)
   return;
 }
 
+/*----------------------------------------------------------------------*
+ |  assemble a matrix  (public)                              mwgee 12/06|
+ *----------------------------------------------------------------------*/
+void LINALG::Assemble(Epetra_CrsMatrix& A, const Epetra_SerialDenseMatrix& Aele, 
+                      const vector<int>& lm, const vector<int>& lmowner)
+{
+  if (A.Filled()) dserror("sparse matrix A already filled, cannot assemble");
+  const int ldim = (int)lm.size();
+  if (ldim!=(int)lmowner.size() || ldim!=Aele.M() || ldim!=Aele.N())
+    dserror("Mismatch in dimensions");
+  
+  const int myrank = A.Comm().MyPID();
+  
+  // loop rows of local matrix
+  for (int lrow=0; lrow<ldim; ++lrow)
+  {
+    // check ownership of row
+    if (lmowner[lrow] != myrank) continue;
+    
+    // check whether I have that global row
+    int rgid = lm[lrow];
+    if (!(A.RowMap().MyGID(rgid))) dserror("Sparse matrix A does not have global row %d",rgid);
+    
+    for (int lcol=0; lcol<ldim; ++lcol)
+    {
+      double val = Aele(lrow,lcol);
+      int    cgid = lm[lcol];
+      int errone = A.SumIntoGlobalValues(rgid,1,&val,&cgid);
+      if (errone>0)
+      {
+        int errtwo = A.InsertGlobalValues(rgid,1,&val,&cgid);
+        if (errtwo<0) dserror("Epetra_CrsMatrix::InsertGlobalValues returned error code %d",errtwo);
+      }
+      else if (errone)
+        dserror("Epetra_CrsMatrix::SumIntoGlobalValues returned error code %d",errone);
+    } // for (int lcol=0; lcol<ldim; ++lcol)
+  } // for (int lrow=0; lrow<ldim; ++lrow)
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ |  assemble a vector  (public)                              mwgee 12/06|
+ *----------------------------------------------------------------------*/
+void LINALG::Assemble(Epetra_Vector& V, const Epetra_SerialDenseVector& Vele, 
+                const vector<int>& lm, const vector<int>& lmowner)
+{
+  const int ldim = (int)lm.size();
+  if (ldim!=(int)lmowner.size() || ldim!=Vele.Length())
+    dserror("Mismatch in dimensions");
+  
+  const int myrank = V.Comm().MyPID();
+  
+  for (int lrow=0; lrow<ldim; ++lrow)
+  {
+    if (lmowner[lrow] != myrank) continue;
+    int rgid = lm[lrow];
+    if (!V.Map().MyGID(rgid)) dserror("Sparse vector V does not have global row %d",rgid);
+    int rlid = V.Map().LID(rgid);
+    V[rlid] += Vele[lrow];
+  } // for (int lrow=0; lrow<ldim; ++lrow)
+  
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ |  FillComplete a matrix  (public)                          mwgee 12/06|
+ *----------------------------------------------------------------------*/
+void LINALG::Complete(Epetra_CrsMatrix& A)
+{
+  if (A.Filled()) return;
+  
+  int err = A.FillComplete(A.OperatorDomainMap(),A.OperatorRangeMap());
+  if (err) dserror("Epetra_CrsMatrix::FillComplete(domain,range) returned err=%d",err);
+  err = A.OptimizeStorage();
+  if (err) dserror("Epetra_CrsMatrix::OptimizeStorage() returned err=%d",err);
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ |  Add a sparse matrix to another  (public)                 mwgee 12/06|
+ *----------------------------------------------------------------------*/
+void LINALG::Add(const Epetra_CrsMatrix& A, 
+                 const bool transposeA, 
+                 const double scalarA,
+                 Epetra_CrsMatrix& B, 
+                 const double scalarB)
+{
+  if (!A.Filled()) dserror("FillComplete was not called on A");
+  if (B.Filled()) dserror("FillComplete was called on B before");
+  
+  Epetra_CrsMatrix*               Aprime = NULL;
+  EpetraExt::RowMatrix_Transpose* Atrans = NULL;
+  if (transposeA)
+  {
+    Atrans = new EpetraExt::RowMatrix_Transpose(false,NULL,false);
+    Aprime = &(dynamic_cast<Epetra_CrsMatrix&>(((*Atrans)(const_cast<Epetra_CrsMatrix&>(A)))));
+  }
+  else
+  {
+    Aprime = const_cast<Epetra_CrsMatrix*>(&A);
+  }
+  
+  B.Scale(scalarB);
+  
+  //Loop over Aprime's rows and sum into
+  int MaxNumEntries = EPETRA_MAX( Aprime->MaxNumEntries(), B.MaxNumEntries() );
+  int NumEntries;
+  vector<int>    Indices(MaxNumEntries);
+  vector<double> Values(MaxNumEntries);
+
+  const int NumMyRows = Aprime->NumMyRows();
+  int Row, err;
+  if (scalarA)
+  {
+    for( int i = 0; i < NumMyRows; ++i )
+    {
+      Row = Aprime->GRID(i);
+      int ierr = Aprime->ExtractGlobalRowCopy(Row,MaxNumEntries,NumEntries,&Values[0],&Indices[0]);
+      if (ierr) dserror("Epetra_CrsMatrix::ExtractGlobalRowCopy returned err=%d",ierr);
+      if( scalarA != 1.0 )
+        for( int j = 0; j < NumEntries; ++j ) Values[j] *= scalarA;
+      for (int j=0; j<NumEntries; ++j)
+      {
+        err = B.SumIntoGlobalValues(Row,1,&Values[j],&Indices[j]);
+        if (err<0 || err==2)
+          err = B.InsertGlobalValues(Row,1,&Values[j],&Indices[j]);
+        if (err < 0)
+          dserror("Epetra_CrsMatrix::InsertGlobalValues returned err=%d",err);
+      }
+    }
+  }
+  if( Atrans ) delete Atrans;
+  return;
+}
+
+
 #ifdef LINUX_MUENCH
 #define CCA_APPEND_U (1)
 #endif
@@ -120,7 +258,6 @@ void LINALG::SymmetricInverse(Epetra_SerialDenseMatrix& A, const int dim)
   for (int i=0; i<dim; ++i)
     for (int j=0; j<i; ++j)
       A(j,i)=A(i,j);
-  
   return;
 }
 
