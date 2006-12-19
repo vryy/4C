22dfb9c0866be83c0456bc403770e2be101a70a1
Michael Gee <gee@lnm.mw.tum.de>
2006-12-19 09:53:44 +0000

    continue on new gen alfa
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2636 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/discret/stru_dyn_nln_drt.cpp
+++ src/discret/stru_dyn_nln_drt.cpp
@@ -104,7 +104,6 @@ void dyn_nlnstructural_drt()
   // vectors and matrices
   // -------------------------------------------------------------------
   const Epetra_Map* dofrowmap = actdis->DofRowMap();
-  const Epetra_Map* dofcolmap = actdis->DofColMap();
 
   // -------------------------------------------------------------------
   // create empty matrices
@@ -123,41 +122,41 @@ void dyn_nlnstructural_drt()
   // create empty vectors
   // -------------------------------------------------------------------
   // original load vector without curve
-  RefCountPtr<Epetra_Vector> rhs0 = LINALG::CreateVector(*dofrowmap,true);
+  //RefCountPtr<Epetra_Vector> rhs0 = LINALG::CreateVector(*dofrowmap,true);
 
   // load vectors at time t and t-dt
   RefCountPtr<Epetra_Vector> rhs1 = LINALG::CreateVector(*dofrowmap,true);
-  RefCountPtr<Epetra_Vector> rhs2 = LINALG::CreateVector(*dofrowmap,true);
+  //RefCountPtr<Epetra_Vector> rhs2 = LINALG::CreateVector(*dofrowmap,true);
 
   // interpolated load vector
-  RefCountPtr<Epetra_Vector> rhs3 = LINALG::CreateVector(*dofrowmap,true);
+  //RefCountPtr<Epetra_Vector> rhs3 = LINALG::CreateVector(*dofrowmap,true);
 
   // forces from dirichlet conditions
-  RefCountPtr<Epetra_Vector> dirich = LINALG::CreateVector(*dofrowmap,true);
+  //RefCountPtr<Epetra_Vector> dirich = LINALG::CreateVector(*dofrowmap,true);
 
   // solution at time t and t-dt
   RefCountPtr<Epetra_Vector> sol0 = LINALG::CreateVector(*dofrowmap,true);
-  RefCountPtr<Epetra_Vector> sol1 = LINALG::CreateVector(*dofrowmap,true);
+  //RefCountPtr<Epetra_Vector> sol1 = LINALG::CreateVector(*dofrowmap,true);
   
   // incremental displacements
   RefCountPtr<Epetra_Vector> dispi = LINALG::CreateVector(*dofrowmap,true);
   
   // velocities
-  RefCountPtr<Epetra_Vector> vel = LINALG::CreateVector(*dofrowmap,true);
+  //RefCountPtr<Epetra_Vector> vel = LINALG::CreateVector(*dofrowmap,true);
   
   // accelerations
-  RefCountPtr<Epetra_Vector> acc = LINALG::CreateVector(*dofrowmap,true);
+  //RefCountPtr<Epetra_Vector> acc = LINALG::CreateVector(*dofrowmap,true);
   
   // internal forces
-  RefCountPtr<Epetra_Vector> intforce = LINALG::CreateVector(*dofrowmap,true);
+  //RefCountPtr<Epetra_Vector> intforce = LINALG::CreateVector(*dofrowmap,true);
   
   // internal forces at t, t-dt and interpolated  
-  RefCountPtr<Epetra_Vector> fie0 = LINALG::CreateVector(*dofrowmap,true);
-  RefCountPtr<Epetra_Vector> fie1 = LINALG::CreateVector(*dofrowmap,true);
-  RefCountPtr<Epetra_Vector> fie2 = LINALG::CreateVector(*dofrowmap,true);
+  //RefCountPtr<Epetra_Vector> fie0 = LINALG::CreateVector(*dofrowmap,true);
+  //RefCountPtr<Epetra_Vector> fie1 = LINALG::CreateVector(*dofrowmap,true);
+  //RefCountPtr<Epetra_Vector> fie2 = LINALG::CreateVector(*dofrowmap,true);
   
   // working vectors
-  RefCountPtr<Epetra_Vector> work0 = LINALG::CreateVector(*dofrowmap,true);
+  //RefCountPtr<Epetra_Vector> work0 = LINALG::CreateVector(*dofrowmap,true);
 
   // -------------------------------------------------------------------
   // call elements to calculate stiffness and mass
@@ -165,28 +164,95 @@ void dyn_nlnstructural_drt()
   {
     // create the parameters for the discretization
     ParameterList params;
+    // action for elements
     params.set("action","calc_struct_nlnstiffmass");
+    // choose what to assemble
+    params.set("assemble matrix 1",true);
+    params.set("assemble matrix 2",true);
+    params.set("assemble vector 1",false);
+    params.set("assemble vector 2",false);
+    params.set("assemble vector 3",false);
+    // other parameters that might be needed by the elements
     params.set("total time",acttime);
     params.set("delta time",sdyn->dt);
-    // set values needed by elements (have to be column map!)
-    RefCountPtr<Epetra_Vector> cfie0 = LINALG::CreateVector(*dofcolmap,false);
-    LINALG::Export(*fie0,*cfie0);
-    RefCountPtr<Epetra_Vector> csol0 = LINALG::CreateVector(*dofcolmap,false);
-    LINALG::Export(*sol0,*csol0);
+    // set vector values needed by elements
     actdis->ClearState();
-    actdis->SetState("residual",cfie0);
-    actdis->SetState("displacement",csol0);
+    actdis->SetState("residual displacement",dispi);
+    actdis->SetState("displacement",sol0);
     actdis->Evaluate(params,stiff_mat,mass_mat,null,null,null);
+    actdis->ClearState();
   }
 
+  // get dirichlet conditions from discretization and apply them to system of equations
 
+  // complete stiffness and mass matrix
+  LINALG::Complete(*stiff_mat);
+  LINALG::Complete(*mass_mat);
+  
+  // build damping matrix if neccessary
+  if (damping)
+  {
+    LINALG::Add(*stiff_mat,false,sdyn->k_damp,*damp_mat,0.0);
+    LINALG::Add(*mass_mat ,false,sdyn->m_damp,*damp_mat,1.0);
+    LINALG::Complete(*damp_mat);
+  }
 
-
-
-
-
-
-
+  /*------------------------------------------- set initial step and time */
+  sdyn->step = -1;
+  sdyn->time = 0.0;
+  
+  /*--------------------------------------- init all applied time curves -*/
+  for (int actcurve=0; actcurve<numcurve; actcurve++)
+    dyn_init_curve(actcurve,sdyn->nstep,sdyn->dt,sdyn->maxtime);
+  
+  /*------------------------------------------------------- printout head */
+  if (myrank==0) dyn_nlnstruct_outhead(&dynvar,sdyn);
+  
+  /*----------------------------------------------------------------------*/
+  /*                     START LOOP OVER ALL STEPS                        */
+  /*----------------------------------------------------------------------*/
+  timeloop:
+  double t0 = ds_cputime();
+  /*--------------------------------------------- increment step and time */
+  sdyn->step++;
+  sdyn->time += sdyn->dt;
+  acttime = sdyn->time;
+  /*-------------------------------------------------- set some constants */
+  dyn_setconstants(&dynvar,sdyn,sdyn->dt);
+  
+  /*---------------------- set incremental displacements dispi[0] to zero */
+  dispi->PutScalar(0.0);
+  
+  /*----------------------------------------------------------------------*/
+  /*                     PREDICTOR                                        */
+  /*----------------------------------------------------------------------*/
+  
+  /*---------------------- this vector holds loads due to external forces */
+  rhs1->PutScalar(0.0);
+  
+  //---------------------------------------------- evaluate external forces
+#if 0
+  {
+    // create the parameters for the discretization
+    ParameterList params;
+    // action for elements
+    params.set("action","calc_struct_eleload");
+    // choose what to assemble
+    params.set("assemble matrix 1",false);
+    params.set("assemble matrix 2",false);
+    params.set("assemble vector 1",true);
+    params.set("assemble vector 2",false);
+    params.set("assemble vector 3",false);
+    // other parameters that might be needed by the elements
+    params.set("total time",acttime);
+    params.set("delta time",sdyn->dt);
+    // set vector values needed by elements
+    actdis->ClearState();
+    actdis->SetState("displacement",sol0);
+    actdis->Evaluate(params,null,null,rhs1,null,null);
+    actdis->ClearState();
+  }
+#endif
 
 
 
