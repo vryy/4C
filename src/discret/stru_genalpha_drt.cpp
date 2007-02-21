df3859129bd06e0fc8592bab02dde2818ad19199
Michael Gee <gee@lnm.mw.tum.de>
2007-02-21 10:05:12 +0000

    added new genalpha routine
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2763 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/discret/stru_genalpha_drt.cpp
@@ -0,0 +1,476 @@
+/*!----------------------------------------------------------------------
+\file
+\brief
+
+<pre>
+Maintainer: Michael Gee
+            gee@lnm.mw.tum.de
+            http://www.lnm.mw.tum.de
+            089 - 289-15239
+</pre>
+
+*----------------------------------------------------------------------*/
+#ifdef CCADISCRET
+#ifdef TRILINOS_PACKAGE
+
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+
+#ifdef PARALLEL
+#include <mpi.h>
+#endif
+
+#include "stru_dyn_nln_drt.H"
+
+/*----------------------------------------------------------------------*
+  |                                                       m.gee 06/01    |
+  | vector of numfld FIELDs, defined in global_control.c                 |
+ *----------------------------------------------------------------------*/
+extern struct _FIELD      *field;
+
+/*----------------------------------------------------------------------*
+  |                                                       m.gee 06/01    |
+  | general problem data                                                 |
+  | global variable GENPROB genprob is defined in global_control.c       |
+ *----------------------------------------------------------------------*/
+extern struct _GENPROB     genprob;
+
+/*!----------------------------------------------------------------------
+\brief file pointers
+
+<pre>                                                         m.gee 8/00
+This structure struct _FILES allfiles is defined in input_control_global.c
+and the type is in standardtypes.h
+It holds all file pointers and some variables needed for the FRSYSTEM
+</pre>
+*----------------------------------------------------------------------*/
+extern struct _FILES  allfiles;
+
+/*----------------------------------------------------------------------*
+ |                                                       m.gee 06/01    |
+ | structure of flags to control output                                 |
+ | defined in out_global.c                                              |
+ *----------------------------------------------------------------------*/
+extern struct _IO_FLAGS     ioflags;
+
+/*----------------------------------------------------------------------*
+ | global variable *solv, vector of lenght numfld of structures SOLVAR  |
+ | defined in solver_control.c                                          |
+ |                                                                      |
+ |                                                       m.gee 11/00    |
+ *----------------------------------------------------------------------*/
+extern struct _SOLVAR  *solv;
+
+/*----------------------------------------------------------------------*
+ |                                                       m.gee 06/01    |
+ | pointer to allocate dynamic variables if needed                      |
+ | dedfined in global_control.c                                         |
+ | ALLDYNA               *alldyn;                                       |
+ *----------------------------------------------------------------------*/
+extern ALLDYNA      *alldyn;
+
+/*----------------------------------------------------------------------*
+ |                                                       m.gee 02/02    |
+ | number of load curves numcurve                                       |
+ | vector of structures of curves                                       |
+ | defined in input_curves.c                                            |
+ | INT                   numcurve;                                      |
+ | struct _CURVE      *curve;                                           |
+ *----------------------------------------------------------------------*/
+extern INT            numcurve;
+extern struct _CURVE *curve;
+
+/*----------------------------------------------------------------------*
+  | structural nonlinear dynamics (gen-alpha)              m.gee 12/06  |
+ *----------------------------------------------------------------------*/
+void stru_genalpha_drt()
+{
+  DSTraceHelper dst("stru_genalpha_drt");
+
+  // -------------------------------------------------------------------
+  // access the discretization
+  // -------------------------------------------------------------------
+  RefCountPtr<DRT::Discretization> actdis = null;
+  {
+    vector<RefCountPtr<DRT::Discretization> >* fool =
+              (vector<RefCountPtr<DRT::Discretization> >*)field[0].ccadis;
+    actdis = (*fool)[0];
+  }
+  // set degrees of freedom in the discretization
+  if (!actdis->Filled()) actdis->FillComplete();
+
+  // -------------------------------------------------------------------
+  // get a communicator and myrank
+  // -------------------------------------------------------------------
+  const Epetra_Comm& Comm = actdis->Comm();
+  
+  // -------------------------------------------------------------------
+  // set some pointers and variables
+  // -------------------------------------------------------------------
+  SOLVAR*         actsolv  = &solv[0];
+  STRUCT_DYNAMIC* sdyn     = alldyn[0].sdyn;
+  STRUCT_DYN_CALC dynvar;
+  memset(&dynvar, 0, sizeof(STRUCT_DYN_CALC));
+  double          acttime = 0.0;
+  
+  //-----------------------------------------------------create a solver
+  RefCountPtr<ParameterList> solveparams = rcp(new ParameterList());
+  LINALG::Solver solver(solveparams,actdis->Comm(),allfiles.out_err);
+  solver.TranslateSolverParameters(*solveparams,actsolv);
+  actdis->ComputeNullSpaceIfNecessary(*solveparams);
+
+  // -------------------------------------------------------------------
+  // get a vector layout from the discretization to construct matching
+  // vectors and matrices
+  // -------------------------------------------------------------------
+  const Epetra_Map* dofrowmap = actdis->DofRowMap();
+
+  // -------------------------------------------------------------------
+  // create empty matrices
+  // -------------------------------------------------------------------
+  RefCountPtr<Epetra_CrsMatrix> stiff_mat = LINALG::CreateMatrix(*dofrowmap,81);
+  RefCountPtr<Epetra_CrsMatrix> mass_mat  = LINALG::CreateMatrix(*dofrowmap,81);
+  RefCountPtr<Epetra_CrsMatrix> damp_mat  = null;
+  bool damping = false;
+  if (sdyn->damp==1) 
+  {
+    damping = true;
+    damp_mat = LINALG::CreateMatrix(*dofrowmap,81);
+  }
+  
+  /*--------------------------------------- init all applied time curves -*/
+  for (int actcurve=0; actcurve<numcurve; actcurve++)
+    dyn_init_curve(actcurve,sdyn->nstep,sdyn->dt,sdyn->maxtime);
+
+  // -------------------------------------------------------------------
+  // create empty vectors
+  // -------------------------------------------------------------------
+  RefCountPtr<Epetra_Vector> zeros        = LINALG::CreateVector(*dofrowmap,true);
+  RefCountPtr<Epetra_Vector> dirichtoggle = LINALG::CreateVector(*dofrowmap,true);
+  RefCountPtr<Epetra_Vector> invtoggle = LINALG::CreateVector(*dofrowmap,false);
+
+  RefCountPtr<Epetra_Vector> dis = LINALG::CreateVector(*dofrowmap,true);
+  RefCountPtr<Epetra_Vector> vel = LINALG::CreateVector(*dofrowmap,true);
+  RefCountPtr<Epetra_Vector> acc = LINALG::CreateVector(*dofrowmap,true);
+  
+  RefCountPtr<Epetra_Vector> disn = LINALG::CreateVector(*dofrowmap,false);
+  RefCountPtr<Epetra_Vector> veln = LINALG::CreateVector(*dofrowmap,false);
+  RefCountPtr<Epetra_Vector> accn = LINALG::CreateVector(*dofrowmap,false);
+
+  RefCountPtr<Epetra_Vector> dism = LINALG::CreateVector(*dofrowmap,false);
+  RefCountPtr<Epetra_Vector> velm = LINALG::CreateVector(*dofrowmap,false);
+  RefCountPtr<Epetra_Vector> accm = LINALG::CreateVector(*dofrowmap,false);
+
+  RefCountPtr<Epetra_Vector> disi = LINALG::CreateVector(*dofrowmap,true);
+
+  RefCountPtr<Epetra_Vector> fint = LINALG::CreateVector(*dofrowmap,true);
+  RefCountPtr<Epetra_Vector> fext = LINALG::CreateVector(*dofrowmap,true);
+  
+  RefCountPtr<Epetra_Vector> fextm = LINALG::CreateVector(*dofrowmap,false);
+
+  RefCountPtr<Epetra_Vector> fextn = LINALG::CreateVector(*dofrowmap,true);
+
+  RefCountPtr<Epetra_Vector> fresm = LINALG::CreateVector(*dofrowmap,false);
+  //-------------------------------------------- calculate external forces
+  {
+    ParameterList params;
+    // action for elements
+    params.set("action","calc_struct_eleload");
+    // choose what to assemble
+    params.set("assemble matrix 1",false);
+    params.set("assemble matrix 2",false);
+    params.set("assemble vector 1",true);
+    params.set("assemble vector 2",false);
+    params.set("assemble vector 3",false);
+    // other parameters needed by the elements
+    params.set("total time",acttime);
+    params.set("delta time",sdyn->dt);
+    // set vector values needed by elements
+    actdis->ClearState();
+    actdis->SetState("displacement",dis);
+    // predicted dirichlet values
+    // sol0 then also holds prescribed new dirichlet displacements
+    actdis->EvaluateDirichlet(params,*dis,*dirichtoggle);
+    actdis->ClearState();
+    actdis->SetState("displacement",dis);
+    // predicted rhs
+    actdis->EvaluateNeumann(params,*fext);
+    actdis->ClearState();
+  }
+
+  //----------------------- compute an inverse of the dirichtoggle vector
+  invtoggle->PutScalar(1.0);
+  invtoggle->Update(-1.0,*dirichtoggle,1.0);
+  
+  // -------------------------------------------------------------------
+  // call elements to calculate stiffness and mass
+  // -------------------------------------------------------------------
+  {
+    // create the parameters for the discretization
+    ParameterList params;
+    // action for elements
+    params.set("action","calc_struct_nlnstiffmass");
+    // choose what to assemble
+    params.set("assemble matrix 1",true);
+    params.set("assemble matrix 2",true);
+    params.set("assemble vector 1",true);
+    params.set("assemble vector 2",false);
+    params.set("assemble vector 3",false);
+    // other parameters that might be needed by the elements
+    params.set("total time",acttime);
+    params.set("delta time",sdyn->dt);
+    // set vector values needed by elements
+    actdis->ClearState();
+    actdis->SetState("residual displacement",zeros);
+    actdis->SetState("displacement",dis);
+    //actdis->SetState("velocity",vel); // not used at the moment
+    actdis->Evaluate(params,stiff_mat,mass_mat,fint,null,null);
+    actdis->ClearState();
+  }
+  
+  // complete stiffness and mass matrix
+  LINALG::Complete(*stiff_mat);
+  LINALG::Complete(*mass_mat);
+  const int maxentriesperrow = stiff_mat->MaxNumEntries();
+
+  // build damping matrix if neccessary
+  if (damping)
+  {
+    LINALG::Add(*stiff_mat,false,sdyn->k_damp,*damp_mat,0.0);
+    LINALG::Add(*mass_mat ,false,sdyn->m_damp,*damp_mat,1.0);
+    LINALG::Complete(*damp_mat);
+  }
+  
+  //--------------------------- calculate consistent initial accelerations
+  {
+    RefCountPtr<Epetra_Vector> rhs = LINALG::CreateVector(*dofrowmap,true);
+    if (damping) damp_mat->Multiply(false,*vel,*rhs);
+    rhs->Update(-1.0,*fint,1.0,*fext,-1.0);
+    Epetra_Vector rhscopy(*rhs);
+    rhs->Multiply(1.0,*invtoggle,rhscopy,0.0);
+    solver.Solve(mass_mat,acc,rhs,true,true);
+  }
+  
+  //----------------------------------- time integration scheme parameters
+  const double beta   = sdyn->beta;
+  const double gamma  = sdyn->gamma;
+  const double alpham = sdyn->alpha_m;
+  const double alphaf = sdyn->alpha_f;
+  const double dt     = sdyn->dt;
+
+  //==================================================== start of timeloop
+  timeloop:
+  //------------------------------------------------- current time t_{n+1}
+  acttime += dt;
+  //----------------------------------------------------- predicting state
+  // constant predictor displacement
+  // using last converged displ. in domain + prescribed new dirichlet BCs
+  disn->Update(1.0,*dis,0.0);
+  {
+    ParameterList params;
+    // action for elements
+    params.set("action","calc_struct_eleload");
+    // choose what to assemble
+    params.set("assemble matrix 1",false);
+    params.set("assemble matrix 2",false);
+    params.set("assemble vector 1",true);
+    params.set("assemble vector 2",false);
+    params.set("assemble vector 3",false);
+    // other parameters needed by the elements
+    params.set("total time",acttime);
+    params.set("delta time",sdyn->dt);
+    // set vector values needed by elements
+    actdis->ClearState();
+    actdis->SetState("displacement",disn);
+    // predicted dirichlet values
+    // disn then also holds prescribed new dirichlet displacements
+    actdis->EvaluateDirichlet(params,*disn,*dirichtoggle);
+    actdis->ClearState();
+    actdis->SetState("displacement",disn);
+    actdis->EvaluateNeumann(params,*fextn);
+    actdis->ClearState();
+  }
+  
+  // predicting velocity vel_{n+1} (veln)
+  veln->Update(1.0,*disn,-1.0,*dis,0.0);
+  veln->Update((beta-gamma)/beta,*vel,
+               (2.*beta-gamma)/(2.*beta),*acc,
+               gamma/(beta*dt));
+  // predicting accelerations acc_{n+1} (accn)
+  accn->Update(1.0,*disn,-1.0,*dis,0.0);
+  accn->Update(-1./(beta*dt),*vel,
+               (2.*beta-1.)/(2.*beta),*acc,
+               1./(beta*dt*dt));
+  
+  //------------------------------ compute interpolated dis, vel and acc
+  dism->Update(1.-alphaf,*disn,alphaf,*dis,0.0);  // dis_{n+1-alpha_f}
+  velm->Update(1.-alphaf,*veln,alphaf,*vel,0.0);  // vel_{n+1-alpha_f}
+  accm->Update(1.-alpham,*accn,alpham,*acc,0.0);  // acc_{n+1-alpha_m}
+  
+  //--------------------------------- compute interpolated external forces
+  fextm->Update(1.-alphaf,*fextn,alphaf,*fext,0.0);
+  
+  //---------------- eval fint at interpolated state, eval stiffness matrix
+  {
+    // zero out stiffness
+    stiff_mat = LINALG::CreateMatrix(*dofrowmap,maxentriesperrow);
+    // create the parameters for the discretization
+    ParameterList params;
+    // action for elements
+    params.set("action","calc_struct_nlnstiff");
+    // choose what to assemble
+    params.set("assemble matrix 1",true);
+    params.set("assemble matrix 2",false);
+    params.set("assemble vector 1",true);
+    params.set("assemble vector 2",false);
+    params.set("assemble vector 3",false);
+    // other parameters that might be needed by the elements
+    params.set("total time",acttime);
+    params.set("delta time",sdyn->dt);
+    // set vector values needed by elements
+    actdis->ClearState();
+    actdis->SetState("residual displacement",disi);
+    actdis->SetState("displacement",dism);
+    //actdis->SetState("velocity",velm); // not used at the moment
+    fint->PutScalar(0.0);  // initialise internal force vector
+    actdis->Evaluate(params,stiff_mat,null,fint,null,null);
+    actdis->ClearState();
+    // finalize the stiffness matrix
+    LINALG::Complete(*stiff_mat);
+  }
+    
+  //--------------------------------------------------- compute residual forces
+  mass_mat->Multiply(false,*accm,*fresm);
+  if (damping)
+  {
+    RefCountPtr<Epetra_Vector> fviscm = LINALG::CreateVector(*dofrowmap,false);
+    damp_mat->Multiply(false,*velm,*fviscm);
+    fresm->Update(1.0,*fviscm,1.0);
+  }
+  fresm->Update(1.0,*fint,-1.0,*fextm,1.0);
+  {
+    Epetra_Vector fresmcopy(*fresm);
+    fresm->Multiply(1.0,*invtoggle,fresmcopy,0.0);
+  }
+
+  //------------------------------------------------------- build residual norm
+  double norm;
+  fresm->Norm2(&norm);
+  cout << "Norm bevor Newton " << norm << endl;
+  
+  //========================================================== equilibrium loop
+  int numiter=0;
+  while (norm>sdyn->toldisp && numiter<=sdyn->maxiter)
+  //debug: while (numiter<=sdyn->maxiter)
+  {
+    //-------------------------------------------------- effective rhs is fresm
+    //----------------------------------------------------- build effective lhs
+    RefCountPtr<Epetra_CrsMatrix> eff_mat = LINALG::CreateMatrix(*dofrowmap,maxentriesperrow);
+    LINALG::Add(*stiff_mat,false,1.-alphaf,*eff_mat,0.0);
+    if (damping)
+    {
+      LINALG::Add(*damp_mat,false,(1.-alphaf)*gamma/(beta*dt),*eff_mat,1.0);
+    }
+    LINALG::Add(*mass_mat,false,(1.-alpham)/(beta*dt*dt),*eff_mat,1.0);
+    LINALG::Complete(*eff_mat);
+    
+    //----------------------------- apply dirichlet BCs to system of equations
+    fresm->Scale(-1.0);  // delete this by building fresm with other sign
+    disi->PutScalar(0.0);  // Useful? depends on solver and more
+    LINALG::ApplyDirichlettoSystem(eff_mat,disi,fresm,zeros,dirichtoggle);
+    
+    //--------------------------------------------------------- solve for disi
+    // Solve K_Teffdyn . IncD = -R  ===>  IncD
+    if (numiter==0)
+    {
+	solver.Solve(eff_mat,disi,fresm,true,true);
+    }
+    else
+    {
+	solver.Solve(eff_mat,disi,fresm,true,false);
+    }
+
+    //---------------------------------------- update mid configuration values
+    // D_{n+1-alpha_f} := D_{n+1-alpha_f} + (1-alpha_f)*IncD_{n+1}
+    dism->Update(1.-alphaf,*disi,1.0);
+    // V_{n+1-alpha_f} := V_{n+1-alpha_f} 
+    //                  + (1-alpha_f)*gamma/beta/dt*IncD_{n+1}
+    velm->Update((1.-alphaf)*gamma/(beta*dt),*disi,1.0);
+    // A_{n+1-alpha_m} := A_{n+1-alpha_m} 
+    //                  + (1-alpha_m)/beta/dt^2*IncD_{n+1}
+    accm->Update((1.-alpham)/(beta*dt*dt),*disi,1.0);
+    
+    //---------------------------------- compute internal forces and stiffness
+    {
+      // zero out stiffness
+      stiff_mat = LINALG::CreateMatrix(*dofrowmap,maxentriesperrow);
+      // create the parameters for the discretization
+      ParameterList params;
+      // action for elements
+      params.set("action","calc_struct_nlnstiff");
+      // choose what to assemble
+      params.set("assemble matrix 1",true);
+      params.set("assemble matrix 2",false);
+      params.set("assemble vector 1",true);
+      params.set("assemble vector 2",false);
+      params.set("assemble vector 3",false);
+      // other parameters that might be needed by the elements
+      params.set("total time",acttime);
+      params.set("delta time",sdyn->dt);
+      // set vector values needed by elements
+      actdis->ClearState();
+      actdis->SetState("residual displacement",disi);
+      actdis->SetState("displacement",dism);
+      //actdis->SetState("velocity",velm); // not used at the moment
+      fint->PutScalar(0.0);  // initialise internal force vector
+      actdis->Evaluate(params,stiff_mat,null,fint,null,null);
+      actdis->ClearState();
+      // finalize the stiffness matrix
+      LINALG::Complete(*stiff_mat);
+    }
+    
+    //------------------------------------------------- compute residual forces
+    // Res = M . A_{n+1-alpha_m} 
+    //     + C . V_{n+1-alpha_f} 
+    //     + F_int(D_{n+1-alpha_f})
+    //     - F_{ext;n+1-alpha_f}
+    mass_mat->Multiply(false,*accm,*fresm);
+    if (damping)
+    {
+      RefCountPtr<Epetra_Vector> fviscm = LINALG::CreateVector(*dofrowmap,false);
+      damp_mat->Multiply(false,*velm,*fviscm);
+      fresm->Update(1.0,*fviscm,1.0);
+    }
+    fresm->Update(1.0,*fint,-1.0,*fextm,1.0);
+    {
+      Epetra_Vector fresmcopy(*fresm);
+      fresm->Multiply(1.0,*invtoggle,fresmcopy,0.0);
+    }
+    
+    //----------------------------------------------------- build residual norm
+    double disinorm;
+    disi->Norm2(&disinorm);
+    
+    fresm->Norm2(&norm);
+    cout << "numiter " << numiter 
+         << " norm " << norm
+         << " disinorm " << disinorm << endl;;
+    
+    
+    //---------------------------------------- increment equilibrium loop index
+    ++numiter;
+  } // while (norm>sdyn->toldisp)
+
+
+
+  //------------- this is the end...
+  return;
+} // end of dyn_nlnstructural_drt()
+
+
+
+
+
+#endif  // #ifdef TRILINOS_PACKAGE
+#endif  // #ifdef CCADISCRET
