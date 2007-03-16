836be21e8145c46cdd85dd90dd0991ac9dfdcb00
Georg Bauer <bauer@lnm.mw.tum.de>
2007-03-16 16:20:02 +0000

    Some changes in NonSymmetricInverse routine in order to get the same results as within the old discretization. But this version is surely not the final one and has to be reviewed !!!
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2859 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/discret/linalg_utils.cpp
+++ src/discret/linalg_utils.cpp
@@ -43,11 +43,11 @@ void LINALG::Export(const Epetra_Vector& source, Epetra_Vector& target)
   bool targetunique = false;
   if (source.Map().UniqueGIDs()) sourceunique = true;
   if (target.Map().UniqueGIDs()) targetunique = true;
-  
+
   // Note:
   // source map of an import must be unique
   // target map of an export must be unique
-  
+
   // both are unique, does not matter whether ex- or import
   if (sourceunique && targetunique)
   {
@@ -79,33 +79,33 @@ void LINALG::Export(const Epetra_Vector& source, Epetra_Vector& target)
     dserror("Neither target nor source maps are unique - cannot export");
   }
   else dserror("VERY strange");
-  
+
   return;
 }
 
 /*----------------------------------------------------------------------*
  |  assemble a matrix  (public)                              mwgee 12/06|
  *----------------------------------------------------------------------*/
-void LINALG::Assemble(Epetra_CrsMatrix& A, const Epetra_SerialDenseMatrix& Aele, 
+void LINALG::Assemble(Epetra_CrsMatrix& A, const Epetra_SerialDenseMatrix& Aele,
                       const vector<int>& lm, const vector<int>& lmowner)
 {
   if (A.Filled()) dserror("sparse matrix A already filled, cannot assemble");
   const int ldim = (int)lm.size();
   if (ldim!=(int)lmowner.size() || ldim!=Aele.M() || ldim!=Aele.N())
     dserror("Mismatch in dimensions");
-  
+
   const int myrank = A.Comm().MyPID();
-  
+
   // loop rows of local matrix
   for (int lrow=0; lrow<ldim; ++lrow)
   {
     // check ownership of row
     if (lmowner[lrow] != myrank) continue;
-    
+
     // check whether I have that global row
     int rgid = lm[lrow];
     if (!(A.RowMap().MyGID(rgid))) dserror("Sparse matrix A does not have global row %d",rgid);
-    
+
     for (int lcol=0; lcol<ldim; ++lcol)
     {
       double val = Aele(lrow,lcol);
@@ -127,15 +127,15 @@ void LINALG::Assemble(Epetra_CrsMatrix& A, const Epetra_SerialDenseMatrix& Aele,
 /*----------------------------------------------------------------------*
  |  assemble a vector  (public)                              mwgee 12/06|
  *----------------------------------------------------------------------*/
-void LINALG::Assemble(Epetra_Vector& V, const Epetra_SerialDenseVector& Vele, 
+void LINALG::Assemble(Epetra_Vector& V, const Epetra_SerialDenseVector& Vele,
                 const vector<int>& lm, const vector<int>& lmowner)
 {
   const int ldim = (int)lm.size();
   if (ldim!=(int)lmowner.size() || ldim!=Vele.Length())
     dserror("Mismatch in dimensions");
-  
+
   const int myrank = V.Comm().MyPID();
-  
+
   for (int lrow=0; lrow<ldim; ++lrow)
   {
     if (lmowner[lrow] != myrank) continue;
@@ -144,7 +144,7 @@ void LINALG::Assemble(Epetra_Vector& V, const Epetra_SerialDenseVector& Vele,
     int rlid = V.Map().LID(rgid);
     V[rlid] += Vele[lrow];
   } // for (int lrow=0; lrow<ldim; ++lrow)
-  
+
   return;
 }
 
@@ -154,7 +154,7 @@ void LINALG::Assemble(Epetra_Vector& V, const Epetra_SerialDenseVector& Vele,
 void LINALG::Complete(Epetra_CrsMatrix& A)
 {
   if (A.Filled()) return;
-  
+
   int err = A.FillComplete(A.OperatorDomainMap(),A.OperatorRangeMap());
   if (err) dserror("Epetra_CrsMatrix::FillComplete(domain,range) returned err=%d",err);
   err = A.OptimizeStorage();
@@ -166,15 +166,15 @@ void LINALG::Complete(Epetra_CrsMatrix& A)
  |  Add a sparse matrix to another  (public)                 mwgee 12/06|
  |  B = B*scalarB + A(transposed)*scalarA                               |
  *----------------------------------------------------------------------*/
-void LINALG::Add(const Epetra_CrsMatrix& A, 
-                 const bool transposeA, 
+void LINALG::Add(const Epetra_CrsMatrix& A,
+                 const bool transposeA,
                  const double scalarA,
-                 Epetra_CrsMatrix& B, 
+                 Epetra_CrsMatrix& B,
                  const double scalarB)
 {
   if (!A.Filled()) dserror("FillComplete was not called on A");
   if (B.Filled()) dserror("FillComplete was called on B before");
-  
+
   Epetra_CrsMatrix*               Aprime = NULL;
   EpetraExt::RowMatrix_Transpose* Atrans = NULL;
   if (transposeA)
@@ -186,9 +186,9 @@ void LINALG::Add(const Epetra_CrsMatrix& A,
   {
     Aprime = const_cast<Epetra_CrsMatrix*>(&A);
   }
-  
+
   B.Scale(scalarB);
-  
+
   //Loop over Aprime's rows and sum into
   int MaxNumEntries = EPETRA_MAX( Aprime->MaxNumEntries(), B.MaxNumEntries() );
   int NumEntries;
@@ -234,8 +234,8 @@ extern "C"
 {
   void dsytrf(char *uplo, int *n, double *a, int *lda, int *ipiv, double *work, int *lwork, int *info);
   void dsytri(char *uplo, int *n, double *a, int *lda, int *ipiv, double *work, int *info);
-  void dgetrf(int *m,int *n, double *a, int *lda, int *ipiv, int* info);  
-  void dgetri(int *n, double *a, int *lda, int *ipiv, double *work, int *lwork, int *info);                         
+  void dgetrf(int *m,int *n, double *a, int *lda, int *ipiv, int* info);
+  void dgetri(int *n, double *a, int *lda, int *ipiv, double *work, int *lwork, int *info);
 }
 
 /*----------------------------------------------------------------------*
@@ -257,39 +257,102 @@ void LINALG::SymmetricInverse(Epetra_SerialDenseMatrix& A, const int dim)
 
   dsytrf(uplo,&m,a,&n,&(ipiv[0]),&(work[0]),&lwork,&info);
   if (info) dserror("dsytrf returned info=%d",info);
-  
+
   dsytri(uplo,&m,a,&n,&(ipiv[0]),&(work[0]),&info);
   if (info) dserror("dsytri returned info=%d",info);
-  
+
   for (int i=0; i<dim; ++i)
     for (int j=0; j<i; ++j)
       A(j,i)=A(i,j);
   return;
 }
 
+
+
+
 /*----------------------------------------------------------------------*
- |  invert a dense nonsymmetric matrix  (public)             g.bau 03/07|
- *----------------------------------------------------------------------*/
+ |  invert a dense nonsymmetric matrix  (public)             g.bau 03/07|
+ *----------------------------------------------------------------------*/
+#include <Epetra_SerialDenseSolver.h>
+
 void LINALG::NonSymmetricInverse(Epetra_SerialDenseMatrix& A, const int dim)
 {
   if (A.M() != A.N()) dserror("Matrix is not square");
   if (A.M() != dim) dserror("Dimension supplied does not match matrix");
 
+
+  //------------------------------------------------------------------------------
+// this routine has to be reviewed and beautified !!!!!!!!!!!!!!!
+// in the current version exactly the same results as in the old
+// discretization are obtained.
+// => necessary for debugging of fluid3 element.
+//------------------------------------------------------------------------------
+//  Take care of:   A^{-1} =  A^T^{-1}^T
+//  But in numerics there will be differences, of course.
+
+
+
+ // more elegant solution to the ordering problem mentioned below
+ // could be:
+/*
+ Epetra_SerialDenseSolver solver;
+ solver.SetMatrix(A);
+int err = solver.Invert();
+ if (err!=0)
+  dserror("Inversion of nonsymmetric matrix failed.");
+*/
+
+  // within Epetra_SerialDenseMatrix entries are stored columnwise. Therefore
+  // it is necessary to transpose before and after the LAPACK routines
+  // to have the same order (rowwise!!) as in the old C code (see src/math/math1.c)
+
+  #if 1
+  double mywork=0.0;
+  for (int i=0; i<dim; ++i)
+    {
+    for (int j=0; j<i; ++j)
+    {mywork = A(i,j);
+      A(i,j)=A(j,i);
+      A(j,i)=mywork;
+     }
+    }
+   #endif
+
   double* a = A.A();
   char uplo[5]; strncpy(uplo,"L ",2);
   vector<int> ipiv(dim);
-  int lwork = 10*dim;
+  int lwork = 6;
+  //int lwork = 10*dim;
   vector<double> work(lwork);
   int info=0;
   int n = dim;
   int m = dim;
-  
+
+
+  #if 0
+  for (int i=0;i<36;++i)
+  printf("a[%d] %22.16e\n",i,a[i]);
+   #endif
+
   dgetrf(&m,&n,a,&m,&(ipiv[0]),&info);
   if (info) dserror("dgetrf returned info=%d",info);
-  
-  dgetri(&m,a,&n,&(ipiv[0]),&(work[0]),&lwork,&info);
+
+  dgetri(&n,a,&n,&(ipiv[0]),&(work[0]),&lwork,&info);
   if (info) dserror("dgetri returned info=%d",info);
-  
+
+  // undo transpose of A
+   #if 1
+  mywork=0.0;
+  for (int i=0; i<dim; ++i)
+    {
+    for (int j=0; j<i; ++j)
+    {mywork = A(i,j);
+      A(i,j)=A(j,i);
+      A(j,i)=mywork;
+     }
+    }
+   #endif
+
   return;
 }
 
@@ -323,8 +386,8 @@ void LINALG::ApplyDirichlettoSystem(RefCountPtr<Epetra_CrsMatrix>&   A,
     if (!rowmap.PointSameAs(dbcval->Map())) dserror("dbcval does not match A");
   if (!rowmap.PointSameAs(dbctoggle->Map())) dserror("dbctoggle does not match A");
   if (A->Filled()!=true) dserror("FillComplete was not called on A");
-#endif  
-  
+#endif
+
   const Epetra_Vector& dbct = *dbctoggle;
   if (x != null && b != null)
   {
@@ -340,7 +403,7 @@ void LINALG::ApplyDirichlettoSystem(RefCountPtr<Epetra_CrsMatrix>&   A,
         B[i] = dbcv[i];
       }
   }
-    
+
   // allocate a new matrix and copy all rows that are not dirichlet
   const int nummyrows     = A->NumMyRows();
   const int maxnumentries = A->MaxNumEntries();
@@ -373,7 +436,7 @@ void LINALG::ApplyDirichlettoSystem(RefCountPtr<Epetra_CrsMatrix>&   A,
   }
   LINALG::Complete(*Anew);
   A = Anew;
-  
+
   return;
 }
 
