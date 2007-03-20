59f0deba2b59f8b0ff041f9816034e916ddc14ec
Georg Bauer <bauer@lnm.mw.tum.de>
2007-03-20 15:30:24 +0000

    Added spatial functions handling to the new discretization
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2873 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/discret/drt_discret_evaluate.cpp
+++ src/discret/drt_discret_evaluate.cpp
@@ -106,7 +106,7 @@ void DRT::Discretization::Evaluate(
 
 extern "C"
 {
-  void dyn_facfromcurve(int actcurve,double T,double *fac);  
+  void dyn_facfromcurve(int actcurve,double T,double *fac); 
 }
 /*----------------------------------------------------------------------*
  |  evaluate Neumann conditions (public)                     mwgee 12/06|
@@ -188,12 +188,19 @@ void DRT::Discretization::EvaluateNeumann(ParameterList& params, Epetra_Vector& 
   return;
 }
 
+
 static void DoDirichletCondition(DRT::Condition&      cond,
                                  DRT::Discretization& dis,
                                  const bool           usetime,
                                  const double         time,
                                  Epetra_Vector&       systemvector,
                                  Epetra_Vector&       toggle);
+				 
+static double EvaluateFunction(DRT::Node*        node,
+		               int               index,
+			       int		 funct_num);				 
+
+				
 /*----------------------------------------------------------------------*
  |  evaluate Dirichlet conditions (public)                   mwgee 01/07|
  *----------------------------------------------------------------------*/
@@ -270,8 +277,10 @@ void DoDirichletCondition(DRT::Condition&      cond,
   if (!nodeids) dserror("Dirichlet condition does not have nodal cloud");
   const int nnode = (*nodeids).size();
   vector<int>*    curve  = cond.Get<vector<int> >("curve");
+  vector<int>*    funct  = cond.Get<vector<int> >("funct");
   vector<int>*    onoff  = cond.Get<vector<int> >("onoff");
   vector<double>* val    = cond.Get<vector<double> >("val");
+
   for (int i=0; i<nnode; ++i)
   {
     // do only nodes in my row map
@@ -285,13 +294,28 @@ void DoDirichletCondition(DRT::Condition&      cond,
       if ((*onoff)[j]==0) continue;
       const int gid = dofs[j];
       double value  = (*val)[j];
+      
+      // factor given by time curve
       double curvefac = 1.0;
       int    curvenum = -1;
       if (curve) curvenum = (*curve)[j];
       if (curvenum>=0 && usetime)
         dyn_facfromcurve(curvenum,time,&curvefac);
       //cout << "Dirichlet value " << value << " curvefac " <<  curvefac << endl;
-      value *= curvefac;
+            
+      // factor given by spatial function
+      double functfac = 1.0;
+      int funct_num = -1;
+      if (funct) funct_num = (*funct)[j];
+       {
+         if (funct_num>0)
+         functfac = EvaluateFunction(actnode,j,funct_num);
+       }
+      //cout << "Dirichlet value " << value << " functfac " <<  functfac << endl;
+	 
+      //apply factors to dirichlet value
+      value *= (curvefac*functfac);
+      
       const int lid = systemvector.Map().LID(gid);
       if (lid<0) dserror("Global id %d not on this proc in system vector",gid);
       systemvector[lid] = value;
@@ -300,6 +324,279 @@ void DoDirichletCondition(DRT::Condition&      cond,
   }
   return;
 }
+
+
+// some extern declarations necessary for routine 'EvaluateFunction'
+extern "C"
+{ 
+double pss_evaluate_funct(struct _ST_NODE* funct, double x, double y, double z);
+}
+#include "../headers/standardtypes.h"
+#include "../pss_full/pss_parser.h"
+
+extern struct _FUNCT* funct;
+extern int            numfunct; 
+
+
+/*----------------------------------------------------------------------*
+ |  evaluate spatial function (public)                       g.bau 03/07|
+ *----------------------------------------------------------------------*/
+double EvaluateFunction(DRT::Node*      node,
+		        int             index,
+			int             funct_num)
+{
+  double  xi;
+  double  length,length_1,length_2;
+  double  xp[3],x1[3],x2[3];
+  double  fac;
+  double  a,d;
+  double  h,um;
+  
+  if (funct_num < 1 || funct_num > numfunct)
+    dserror("Unknown function: FUNCT%d",funct_num); 
+  
+  // function number funct_num has now to be reduced by one
+  funct_num -= 1;  
+  
+  // get coordinates of current node
+  xp[0]     = node->X()[0];
+  xp[1]     = node->X()[1];
+  xp[2]     = node->X()[2];
+
+  //switch to the correct funtion 
+  switch (funct[funct_num].functtyp)
+  {
+    case funct_line_lin:  // linear function
+      
+      FUNCT_LINE_LIN     *f_line_lin;
+       
+      f_line_lin = funct[funct_num].typ.funct_line_lin;
+      if (f_line_lin==NULL)
+	dserror("failed to read function %d",index);
+
+      length = f_line_lin->length;
+
+      // x1: vector along the line 
+      x1[0] = f_line_lin->x2[0] - f_line_lin->x1[0];
+      x1[1] = f_line_lin->x2[1] - f_line_lin->x1[1];
+      x1[2] = f_line_lin->x2[2] - f_line_lin->x1[2];
+
+      // x2: vector from the beginning of the line the the point 
+      x2[0] = xp[0] - f_line_lin->x1[0];
+      x2[1] = xp[1] - f_line_lin->x1[1];
+      x2[2] = xp[2] - f_line_lin->x1[2];
+
+      // length_1 = projection of x2 onto x1 
+      length_1 = ( x1[0]*x2[0] + x1[1]*x2[1] + x1[2]*x2[2] ) / length;
+      // length_2 = length of the vector x2 
+      length_2 = sqrt( x2[0]*x2[0] + x2[1]*x2[1] + x2[2]*x2[2] );
+
+      // check for a point not on the line 
+      if ( FABS(length_1 - length_2) > 10e-6 )
+        //dswarning(1,6);
+	;
+	
+      // calculate xi and check for a point outside the range of the funct 
+      xi =  length_1/length;
+      if (xi < 0.0 || xi > 1.0)
+        //dswarning(1,5);
+	;
+      // calculate function value at point p 
+      fac = f_line_lin->b + xi * f_line_lin->m;
+      break;
+
+  //----------------------------------------------------------------
+
+    case funct_radius_lin:  // linear function 
+    
+      FUNCT_RADIUS_LIN   *f_radius_lin;
+    
+      f_radius_lin = funct[funct_num].typ.funct_radius_lin;
+
+      length = f_radius_lin->length;
+
+      // x2: vector from the beginning of the line the the point 
+      x1[0] = xp[0] - f_radius_lin->x1[0];
+      x1[1] = xp[1] - f_radius_lin->x1[1];
+      x1[2] = xp[2] - f_radius_lin->x1[2];
+
+      // length_1 = length of the vector x1 
+      length_1 = sqrt( x1[0]*x1[0] + x1[1]*x1[1] + x1[2]*x1[2] );
+
+      // calculate xi and check for a point outside the range of the funct 
+      xi =  length_1/length;
+      if (xi < 0.0 || xi > 1.0)
+        //dswarning(1,5);
+	;
+
+      // calculate function value at point p 
+      fac = f_radius_lin->b + xi * f_radius_lin->m;
+      break;
+
+  //----------------------------------------------------------------
+
+    case funct_line_quad: //quadratic parabola 
+    
+      FUNCT_LINE_QUAD    *f_line_quad;
+    
+      f_line_quad = funct[funct_num].typ.funct_line_quad;
+
+      length = f_line_quad->length;
+
+      // x1: vector along the line 
+      x1[0] = f_line_quad->x2[0] - f_line_quad->x1[0];
+      x1[1] = f_line_quad->x2[1] - f_line_quad->x1[1];
+      x1[2] = f_line_quad->x2[2] - f_line_quad->x1[2];
+
+      // x2: vector from the beginning of the line the the point 
+      x2[0] = xp[0] - f_line_quad->x1[0];
+      x2[1] = xp[1] - f_line_quad->x1[1];
+      x2[2] = xp[2] - f_line_quad->x1[2];
+
+      // length_1 = projection of x2 onto x1 
+      length_1 = ( x1[0]*x2[0] + x1[1]*x2[1] + x1[2]*x2[2] ) / length;
+      // length_2 = length of the vector x2 
+      length_2 = sqrt( x2[0]*x2[0] + x2[1]*x2[1] + x2[2]*x2[2] );
+
+      // check for a point not on the line 
+      if ( FABS(length_1 - length_2) > 10e-6 )
+        //dswarning(1,6);
+	;
+
+      // calculate xi and check for a point outside the range of the funct 
+      xi =  length_1/length;
+      if (xi < 0.0 || xi > 1.0)
+        //dswarning(1,5);
+	;
+
+      // calculate function value at point p 
+      fac = 1.0 - 4 * (xi - 1.0/2.0)*(xi - 1.0/2.0);
+      break;
+
+  //----------------------------------------------------------------
+
+    case funct_radius_quad: //quadratic parabola 
+    
+      FUNCT_RADIUS_QUAD  *f_radius_quad;
+    
+      f_radius_quad = funct[funct_num].typ.funct_radius_quad;
+
+      length = f_radius_quad->length;
+
+      // x1: vector from the beginning of the line the the point 
+      x1[0] = xp[0] - f_radius_quad->x1[0];
+      x1[1] = xp[1] - f_radius_quad->x1[1];
+      x1[2] = xp[2] - f_radius_quad->x1[2];
+
+      // length_1 = length of the vector x1 */
+      length_1 = sqrt( x1[0]*x1[0] + x1[1]*x1[1] + x1[2]*x1[2] );
+
+      // calculate xi and check for a point outside the range of the funct 
+      xi =  length_1/length;
+      if ( xi > 1.0)
+        //dswarning(1,5);
+	;
+
+      // calculate function value at point p 
+      fac = 1.0 - xi * xi ;
+      break;
+
+  //----------------------------------------------------------------
+
+    case funct_bel:  // spatial function for beltrami flow 
+      // set some constants 
+      a    = PI/4.0;
+      d    = PI/2.0;
+      // calculate values 
+      switch (index)
+      {
+        case 0:
+          fac  = -a * ( exp(a*xp[0]) * sin(a*xp[1] + d*xp[2]) +
+              exp(a*xp[2]) * cos(a*xp[0] + d*xp[1]) );
+          break;
+        case 1:
+          fac  = -a * ( exp(a*xp[1]) * sin(a*xp[2] + d*xp[0]) +
+              exp(a*xp[0]) * cos(a*xp[1] + d*xp[2]) );
+          break;
+        case 2:
+          fac  = -a * ( exp(a*xp[2]) * sin(a*xp[0] + d*xp[1]) +
+              exp(a*xp[1]) * cos(a*xp[2] + d*xp[0]) );
+          break;
+        case 3:
+          fac  = -a*a/2 * ( exp(2*a*xp[0]) + exp(2*a*xp[1]) + exp(2*a*xp[2])
+              + 2* sin(a*xp[0]+d*xp[1]) * cos(a*xp[2]+d*xp[0]) * exp(a*(xp[1]+xp[2]))
+              + 2* sin(a*xp[1]+d*xp[2]) * cos(a*xp[0]+d*xp[1]) * exp(a*(xp[2]+xp[0]))
+              + 2* sin(a*xp[2]+d*xp[0]) * cos(a*xp[1]+d*xp[2]) * exp(a*(xp[0]+xp[1])));
+          break;
+        default:
+          fac = 1.0;
+          break;
+      }
+      break;
+
+  //----------------------------------------------------------------
+
+    case funct_kim:  // spatial function for kim-moin flow 
+      // set some constants 
+      a    = 2.0;
+      // calculate values 
+      switch (index)
+      {
+        case 0:
+          fac  = - cos(a*PI*xp[0]) * sin(a*PI*xp[1]);
+          break;
+        case 1:
+          fac  = + sin(a*PI*xp[0]) * cos(a*PI*xp[1]);
+          break;
+        case 2:
+          fac  = -1.0/4.0 * ( cos(2.0*a*PI*xp[0]) + cos(2.0*a*PI*xp[1]) );
+          break;
+        default:
+          fac = 1.0;
+          break;
+      }
+      break;
+
+  //----------------------------------------------------------------
+
+    case funct_cyl:
+    
+      FUNCT_CYL          *f_cyl;  
+      f_cyl = funct[funct_num].typ.funct_cyl;
+
+      // set some constants 
+      h    = 0.41;
+      um = f_cyl->um;
+
+      // calculate values 
+      fac = 16*um*xp[1]*xp[2]*(h-xp[1])*(h-xp[2]) / (h*h*h*h);
+      break;
+
+  //----------------------------------------------------------------
+
+  case funct_explicit:
+  {
+    FUNCT_EXPLICIT* f = funct[funct_num].typ.funct_explicit;
+    fac = pss_evaluate_funct(f->funct,
+		       xp[0] - f->x[0],
+		       xp[1] - f->x[1],
+		       xp[2] - f->x[2]);
+    break;
+  }
+
+
+    default:  // default: no function 
+      fac = 1.0;
+      break;
+  } // end of switch (funct[funct_num].functtyp)
+
+
+  return fac;
+
+} // end of EvaluateFunction	
+
+
+
 
 #endif  // #ifdef TRILINOS_PACKAGE
 #endif  // #ifdef CCADISCRET
