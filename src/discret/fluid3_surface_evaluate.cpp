7a55317713bd8cd96b70719f20f62190e5f7ac02
Georg Bauer <bauer@lnm.mw.tum.de>
2007-03-28 12:20:00 +0000

    cleanup of unused variables
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2897 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/discret/fluid3_surface_evaluate.cpp
+++ src/discret/fluid3_surface_evaluate.cpp
@@ -40,24 +40,7 @@ int DRT::Elements::Fluid3Surface::EvaluateNeumann(
 {  
   DSTraceHelper dst("Fluid3Surface::EvaluateNeumann");
   dserror("Surface Neumann condition not yet implemented for Fluid3");
-  // find out whether we will use a time curve
-  
 
-  // find out whether we will use a time curve and get the factor
-
-
-  // init gaussian points of parent element
-  FLUID_DATA data;
-  parent_->f3_integration_points(data);
-
-  // number of parent element nodes
-  const int iel = parent_->NumNode();
-
-  const int nir = parent_->ngp_[0];
-  const int nis = parent_->ngp_[1];
-  const int nit = parent_->ngp_[2];
-  const int numdf = 4;
-  
   return 0;
 }
 
