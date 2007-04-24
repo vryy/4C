69951eec51c99b5d417f8e077e0b60d1d63512b0
Peter Gamnitzer <gamnitzer@lnm.mw.tum.de>
2007-04-24 13:52:51 +0000

    added a commented version of the old stabilisation parameters
     -> using the new version of tau changes the solution considerably,
        more then 10 percent!!!
    
     -> to compare the baci code with the old ccarat you have to use the 
        same tau!!!!!
    
    
    
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@2984 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/discret/fluid3_evaluate.cpp
+++ src/discret/fluid3_evaluate.cpp
@@ -238,7 +238,6 @@ if(!is_ale_)
    Epetra_SerialDenseMatrix 	wa1(100,100);  // working matrix used as dummy
    vector<double>    		histvec(3); /* history data at integration point              */
    double         		hk;
-   double         		vel_norm, re1, re2, xi1, xi2;
    double         		val, strle;
    vector<double>         	velino(3); /* normed velocity at element centre */
    double         		det, vol;
@@ -306,66 +305,135 @@ for (int i=0;i<3;i++)
    }
 } //end loop over i
 
-/*------------------------------ get Jacobian matrix and determinant ---*/
-f3_jaco(xyze,deriv,xjm,&det,iel);
-vol=facr*facs*fact*det;
-
-/* get element length for tau_Mp/tau_C: volume-equival. diameter/sqrt(3)*/
-hk = pow((SIX*vol/PI),(ONE/THREE))/sqrt(THREE);
-
-/*------------------------------------------------- get streamlength ---*/
-f3_gder(derxy,deriv,xjm,det,iel);
-val = ZERO;
-
-/* get velocity norm */
-vel_norm=sqrt( velint[0]*velint[0]
-           + velint[1]*velint[1]
-           + velint[2]*velint[2]);
-if(vel_norm>=EPS6)
+#if 0
 {
-   velino[0] = velint[0]/vel_norm;
-   velino[1] = velint[1]/vel_norm;
-   velino[2] = velint[2]/vel_norm;
+  // stabilisation parameter a la Foerster
+  double norm_p, pe, re, xi1, xi2;
+  
+  /*------------------------------ get Jacobian matrix and determinant ---*/
+  f3_jaco(xyze,deriv,xjm,&det,iel);
+  vol=facr*facs*fact*det;
+
+  /*----------------------------------------------- get element length ---*/
+  hk = pow((SIX*vol/PI),(ONE/THREE))/sqrt(THREE);
+
+  /*------------------------------------------------- get streamlength ---*/
+  f3_gder(derxy,deriv,xjm,det,iel);
+  val = ZERO;
+  /* get p-norm */
+  norm_p=sqrt( velint[0]*velint[0]
+               + velint[1]*velint[1]
+               + velint[2]*velint[2]);
+  if(norm_p>=EPS6)
+  {
+    velino[0] = velint[0]/norm_p;
+    velino[1] = velint[1]/norm_p;
+    velino[2] = velint[2]/norm_p;
+  }
+  else
+  {
+    velino[0] = ONE;
+    velino[1] = ZERO;
+    velino[2] = ZERO;
+  }
+  for (int i=0;i<iel;i++) /* loop element nodes */
+  {
+    val += FABS(velino[0]*derxy(0,i) \
+                +velino[1]*derxy(1,i) \
+                +velino[2]*derxy(2,i));
+  } /* end of loop over elements */
+  strle=TWO/val;
+
+  /*--------------------------------------------------------- get tau_Mu ---*/
+  pe = 4.0 * timefac * visc / (mk * DSQR(strle)); /* viscous : reactive forces */
+  re = mk * norm_p * strle / (2.0 * visc);       /* advective : viscous forces */
+
+  xi1 = DMAX(pe,1.0);
+  /*xi1 = pe; */
+  xi2 = DMAX(re,1.0);
+
+  tau[0] = DSQR(strle) / (DSQR(strle)*xi1+(4.0*timefac*visc/mk)*xi2);
+
+/*--------------------------------------------------------- get tau_Mp ---*/
+  pe = 4.0 * timefac * visc / (mk * DSQR(hk)); /* viscous : reactive forces */
+  re = mk * norm_p * hk / (2.0 * visc);       /* advective : viscous forces */
+
+  xi1 = DMAX(pe,1.0);
+  /*xi1 = pe; */
+  xi2 = DMAX(re,1.0);
+
+  tau[1] = DSQR(hk) / (DSQR(hk) * xi1 + (4.0*timefac * visc/mk) * xi2);
+
+  /*---------------------------------------------------------- get tau_C ---*/
+  tau[2] = norm_p * hk * 0.5 * xi2;
 }
-else
+#endif
+#if 1
 {
-   velino[0] = ONE;
-   velino[1] = ZERO;
-   velino[2] = ZERO;
+  // stabilisation parameter a la Gravemeier
+  double vel_norm, re1, re2, xi1, xi2;
+
+  /*------------------------------ get Jacobian matrix and determinant ---*/
+  f3_jaco(xyze,deriv,xjm,&det,iel);
+  vol=facr*facs*fact*det;
+
+  /* get element length for tau_Mp/tau_C: volume-equival. diameter/sqrt(3)*/
+  hk = pow((SIX*vol/PI),(ONE/THREE))/sqrt(THREE);
+
+  /*------------------------------------------------- get streamlength ---*/
+  f3_gder(derxy,deriv,xjm,det,iel);
+  val = ZERO;
+
+  
+  /* get velocity norm */
+  vel_norm=sqrt( velint[0]*velint[0]
+                 + velint[1]*velint[1]
+                 + velint[2]*velint[2]);
+  if(vel_norm>=EPS6)
+  {
+    velino[0] = velint[0]/vel_norm;
+    velino[1] = velint[1]/vel_norm;
+    velino[2] = velint[2]/vel_norm;
+  }
+  else
+  {
+    velino[0] = ONE;
+    velino[1] = ZERO;
+    velino[2] = ZERO;
+  }
+  for (int i=0;i<iel;i++) /* loop element nodes */
+  {
+    val += FABS(velino[0]*derxy(0,i) \
+                +velino[1]*derxy(1,i) \
+                +velino[2]*derxy(2,i));
+  } /* end of loop over elements */
+  strle=TWO/val;
+
+  /*----------------------------------------------------- compute tau_Mu ---*/
+  /* stability parameter definition according to Franca and Valentin (2000) */
+  re1 = 2.0 * timefac * visc / (mk * DSQR(strle)); /* viscous : reactive forces */
+  re2 = mk * vel_norm * strle / visc;            /* convective : viscous forces */
+
+  xi1 = DMAX(re1,1.0);
+  xi2 = DMAX(re2,1.0);
+
+  tau[0] = DSQR(strle) / (DSQR(strle)*xi1+(2.0*timefac*visc/mk)*xi2);
+
+  /*------------------------------------------------------compute tau_Mp ---*/
+  /* stability parameter definition according to Franca and Valentin (2000) */
+  re1 = 2.0 * timefac * visc / (mk * DSQR(hk)); /* viscous : reactive forces */
+  re2 = mk * vel_norm * hk / visc;       /* convective : viscous forces */
+
+  xi1 = DMAX(re1,1.0);
+  xi2 = DMAX(re2,1.0);
+
+  tau[1] = DSQR(hk) / (DSQR(hk) * xi1 + (2.0*timefac * visc/mk) * xi2);
+
+  /*------------------------------------------------------ compute tau_C ---*/
+  /*-- stability parameter definition according to Codina (2002), CMAME 191 */
+  tau[2] = sqrt(DSQR(visc)+DSQR(0.5*vel_norm*hk));
 }
-for (int i=0;i<iel;i++) /* loop element nodes */
-{
-   val += FABS(velino[0]*derxy(0,i) \
-              +velino[1]*derxy(1,i) \
-              +velino[2]*derxy(2,i));
-} /* end of loop over elements */
-strle=TWO/val;
-
-/*----------------------------------------------------- compute tau_Mu ---*/
-/* stability parameter definition according to Franca and Valentin (2000) */
-re1 = 2.0 * timefac * visc / (mk * DSQR(strle)); /* viscous : reactive forces */
-re2 = mk * vel_norm * strle / visc;            /* convective : viscous forces */
-
-xi1 = DMAX(re1,1.0);
-xi2 = DMAX(re2,1.0);
-
-tau[0] = DSQR(strle) / (DSQR(strle)*xi1+(2.0*timefac*visc/mk)*xi2);
-
-/*------------------------------------------------------compute tau_Mp ---*/
-/* stability parameter definition according to Franca and Valentin (2000) */
-re1 = 2.0 * timefac * visc / (mk * DSQR(hk)); /* viscous : reactive forces */
-re2 = mk * vel_norm * hk / visc;       /* convective : viscous forces */
-
-xi1 = DMAX(re1,1.0);
-xi2 = DMAX(re2,1.0);
-
-tau[1] = DSQR(hk) / (DSQR(hk) * xi1 + (2.0*timefac * visc/mk) * xi2);
-
-/*------------------------------------------------------ compute tau_C ---*/
-/*-- stability parameter definition according to Codina (2002), CMAME 191 */
-tau[2] = sqrt(DSQR(visc)+DSQR(0.5*vel_norm*hk));
-
-
+#endif
 /*----------------------------------------------------------------------*/
 // end of old f3_caltau function
 /*----------------------------------------------------------------------*/
