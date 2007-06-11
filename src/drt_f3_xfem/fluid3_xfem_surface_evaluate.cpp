3d696ee078d39cb3be95f3e216ed8a4e3b0094be
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2007-06-11 08:04:53 +0000

    added headerfile drt_timecurve.h
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@3264 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_f3_xfem/fluid3_xfem_surface_evaluate.cpp
+++ src/drt_f3_xfem/fluid3_xfem_surface_evaluate.cpp
@@ -22,6 +22,7 @@ Maintainer: Axel Gerstenberger
 #include "../drt_lib/drt_utils.H"
 #include "../drt_lib/drt_discret.H"
 #include "../drt_lib/drt_dserror.H"
+#include "../drt_lib/drt_timecurve.H"
 
 extern "C"
 {
