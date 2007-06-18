5c5cf4f08e53c82b2e4ecbb767d37cff88522b38
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2007-06-18 08:51:09 +0000

    node numbering scheme from guide implemented in FLUID3 XFEM element
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@3362 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_f3_xfem/fluid3_xfem_input.cpp
+++ src/drt_f3_xfem/fluid3_xfem_input.cpp
@@ -57,51 +57,9 @@ bool DRT::Elements::XFluid3::ReadElement()
     distype2NumNodes[tet4]  = 4;
     distype2NumNodes[tet10] = 10;
     
-//    typedef vector<int> Volume2Surface(9);
-//    typedef vector<Volume2Surface> Volume2Surfaces(6);
-//    Volume2Surfaces vol2surf;
-//    vol2surf[0][0] = 0;
-//    vol2surf[0][1] = 3;
-//    vol2surf[0][2] = 2;
-//    vol2surf[0][3] = 1;
-//    vol2surf[0][4] = 11;
-//    vol2surf[0][5] = 10;
-//    vol2surf[0][6] = 9;
-//    vol2surf[0][7] = 8;
-//    vol2surf[0][8] = 20;
-    
-    vector<int> gid2baciNodeNumbering(27);
-    gid2baciNodeNumbering[0] =   1;
-    gid2baciNodeNumbering[1] =   2;
-    gid2baciNodeNumbering[2] =   3;
-    gid2baciNodeNumbering[3] =   0;
-    gid2baciNodeNumbering[4] =   5;
-    gid2baciNodeNumbering[5] =   6;
-    gid2baciNodeNumbering[6] =   7;
-    gid2baciNodeNumbering[7] =   4;
-    gid2baciNodeNumbering[8] =   9;
-    gid2baciNodeNumbering[9] =  10;
-    gid2baciNodeNumbering[10] = 11;
-    gid2baciNodeNumbering[11] =  8;
-    gid2baciNodeNumbering[12] = 17;
-    gid2baciNodeNumbering[13] = 18;
-    gid2baciNodeNumbering[14] = 19;
-    gid2baciNodeNumbering[15] = 16;
-    gid2baciNodeNumbering[16] = 13;
-    gid2baciNodeNumbering[17] = 14;
-    gid2baciNodeNumbering[18] = 15;
-    gid2baciNodeNumbering[19] = 12;       
-    gid2baciNodeNumbering[20] = 20;
-    gid2baciNodeNumbering[21] = 25;
-    gid2baciNodeNumbering[22] = 23;
-    gid2baciNodeNumbering[23] = 24;
-    gid2baciNodeNumbering[24] = 22;
-    gid2baciNodeNumbering[25] = 21;
-    gid2baciNodeNumbering[26] = 26;
-    
     int   ierr = 0;
     int   nnode = 0;
-    int   nodes_read[27];
+    int   nodes[27];
     
     Gid2DisType::iterator iter;
     for( iter = gid2distype.begin(); iter != gid2distype.end(); iter++ ) 
@@ -112,19 +70,14 @@ bool DRT::Elements::XFluid3::ReadElement()
         {
             DiscretizationType distype = gid2distype[eletext];
             nnode = distype2NumNodes[distype];
-            frint_n(eletext.c_str(), nodes_read, nnode, &ierr);
+            frint_n(eletext.c_str(), nodes, nnode, &ierr);
             dsassert(ierr==1, "Reading of ELEMENT Topology failed\n");
             break;
         }
     }
   
     // reduce node numbers by one
-    for (int i=0; i<nnode; ++i) nodes_read[i]--;
-    
-    // map gid numbering to baci numbering
-    int   nodes[27];
-    //for (int i=0; i<nnode; ++i) nodes[i] = nodes_read[gid2baciNodeNumbering[i]];
-    for (int i=0; i<nnode; ++i) nodes[i] = nodes_read[i];
+    for (int i=0; i<nnode; ++i) nodes[i]--;
     
     SetNodeIds(nnode,nodes);
 
--- src/drt_f3_xfem/fluid3_xfem_nodemap.cpp
+++ src/drt_f3_xfem/fluid3_xfem_nodemap.cpp
@@ -19,95 +19,44 @@ Maintainer: Axel Gerstenberger
 #include "../drt_lib/drt_utils.H"
 
 
-//Doku: reference on guide or pictures in here
-
-// update!!
+//!!! Doku: reference on guide, chapter conventions !!!
 const int DRT::Elements::XFluid3::hex27_surfaces_[6][9] = {{0,  3,  2,  1, 11, 10,  9,  8, 20},
-                                                           {4,  5,  6,  7, 12, 13, 14, 15, 21},
-                                                           {0,  1,  5,  4,  8, 17, 12, 16, 22},
-                                                           {2,  3,  7,  6, 10, 19, 14, 18, 23},
-                                                           {0,  4,  7,  3, 16, 15, 19, 11, 24},
-                                                           {1,  2,  6,  5,  9, 18, 13, 17, 25}};
+                                                           {0,  1,  5,  4,  8, 13, 16, 12, 21},
+                                                           {1,  2,  6,  5,  9, 14, 17, 13, 22},
+                                                           {2,  3,  7,  6, 10, 15, 18, 14, 23},
+                                                           {0,  4,  7,  3, 12, 19, 15, 11, 24},
+                                                           {4,  5,  6,  7, 16, 17, 18, 19, 25}};
 
-// update!!
 const int DRT::Elements::XFluid3::hex27_lines_[12][3] = {{0,  1,  8},
                                                          {1,  2,  9},
                                                          {2,  3, 10},
-                                                         {3,  0, 11},
-                                                         {0,  4, 16},
-                                                         {1,  5, 17},
-                                                         {2,  6, 18},
-                                                         {3,  7, 19},
-                                                         {4,  5, 12},
-                                                         {5,  6, 13},
-                                                         {6,  7, 14},
-                                                         {7,  4, 15}};
+                                                         {0,  3, 11},
+                                                         {0,  4, 12},
+                                                         {1,  5, 13},
+                                                         {2,  6, 14},
+                                                         {3,  7, 15},
+                                                         {4,  5, 16},
+                                                         {5,  6, 17},
+                                                         {6,  7, 18},
+                                                         {4,  7, 19}};
 
-// update!!
-const int DRT::Elements::XFluid3::tet10_surfaces_[4][6] = {{0,  1,  8,  0,  1,  8},
-                                                           {1,  2,  9,  0,  1,  8},
-                                                           {2,  3, 10,  0,  1,  8},
-                                                           {3,  0, 11,  0,  1,  8}};
+const int DRT::Elements::XFluid3::tet10_surfaces_[4][6] = {{0,  1,  3,  4,  8,  7},
+                                                           {1,  2,  3,  5,  9,  8},
+                                                           {0,  3,  2,  7,  9,  6},
+                                                           {0,  2,  1,  6,  5,  4}};
 
-// update!!
-const int DRT::Elements::XFluid3::tet10_lines_[6][3] = {{0,  1,  8},
-                                                        {1,  2,  9},
-                                                        {2,  3, 10},
-                                                        {3,  0, 11},
-                                                        {0,  4, 16},
-                                                        {1,  5, 17}};
+const int DRT::Elements::XFluid3::tet10_lines_[6][3] = {{0,  1,  4},
+                                                        {1,  2,  5},
+                                                        {0,  2,  6},
+                                                        {0,  3,  7},
+                                                        {1,  3,  8},
+                                                        {2,  3,  9}};
 
-
-
-/*
- *
- *                  
- *                  edge2     
- *             (line2)
- *           
- *              3     6     2
- *               X----o----X
- *               |         |
- *               |         |   
- *   edge3      7o    O    o5      edge1      
- *  (line3)      |    8    |      (line1)
- *               |         |
- *               X----o----X
- *              0     4     1
- *
- *                  edge0
- *                 (line0)
- *
- *
- *      X nodes for quad
- *      o addotional nodes for quad8
- *      O addotional nodes for full quadratic quad9
- */
 const int DRT::Elements::XFluid3Surface::quad9_lines_[4][3] = {{0,  1,  4},
                                                                {1,  2,  5},
                                                                {2,  3,  6},
-                                                               {3,  0,  7}};
+                                                               {0,  3,  7}};
 
-
-/*
- *
- *               2
- *                X
- *                |\
- *                | \       edge1
- *   edge2       6o  o5    (line1)
- *  (line2)       |   \
- *                |    \
- *                X--o--X
- *               0   4   1
- *
- *             edge0
- *            (line0)
- *
- *
- *      X nodes for tri3
- *      o addotional nodes for tri6
- */
 const int DRT::Elements::XFluid3Surface::tri6_lines_[3][3] = {{0,  1,  3},
                                                               {1,  2,  4},
                                                               {2,  0,  5}};
