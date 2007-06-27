fca51f3d4852d22ff48dfaffc8159bd0c03b29fe
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2007-06-27 11:24:59 +0000

    using node numbering scheme from DRT::Utils
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@3468 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_f2/fluid2.cpp
+++ src/drt_f2/fluid2.cpp
@@ -169,179 +169,75 @@ RefCountPtr<DRT::ElementRegister> DRT::Elements::Fluid2::ElementRegister() const
   return rcp(new DRT::Elements::Fluid2Register(Type()));
 }
 
-/*----------------------------------------------------------------------*
- |  get vector of lines (public)                             gammi 04/07|
- *----------------------------------------------------------------------*/
+//
+// get vector of lines
+//
 DRT::Element** DRT::Elements::Fluid2::Lines()
 {
+    const DiscretizationType distype = Shape();
+    const int nline   = NumLine();
+    lines_.resize(nline);
+    lineptrs_.resize(nline);
 
-  const int nline   = NumLine();
-  const int numnode = NumNode();
-  lines_.resize(nline);
-  lineptrs_.resize(nline);
-  int nodeids[100];
-  DRT::Node* nodes[100];
-
-  /* triangle elements */
-  if (nline==3)
-  {
-    /* mind: The displayed element is not the reference element!
-     *
-     *               2
-     *                X
-     *                |\
-     *                | \       edge1
-     *   edge2       6o  o5    (line1)
-     *  (line2)       |   \
-     *                |    \
-     *                X--o--X
-     *               0   4   1
-     *
-     *             edge0
-     *            (line0)
-     *
-     *
-     *      X nodes for tri3
-     *      o addotional nodes for tri6
-     *                                                                       */
-    /* linear triangles*/
-    if (numnode==3)
+    switch (distype)
     {
-      /* first edge */
-      // set node id's
-      nodeids[0] = NodeIds()[0];
-      nodeids[1] = NodeIds()[1];
-      // get nodes
-      nodes[0] = Nodes()[0];
-      nodes[1] = Nodes()[1];
-      // create the line and get the line pointer
-      lines_[0] =
-        rcp(new DRT::Elements::Fluid2Line(0,Owner(),2,nodeids,nodes,this,0));
-      lineptrs_[0] = lines_[0].get();
-
-      /* second edge */
-      // set node id's
-      nodeids[0] = NodeIds()[1];
-      nodeids[1] = NodeIds()[2];
-      // get nodes
-      nodes[0] = Nodes()[1];
-      nodes[1] = Nodes()[2];
-      // create the line and get the line pointer
-      lines_[1] =
-        rcp(new DRT::Elements::Fluid2Line(1,Owner(),2,nodeids,nodes,this,1));
-      lineptrs_[1] = lines_[1].get();
-
-      /* third edge */
-      // set node id's
-      nodeids[0] = NodeIds()[2];
-      nodeids[1] = NodeIds()[0];
-      // get nodes
-      nodes[0] = Nodes()[2];
-      nodes[1] = Nodes()[0];
-      // create the line and get the line pointer
-      lines_[2] =
-        rcp(new DRT::Elements::Fluid2Line(2,Owner(),2,nodeids,nodes,this,2));
-      lineptrs_[2] = lines_[2].get();
+    case tri3:
+        CreateLinesTri(nline, 2);
+        break;
+    case tri6:
+        CreateLinesTri(nline, 3);
+        break;
+    case quad4:
+        CreateLinesQuad(nline, 2);
+        break;
+    case quad8:
+        CreateLinesQuad(nline, 3);
+        break;
+    case quad9:
+        CreateLinesQuad(nline, 3);
+        break;
+    default:
+        dserror("distype not supported");
     }
-    /* quadratic triangles*/
-    else if (numnode==6)
-    {
-      dserror("TRI6 lines not implemented.");
-    }
-  }
-  /* quad elements*/
-  else if (nline==4)
-  {
-    /* mind: The displayed element is not the reference element!
-     *
-     *                  
-     *                  edge2	  
-     *		       (line2)
-     *           
-     *              3     6     2
-     *               X----o----X
-     *               |         |
-     *               |         |   
-     *   edge3      7o    O    o5      edge1	  
-     *  (line3)      |    8    |      (line1)
-     *               |         |
-     *               X----o----X
-     *              0     4     1
-     *
-     *                  edge0
-     *                 (line0)
-     *
-     *
-     *      X nodes for quad
-     *      o addotional nodes for quad8
-     *      O addotional nodes for full quadratic quad9
-     *      
-     *                                                                 */
-    if (numnode==4)
-    {
-      /* first edge */
-      // set node id's
-      nodeids[0] = NodeIds()[0];
-      nodeids[1] = NodeIds()[1];
-      // get nodes
-      nodes[0] = Nodes()[0];
-      nodes[1] = Nodes()[1];
-      // create the line and get the line pointer
-      lines_[0] =
-        rcp(new DRT::Elements::Fluid2Line(0,Owner(),2,nodeids,nodes,this,0));
-      lineptrs_[0] = lines_[0].get();
 
-      /* second edge */
-      // set node id's
-      nodeids[0] = NodeIds()[1];
-      nodeids[1] = NodeIds()[2];
-      // get nodes
-      nodes[0] = Nodes()[1];
-      nodes[1] = Nodes()[2];
-      // create the line and get the line pointer
-      lines_[1] =
-        rcp(new DRT::Elements::Fluid2Line(1,Owner(),2,nodeids,nodes,this,1));
-      lineptrs_[1] = lines_[1].get();
-
-      /* third edge */
-      // set node id's
-      nodeids[0] = NodeIds()[2];
-      nodeids[1] = NodeIds()[3];
-      // get nodes
-      nodes[0] = Nodes()[2];
-      nodes[1] = Nodes()[3];
-      // create the line and get the line pointer
-      lines_[2] =
-        rcp(new DRT::Elements::Fluid2Line(2,Owner(),2,nodeids,nodes,this,2));
-      lineptrs_[2] = lines_[2].get();
-
-      /* fourth edge */
-      // set node id's
-      nodeids[0] = NodeIds()[3];
-      nodeids[1] = NodeIds()[0];
-      // get nodes
-      nodes[0] = Nodes()[3];
-      nodes[1] = Nodes()[0];
-      // create the line and get the line pointer
-      lines_[3] =
-        rcp(new DRT::Elements::Fluid2Line(3,Owner(),2,nodeids,nodes,this,3));
-      lineptrs_[3] = lines_[3].get();
-
-    }
-    else if (numnode==8)
-    {
-      dserror("quad8 lines not implemented.");
-    }
-    else if (numnode==9)
-    {
-      dserror("quad9 lines not implemented.");
-    }
-    else dserror("Number of nodes not supported");
-  }
-  else dserror("Number of lines not supported");
-
-  return (DRT::Element**)(&(lineptrs_[0]));
+    return (DRT::Element**)(&(lineptrs_[0]));
 }
+
+void DRT::Elements::Fluid2::CreateLinesTri(const int& nline,
+                                           const int& nnode)
+{
+    for(int iline=0;iline<nline;iline++)
+    {
+        int nodeids[nnode];
+        DRT::Node* nodes[nnode];
+        
+        for (int inode=0;inode<nnode;inode++)
+        {
+             nodeids[inode] = NodeIds()[eleNodeNumbering_tri6_lines[iline][inode]];
+             nodes[inode]   = Nodes()[  eleNodeNumbering_tri6_lines[iline][inode]];
+        }
+        lines_[iline] = rcp(new DRT::Elements::Fluid2Line(iline,Owner(),nnode,nodeids,nodes,this,iline));
+        lineptrs_[iline] = lines_[iline].get();
+    }
+}        
+
+void DRT::Elements::Fluid2::CreateLinesQuad(const int& nline,
+                                            const int& nnode)
+{
+    for(int iline=0;iline<nline;iline++)
+    {
+        int nodeids[nnode];
+        DRT::Node* nodes[nnode];
+        
+        for (int inode=0;inode<nnode;inode++)
+        {
+             nodeids[inode] = NodeIds()[eleNodeNumbering_quad9_lines[iline][inode]];
+             nodes[inode]   = Nodes()[  eleNodeNumbering_quad9_lines[iline][inode]];
+        }
+        lines_[iline] = rcp(new DRT::Elements::Fluid2Line(iline,Owner(),nnode,nodeids,nodes,this,iline));
+        lineptrs_[iline] = lines_[iline].get();
+    }
+}  
 
 
 /*----------------------------------------------------------------------*
