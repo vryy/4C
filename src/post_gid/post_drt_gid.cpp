678117e990c9ce3755ba4dce9a4d5418fc43bff2
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2007-06-29 08:47:30 +0000

    added support for tet4 GiD output
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@3486 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/post_gid/post_drt_gid.cpp
+++ src/post_gid/post_drt_gid.cpp
@@ -149,6 +149,37 @@ void write_mesh(PostProblem* problem, int disnum)
     GiD_EndElements();
     GiD_EndMesh();
     break;
+  case DRT::Element::tet4:
+    GiD_BeginGaussPoint("tet4", GiD_Tetrahedra, "tet4", 4, 0, 1);
+    GiD_EndGaussPoint();
+
+    GiD_BeginMesh("tet4",GiD_3D,GiD_Tetrahedra,4);
+    // We have ony one mesh, so it's the first
+    GiD_BeginCoordinates();
+    for (int i = 0; i < field->discretization()->NumGlobalNodes(); ++i)
+    {
+      for (int j = 0; j < field->problem()->num_dim(); ++j)
+      {
+        x[j] = field->discretization()->gNode(i)->X()[j];
+      }
+      int id = field->discretization()->gNode(i)->Id();
+      GiD_WriteCoordinates(id+1, x[0], x[1], x[2]);
+    }
+    GiD_EndCoordinates();
+
+    GiD_BeginElements();
+    for (int i=0; i<field->discretization()->NumGlobalElements(); ++i)
+    {
+      int mesh_entry[MAXNOD];
+      for (int j = 0; j < field->discretization()->gElement(i)->NumNode(); ++j)
+      {
+        mesh_entry[j] = field->discretization()->gElement(i)->NodeIds()[j]+1;
+      }
+      GiD_WriteElement(field->discretization()->gElement(i)->Id()+1,mesh_entry);
+    }
+    GiD_EndElements();
+    GiD_EndMesh();
+    break;
   case DRT::Element::quad4:
     // Let's assume there are only shell8_4_22 elements
     GiD_BeginGaussPoint("shell8_4_22", GiD_Quadrilateral, "shell8_4_22", 4, 0, 1);
