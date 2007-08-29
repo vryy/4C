e4985dd521d15ada1f832fcb37d11242e9388ac1
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2007-08-29 11:24:51 +0000

    definition of 2 new types for dof management
    cleanup of Enrichment class
    
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@3716 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_xfem/xfem_enrichment.cpp
+++ src/drt_xfem/xfem_enrichment.cpp
@@ -15,64 +15,16 @@ Maintainer: Axel Gerstenberger
 #ifdef TRILINOS_PACKAGE
 
 #include "xfem_enrichment.H"
-#include "../drt_lib/drt_dserror.H"
 
 using namespace Enrichments;
 
 /*----------------------------------------------------------------------*
  |  ctor (public)                                            mwgee 11/06|
- |  id             (in)  this element's global id                       |
  *----------------------------------------------------------------------*/
-ElementEnrichment::ElementEnrichment(const int enr_id, const EnrichmentType enr_type) :
-enr_id_(enr_id),
-enr_type_(enr_type)
-{
-    return;
-}
-
-/*----------------------------------------------------------------------*
- |  copy-ctor (public)                                       mwgee 11/06|
- |  id             (in)  this element's global id                       |
- *----------------------------------------------------------------------*/
-ElementEnrichment::ElementEnrichment(const ElementEnrichment& other) :
-enr_id_(other.enr_id_),
-enr_type_(other.enr_type_)
-{
-    assert(&other != this);
-    return;
-}
-
-
-/*----------------------------------------------------------------------*
- |  dtor (public)                                            mwgee 11/06|
- *----------------------------------------------------------------------*/
-ElementEnrichment::~ElementEnrichment()
-{
-    return;
-}
-
-
-/*----------------------------------------------------------------------*
- |  print this element (public)                              mwgee 11/06|
- *----------------------------------------------------------------------*/
-void ElementEnrichment::Print(ostream& os) const
-{
-    os << "ElementEnrichment ";
-    cout << "enr_id: " << enr_id_ << "enr_type: " << enr_type_ << endl;
-    return;
-}
-
-
-
-
-
-
-/*----------------------------------------------------------------------*
- |  ctor (public)                                            mwgee 11/06|
- *----------------------------------------------------------------------*/
-NodeEnrichment::NodeEnrichment(const int enr_id, const EnrichmentType enr_type) :
-enr_id_(enr_id),
-enr_type_(enr_type)
+Enrichment::Enrichment(
+        const EnrId enr_id,
+        const EnrType enr_type) :
+    enr_id_(enr_id), enr_type_(enr_type)
 {
     return;
 }
@@ -80,31 +32,30 @@ enr_type_(enr_type)
 /*----------------------------------------------------------------------*
  |  copy-ctor (public)                                       mwgee 11/06|
  *----------------------------------------------------------------------*/
-NodeEnrichment::NodeEnrichment(const NodeEnrichment& other) :
-enr_id_(other.enr_id_),
-enr_type_(other.enr_type_)
+Enrichment::Enrichment(
+        const Enrichment& other) :
+    enr_id_(other.enr_id_), enr_type_(other.enr_type_)
 {
     assert(&other != this);
     return;
 }
 
-
 /*----------------------------------------------------------------------*
  |  dtor (public)                                            mwgee 11/06|
  *----------------------------------------------------------------------*/
-NodeEnrichment::~NodeEnrichment()
+Enrichment::~Enrichment()
 {
     return;
 }
 
-
 /*----------------------------------------------------------------------*
  |  print this element (public)                              mwgee 11/06|
  *----------------------------------------------------------------------*/
-void NodeEnrichment::Print(ostream& os) const
+void Enrichment::Print(
+        ostream& os) const
 {
-    os << "NodeEnrichment ";
-    cout << "enr_id: " << enr_id_ << "enr_type: " << enr_type_ << endl;
+    os << "Enrichment ";
+    cout << "enr_id: "<< enr_id_ << "enr_type: "<< enr_type_ << endl;
     return;
 }
 
