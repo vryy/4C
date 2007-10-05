83e8ff3a3de83d145eff20084559cc095a6011d5
Michael Gee <gee@lnm.mw.tum.de>
2007-10-05 17:05:53 +0000

    added input of nonlinear solver choice for structural dynamics
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@3957 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/headers/dynamic.h
+++ src/headers/dynamic.h
@@ -35,10 +35,18 @@ typedef struct _STRUCT_DYNAMIC
 {
 enum
    {
-    gen_alfa,                   /* generalised alpha-time integrator */
-    centr_diff,                 /* central differences */
+    gen_alfa,
+    centr_diff,                  /* generalised alpha-time integrator */
     Gen_EMM                     /* generalised energy-momentum method */
    }               Typ;         /* type of time integration algorithm */
+enum
+   {
+    fullnewton,
+    modnewton,
+    matfreenewton,
+    nlncg,
+    ptc
+   }               nlnSolvTyp;  /* type of nonlinear solver to be used */
 INT                updevry_disp;/* write result very updevry step */
 INT                updevry_stress;/* write result very updevry step */
 INT                res_write_evry;/* write restart every res_write_evry step */
