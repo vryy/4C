042e3920763c7268f4fd07b2456f66af3b453f08
Moritz Frenzel <frenzel@lnm.mw.tum.de>
2007-10-23 17:06:27 +0000

    beginning of work on wedge-shaped solid shell inherited from solid wedge6.
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@4051 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/drt_so3/so_shw6.cpp
@@ -0,0 +1,222 @@
+/*!----------------------------------------------------------------------
+\file so_shw6.cpp
+\brief
+
+<pre>
+Maintainer: Moritz Frenzel
+            frenzel@lnm.mw.tum.de
+            http://www.lnm.mw.tum.de
+            089 - 289-15240
+</pre>
+
+*----------------------------------------------------------------------*/
+#ifdef D_SOH8
+#ifdef CCADISCRET
+#ifdef TRILINOS_PACKAGE
+
+#include "so_shw6.H"
+#include "so_weg6.H"
+
+using namespace DRT::Utils;
+
+
+/*----------------------------------------------------------------------*
+ |  ctor (public)                                              maf 04/07|
+ |  id             (in)  this element's global id                       |
+ *----------------------------------------------------------------------*/
+DRT::Elements::So_shw6::So_shw6(int id, int owner) :
+DRT::Elements::So_weg6(id,owner)
+{
+  SetType(element_soshw6);
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ |  copy-ctor (public)                                         maf 04/07|
+ |  id             (in)  this element's global id                       |
+ *----------------------------------------------------------------------*/
+DRT::Elements::So_shw6::So_shw6(const DRT::Elements::So_shw6& old) :
+DRT::Elements::So_weg6(old)
+{
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ |  Deep copy this instance of Solid3 and return pointer to it (public) |
+ |                                                            maf 04/07 |
+ *----------------------------------------------------------------------*/
+DRT::Element* DRT::Elements::So_shw6::Clone() const
+{
+  DRT::Elements::So_shw6* newelement = new DRT::Elements::So_shw6(*this);
+  return newelement;
+}
+
+
+/*----------------------------------------------------------------------*
+ |  Pack data                                                  (public) |
+ |                                                            maf 04/07 |
+ *----------------------------------------------------------------------*/
+void DRT::Elements::So_shw6::Pack(vector<char>& data) const
+{
+  data.resize(0);
+
+  // pack type of this instance of ParObject
+  int type = UniqueParObjectId();
+  AddtoPack(data,type);
+  // add base class So_weg6 Element
+  vector<char> basedata(0);
+  DRT::Elements::So_weg6::Pack(basedata)
+
+  return;
+}
+
+
+/*----------------------------------------------------------------------*
+ |  Unpack data                                                (public) |
+ |                                                            maf 04/07 |
+ *----------------------------------------------------------------------*/
+void DRT::Elements::So_shw6::Unpack(const vector<char>& data)
+{
+  int position = 0;
+  // extract type
+  int type = 0;
+  ExtractfromPack(position,data,type);
+  if (type != UniqueParObjectId()) dserror("wrong instance type data");
+  // extract base class So_weg6 Element
+  vector<char> basedata(0);
+  ExtractfromPack(position,data,basedata);
+  DRT::Elements::So_weg6::Unpack(basedata);
+
+  if (position != (int)data.size())
+    dserror("Mismatch in size of data %d <-> %d",(int)data.size(),position);
+  return;
+}
+
+
+/*----------------------------------------------------------------------*
+ |  dtor (public)                                              maf 04/07|
+ *----------------------------------------------------------------------*/
+DRT::Elements::So_shw6::~So_shw6()
+{
+  return;
+}
+
+
+/*----------------------------------------------------------------------*
+ |  print this element (public)                                maf 04/07|
+ *----------------------------------------------------------------------*/
+void DRT::Elements::So_shw6::Print(ostream& os) const
+{
+  os << "So_shw6 ";
+  Element::Print(os);
+  cout << endl;
+  cout << data_;
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ |  allocate and return So_weg6Register (public)                maf 04/07|
+ *----------------------------------------------------------------------*/
+RefCountPtr<DRT::ElementRegister> DRT::Elements::So_shw6::ElementRegister() const
+{
+  return rcp(new DRT::Elements::Soshw6Register(Type()));
+}
+
+
+//=======================================================================
+//=======================================================================
+//=======================================================================
+//=======================================================================
+
+/*----------------------------------------------------------------------*
+ |  ctor (public)                                              maf 04/07|
+ *----------------------------------------------------------------------*/
+DRT::Elements::Soshw6Register::Soshw6Register(DRT::Element::ElementType etype) :
+DRT::Elements::Sow6Register::Sow6Register(etype)
+{
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ |  copy-ctor (public)                                         maf 04/07|
+ *----------------------------------------------------------------------*/
+DRT::Elements::Soshw6Register::Soshw6Register(
+                               const DRT::Elements::Soshw6Register& old) :
+DRT::Elements::Sow6Register::Sow6Register(old)
+{
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ |  Deep copy this instance return pointer to it               (public) |
+ |                                                            maf 04/07 |
+ *----------------------------------------------------------------------*/
+DRT::Elements::Sow6Register* DRT::Elements::Soshw6Register::Clone() const
+{
+  return new DRT::Elements::Soshw6Register(*this);
+}
+
+/*----------------------------------------------------------------------*
+ |  Pack data                                                  (public) |
+ |                                                            maf 04/07 |
+ *----------------------------------------------------------------------*/
+void DRT::Elements::Soshw6Register::Pack(vector<char>& data) const
+{
+  data.resize(0);
+
+  // pack type of this instance of ParObject
+  int type = UniqueParObjectId();
+  AddtoPack(data,type);
+  // add base class Sow6Register
+  vector<char> basedata(0);
+  DRT::Elements::Sow6Register::Pack(basedata);
+  AddtoPack(data,basedata);
+
+  return;
+}
+
+
+/*----------------------------------------------------------------------*
+ |  Unpack data                                                (public) |
+ |                                                            maf 04/07 |
+ *----------------------------------------------------------------------*/
+void DRT::Elements::Sow6Register::Unpack(const vector<char>& data)
+{
+  int position = 0;
+  // extract type
+  int type = 0;
+  ExtractfromPack(position,data,type);
+  if (type != UniqueParObjectId()) dserror("wrong instance type data");
+  // base class Sow6Register
+  vector<char> basedata(0);
+  ExtractfromPack(position,data,basedata);
+  DRT::Elements::Sow6Register::Unpack(basedata);
+
+
+  if (position != (int)data.size())
+    dserror("Mismatch in size of data %d <-> %d",(int)data.size(),position);
+  return;
+}
+
+
+/*----------------------------------------------------------------------*
+ |  dtor (public)                                              maf 04/07|
+ *----------------------------------------------------------------------*/
+DRT::Elements::Soshw6Register::~Soshw6Register()
+{
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ |  print (public)                                             maf 04/07|
+ *----------------------------------------------------------------------*/
+void DRT::Elements::Soshw6Register::Print(ostream& os) const
+{
+  os << "Soshw6Register ";
+  ElementRegister::Print(os);
+  return;
+}
+
+#endif  // #ifdef TRILINOS_PACKAGE
+#endif  // #ifdef CCADISCRET
+#endif  // #ifdef D_SOH8
