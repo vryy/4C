4ff84e181a143ac3f1ee906c4da9b3fd658d8907
Moritz Frenzel <frenzel@lnm.mw.tum.de>
2007-10-26 09:18:06 +0000

    ANS for wedge solid-shell ready. Needs testing, but I have to find a wedge mesh first.
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@4064 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_so3/so_shw6_evaluate.cpp
+++ src/drt_so3/so_shw6_evaluate.cpp
@@ -99,6 +99,8 @@ void DRT::Elements::So_shw6::soshw6_nlnstiffmass(
   Epetra_SerialDenseMatrix* deriv_sp; //[NUMDIM_SOH8*numsp][NUMNOD_SOH8]
   // evaluate all necessary variables for ANS
   soshw6_anssetup(num_sp,num_ans,xrefe,xcurr,&deriv_sp,jac_sps,jac_cur_sps,B_ans_loc);
+  const DRT::Utils::GaussRule3D gaussrule_ = DRT::Utils::intrule_wedge_6point;
+  const DRT::Utils::IntegrationPoints3D intpoints = getIntegrationPoints3D(gaussrule_);
 
   
   /* =========================================================================*/
@@ -132,80 +134,110 @@ void DRT::Elements::So_shw6::soshw6_nlnstiffmass(
     if (abs(detJ) < 1E-16) dserror("ZERO JACOBIAN DETERMINANT");
     else if (detJ < 0.0) dserror("NEGATIVE JACOBIAN DETERMINANT");
 
-    /* compute derivatives N_XYZ at gp w.r.t. material coordinates
-    ** by solving   Jac . N_XYZ = N_rst   for N_XYZ
-    ** Inverse of Jacobian is therefore not explicitly computed
+    /* compute the CURRENT Jacobian matrix which looks like:
+    **         [ xcurr_,r  ycurr_,r  zcurr_,r ]
+    **  Jcur = [ xcurr_,s  ycurr_,s  zcurr_,s ]
+    **         [ xcurr_,t  ycurr_,t  zcurr_,t ]
+    ** Used to transform the global displacements into parametric space
     */
-    Epetra_SerialDenseMatrix N_XYZ(NUMDIM_WEG6,NUMNOD_WEG6);
-    Epetra_SerialDenseSolver solve_for_inverseJac;  // solve A.X=B
-    solve_for_inverseJac.SetMatrix(jac);            // set A=jac
-    solve_for_inverseJac.SetVectors(N_XYZ,deriv_gp);// set X=N_XYZ, B=deriv_gp
-    solve_for_inverseJac.FactorWithEquilibration(true);
-    int err2 = solve_for_inverseJac.Factor();        
-    int err = solve_for_inverseJac.Solve();         // N_XYZ = J^-1.N_rst
-    if ((err != 0) && (err2!=0)) dserror("Inversion of Jacobian failed");
+    Epetra_SerialDenseMatrix jac_cur(NUMDIM_WEG6,NUMDIM_WEG6);
+    jac_cur.Multiply('N','N',1.0,deriv_gp,xcurr,1.0);
 
-    // (material) deformation gradient F = d xcurr / d xrefe = xcurr^T * N_XYZ^T
-    Epetra_SerialDenseMatrix defgrd(NUMDIM_WEG6,NUMDIM_WEG6);
-    defgrd.Multiply('T','T',1.0,xcurr,N_XYZ,1.0);
-
-    // Right Cauchy-Green tensor = F^T * F
-    Epetra_SerialDenseMatrix cauchygreen(NUMDIM_WEG6,NUMDIM_WEG6);
-    cauchygreen.Multiply('T','N',1.0,defgrd,defgrd,1.0);
-
-    // Green-Lagrange strains matrix E = 0.5 * (Cauchygreen - Identity)
-    // GL strain vector glstrain={E11,E22,E33,2*E12,2*E23,2*E31}
-    Epetra_SerialDenseVector glstrain(NUMSTR_WEG6);
-    glstrain(0) = 0.5 * (cauchygreen(0,0) - 1.0);
-    glstrain(1) = 0.5 * (cauchygreen(1,1) - 1.0);
-    glstrain(2) = 0.5 * (cauchygreen(2,2) - 1.0);
-    glstrain(3) = cauchygreen(0,1);
-    glstrain(4) = cauchygreen(1,2);
-    glstrain(5) = cauchygreen(2,0);
-
-
-    /* non-linear B-operator (may so be called, meaning
-    ** of B-operator is not so sharp in the non-linear realm) *
-    ** B = F . Bl *
-    **
-    **      [ ... | F_11*N_{,1}^k  F_21*N_{,1}^k  F_31*N_{,1}^k | ... ]
-    **      [ ... | F_12*N_{,2}^k  F_22*N_{,2}^k  F_32*N_{,2}^k | ... ]
-    **      [ ... | F_13*N_{,3}^k  F_23*N_{,3}^k  F_33*N_{,3}^k | ... ]
-    ** B =  [ ~~~   ~~~~~~~~~~~~~  ~~~~~~~~~~~~~  ~~~~~~~~~~~~~   ~~~ ]
-    **      [       F_11*N_{,2}^k+F_12*N_{,1}^k                       ]
-    **      [ ... |          F_21*N_{,2}^k+F_22*N_{,1}^k        | ... ]
-    **      [                       F_31*N_{,2}^k+F_32*N_{,1}^k       ]
-    **      [                                                         ]
-    **      [       F_12*N_{,3}^k+F_13*N_{,2}^k                       ]
-    **      [ ... |          F_22*N_{,3}^k+F_23*N_{,2}^k        | ... ]
-    **      [                       F_32*N_{,3}^k+F_33*N_{,2}^k       ]
-    **      [                                                         ]
-    **      [       F_13*N_{,1}^k+F_11*N_{,3}^k                       ]
-    **      [ ... |          F_23*N_{,1}^k+F_21*N_{,3}^k        | ... ]
-    **      [                       F_33*N_{,1}^k+F_31*N_{,3}^k       ]
-    */
-    Epetra_SerialDenseMatrix bop(NUMSTR_WEG6,NUMDOF_WEG6);
-    for (int i=0; i<NUMNOD_WEG6; ++i) {
-      bop(0,NODDOF_WEG6*i+0) = defgrd(0,0)*N_XYZ(0,i);
-      bop(0,NODDOF_WEG6*i+1) = defgrd(1,0)*N_XYZ(0,i);
-      bop(0,NODDOF_WEG6*i+2) = defgrd(2,0)*N_XYZ(0,i);
-      bop(1,NODDOF_WEG6*i+0) = defgrd(0,1)*N_XYZ(1,i);
-      bop(1,NODDOF_WEG6*i+1) = defgrd(1,1)*N_XYZ(1,i);
-      bop(1,NODDOF_WEG6*i+2) = defgrd(2,1)*N_XYZ(1,i);
-      bop(2,NODDOF_WEG6*i+0) = defgrd(0,2)*N_XYZ(2,i);
-      bop(2,NODDOF_WEG6*i+1) = defgrd(1,2)*N_XYZ(2,i);
-      bop(2,NODDOF_WEG6*i+2) = defgrd(2,2)*N_XYZ(2,i);
-      /* ~~~ */
-      bop(3,NODDOF_WEG6*i+0) = defgrd(0,0)*N_XYZ(1,i) + defgrd(0,1)*N_XYZ(0,i);
-      bop(3,NODDOF_WEG6*i+1) = defgrd(1,0)*N_XYZ(1,i) + defgrd(1,1)*N_XYZ(0,i);
-      bop(3,NODDOF_WEG6*i+2) = defgrd(2,0)*N_XYZ(1,i) + defgrd(2,1)*N_XYZ(0,i);
-      bop(4,NODDOF_WEG6*i+0) = defgrd(0,1)*N_XYZ(2,i) + defgrd(0,2)*N_XYZ(1,i);
-      bop(4,NODDOF_WEG6*i+1) = defgrd(1,1)*N_XYZ(2,i) + defgrd(1,2)*N_XYZ(1,i);
-      bop(4,NODDOF_WEG6*i+2) = defgrd(2,1)*N_XYZ(2,i) + defgrd(2,2)*N_XYZ(1,i);
-      bop(5,NODDOF_WEG6*i+0) = defgrd(0,2)*N_XYZ(0,i) + defgrd(0,0)*N_XYZ(2,i);
-      bop(5,NODDOF_WEG6*i+1) = defgrd(1,2)*N_XYZ(0,i) + defgrd(1,0)*N_XYZ(2,i);
-      bop(5,NODDOF_WEG6*i+2) = defgrd(2,2)*N_XYZ(0,i) + defgrd(2,0)*N_XYZ(2,i);
+    // need gp-locations for ANS interpolation
+    const double r = intpoints.qxg[gp][0];
+    const double s = intpoints.qxg[gp][1];
+    //const double t = intpoints.qxg[gp][2]; // not needed
+    // set up B-Operator in local(parameter) element space including ANS
+    Epetra_SerialDenseMatrix bop_loc(NUMSTR_WEG6,NUMDOF_WEG6);
+    for (int inode = 0; inode < NUMNOD_WEG6; ++inode) {
+      for (int dim = 0; dim < NUMDIM_WEG6; ++dim) {
+        // B_loc_rr = N_r.X_r
+        bop_loc(0,inode*3+dim) = deriv_gp(0,inode) * jac_cur(0,dim);
+        // B_loc_ss = N_s.X_s
+        bop_loc(1,inode*3+dim) = deriv_gp(1,inode) * jac_cur(1,dim);
+        // B_loc_tt = interpolation along (r x s) of ANS B_loc_tt
+        //          = (1-r-s) * B_ans(SP C) + r * B_ans(SP D) + s * B_ans(SP E)
+        bop_loc(2,inode*3+dim) = (1-r-s) * B_ans_loc(0+2*num_ans,inode*3+dim)
+                                + r      * B_ans_loc(0+3*num_ans,inode*3+dim)
+                                + s      * B_ans_loc(0+4*num_ans,inode*3+dim);
+        // B_loc_rs = N_r.X_s + N_s.X_r
+        bop_loc(3,inode*3+dim) = deriv_gp(0,inode) * jac_cur(1,dim)
+                                +deriv_gp(1,inode) * jac_cur(0,dim);
+        // B_loc_st = interpolation along r of ANS B_loc_st
+        //          = r * B_ans(SP B)
+        bop_loc(4,inode*3+dim) = r * B_ans_loc(1+1*num_ans,inode*3+dim);
+        // B_loc_rt = interpolation along s of ANS B_loc_rt
+        //          = s * B_ans(SP A)
+        bop_loc(5,inode*3+dim) = s * B_ans_loc(2+0*num_ans,inode*3+dim);
+      }
     }
+
+    // transformation from local (parameter) element space to global(material) space
+    // with famous 'T'-matrix already used for EAS but now evaluated at each gp
+    Epetra_SerialDenseMatrix TinvT(NUMSTR_WEG6,NUMSTR_WEG6);
+    soshw6_evaluateT(jac,TinvT);
+    Epetra_SerialDenseMatrix bop(NUMSTR_WEG6,NUMDOF_WEG6);
+    bop.Multiply('N','N',1.0,TinvT,bop_loc,1.0);
+    
+    // local GL strain vector lstrain={Err,Ess,Ett,2*Ers,2*Est,2*Ert}
+    // but with modified ANS strains Ett, Est and Ert
+    Epetra_SerialDenseVector lstrain(NUMSTR_WEG6);
+    // evaluate glstrains in local(parameter) coords
+    // Err = 0.5 * (dx/dr * dx/dr^T - dX/dr * dX/dr^T)
+    lstrain(0)= 0.5 * (
+       +(jac_cur(0,0)*jac_cur(0,0) + jac_cur(0,1)*jac_cur(1,0) + jac_cur(0,2)*jac_cur(2,0))
+       -(jac(0,0)*jac(0,0)         + jac(0,1)*jac(1,0)         + jac(0,2)*jac(2,0)));
+    // Ess = 0.5 * (dy/ds * dy/ds^T - dY/ds * dY/ds^T)
+    lstrain(1)= 0.5 * (
+       +(jac_cur(1,0)*jac_cur(0,1) + jac_cur(1,1)*jac_cur(1,1) + jac_cur(1,2)*jac_cur(2,1))
+       -(jac(1,0)*jac(0,1)         + jac(1,1)*jac(1,1)         + jac(1,2)*jac(2,1)));
+    // Ers = (dx/ds * dy/dr^T - dX/ds * dY/dr^T)
+    lstrain(3)= (
+       +(jac_cur(0,0)*jac_cur(0,1) + jac_cur(0,1)*jac_cur(1,1) + jac_cur(0,2)*jac_cur(2,1))
+       -(jac(0,0)*jac(0,1)         + jac(0,1)*jac(1,1)         + jac(0,2)*jac(2,1)));
+
+    // ANS modification of strains ************************************** ANS
+    double dydt_A = 0.0; double dYdt_A = 0.0; const int spA = 0;
+    double dxdt_B = 0.0; double dXdt_B = 0.0; const int spB = 1;
+    double dzdt_C = 0.0; double dZdt_C = 0.0; const int spC = 2;
+    double dzdt_D = 0.0; double dZdt_D = 0.0; const int spD = 3;
+    double dzdt_E = 0.0; double dZdt_E = 0.0; const int spE = 4;
+    
+    const int xdir = 0; // index to matrix x-row, r-row respectively
+    const int ydir = 1; // index to matrix y-row, s-row respectively
+    const int zdir = 2; // index to matrix z-row, t-row respectively
+
+    // vector product of rows of jacobians at corresponding sampling point
+    for (int dim = 0; dim < NUMDIM_WEG6; ++dim) {
+      dydt_A += jac_cur_sps(ydir+spA*NUMDIM_WEG6,dim) * jac_cur_sps(dim+spA*NUMDIM_WEG6,zdir);
+      dYdt_A += jac_sps(ydir+spA*NUMDIM_WEG6,dim)     * jac_sps(dim+spA*NUMDIM_WEG6,zdir);
+      dxdt_B += jac_cur_sps(xdir+spB*NUMDIM_WEG6,dim) * jac_cur_sps(dim+spB*NUMDIM_WEG6,zdir);
+      dXdt_B += jac_sps(xdir+spB*NUMDIM_WEG6,dim)     * jac_sps(dim+spB*NUMDIM_WEG6,zdir);
+
+      dzdt_C += jac_cur_sps(zdir+spC*NUMDIM_WEG6,dim) * jac_cur_sps(dim+spC*NUMDIM_WEG6,zdir);
+      dZdt_C += jac_sps(zdir+spC*NUMDIM_WEG6,dim)     * jac_sps(dim+spC*NUMDIM_WEG6,zdir);
+      dzdt_D += jac_cur_sps(zdir+spD*NUMDIM_WEG6,dim) * jac_cur_sps(dim+spD*NUMDIM_WEG6,zdir);
+      dZdt_D += jac_sps(zdir+spD*NUMDIM_WEG6,dim)     * jac_sps(dim+spD*NUMDIM_WEG6,zdir);
+      dzdt_E += jac_cur_sps(zdir+spE*NUMDIM_WEG6,dim) * jac_cur_sps(dim+spE*NUMDIM_WEG6,zdir);
+      dZdt_E += jac_sps(zdir+spE*NUMDIM_WEG6,dim)     * jac_sps(dim+spE*NUMDIM_WEG6,zdir);
+    }
+    // E33: remedy of curvature thickness locking
+    // Ett = 0.5* ( (1-r-s) * Ett(SP C) + r * Ett(SP D) + s * Ett(SP E) )
+    lstrain(2) = 0.5 * ( (1-r-s) * (dzdt_C - dZdt_C)
+                        + r * (dzdt_D - dZdt_D)
+                        + s * (dzdt_E - dZdt_E));
+    // E23: remedy of transverse shear locking
+    // Est = r * Est(SP B)
+    lstrain(4) = r * (dxdt_B - dXdt_B);
+    // E13: remedy of transverse shear locking
+    // Ert = s * Est(SP A)
+    lstrain(5) = s * (dydt_A - dYdt_A);
+    // ANS modification of strains ************************************** ANS
+
+    // transformation of local glstrains 'back' to global(material) space
+    Epetra_SerialDenseVector glstrain(NUMSTR_WEG6);
+    glstrain.Multiply('N','N',1.0,TinvT,lstrain,1.0);
+    
 
     /* call material law cccccccccccccccccccccccccccccccccccccccccccccccccccccc
     ** Here all possible material laws need to be incorporated,
@@ -228,22 +260,37 @@ void DRT::Elements::So_shw6::soshw6_nlnstiffmass(
     (*stiffmatrix).Multiply('T','N',detJ * (*weights)(gp),bop,cb,1.0);
 
     // integrate `geometric' stiffness matrix and add to keu *****************
-    Epetra_SerialDenseVector sfac(stress); // auxiliary integrated stress
-    sfac.Scale(detJ * (*weights)(gp));     // detJ*w(gp)*[S11,S22,S33,S12=S21,S23=S32,S13=S31]
-    vector<double> SmB_L(NUMDIM_WEG6);     // intermediate Sm.B_L
-    // kgeo += (B_L^T . sigma . B_L) * detJ * w(gp)  with B_L = Ni,Xj see NiliFEM-Skript
     for (int inod=0; inod<NUMNOD_WEG6; ++inod){
-      SmB_L[0] = sfac(0) * N_XYZ(0,inod) + sfac(3) * N_XYZ(1,inod) + sfac(5) * N_XYZ(2,inod);
-      SmB_L[1] = sfac(3) * N_XYZ(0,inod) + sfac(1) * N_XYZ(1,inod) + sfac(4) * N_XYZ(2,inod);
-      SmB_L[2] = sfac(5) * N_XYZ(0,inod) + sfac(4) * N_XYZ(1,inod) + sfac(2) * N_XYZ(2,inod);
       for (int jnod=0; jnod<NUMNOD_WEG6; ++jnod){
-        double bopstrbop = 0.0;            // intermediate value
-        for (int idim=0; idim<NUMDIM_WEG6; ++idim) bopstrbop += N_XYZ(idim,jnod) * SmB_L[idim];
-        (*stiffmatrix)(NUMDIM_WEG6*inod+0,NUMDIM_WEG6*jnod+0) += bopstrbop;
-        (*stiffmatrix)(NUMDIM_WEG6*inod+1,NUMDIM_WEG6*jnod+1) += bopstrbop;
-        (*stiffmatrix)(NUMDIM_WEG6*inod+2,NUMDIM_WEG6*jnod+2) += bopstrbop;
+        Epetra_SerialDenseVector G_ij(NUMSTR_WEG6);
+        G_ij(0) = deriv_gp(0,inod) * deriv_gp(0,jnod); // rr-dir
+        G_ij(1) = deriv_gp(1,inod) * deriv_gp(1,jnod); // ss-dir
+        G_ij(3) = deriv_gp(0,inod) * deriv_gp(1,jnod) + deriv_gp(1,inod) * deriv_gp(0,jnod); //rs-dir
+        // ANS modification in tt-dir
+        G_ij(2) = (1-r-s) * (*deriv_sp)(zdir+spC*NUMDIM_WEG6,inod) * (*deriv_sp)(zdir+spC*NUMDIM_WEG6,jnod)
+                 + r * (*deriv_sp)(zdir+spD*NUMDIM_WEG6,inod) * (*deriv_sp)(zdir+spD*NUMDIM_WEG6,jnod)
+                 + s * (*deriv_sp)(zdir+spE*NUMDIM_WEG6,inod) * (*deriv_sp)(zdir+spE*NUMDIM_WEG6,jnod);
+        // ANS modification in st-dir
+        G_ij(4) = 0.5*( r * ((*deriv_sp)(ydir+spB*NUMDIM_WEG6,inod) * (*deriv_sp)(zdir+spB*NUMDIM_WEG6,jnod)
+                            +(*deriv_sp)(zdir+spB*NUMDIM_WEG6,inod) * (*deriv_sp)(ydir+spB*NUMDIM_WEG6,jnod)));
+        // ANS modification in rt-dir
+        G_ij(5) = 0.5*( s * ((*deriv_sp)(xdir+spA*NUMDIM_WEG6,inod) * (*deriv_sp)(zdir+spA*NUMDIM_WEG6,jnod)
+                            +(*deriv_sp)(zdir+spA*NUMDIM_WEG6,inod) * (*deriv_sp)(xdir+spA*NUMDIM_WEG6,jnod)));
+        // transformation of local(parameter) space 'back' to global(material) space
+        Epetra_SerialDenseVector G_ij_glob(NUMSTR_WEG6);
+        G_ij_glob.Multiply('N','N',1.0,TinvT,G_ij,0.0);
+
+        // Scalar Gij results from product of G_ij with stress, scaled with detJ*weights
+        Epetra_SerialDenseVector Gij(1); // this is a scalar
+        Gij.Multiply('T','N',detJ * (*weights)(gp),stress,G_ij_glob,0.0);
+
+        // add "geometric part" Gij times detJ*weights to stiffness matrix
+        (*stiffmatrix)(NUMDIM_WEG6*inod+0,NUMDIM_WEG6*jnod+0) += Gij(0);
+        (*stiffmatrix)(NUMDIM_WEG6*inod+1,NUMDIM_WEG6*jnod+1) += Gij(0);
+        (*stiffmatrix)(NUMDIM_WEG6*inod+2,NUMDIM_WEG6*jnod+2) += Gij(0);
       }
-    } // end of integrate `geometric' stiffness ******************************
+    } // end of intergrate `geometric' stiffness ******************************
+
 
 
     if (massmatrix != NULL){ // evaluate mass matrix +++++++++++++++++++++++++
@@ -380,6 +427,68 @@ void DRT::Elements::So_shw6::soshw6_anssetup(
   return;
 }
 
+/*----------------------------------------------------------------------*
+ |  evaluate 'T'-transformation matrix )                       maf 05/07|
+ *----------------------------------------------------------------------*/
+void DRT::Elements::So_shw6::soshw6_evaluateT(const Epetra_SerialDenseMatrix jac,
+                                            Epetra_SerialDenseMatrix& TinvT)
+{
+  // build T^T transformation matrix which maps
+  // between global (r,s,t)-coordinates and local (x,y,z)-coords
+  // later, invert the transposed to map from local to global
+  // see literature for details (e.g. Andelfinger)
+  // it is based on the voigt notation for strains: xx,yy,zz,xy,yz,xz
+  TinvT(0,0) = jac(0,0) * jac(0,0);
+  TinvT(1,0) = jac(1,0) * jac(1,0);
+  TinvT(2,0) = jac(2,0) * jac(2,0);
+  TinvT(3,0) = 2 * jac(0,0) * jac(1,0);
+  TinvT(4,0) = 2 * jac(1,0) * jac(2,0);
+  TinvT(5,0) = 2 * jac(0,0) * jac(2,0);
+  
+  TinvT(0,1) = jac(0,1) * jac(0,1);
+  TinvT(1,1) = jac(1,1) * jac(1,1);
+  TinvT(2,1) = jac(2,1) * jac(2,1);
+  TinvT(3,1) = 2 * jac(0,1) * jac(1,1);
+  TinvT(4,1) = 2 * jac(1,1) * jac(2,1);
+  TinvT(5,1) = 2 * jac(0,1) * jac(2,1);
+
+  TinvT(0,2) = jac(0,2) * jac(0,2);
+  TinvT(1,2) = jac(1,2) * jac(1,2);
+  TinvT(2,2) = jac(2,2) * jac(2,2);
+  TinvT(3,2) = 2 * jac(0,2) * jac(1,2);
+  TinvT(4,2) = 2 * jac(1,2) * jac(2,2);
+  TinvT(5,2) = 2 * jac(0,2) * jac(2,2);
+  
+  TinvT(0,3) = jac(0,0) * jac(0,1);
+  TinvT(1,3) = jac(1,0) * jac(1,1);
+  TinvT(2,3) = jac(2,0) * jac(2,1);
+  TinvT(3,3) = jac(0,0) * jac(1,1) + jac(1,0) * jac(0,1);
+  TinvT(4,3) = jac(1,0) * jac(2,1) + jac(2,0) * jac(1,1);
+  TinvT(5,3) = jac(0,0) * jac(2,1) + jac(2,0) * jac(0,1);
+
+
+  TinvT(0,4) = jac(0,1) * jac(0,2);
+  TinvT(1,4) = jac(1,1) * jac(1,2);
+  TinvT(2,4) = jac(2,1) * jac(2,2);
+  TinvT(3,4) = jac(0,1) * jac(1,2) + jac(1,1) * jac(0,2);
+  TinvT(4,4) = jac(1,1) * jac(2,2) + jac(2,1) * jac(1,2);
+  TinvT(5,4) = jac(0,1) * jac(2,2) + jac(2,1) * jac(0,2);
+  
+  TinvT(0,5) = jac(0,0) * jac(0,2);
+  TinvT(1,5) = jac(1,0) * jac(1,2);
+  TinvT(2,5) = jac(2,0) * jac(2,2);
+  TinvT(3,5) = jac(0,0) * jac(1,2) + jac(1,0) * jac(0,2);
+  TinvT(4,5) = jac(1,0) * jac(2,2) + jac(2,0) * jac(1,2);
+  TinvT(5,5) = jac(0,0) * jac(2,2) + jac(2,0) * jac(0,2);
+
+  // now evaluate T^{-T} with solver
+  Epetra_SerialDenseSolver solve_for_inverseT;
+  solve_for_inverseT.SetMatrix(TinvT);
+  int err2 = solve_for_inverseT.Factor();        
+  int err = solve_for_inverseT.Invert();
+  if ((err != 0) && (err2!=0)) dserror("Inversion of Tinv (Jacobian) failed");
+  return;
+}
 
 
 
