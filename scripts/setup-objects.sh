edac1fa473edaaabba17562db793a9e3033a4849
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2007-11-02 15:18:21 +0000

    keep D_FLUID for old discretization
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@4120 060e22d8-9d1c-0410-868a-e3a950d63d95

--- scripts/setup-objects.sh
+++ scripts/setup-objects.sh
@@ -48,6 +48,7 @@ fi
 if grep '^[[:blank:]]*D_FLUID2' "$definefile" 2>&1 > /dev/null ; then
     if [ "x$OBJ_FLUID" = "x0" ] ; then
         OBJECTS="$OBJECTS \$(OBJS_FLUID2) \$(OBJS_FLUID) \$(OBJS_DRT_F2) \$(OBJS_DRT_F2_LIB)"
+        DEFINES="$DEFINES -DD_FLUID"
         OBJ_FLUID=1
     else
         OBJECTS="$OBJECTS \$(OBJS_FLUID2) \$(OBJS_DRT_F2) \$(OBJS_DRT_F2_LIB)"
@@ -58,6 +59,7 @@ fi
 if grep '^[[:blank:]]*FLUID2_ML' "$definefile" 2>&1 > /dev/null ; then
     if [ "x$OBJ_FLUID" = "x0" ] ; then
         OBJECTS="$OBJECTS \$(OBJS_F2ML) \$(OBJS_FLUID)"
+        DEFINES="$DEFINES -DD_FLUID"
         OBJ_FLUID=1
     else
         OBJECTS="$OBJECTS \$(OBJS_F2ML)"
