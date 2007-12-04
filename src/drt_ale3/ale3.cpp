28fca5f8051e4fb3f6482bfe0be7bef09bbc0a9f
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2007-12-04 10:45:06 +0000

    did anybody ever use wedge elements with applied neumann conditions?
    the routines to create the surface elements were wrong (nnode not initialized)
    -> fixed
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@4364 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_ale3/ale3.cpp
+++ src/drt_ale3/ale3.cpp
@@ -201,70 +201,76 @@ void DRT::Elements::Ale3::CreateSurfacesHex(const int& nsurf,
 
 void DRT::Elements::Ale3::CreateSurfaceWedge(const int& nsurf, const int& wedgetype)
 {
-  int nnode, trisurfacenodes, quadsurfacenodes;
-
   switch (wedgetype){
   case 6:
-    trisurfacenodes=3;
-    quadsurfacenodes=4;
+  {
+    const int trisurfacenodes=3;
+    const int quadsurfacenodes=4;
     for (int isurf=0; isurf<nsurf; isurf++)
     {
-      int nodeids[nnode];
-      DRT::Node* nodes[nnode];
+        int nnode = 0;
+        if (isurf <=2)
+            nnode=trisurfacenodes;
+        else
+            nnode=quadsurfacenodes;
+        
+        int nodeids[nnode];
+        DRT::Node* nodes[nnode];
 
-      if (isurf <=2)
-        nnode=trisurfacenodes;
-      else
-        nnode=quadsurfacenodes;
-
-
-      for (int inode=0; inode<nnode;inode++)
-      {
-        if (isurf < 2){
-        nodeids[inode] = NodeIds()[eleNodeNumbering_wedge15_trisurfaces[isurf][inode]];
-      nodes[inode] = Nodes()[ eleNodeNumbering_wedge15_trisurfaces[isurf][inode]];
-    }
-    else
-      nodeids[inode] = NodeIds()[eleNodeNumbering_wedge15_quadsurfaces[isurf][inode]];
-      nodes[inode] = Nodes()[ eleNodeNumbering_wedge15_quadsurfaces[isurf][inode]];
-  }
-       surfaces_[isurf] = rcp(new DRT::Elements::Ale3Surface(isurf,Owner(),nnode,nodeids,nodes,this,isurf));
-        surfaceptrs_[isurf] = surfaces_[isurf].get();
+        for (int inode=0; inode<nnode;inode++)
+        {
+            if (isurf < 2)
+            {
+                nodeids[inode] = NodeIds()[eleNodeNumbering_wedge15_trisurfaces[isurf][inode]];
+                nodes[inode] = Nodes()[ eleNodeNumbering_wedge15_trisurfaces[isurf][inode]];
+            }
+            else
+            {
+                nodeids[inode] = NodeIds()[eleNodeNumbering_wedge15_quadsurfaces[isurf][inode]];
+                nodes[inode] = Nodes()[ eleNodeNumbering_wedge15_quadsurfaces[isurf][inode]];
+            }
+            surfaces_[isurf] = rcp(new DRT::Elements::Ale3Surface(isurf,Owner(),nnode,nodeids,nodes,this,isurf));
+            surfaceptrs_[isurf] = surfaces_[isurf].get();
+        }
     }
     break;
-
+  }
   case 15:
-    trisurfacenodes=6;
-    quadsurfacenodes=8;
+  {
+    const int trisurfacenodes=6;
+    const int quadsurfacenodes=8;
 
     for (int isurf=0; isurf<nsurf; isurf++)
     {
-
-      int nodeids[nnode];
-      DRT::Node* nodes[nnode];
-
-      if (isurf <=2)
-        nnode=trisurfacenodes;
-      else
-        nnode=quadsurfacenodes;
-
-
-      for (int inode=0; inode<nnode;inode++)
-      {
-        if (isurf < 2){
-        nodeids[inode] = NodeIds()[eleNodeNumbering_wedge15_trisurfaces[isurf][inode]];
-      nodes[inode] = Nodes()[ eleNodeNumbering_wedge15_trisurfaces[isurf][inode]];
-    }
-    else
-      nodeids[inode] = NodeIds()[eleNodeNumbering_wedge15_quadsurfaces[isurf][inode]];
-      nodes[inode] = Nodes()[ eleNodeNumbering_wedge15_quadsurfaces[isurf][inode]];
-  }
-       surfaces_[isurf] = rcp(new DRT::Elements::Ale3Surface(isurf,Owner(),nnode,nodeids,nodes,this,isurf));
-        surfaceptrs_[isurf] = surfaces_[isurf].get();
+        int nnode = 0;
+        if (isurf <=2)
+            nnode=trisurfacenodes;
+        else
+            nnode=quadsurfacenodes;
+        
+        int nodeids[nnode];
+        DRT::Node* nodes[nnode];
+        for (int inode=0; inode<nnode;inode++)
+        {
+            if (isurf < 2)
+            {
+                nodeids[inode] = NodeIds()[eleNodeNumbering_wedge15_trisurfaces[isurf][inode]];
+                nodes[inode] = Nodes()[ eleNodeNumbering_wedge15_trisurfaces[isurf][inode]];
+            }
+            else
+            {
+                nodeids[inode] = NodeIds()[eleNodeNumbering_wedge15_quadsurfaces[isurf][inode]];
+                nodes[inode] = Nodes()[ eleNodeNumbering_wedge15_quadsurfaces[isurf][inode]];
+            }
+            surfaces_[isurf] = rcp(new DRT::Elements::Ale3Surface(isurf,Owner(),nnode,nodeids,nodes,this,isurf));
+            surfaceptrs_[isurf] = surfaces_[isurf].get();
+        }
     }
     break;
-
-}
+  }
+  default:
+      dserror("incorrect wedgetype");
+  } // end switch wedge type
 }
 
 void DRT::Elements::Ale3::CreateSurfacesPyramid()
