327cd26f64da191443cc2a127863e2cc7d1fe796
Moritz Frenzel <frenzel@lnm.mw.tum.de>
2007-12-21 10:56:46 +0000

    Progress on exodus preprocessor: added example suggestions for ELEMENTS, DIRICHLET and NEUMANN.
    pre_exodus is now ready to use as follows:
    - compile pre_exodus
    - run pre_exodus --exo="your_exodus_file.e"
    
    pre_exodus will write (and overwrite, carefull!) the files:
    - default.head: a baci header based on the "valid-parameters"
    - default.bc: a suggestion for the exofilter command-file, which comes with all kinds of information of the underlying mesh, ElementBlocks, NodeSets, and SideSets
    
    You should edit those 2 files according to your problem and then run the existing exofilter on those edited files.
    
    In future, the workflow can be simplified based on something like a "valid-boundary-conditions" list or a "valid-elements" list.
    
    Have fun!
    Moritz & Georg
    
    
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@4536 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/pre_exodus/pre_exodus.cpp
+++ src/pre_exodus/pre_exodus.cpp
@@ -98,11 +98,27 @@ int main(
       }
       // show all necessary bc and element specifications (suggestion for user)     
       defaultbc<<"-----------Please specify:-----------"<<endl<<endl;
-	  defaultbc<<"\\\\SYNTAX:"<<endl;
+	    defaultbc<<"\\\\SYNTAX:"<<endl;
       defaultbc<<"\\\\"<<"*matrID"<<"=\"<DESIGN TYPE or ELEMENT>\"\"<DESIGN-number or ELEMENT-TYPE>\""<<endl
       <<"\\\\"<<"boundr_cond=\"<BC TYPE>\"\"<the BC itself>\""<<endl
       <<"\\\\"<<"type=\"<ELEMENT-type>\"\"<ELEMENT-type2>\"\"<ELEMENT-type3>\""<<endl
       <<"\\\\"<<"prop=\"<element flags>\""<<endl<<endl;
+      
+      defaultbc<<"\\\\ For Example:"<<endl
+      <<"\\\\*matr1 =\"ELEMENT\"\"STRUCTURE\""<<endl
+      <<"\\\\boundr_cond=\" \"\" \" "<<endl
+      <<"\\\\type=\"STRUCTURE\"\"SOLIDSH8\"\"HEX8\" "<<endl
+      <<"\\\\prop=\"MAT       1 GP 2 2 2 STRESS None KINEM Totlag  EAS sosh8 THICKDIR AUTO\" "<<endl<<endl;
+      defaultbc<<"\\\\*matr1 =\"ELEMENT\"\"FLUID\" "<<endl
+      <<"\\\\boundr_cond=\" \"\" \" "<<endl
+      <<"\\\\type=\"FLUID\"\"FLUID3\"\"HEX8\" " <<endl
+      <<"\\\\prop=\"MAT       1 NA ALE GP 2 2 2\" "<<endl<<endl;
+      defaultbc<<"\\\\*matr2 =\"DSURF\"\"1\" "<<endl
+      << "\\\\boundr_cond=\"NEUMANN\"\"E 1 - 1  0 0 1 0 0 0 0.0 0.0 1.0 0.0 0.0 0.0 Live Mid\" "<<endl<<endl;
+      defaultbc<<"\\\\*matr6 =\"DSURF\"\"4\" "<<endl
+      <<"\\\\boundr_cond=\"DIRICH\"\"E 4 - 1 1 1 0 0 0 0.0 0.0 0.0 0.0 0.0 0.0 none none none none none none 0 0 0 0 0 0\" "<<endl<<endl;
+
+      defaultbc<<"------------------------------------------------"<<endl<<endl;
       
       for (int i = 0; i < mymesh.GetNumberEntities(); ++i) 
       {    
