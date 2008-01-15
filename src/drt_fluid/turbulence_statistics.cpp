a03a522ab759feab726c313a239e48c61733d180
Peter Gamnitzer <gamnitzer@lnm.mw.tum.de>
2008-01-15 09:45:26 +0000

    Additional pointwise computation of statistics for turbulent channel flow
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@4607 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_fluid/turbulence_statistics.cpp
+++ src/drt_fluid/turbulence_statistics.cpp
@@ -317,7 +317,38 @@ TurbulenceStatistics::TurbulenceStatistics(
   
   sumsqp_ =  rcp(new vector<double> );
   sumsqp_->resize(size,0.0);
+#if 1
+  pointsquaredvelnp_  = LINALG::CreateVector(*dofrowmap,true);
 
+
+// first order moments
+  pointsumu_ =  rcp(new vector<double> );
+  pointsumu_->resize(size,0.0);
+    
+  pointsumv_ =  rcp(new vector<double> );
+  pointsumv_->resize(size,0.0);
+
+  pointsumw_ =  rcp(new vector<double> );
+  pointsumw_->resize(size,0.0);
+
+  pointsump_ =  rcp(new vector<double> );
+  pointsump_->resize(size,0.0);
+
+  // now the second order moments
+  pointsumsqu_ =  rcp(new vector<double> );
+  pointsumsqu_->resize(size,0.0);
+    
+  pointsumsqv_ =  rcp(new vector<double> );
+  pointsumsqv_->resize(size,0.0);
+
+  pointsumsqw_ =  rcp(new vector<double> );
+  pointsumsqw_->resize(size,0.0);
+  
+  pointsumsqp_ =  rcp(new vector<double> );
+  pointsumsqp_->resize(size,0.0);
+
+
+#endif
   // means for the Smagorinsky constant
   sumCs_  =  rcp(new vector<double> );
   sumCs_->resize(nodeplanes_->size()-1,0.0);
@@ -421,15 +452,130 @@ void TurbulenceStatistics::DoTimeSample(
   numsamp_++;
   
   //----------------------------------------------------------------------
-  // pointwise multiplication to get squared values
+  // meanvelnp is a refcount copy of velnp
   //----------------------------------------------------------------------
   meanvelnp_->Update(1.0,*velnp,0.0);
 
   //----------------------------------------------------------------------
-  // loop planes and calculate means in each plane
+  // loop planes and calculate integral means in each plane
   //----------------------------------------------------------------------
   this->EvaluateMeanValuesInPlanes();
+#if 1
+  int planenum = 0;
+
+  //----------------------------------------------------------------------
+  // pointwise multiplication to get squared values
+  //----------------------------------------------------------------------
+  pointsquaredvelnp_->Multiply(1.0,*velnp,*velnp,0.0);
+
+  
+  //----------------------------------------------------------------------
+  // loop planes and calculate pointwise means in each plane
+  //----------------------------------------------------------------------
+  for(vector<double>::iterator plane=planecoordinates_->begin();
+      plane!=planecoordinates_->end();
+      ++plane)
+  {
+
+    // toggle vectors are one in the position of a dof in this plane,
+    // else 0
+    toggleu_->PutScalar(0.0);
+    togglev_->PutScalar(0.0);
+    togglew_->PutScalar(0.0);
+    togglep_->PutScalar(0.0);
+
+    // count the number of nodes in plane (required to calc. in plane mean)
+    int countnodesinplane=0;
+
+    //----------------------------------------------------------------------
+    // activate toggles for in plane dofs
+    //----------------------------------------------------------------------
+    for (int nn=0; nn<discret_->NumMyRowNodes(); ++nn)
+    {
+      DRT::Node* node = discret_->lRowNode(nn);
+
+      // this node belongs to the plane under consideration
+      if (node->X()[dim_]<*plane+2e-9 && node->X()[dim_]>*plane-2e-9)
+      {
+        vector<int> dof = discret_->Dof(node);
+        double      one = 1.0;
+
+        toggleu_->ReplaceGlobalValues(1,&one,&(dof[0]));
+        togglev_->ReplaceGlobalValues(1,&one,&(dof[1]));
+        togglew_->ReplaceGlobalValues(1,&one,&(dof[2]));
+        togglep_->ReplaceGlobalValues(1,&one,&(dof[3]));
+ 
+        // now check whether we have a pbc condition on this node
+        vector<DRT::Condition*> mypbc;
+    
+        node->GetCondition("SurfacePeriodic",mypbc);
+
+        // yes, we have a pbc
+        if (mypbc.size()>0)
+        {
+          // loop them and check, whether this is a pbc pure master node
+          // for all previous conditions
+          unsigned ntimesmaster = 0;
+          for (unsigned numcond=0;numcond<mypbc.size();++numcond)
+          {
+            const vector<int>* mymasterslavetoggle
+              = mypbc[numcond]->Get<vector<int> >("Is slave periodic boundary condition");
+
+            if(mymasterslavetoggle[0][0]==0)
+            {
+              ++ntimesmaster;
+            } // end is slave?
+          } // end loop this conditions
+
+          if(ntimesmaster!=mypbc.size())
+          {
+            continue;
+          }
+          // we have a master. Remember this cause we have to extend the patch
+          // to the other side...
+        }
+        countnodesinplane++;
+      }
+    }
+
+    int countnodesinplaneonallprocs=0;
       
+    discret_->Comm().SumAll(&countnodesinplane,&countnodesinplaneonallprocs,1);
+    
+    if (countnodesinplaneonallprocs)
+    {
+      //----------------------------------------------------------------------
+      // compute scalar products from velnp and toggle vec to sum up
+      // values in this plane
+      //----------------------------------------------------------------------
+      double inc;
+      velnp->Dot(*toggleu_,&inc);
+      (*pointsumu_)[planenum]+=inc/countnodesinplaneonallprocs;
+      velnp->Dot(*togglev_,&inc);
+      (*pointsumv_)[planenum]+=inc/countnodesinplaneonallprocs;
+      velnp->Dot(*togglew_,&inc);
+      (*pointsumw_)[planenum]+=inc/countnodesinplaneonallprocs;
+      velnp->Dot(*togglep_,&inc);
+      (*pointsump_)[planenum]+=inc/countnodesinplaneonallprocs;
+
+      //----------------------------------------------------------------------
+      // compute scalar products from squaredvelnp and toggle vec to
+      // sum up values for second order moments in this plane
+      //----------------------------------------------------------------------
+      pointsquaredvelnp_->Dot(*toggleu_,&inc);
+      (*pointsumsqu_)[planenum]+=inc/countnodesinplaneonallprocs;
+      pointsquaredvelnp_->Dot(*togglev_,&inc);
+      (*pointsumsqv_)[planenum]+=inc/countnodesinplaneonallprocs;
+      pointsquaredvelnp_->Dot(*togglew_,&inc);
+      (*pointsumsqw_)[planenum]+=inc/countnodesinplaneonallprocs;
+      pointsquaredvelnp_->Dot(*togglep_,&inc);
+      (*pointsumsqp_)[planenum]+=inc/countnodesinplaneonallprocs;
+    }
+    planenum++;
+  }
+
+
+#endif
   for(vector<double>::iterator plane=planecoordinates_->begin();
       plane!=planecoordinates_->end();
       ++plane)
@@ -697,16 +843,29 @@ void TurbulenceStatistics::TimeAverageMeansAndOutputOfStatistics(int step)
   for(unsigned i=0; i<planecoordinates_->size(); ++i)
   {
     
-    (*sumu_)[i]  /=aux;
-    (*sumv_)[i]  /=aux;
-    (*sumw_)[i]  /=aux;
-    (*sump_)[i]  /=aux;
+    (*sumu_)[i]   /=aux;
+    (*sumv_)[i]   /=aux;
+    (*sumw_)[i]   /=aux;
+    (*sump_)[i]   /=aux;
 
-
-    (*sumsqu_)[i]/=aux;
-    (*sumsqv_)[i]/=aux;
-    (*sumsqw_)[i]/=aux;
-    (*sumsqp_)[i]/=aux;
+    (*sumuv_)[i]  /=aux;
+    (*sumuw_)[i]  /=aux;
+    (*sumvw_)[i]  /=aux;
+ 
+    (*sumsqu_)[i] /=aux;
+    (*sumsqv_)[i] /=aux;
+    (*sumsqw_)[i] /=aux;
+    (*sumsqp_)[i] /=aux;
+    
+    (*pointsumu_)[i]   /=numsamp_;
+    (*pointsumv_)[i]   /=numsamp_;
+    (*pointsumw_)[i]   /=numsamp_;
+    (*pointsump_)[i]   /=numsamp_;
+ 
+    (*pointsumsqu_)[i] /=numsamp_;
+    (*pointsumsqv_)[i] /=numsamp_;
+    (*pointsumsqw_)[i] /=numsamp_;
+    (*pointsumsqp_)[i] /=numsamp_;
   }
 
   sumforceu_/=numsamp_;
@@ -793,7 +952,16 @@ void TurbulenceStatistics::TimeAverageMeansAndOutputOfStatistics(int step)
       (*log) << "   " << setw(11) << setprecision(4) << ((*sumuv_) [i]);
       (*log) << "   " << setw(11) << setprecision(4) << ((*sumuw_) [i]);
       (*log) << "   " << setw(11) << setprecision(4) << ((*sumvw_) [i]);
-      (*log) << "   " << setw(11) << setprecision(4) << ((*sumsqp_)[i]) << "   \n";
+      (*log) << "   " << setw(11) << setprecision(4) << ((*sumsqp_)[i]);
+      (*log) << "   " << setw(11) << setprecision(4) << (*pointsumu_)   [i];
+      (*log) << "   " << setw(11) << setprecision(4) << (*pointsumv_)   [i];
+      (*log) << "   " << setw(11) << setprecision(4) << (*pointsumw_)   [i];
+      (*log) << "   " << setw(11) << setprecision(4) << (*pointsump_)   [i];
+      (*log) << "   " << setw(11) << setprecision(4) << ((*pointsumsqu_)[i]);
+      (*log) << "   " << setw(11) << setprecision(4) << ((*pointsumsqv_)[i]);
+      (*log) << "   " << setw(11) << setprecision(4) << ((*pointsumsqw_)[i]);
+      (*log) << "   " << setw(11) << setprecision(4) << ((*pointsumsqp_)[i]);
+      (*log) << "   \n";
     }
     log->flush();
   }
@@ -891,7 +1059,9 @@ void TurbulenceStatistics::TimeAverageMeansAndOutputOfStatistics(int step)
 
       (*log_res)  << setw(11) << setprecision(4) << (*sumsacc_sq_)[3*rr  ]/(numele_*numsamp_) << "  ";
       (*log_res)  << setw(11) << setprecision(4) << (*sumsacc_sq_)[3*rr+1]/(numele_*numsamp_) << "  ";
-      (*log_res)  << setw(11) << setprecision(4) << (*sumsacc_sq_)[3*rr+2]/(numele_*numsamp_) << &endl;
+      (*log_res)  << setw(11) << setprecision(4) << (*sumsacc_sq_)[3*rr+2]/(numele_*numsamp_) << "  ";
+      
+      (*log_res)  << &endl;
     }
     log_res->flush();
   }
@@ -923,10 +1093,24 @@ void TurbulenceStatistics::ClearStatistics()
     (*sumw_)[i]  =0;
     (*sump_)[i]  =0;
 
+    (*sumuv_ )[i]=0;
+    (*sumuw_ )[i]=0;
+    (*sumvw_ )[i]=0;
     (*sumsqu_)[i]=0;
     (*sumsqv_)[i]=0;
     (*sumsqw_)[i]=0;
     (*sumsqp_)[i]=0;
+
+    (*pointsumu_)[i]  =0;
+    (*pointsumv_)[i]  =0;
+    (*pointsumw_)[i]  =0;
+    (*pointsump_)[i]  =0;
+
+    (*pointsumsqu_)[i]=0;
+    (*pointsumsqv_)[i]=0;
+    (*pointsumsqw_)[i]=0;
+    (*pointsumsqp_)[i]=0;
+
   }
   
   meanvelnp_->PutScalar(0.0);
