4be54bc9528f3a167393c03c6e41192798db21a5
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2008-02-12 15:26:28 +0000

    reduced number of compiler warnings by one: removed unused variable
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@4923 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_f3/xfluid3_line_evaluate.cpp
+++ src/drt_f3/xfluid3_line_evaluate.cpp
@@ -38,7 +38,6 @@ int DRT::ELEMENTS::XFluid3Line::Evaluate(   ParameterList& params,
 	if (action == "none") dserror("No action supplied");
   	else dserror("Unknown type of action for XFluid3_Line");
   	
-  	const DiscretizationType distype = this->Shape();
     switch(act)
     {
     default:
