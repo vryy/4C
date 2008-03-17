13b46f6e835fe366ef03c7888862406c6ac4bf7f
Georg Bauer <bauer@lnm.mw.tum.de>
2008-03-17 15:30:53 +0000

    added (currently empty) entry point for ELCH calculations in BACI
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@5340 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/drt_elch/elch_dyn.cpp
@@ -0,0 +1,42 @@
+
+#ifdef CCADISCRET
+
+#include <string>
+#include <iostream>
+
+#ifdef PARALLEL
+#include <mpi.h>
+#endif
+
+#ifdef PARALLEL
+#include <Epetra_MpiComm.h>
+#else
+#include <Epetra_SerialComm.h>
+#endif
+
+#include "elch_dyn.H"
+
+/*----------------------------------------------------------------------*
+ |                                                       m.gee 06/01    |
+ | general problem data                                                 |
+ | global variable GENPROB genprob is defined in global_control.c       |
+ *----------------------------------------------------------------------*/
+extern struct _GENPROB     genprob;
+
+
+/*----------------------------------------------------------------------*/
+// entry point for ELCH in DRT
+/*----------------------------------------------------------------------*/
+void elch_dyn()
+{
+#ifdef PARALLEL
+  Epetra_MpiComm comm(MPI_COMM_WORLD);
+#else
+  Epetra_SerialComm comm;
+#endif
+
+cout<<"ELCH problemtype under development..."<<endl;
+
+} // elch_dyn()
+
+#endif  // CCADISCRET
