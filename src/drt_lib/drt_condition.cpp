7d43c61fcd85cb7e8a6d8662e71e61287e2a502e
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2008-04-18 12:23:01 +0000

    added FSICoupling in Condition::Print() to allow printing on screen
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@5689 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_lib/drt_condition.cpp
+++ src/drt_lib/drt_condition.cpp
@@ -95,6 +95,7 @@ void DRT::Condition::Print(ostream& os) const
   else if (Type()==SurfaceNeumann)              os << "Surface Neumann boundary condition: ";
   else if (Type()==VolumeNeumann)               os << "Volume Neumann boundary condition: ";
   else if (Type()==Contact)                     os << "Contact boundary condition: ";
+  else if (Type()==FSICoupling)                 os << "FSI Coupling condition:";
   else if (Type()==XFEMCoupling)                os << "XFEM Coupling condition:";
   else if (Type()==LineLIFTDRAG)                os << "Line LIFTDRAG condition:";
   else if (Type()==SurfLIFTDRAG)                os << "Surf LIFTDRAG condition:";
