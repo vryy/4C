c9dfdd691bbd018cca8b1fded070f5cb3398cf40
Georg Bauer <bauer@lnm.mw.tum.de>
2008-04-23 14:13:33 +0000

    minor
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@5753 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/post_ensight/post_drt_ensight_single_field_writers.cpp
+++ src/post_ensight/post_drt_ensight_single_field_writers.cpp
@@ -89,7 +89,7 @@ void ConDifEnsightWriter::WriteAllResults(PostField* field)
   // (equals 2 or 3) as a number of dofs
   EnsightWriter::WriteResult("phinp", "phi", dofbased, 1);
   // write velocity field (always 3D)
-  EnsightWriter::WriteResult("conv_velocity", "velocity", nodebased, 3);
+  EnsightWriter::WriteResult("convec_velocity", "velocity", nodebased, 3);
   WriteElementResults(field);
 }
 
