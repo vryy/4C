b292642db4ef01a9fb4ff8dc0e5c3654b83e928f
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2008-04-24 09:16:55 +0000

    created files, where the new searchtree will be located
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@5762 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/drt_xfem/xfsi_searchtree.cpp
@@ -0,0 +1,25 @@
+/*!
+\file xfsi_searchtree.cpp
+
+\brief provides a class with search tree
+
+<pre>
+Maintainer: Ursula Mayer
+            mayer@lnm.mw.tum.de
+            http://www.lnm.mw.tum.de
+            089 - 289-15257
+</pre>
+*/
+#ifdef CCADISCRET
+
+#include "xfsi_searchtree.H"
+
+
+
+using namespace std;
+
+
+
+
+
+#endif  // #ifdef CCADISCRET
