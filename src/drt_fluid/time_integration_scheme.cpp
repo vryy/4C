3aaa2f38683f469e9c6f735db86f0fcf56bfd5b3
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2008-05-28 09:29:25 +0000

    fixed indentation
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@6202 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_fluid/time_integration_scheme.cpp
+++ src/drt_fluid/time_integration_scheme.cpp
@@ -60,8 +60,8 @@ void TIMEINT_THETA_BDF2::SetOldPartOfRighthandside(
   switch (timealgo)
   {
   case timeint_stationary: /* One step Theta time integration */
-        hist->Scale(0.0);
-        break;
+    hist->Scale(0.0);
+    break;
       
   case timeint_one_step_theta: /* One step Theta time integration */
     hist->Update(1.0, *veln, dta*(1.0-theta), *accn, 0.0);
