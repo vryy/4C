7f4e758455f7056a08d5b2a7907739ac089242e0
Ulrich Kuettler <kuettler@lnm.mw.tum.de>
2008-05-30 10:54:06 +0000

    revert fix for consistency
    AAANeoHooke needs to be fixed now.
    
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@6259 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_so3/so_hex8_evaluate.cpp
+++ src/drt_so3/so_hex8_evaluate.cpp
@@ -737,10 +737,8 @@ void DRT::ELEMENTS::So_hex8::soh8_nlnstiffmass(
     ** the stress vector, a C-matrix, and a density must be retrieved,
     ** every necessary data must be passed.
     */
-    LINALG::SerialDenseMatrix cmat(NUMSTR_SOH8,NUMSTR_SOH8);
-    LINALG::SerialDenseVector stress(NUMSTR_SOH8);
-    memset(cmat.A(),0,NUMSTR_SOH8*NUMSTR_SOH8*sizeof(double));
-    memset(stress.A(),0,NUMSTR_SOH8*sizeof(double));
+    Epetra_SerialDenseMatrix cmat(NUMSTR_SOH8,NUMSTR_SOH8);
+    Epetra_SerialDenseVector stress(NUMSTR_SOH8);
     double density;
     const int ele_ID = Id();
     soh8_mat_sel(&stress,&cmat,&density,&glstrain,&defgrd,gp,ele_ID,params);
