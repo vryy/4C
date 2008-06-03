de7255ca6a7ea8c0916e970053d123b218d98885
Ulrich Kuettler <kuettler@lnm.mw.tum.de>
2008-06-03 18:25:10 +0000

    you can do Richardson iterations inside the preconditioner -- it just doesn't buy you much
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@6309 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_fsi/fsi_overlapprec.cpp
+++ src/drt_fsi/fsi_overlapprec.cpp
@@ -169,6 +169,8 @@ void FSI::OverlappingBlockMatrix::FSALowerGS(const Epetra_MultiVector &X, Epetra
   Teuchos::RCP<Epetra_Vector> fy = RangeExtractor().ExtractVector(y,1);
   Teuchos::RCP<Epetra_Vector> ay = RangeExtractor().ExtractVector(y,2);
 
+#if 1
+
   {
     // Solve fluid equations for fy with the rhs fx - F(I,Gamma) sy - F(Mesh) ay
 
@@ -177,16 +179,6 @@ void FSI::OverlappingBlockMatrix::FSALowerGS(const Epetra_MultiVector &X, Epetra
 
     Epetra_Time tf(Comm());
 
-#if 0
-    // respect initial guess
-    const LINALG::SparseMatrix& fluidBoundOp  = Matrix(1,0);
-    Teuchos::RCP<Epetra_Vector> tmpfx = Teuchos::rcp(new Epetra_Vector(DomainMap(1)));
-    fluidBoundOp.Multiply(false,*sy,*tmpfx);
-    fx->Update(-1.0,*tmpfx,1.0);
-#endif
-
-//     fluidMeshOp.Multiply(false,*ay,*tmpfx);
-//     fx->Update(-1.0,*tmpfx,1.0);
     fluidsolver_->Solve(fluidInnerOp.EpetraMatrix(),fy,fx,true);
 
     if (Comm().MyPID()==0)
@@ -204,12 +196,48 @@ void FSI::OverlappingBlockMatrix::FSALowerGS(const Epetra_MultiVector &X, Epetra
 
     Teuchos::RCP<Epetra_Vector> tmpsx = Teuchos::rcp(new Epetra_Vector(DomainMap(0)));
     structBoundOp.Multiply(false,*fy,*tmpsx);
-    sx->Update(-1.0,*tmpsx,1.0);
-    structuresolver_->Solve(structInnerOp.EpetraMatrix(),sy,sx,true);
+    //sx->Update(-1.0,*tmpsx,1.0);
+    tmpsx->Update(1.0,*sx,-1.0);
+    structuresolver_->Solve(structInnerOp.EpetraMatrix(),sy,tmpsx,true);
 
     if (Comm().MyPID()==0)
       std::cout << ts.ElapsedTime() << std::flush;
   }
+
+#else
+
+  const LINALG::SparseMatrix& fluidBoundOp  = Matrix(1,0);
+  const LINALG::SparseMatrix& structBoundOp = Matrix(0,1);
+
+#if 0
+  BlockRichardson(fluidInnerOp,
+                  fluidBoundOp,
+                  structBoundOp,
+                  structInnerOp,
+                  fluidsolver_,
+                  structuresolver_,
+                  fy,
+                  sy,
+                  fx,
+                  sx,
+                  1.0,
+                  200);
+#else
+  BlockRichardson(structInnerOp,
+                  structBoundOp,
+                  fluidBoundOp,
+                  fluidInnerOp,
+                  structuresolver_,
+                  fluidsolver_,
+                  sy,
+                  fy,
+                  sx,
+                  fx,
+                  1.0,
+                  200);
+#endif
+
+#endif
 
   {
     // Solve ale equations for ay with the rhs ax - A(I,Gamma) sy
@@ -246,6 +274,66 @@ void FSI::OverlappingBlockMatrix::FSALowerGS(const Epetra_MultiVector &X, Epetra
   RangeExtractor().InsertVector(*sy,0,y);
   RangeExtractor().InsertVector(*fy,1,y);
   RangeExtractor().InsertVector(*ay,2,y);
+}
+
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+void FSI::OverlappingBlockMatrix::BlockRichardson(const LINALG::SparseMatrix& Aii,
+                                                  const LINALG::SparseMatrix& Aig,
+                                                  const LINALG::SparseMatrix& Agi,
+                                                  const LINALG::SparseMatrix& Agg,
+                                                  Teuchos::RCP<LINALG::Preconditioner> Sii,
+                                                  Teuchos::RCP<LINALG::Preconditioner> Sgg,
+                                                  Teuchos::RCP<Epetra_Vector> xi,
+                                                  Teuchos::RCP<Epetra_Vector> xg,
+                                                  Teuchos::RCP<Epetra_Vector> bi,
+                                                  Teuchos::RCP<Epetra_Vector> bg,
+                                                  double omega,
+                                                  int itenum) const
+{
+  Epetra_Time tr(Comm());
+  if (Comm().MyPID()==0)
+    std::cout << "    richardson: " << std::flush;
+
+  Teuchos::RCP<Epetra_Vector> ti1 = Teuchos::rcp(new Epetra_Vector(bi->Map()));
+  Teuchos::RCP<Epetra_Vector> ti2 = Teuchos::rcp(new Epetra_Vector(bi->Map()));
+  Teuchos::RCP<Epetra_Vector> tg1 = Teuchos::rcp(new Epetra_Vector(bg->Map()));
+  Teuchos::RCP<Epetra_Vector> tg2 = Teuchos::rcp(new Epetra_Vector(bg->Map()));
+
+  for (int i=0; i<itenum; ++i)
+  {
+    Aii.Multiply(false,*xi,*ti1);
+    Aig.Multiply(false,*xg,*ti2);
+    Agi.Multiply(false,*xi,*tg1);
+    Agg.Multiply(false,*xg,*tg2);
+
+    ti1->Update(-1.0,*ti2,1.0,*bi,-1.0);
+    tg1->Update(-1.0,*tg2,1.0,*bg,-1.0);
+
+    if (i%10==0)
+    {
+      double n1,n2;
+      ti1->Norm2(&n1);
+      ti2->Norm2(&n2);
+      if (Comm().MyPID()==0)
+        std::cout << " " << sqrt(n1*n1+n2*n2);
+    }
+
+    Sii->Solve(Aii.EpetraMatrix(),ti2,ti1,true);
+    Agi.Multiply(false,*ti2,*tg2);
+    tg1->Update(-1.0,*tg2,1.0);
+    Sgg->Solve(Agg.EpetraMatrix(),tg2,tg1,true);
+
+    xi->Update(omega,*ti2,1.0);
+    xg->Update(omega,*tg2,1.0);
+
+    if (Comm().MyPID()==0)
+      std::cout << "." << std::flush;
+  }
+
+  if (Comm().MyPID()==0)
+    std::cout << " " << tr.ElapsedTime() << std::flush;
 }
 
 
