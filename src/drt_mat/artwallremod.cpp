5fb6511c2b4fd5020727c32ac46d402a32b60f91
Moritz Frenzel <frenzel@lnm.mw.tum.de>
2008-07-14 16:16:54 +0000

    Aniso Holzapfel model with constant fibers. Testing follows.
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@6840 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_mat/artwallremod.cpp
+++ src/drt_mat/artwallremod.cpp
@@ -191,6 +191,129 @@ void MAT::ArtWallRemod::Evaluate(const Epetra_SerialDenseVector* glstrain,
                                   int eleId)
 
 {
+  const double mue = matdata_->m.artwallremod->mue;
+  const double kappa = matdata_->m.artwallremod->kappa;
+  const double k1 = matdata_->m.artwallremod->k1;
+  const double k2 = matdata_->m.artwallremod->k2;
+  
+  // right Cauchy-Green Tensor  C = 2 * E + I
+  // build identity tensor I
+  Epetra_SerialDenseVector Id(6);
+  for (int i = 0; i < 3; i++) Id(i) = 1.0;
+  Epetra_SerialDenseVector C(*glstrain);
+  C.Scale(2.0);
+  C += Id;
+  
+  // invariants
+  const double I1 = C(0) + C(1) + C(2);  // 1st invariant, trace
+  const double I3 = C(0)*C(1)*C(2)
+        + 0.25 * C(3)*C(4)*C(5)
+        - 0.25 * C(1)*C(5)*C(5)
+        - 0.25 * C(2)*C(3)*C(3)
+        - 0.25 * C(0)*C(4)*C(4);    // 3rd invariant, determinant
+  const double J = sqrt(I3);
+  const double I3invcubroot = pow(I3,-1.0/3.0);
+  
+  // invert C
+  Epetra_SerialDenseVector Cinv(6);
+
+  Cinv(0) = C(1)*C(2) - 0.25*C(4)*C(4);
+  Cinv(1) = C(0)*C(2) - 0.25*C(5)*C(5);
+  Cinv(2) = C(0)*C(1) - 0.25*C(3)*C(3);
+  Cinv(3) = 0.25*C(5)*C(4) - 0.5*C(3)*C(2);
+  Cinv(4) = 0.25*C(3)*C(5) - 0.5*C(0)*C(4);
+  Cinv(5) = 0.25*C(3)*C(4) - 0.5*C(5)*C(1);
+  Cinv.Scale(1.0/I3);
+
+  // isotropic part: NeoHooke  ************************************************
+  // NeoHooke with penalty W = W^dev(C) + U(J) 
+  // W = 1/2 mue (^I1-3) + 1/2 kappa (J-1)^2
+
+  // Volumetric part of PK2 stress
+  Epetra_SerialDenseVector SVol(Cinv);
+  SVol.Scale(kappa*(J-1.0)*J);
+  *stress+=SVol;
+
+  // Deviatoric elastic part (2 d W^dev/d C)
+  Epetra_SerialDenseVector SDev(Cinv);
+  SDev.Scale(-1.0/3.0*I1);
+  SDev+=Id;
+  SDev.Scale(mue*I3invcubroot);  //mue*I3^(-1/3) (Id-1/3*I1*Cinv)
+  *stress+=SDev;
+
+  // elasticity matrix
+  double scalar1 = 2.0*kappa*J*J - kappa*J;
+  double scalar2 = -2.0*kappa*J*J + 2.0*kappa*J;
+  double scalar3 = 2.0/3.0*mue*I3invcubroot*I1;
+  double scalar4 = 2.0/3.0*mue*I3invcubroot;
+
+  // add scalar2 Cinv o Cinv (see Holzapfel p. 254)
+  AddtoCmatHolzapfelProduct((*cmat),Cinv,scalar2);
+  for (int i=0; i<6; ++i)
+  {
+     for (int j=0; j<6; ++j)
+     {
+       // add volumetric elastic part 2
+       (*cmat)(i,j) += scalar1 * Cinv(i) * Cinv(j) // add scalar Cinv x Cinv
+       // add visco-elastic deviatoric part 2
+           + (-scalar4)*Id(i)*Cinv(j)// add scalar Id x Cinv
+           + (-scalar4)*Id(j)*Cinv(i)// add scalar Cinv x Id
+           + (scalar3)*Cinv(i)*Cinv(j)/3.0;// add scalar Cinv x Cinv
+     }
+  }
+  // anisotropic part: ***************************************************
+  // W_aniso=(k1/(2.0*k2))*(exp(k2*pow((I_{5,6} - 1.0),2)-1.0)); fiber SEF
+  
+  // fiber directions
+  vector<double> a1(3,0.0);
+  vector<double> a2(3,0.0);
+  
+  // structural tensors in voigt-notation
+  Epetra_SerialDenseVector A1(6);
+  Epetra_SerialDenseVector A2(6);
+  for (int i = 0; i < 3; ++i) {
+    A1(i) = a1[i]*a1[i];
+    A2(i) = a2[i]*a2[i];
+  }
+  A1(3) = a1[0]*a1[1]; A1(4) = a1[1]*a1[2]; A1(5) = a1[0]*a1[2];
+  A2(3) = a2[0]*a2[1]; A2(4) = a2[1]*a2[2]; A2(5) = a2[0]*a2[2];
+  
+  // modified (fiber-) invariants
+  double I4 = 0.0;  // I4 = A1:C^dev
+  double I6 = 0.0;  // I6 = A2:C^dev
+  for (int i=0; i<6; ++i){
+    for (int j = 0; j < 6; ++j) {
+      I4 +=  I3invcubroot*C(i) * A1(j);
+      I6 +=  I3invcubroot*C(i) * A2(j);
+    }
+  }
+  
+  // scalars
+  const double expo1 = k2 * (I4 - 1.0) * (I4 - 1.0);
+  const double dWdI4 = I3invcubroot * k1 * exp(expo1);
+  const double expo2 = k2 * (I6 - 1.0) * (I6 - 1.0);
+  const double dWdI6 = I3invcubroot * k1 * exp(expo2);
+  
+  // add anisotropic PK2 stress
+  Epetra_SerialDenseVector Saniso(A1);
+  Saniso.Scale(dWdI4);
+  *stress += Saniso;
+  Saniso = A2;
+  Saniso.Scale(dWdI6);
+  *stress += Saniso;
+  
+  // add anistropic elasticity
+  const double scalar = I3invcubroot*I3invcubroot * k1 *2*k2;
+  for (int i=0; i<6; ++i){
+     for (int j=0; j<6; ++j){
+       // add volumetric elastic part 2
+       (*cmat)(i,j) += scalar * exp(expo1) * Cinv(i) * A1(j)  // add d2WdI4 Cinv x A1
+                    +  scalar * exp(expo1) * Cinv(j) * A1(i)  // add d2WdI4 A1 x Cinv
+                    +  scalar * exp(expo2) * Cinv(i) * A2(j)  // add d2WdI6 Cinv x A2
+                    +  scalar * exp(expo2) * Cinv(j) * A2(i); // add d2WdI6 A2 x Cinv
+     }
+  }
+
   return;
 }
 
