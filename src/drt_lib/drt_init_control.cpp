1e95cd647243b326406f28c7e7131d3048206a1b
Ulrich Kuettler <kuettler@lnm.mw.tum.de>
2008-08-06 11:52:28 +0000

    fix error file creation
    Remember: Do not commit to drt_lib without permission. Not even accidentally!
    
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@7282 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_lib/drt_init_control.cpp
+++ src/drt_lib/drt_init_control.cpp
@@ -98,7 +98,7 @@ void ntaini_ccadiscret(int argc, char** argv)
 
   sprintf(allfiles.outputfile_name, "%s%d.err",
           allfiles.outputfile_kenner, myrank);
-  if ((allfiles.out_err = fopen("/dev/null","w"))==NULL)
+  if ((allfiles.out_err = fopen(allfiles.outputfile_name,"w"))==NULL)
   {
     printf("Opening of output file .err failed\n");
 #ifdef PARALLEL
