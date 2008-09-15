7260b8af0c5632b188494fa6b00f22b633f191e1
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2008-09-15 07:24:32 +0000

    Copied geometry 3 related functions from drt_utils, which have been put there long ago when no better place was found. Now we have src/drt_geometry ...
    All source that relies on these 3 functions will be changed in a minute and eventually drt_fem_general and drt_geometry will not depend on drt_utils.H anymore
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@7702 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_geometry/intersection_service.cpp
+++ src/drt_geometry/intersection_service.cpp
@@ -20,7 +20,6 @@ Maintainer: Ursula Mayer
 #include "../drt_fem_general/drt_utils_fem_shapefunctions.H"
 #include "../drt_fem_general/drt_utils_local_connectivity_matrices.H"
 #include "../drt_lib/drt_element.H"
-#include "../drt_lib/drt_utils.H"
 
 
 
@@ -1100,6 +1099,55 @@ void GEO::CurrentToLineElementCoordinates(
         default:
             dserror("please add your line element type here");
     }
+}
+
+
+/*!
+ * \brief create an often used array with 3D nodal positions (Blitz array)
+ */
+BlitzMat GEO::InitialPositionArrayBlitz(
+        const DRT::Element* ele
+        )
+{
+    const int numnode = ele->NumNode();
+    BlitzMat xyze(3,numnode,blitz::ColumnMajorArray<2>());
+    const DRT::Node*const* nodes = ele->Nodes();
+    if (nodes == NULL)
+    {
+        dserror("element has no nodal pointers, so getting a position array doesn't make sense!");
+    }
+    for (int inode=0; inode<numnode; inode++)
+    {
+        const double* x = nodes[inode]->X();
+        xyze(0,inode) = x[0];
+        xyze(1,inode) = x[1];
+        xyze(2,inode) = x[2];
+    }
+    return xyze;
+}
+
+
+/*!
+\brief  fill array with current nodal positions
+
+\return array with element nodal positions (3,numnode)
+*/
+BlitzMat GEO::getCurrentNodalPositions(
+    const DRT::Element*         ele,                      ///< element with nodal pointers
+    const map<int,BlitzVec3 >&  currentcutterpositions    ///< current positions of all cutter nodes
+    )
+{
+    const int numnode = ele->NumNode();
+    BlitzMat xyze(3,numnode);
+    const int* nodeids = ele->NodeIds();
+    for (int inode = 0; inode < numnode; ++inode)
+    {
+      const BlitzVec3 x = currentcutterpositions.find(nodeids[inode])->second;
+      xyze(0,inode) = x(0);
+      xyze(1,inode) = x(1);
+      xyze(2,inode) = x(2);
+    }
+    return xyze;
 }
 
 
