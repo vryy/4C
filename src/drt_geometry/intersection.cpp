61a3aace59d484c1ed63acd477b197050bd200b3
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2008-09-24 07:17:50 +0000

    remove Y switch yet again since it works for many, but not for all situations as the buildbot happily pointed out last night. Sorry for the confusion and noise. ---
    instead of ./run_test reldrt ...
    I did
    instead of ./run_test rel ...
    and thought all tests were fine. :-(
    
    12 Kudos to the BuildBot for reminding me!
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@7839 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_geometry/intersection.cpp
+++ src/drt_geometry/intersection.cpp
@@ -1558,7 +1558,7 @@ void GEO::Intersection::computeCDT(
   const int dim = 3; 
   tetgenio in;
   tetgenio out;
-  char switches[] = "pnnQY";    //o2 Y R
+  char switches[] = "pnnQ";    //o2 Y R
   tetgenio::facet *f;
   tetgenio::polygon *p;
 
