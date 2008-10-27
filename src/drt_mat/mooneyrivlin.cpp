ba643f341c0ee4fe0f667455de2154780c9f1550
Moritz Frenzel <frenzel@lnm.mw.tum.de>
2008-10-27 10:35:20 +0000

    Mooney-Rivlin transferred to FixedSize.
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@8032 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_mat/mooneyrivlin.cpp
+++ src/drt_mat/mooneyrivlin.cpp
@@ -108,46 +108,50 @@ to effectuate incompressibility (shear modulus mu = (mu_1 a_1 + mu_2 a_2)/2 )
 Mind that it is not stress-free in reference configuration!
 */
 
-void MAT::MooneyRivlin::Evaluate(const Epetra_SerialDenseVector* glstrain,
-                                       Epetra_SerialDenseMatrix* cmat,
-                                       Epetra_SerialDenseVector* stress)
+void MAT::MooneyRivlin::Evaluate(const Epetra_SerialDenseVector* glstrain_e,
+                                       Epetra_SerialDenseMatrix* cmat_e,
+                                       Epetra_SerialDenseVector* stress_e)
 
 {
-  // get material parameters
-  double m1  = matdata_->m.mooneyrivlin->mu1;
-  double m2  = matdata_->m.mooneyrivlin->mu2;
-  double c1  = m1 / matdata_->m.mooneyrivlin->alpha1;  // c1 = mu1/a1
-  double c2  = m2 / matdata_->m.mooneyrivlin->alpha2;  // c2 = mu2/a2
-  double pen = matdata_->m.mooneyrivlin->penalty;
+  // this is temporary as long as the material does not have a 
+  // FixedSizeSerialDenseMatrix-type interface
+  const LINALG::FixedSizeSerialDenseMatrix<NUM_STRESS_3D,1> glstrain(glstrain_e->A(),true);
+        LINALG::FixedSizeSerialDenseMatrix<NUM_STRESS_3D,NUM_STRESS_3D> cmat(cmat_e->A(),true);
+        LINALG::FixedSizeSerialDenseMatrix<NUM_STRESS_3D,1> stress(stress_e->A(),true);
 
-  // Identity Matrix
-  Epetra_SerialDenseMatrix I(3,3);
-  for (int i = 0; i < 3; ++i) I(i,i) = 1.0;
-
-  // Green-Lagrange Strain Tensor
-  Epetra_SerialDenseMatrix E(3,3);
-  E(0,0) = (*glstrain)(0);
-  E(1,1) = (*glstrain)(1);
-  E(2,2) = (*glstrain)(2);
-  E(0,1) = 0.5 * (*glstrain)(3);  E(1,0) = 0.5 * (*glstrain)(3);
-  E(1,2) = 0.5 * (*glstrain)(4);  E(2,1) = 0.5 * (*glstrain)(4);
-  E(0,2) = 0.5 * (*glstrain)(5);  E(2,0) = 0.5 * (*glstrain)(5);
-
-  // Right Cauchy-Green Tensor  C = 2 * E + I
-  Epetra_SerialDenseMatrix C(E);
+        // get material parameters
+  const double m1  = matdata_->m.mooneyrivlin->mu1;
+  const double m2  = matdata_->m.mooneyrivlin->mu2;
+  const double c1  = m1 / matdata_->m.mooneyrivlin->alpha1;  // c1 = mu1/a1
+  const double c2  = m2 / matdata_->m.mooneyrivlin->alpha2;  // c2 = mu2/a2
+  const double pen = matdata_->m.mooneyrivlin->penalty;
+  
+  // right Cauchy-Green Tensor  C = 2 * E + I
+  // build identity tensor I
+  LINALG::FixedSizeSerialDenseMatrix<NUM_STRESS_3D,1> Id(true);
+  for (int i = 0; i < 3; i++) Id(i) = 1.0;
+  LINALG::FixedSizeSerialDenseMatrix<NUM_STRESS_3D,1> C(glstrain);
   C.Scale(2.0);
-  C += I;
+  C += Id;
 
-  // compute Invariants
-  double I1 = C(0,0) + C(1,1) + C(2,2);    // I1 = trace(C)
+  // invariants
+  const double I1 = C(0) + C(1) + C(2);  // 1st invariant, trace
+  const double I3 = C(0)*C(1)*C(2)
+        + 0.25 * C(3)*C(4)*C(5)
+        - 0.25 * C(1)*C(5)*C(5)
+        - 0.25 * C(2)*C(3)*C(3)
+        - 0.25 * C(0)*C(4)*C(4);    // 3rd invariant, determinant
 
-  // compute determinant of C by Sarrus' rule
-  double I3  = C(0,0) * C(1,1) * C(2,2)    // I3 = det(C)
-             + C(0,1) * C(1,2) * C(2,0)
-             + C(0,2) * C(1,0) * C(2,1)
-             - C(0,0) * C(1,2) * C(2,1)
-             - C(0,1) * C(1,0) * C(2,2)
-             - C(0,2) * C(1,1) * C(2,0);
+  // invert C
+  LINALG::FixedSizeSerialDenseMatrix<NUM_STRESS_3D,1> Cinv(false);
+
+  Cinv(0) = C(1)*C(2) - 0.25*C(4)*C(4);
+  Cinv(1) = C(0)*C(2) - 0.25*C(5)*C(5);
+  Cinv(2) = C(0)*C(1) - 0.25*C(3)*C(3);
+  Cinv(3) = 0.25*C(5)*C(4) - 0.5*C(3)*C(2);
+  Cinv(4) = 0.25*C(3)*C(5) - 0.5*C(0)*C(4);
+  Cinv(5) = 0.25*C(3)*C(4) - 0.5*C(5)*C(1);
+  Cinv.Scale(1.0/I3);
 
   /* //Compute strain-energy function W
   // I2 = 1/2 (trace(C)^2 - trace(CxC))
@@ -158,34 +162,17 @@ void MAT::MooneyRivlin::Evaluate(const Epetra_SerialDenseVector* glstrain,
   double W = c1*(I1-3.0) + c2*(I2-3.0) - m1*log(J) - m2*log(J) + 0.25*pen*(J*J -1.0 -2.0*log(J));
   */
 
-  // compute C^-1
-  Epetra_SerialDenseMatrix Cinv(C);
-  Epetra_SerialDenseSolver solve_for_inverseC;
-  solve_for_inverseC.SetMatrix(Cinv);
-  int err2 = solve_for_inverseC.Factor();
-  int err = solve_for_inverseC.Invert();
-  if ((err != 0) && (err2!=0)) dserror("Inversion of Cauchy-Green failed");
 
   // ******* evaluate 2nd PK stress ********************
-  Epetra_SerialDenseMatrix S(C);    // S = C
-  double scalar1 = -2.0 * c2;       // -2 dW/dI2
-  S.Scale(scalar1);
-
+  double scalar1 = -2.0 * c2;     // -2 dW/dI2
+  stress.Update(scalar1,C);       // S = -2 dW/dI2 C
+  
   // S += 2 (dW/dI1 + I1 dW/dI2) times Identity
   double scalar2 = 2.0 * (c1 + I1*c2);
-  for (int i = 0; i < 3; ++i) S(i,i) += scalar2;
+  stress.Update(scalar2,Id);
 
-  Epetra_SerialDenseMatrix Cinvscale(Cinv);
   double scalar3 = - m1 - m2 + 0.5*I3*pen - 0.5*pen;   // 2 I3 dW/dI3
-  Cinvscale.Scale(scalar3);
-  S += Cinvscale;
-
-  (*stress)(0) = S(0,0);
-  (*stress)(1) = S(1,1);
-  (*stress)(2) = S(2,2);
-  (*stress)(3) = S(0,1);
-  (*stress)(4) = S(1,2);
-  (*stress)(5) = S(0,2);
+  stress.Update(scalar3,Cinv);    // S += 2 I3 dW/dI3 Cinv
   // end of ******* evaluate 2nd PK stress ********************
 
   // ********** evaluate C-Matrix *****************************
@@ -194,10 +181,16 @@ void MAT::MooneyRivlin::Evaluate(const Epetra_SerialDenseVector* glstrain,
   double delta7 = 2.0*m1 + 2.0*m2 - pen*I3 + pen;
   double delta8 = - 4.0*c2;
 
-  ElastSymTensorMultiply((*cmat),delta1,I,I,0.0);  // initialize cmat and add d1 I x I
-  ElastSymTensorMultiply((*cmat),delta6,Cinv,Cinv,1.0);    // add d6 Cinv x Cinv
-  ElastSymTensor_o_Multiply((*cmat),delta7,Cinv,Cinv,1.0); // add d7 Cinv o Cinv
-  ElastSymTensor_o_Multiply((*cmat),delta8,I,I,1.0);       // add d8 I o I
+  
+  for (unsigned int i = 0; i < NUM_STRESS_3D; ++i) {
+    for (unsigned int j = 0; j < NUM_STRESS_3D; ++j) {
+      cmat(i,j) += delta1 * Id(i)*Id(j)         // add d1 I x I
+                 + delta6 * Cinv(i)*Cinv(j);    // add d6 Cinv x Cinv
+    }
+  }
+
+  AddtoCmatHolzapfelProduct(cmat,Cinv,delta7);  // add d7 Cinv o Cinv
+  AddtoCmatHolzapfelProduct(cmat,Id,delta8);    // add d8 I o I
   // end of ********** evaluate C-Matrix *****************************
 
 
