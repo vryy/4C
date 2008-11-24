4c3ed83f2f46fde7a50a5673f69a716b9201ee02
Ursula Mayer <mayer@lnm.mw.tum.de>
2008-11-24 14:36:20 +0000

    forgot file
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@8360 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_structure/stru_dyn_nln_drt.cpp
+++ src/drt_structure/stru_dyn_nln_drt.cpp
@@ -35,7 +35,6 @@ Maintainer: Michael Gee
 
 #include "../drt_inv_analysis/inv_analysis.H"
 #include "../drt_statmech/statmech_time.H"
-#include "../drt_statmech/bromotion_timeint.H"
 
 
 /*----------------------------------------------------------------------*
@@ -108,7 +107,6 @@ void dyn_nlnstructural_drt()
   const Teuchos::ParameterList& scontact = DRT::Problem::Instance()->StructuralContactParams();
   const Teuchos::ParameterList& statmech = DRT::Problem::Instance()->StatisticalMechanicsParams();
   const Teuchos::ParameterList& iap      = DRT::Problem::Instance()->InverseAnalysisParams();
-  const Teuchos::ParameterList& bromop   = DRT::Problem::Instance()->BrownianMotionParams();
 
   if (actdis->Comm().MyPID()==0)
     DRT::INPUT::PrintDefaultParameters(std::cout, sdyn);
@@ -308,15 +306,11 @@ void dyn_nlnstructural_drt()
           dserror("Cannot cope with choice of thermal bath");
           break;
       }
-     
-      // brownain motion
-      genalphaparams.set<bool>  ("bro_motion",Teuchos::getIntegralValue<int>(bromop,"BROWNIAN_MOTION"));
-      bool bromotion = genalphaparams.get("bro_motion",false);
       
       // create the time integrator
       bool inv_analysis = genalphaparams.get("inv_analysis",false);
       RCP<StruGenAlpha> tintegrator;
-      if (!contact && !inv_analysis && !thermalbath && !bromotion)
+      if (!contact && !inv_analysis && !thermalbath)
         tintegrator = rcp(new StruGenAlpha(genalphaparams,*actdis,solver,output));
       else
       {
@@ -326,8 +320,6 @@ void dyn_nlnstructural_drt()
           tintegrator = rcp(new Inv_analysis(genalphaparams,*actdis,solver,output));
         if (thermalbath)
           tintegrator = rcp(new StatMechTime(genalphaparams,*actdis,solver,output));
-        if (bromotion)
-          tintegrator = rcp(new BroMotion_TimeInt(genalphaparams,*actdis,solver,output));
       }
 
       // do restart if demanded from input file
