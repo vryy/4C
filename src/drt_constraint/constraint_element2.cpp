524c1d46a5791920f51a71595a466310299ebedc
Thomas Kloeppel <kloeppel@lnm.mw.tum.de>
2008-12-16 13:08:35 +0000

    Split ConstraintElement class into to classes ConstraintElement2 and ConstraintElement3. There is no need to have a generalized class anymore. This elements are only used by the multi point constraints that have been splitted into a 2d and a 3d implementation a while ago. We save a couple of 'if'-statements and methods. Structure of element routines looks better now.
    -forgot this new files
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@8591 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/drt_constraint/constraint_element2.cpp
@@ -0,0 +1,208 @@
+/*!----------------------------------------------------------------------
+\file constraint_element.cpp
+\brief
+
+<pre>
+Maintainer: Thomas Kloeppel
+            kloeppel@lnm.mw.tum.de
+            http://www.lnm.mw.tum.de
+            089 - 289-15257
+</pre>
+
+*----------------------------------------------------------------------*/
+#ifdef CCADISCRET
+
+#include "constraint_element2.H"
+#include "../drt_lib/drt_discret.H"
+#include "../drt_lib/drt_utils.H"
+#include "../drt_lib/drt_dserror.H"
+#include "../drt_lib/drt_utils.H"
+
+using namespace DRT::UTILS;
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+DRT::ELEMENTS::ConstraintElement2::ConstraintElement2(int id, int owner) :
+DRT::Element(id, element_constraintelement2, owner),
+data_()
+{
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+DRT::ELEMENTS::ConstraintElement2::ConstraintElement2(const DRT::ELEMENTS::ConstraintElement2& old) :
+DRT::Element(old),
+data_(old.data_)
+{
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+DRT::Element* DRT::ELEMENTS::ConstraintElement2::Clone() const
+{
+  DRT::ELEMENTS::ConstraintElement2* newelement = new DRT::ELEMENTS::ConstraintElement2(*this);
+  return newelement;
+}
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+void DRT::ELEMENTS::ConstraintElement2::Pack(vector<char>& data) const
+{
+  data.resize(0);
+
+  // pack type of this instance of ParObject
+  int type = UniqueParObjectId();
+  AddtoPack(data,type);
+  // add base class Element
+  vector<char> basedata(0);
+  Element::Pack(basedata);
+  AddtoPack(data,basedata);
+
+  // data_
+  vector<char> tmp(0);
+  data_.Pack(tmp);
+  AddtoPack(data,tmp);
+
+  return;
+}
+
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+void DRT::ELEMENTS::ConstraintElement2::Unpack(const vector<char>& data)
+{
+  int position = 0;
+  // extract type
+  int type = 0;
+  ExtractfromPack(position,data,type);
+  if (type != UniqueParObjectId()) dserror("wrong instance type data");
+  // extract base class Element
+  vector<char> basedata(0);
+  ExtractfromPack(position,data,basedata);
+  Element::Unpack(basedata);
+
+  // data_
+  vector<char> tmp(0);
+  ExtractfromPack(position,data,tmp);
+  data_.Unpack(tmp);
+
+  if (position != (int)data.size())
+    dserror("Mismatch in size of data %d <-> %d",(int)data.size(),position);
+  return;
+}
+
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+DRT::ELEMENTS::ConstraintElement2::~ConstraintElement2()
+{
+  return;
+}
+
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+void DRT::ELEMENTS::ConstraintElement2::Print(ostream& os) const
+{
+  os << "ConstraintElement2 ";
+  Element::Print(os);
+  cout << endl;
+  cout << data_;
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+RefCountPtr<DRT::ElementRegister> DRT::ELEMENTS::ConstraintElement2::ElementRegister() const
+{
+  return rcp(new DRT::ELEMENTS::ConstraintElement2Register(Type()));
+}
+
+
+
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+DRT::ELEMENTS::ConstraintElement2Register::ConstraintElement2Register(DRT::Element::ElementType etype) :
+ElementRegister(etype)
+{
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+DRT::ELEMENTS::ConstraintElement2Register::ConstraintElement2Register(
+                               const DRT::ELEMENTS::ConstraintElement2Register& old) :
+ElementRegister(old)
+{
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+DRT::ELEMENTS::ConstraintElement2Register* DRT::ELEMENTS::ConstraintElement2Register::Clone() const
+{
+  return new DRT::ELEMENTS::ConstraintElement2Register(*this);
+}
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+void DRT::ELEMENTS::ConstraintElement2Register::Pack(vector<char>& data) const
+{
+  data.resize(0);
+
+  // pack type of this instance of ParObject
+  int type = UniqueParObjectId();
+  AddtoPack(data,type);
+  // add base class ElementRegister
+  vector<char> basedata(0);
+  ElementRegister::Pack(basedata);
+  AddtoPack(data,basedata);
+
+  return;
+}
+
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+void DRT::ELEMENTS::ConstraintElement2Register::Unpack(const vector<char>& data)
+{
+  int position = 0;
+  // extract type
+  int type = 0;
+  ExtractfromPack(position,data,type);
+  if (type != UniqueParObjectId()) dserror("wrong instance type data");
+  // base class ElementRegister
+  vector<char> basedata(0);
+  ExtractfromPack(position,data,basedata);
+  ElementRegister::Unpack(basedata);
+
+  if (position != (int)data.size())
+    dserror("Mismatch in size of data %d <-> %d",(int)data.size(),position);
+  return;
+}
+
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+DRT::ELEMENTS::ConstraintElement2Register::~ConstraintElement2Register()
+{
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+void DRT::ELEMENTS::ConstraintElement2Register::Print(ostream& os) const
+{
+  os << "ConstraintElement2Register ";
+  ElementRegister::Print(os);
+  return;
+}
+
+
+
+
+
+#endif  // #ifdef CCADISCRET
