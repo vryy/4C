a1c62861b80a66c3650812873a42beb34d946919
Georg Bauer <bauer@lnm.mw.tum.de>
2009-03-13 08:52:15 +0000

    support for line3 elements + removed some tabs
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@8996 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/post_ensight/post_drt_ensight_writer.cpp
+++ src/post_ensight/post_drt_ensight_writer.cpp
@@ -61,6 +61,7 @@ EnsightWriter::EnsightWriter(PostField* field,
   // you need to manually switch to other types distypes before querying this map
   distype2ensightstring_.clear();
   distype2ensightstring_[DRT::Element::line2] = "bar2";
+  distype2ensightstring_[DRT::Element::line3] = "bar3";
   distype2ensightstring_[DRT::Element::hex8] = "hexa8";
   distype2ensightstring_[DRT::Element::hex20] = "hexa20";
   distype2ensightstring_[DRT::Element::tet4] = "tetra4";
@@ -384,6 +385,7 @@ void EnsightWriter::WriteCells(
         switch (actele->Shape())
         {
         case DRT::Element::line2:
+        case DRT::Element::line3:
         case DRT::Element::hex8:
         case DRT::Element::quad4:
         case DRT::Element::quad8:
