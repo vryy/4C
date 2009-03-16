657c3dc4e600d9fb078c09f04b1805c6ba739003
Michael Gee <gee@lnm.mw.tum.de>
2009-03-16 17:56:08 +0000

    progress on monolithic FSIAMG - extracted all AMG hierarchy components for the three fields and wrapped everything in MLAPI operators -> AMG building blocks for all levels and all fields are available.
    Reproduced already existing Uli's block Gauss-Seidel with AMG main diagonal block inverses exclusively in MLAPI using its own V-cycle -> convergence is identical
    
    tidy up of everything, tested all smoothers that can be configured from the input file
    
    work in progress....
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@9007 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_fsi/fsi_overlapprec_fsiamg.cpp
+++ src/drt_fsi/fsi_overlapprec_fsiamg.cpp
@@ -5,12 +5,12 @@
 #include <ml_MultiLevelPreconditioner.h>
 #include "MLAPI_Operator_Utils.h"
 //#include "MLAPI_Error.h"
-//#include "MLAPI_CompObject.h"
+#include "MLAPI_CompObject.h"
 //#include "MLAPI_TimeObject.h"
-//#include "MLAPI_MultiVector.h"
-//#include "MLAPI_Expressions.h"
+#include "MLAPI_MultiVector.h"
+#include "MLAPI_Expressions.h"
 //#include "MLAPI_BaseOperator.h"
-//#include "MLAPI_Workspace.h"
+#include "MLAPI_Workspace.h"
 //#include "MLAPI_Aggregation.h"
 //#include "MLAPI_Eig.h"
 
@@ -97,7 +97,6 @@ void FSI::OverlappingBlockMatrixFSIAMG::SetupPreconditioner()
   bool SisPetrovGalerkin = sparams_.get("energy minimization: enable",false);
   bool FisPetrovGalerkin = fparams_.get("energy minimization: enable",false);
   bool AisPetrovGalerkin = aparams_.get("energy minimization: enable",false);
-  if (AisPetrovGalerkin) dserror("MLFLUID2 hierarchy not supported for Ale, choose ML");
 
   // get ML handle
   ML* sml = const_cast<ML*>(smlclass->GetML());
@@ -131,6 +130,7 @@ void FSI::OverlappingBlockMatrixFSIAMG::SetupPreconditioner()
   
   Aaa_.resize(anlevel_);
   Paa_.resize(anlevel_-1);
+  Raa_.resize(anlevel_-1);
   Saa_.resize(anlevel_);
 
   //------------------------------------------------------- Structure
@@ -159,9 +159,7 @@ void FSI::OverlappingBlockMatrixFSIAMG::SetupPreconditioner()
         R.Reshape(fspace,cspace,Rml,false);
       }
       else 
-      {
         R = MLAPI::GetTranspose(P);
-      }
       Pss_[i-1] = P;
       Rss_[i-1] = R;
     }
@@ -231,6 +229,7 @@ void FSI::OverlappingBlockMatrixFSIAMG::SetupPreconditioner()
     MLAPI::Space fspace;
     MLAPI::Space cspace;
     MLAPI::Operator P;
+    MLAPI::Operator R;
     for (int i=1; i<anlevel_; ++i)
     {
       ML_Operator* Pml = &(aml->Pmat[i]);
@@ -239,7 +238,17 @@ void FSI::OverlappingBlockMatrixFSIAMG::SetupPreconditioner()
       else      fspace.Reshape(-1,Pml->outvec_leng);
       cspace.Reshape(-1,Pml->invec_leng);
       P.Reshape(cspace,fspace,Pml,false);
+      if (AisPetrovGalerkin)
+      {
+        ML_Operator* Rml = &(aml->Rmat[i-1]);
+        if (!myrank) printf("Ale R: level %d domain %d range %d\n",i,Rml->invec_leng,Rml->outvec_leng);
+        R.Reshape(fspace,cspace,Rml,false);
+      }
+      else
+        R = MLAPI::GetTranspose(P);
+        
       Paa_[i-1] = P;
+      Raa_[i-1] = R;
     }
     // extract matrix A from ML
     MLAPI::Space space;
@@ -281,7 +290,7 @@ void FSI::OverlappingBlockMatrixFSIAMG::SetupPreconditioner()
     //cout << "*********************level " << i << " sublist:\n" << subp;
     string type = "";
     SelectMLAPISmoother(type,subp,p);
-    S.Reshape(Ass_[i],type,p);
+    S.Reshape(Aff_[i],type,p);
     Sff_[i] = S;
   }
   // coarse grid:
@@ -311,32 +320,26 @@ void FSI::OverlappingBlockMatrixFSIAMG::SetupPreconditioner()
   const LINALG::SparseMatrix& fluidMeshOp   = Matrix(1,2); // Afa
   const LINALG::SparseMatrix& aleBoundOp    = Matrix(2,1); // Aaf
 
-  MLAPI::Operator  Asf;
   {
     MLAPI::Space dspace(structBoundOp.EpetraMatrix()->DomainMap());
     MLAPI::Space rspace(structBoundOp.EpetraMatrix()->RangeMap());
-    Asf.Reshape(dspace,rspace,structBoundOp.EpetraMatrix().get(),false);
+    Asf_.Reshape(dspace,rspace,structBoundOp.EpetraMatrix().get(),false);
   }
-  MLAPI::Operator  Afs;
   {
     MLAPI::Space dspace(fluidBoundOp.EpetraMatrix()->DomainMap());
     MLAPI::Space rspace(fluidBoundOp.EpetraMatrix()->RangeMap());
-    Afs.Reshape(dspace,rspace,fluidBoundOp.EpetraMatrix().get(),false);
+    Afs_.Reshape(dspace,rspace,fluidBoundOp.EpetraMatrix().get(),false);
   }
-  MLAPI::Operator  Afa;
   {
     MLAPI::Space dspace(fluidMeshOp.EpetraMatrix()->DomainMap());
     MLAPI::Space rspace(fluidMeshOp.EpetraMatrix()->RangeMap());
-    Afa.Reshape(dspace,rspace,fluidMeshOp.EpetraMatrix().get(),false);
+    Afa_.Reshape(dspace,rspace,fluidMeshOp.EpetraMatrix().get(),false);
   }
-  MLAPI::Operator  Aaf;
   {
     MLAPI::Space dspace(aleBoundOp.EpetraMatrix()->DomainMap());
     MLAPI::Space rspace(aleBoundOp.EpetraMatrix()->RangeMap());
-    Aaf.Reshape(dspace,rspace,aleBoundOp.EpetraMatrix().get(),false);
+    Aaf_.Reshape(dspace,rspace,aleBoundOp.EpetraMatrix().get(),false);
   }
-  exit(0);
-  
 
   return;
 }
@@ -359,8 +362,8 @@ void FSI::OverlappingBlockMatrixFSIAMG::SelectMLAPISmoother(std::string& type,
   }
   else if (type=="IFPACK")
   {
-    type              = subp.get("smoother: ifpack type","ILU");
-    const int lof     = subp.get("smoother: ifpack level-of-fill",0);
+    type             = subp.get("smoother: ifpack type","ILU");
+    const double lof = subp.get<double>("smoother: ifpack level-of-fill",0);
     p.set("fact: level-of-fill",lof);
   }
   else if (type=="MLS")
@@ -371,19 +374,64 @@ void FSI::OverlappingBlockMatrixFSIAMG::SelectMLAPISmoother(std::string& type,
   else if (type=="Amesos-KLU"); // nothing to do
   else if (type=="Amesos-Superludist") dserror("No SuperLUDist support in MLAPI");
   else dserror("Smoother not recognized");
+  p.set("relaxation: zero starting solution", false);
   return;
 }                                                            
 
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+void FSI::OverlappingBlockMatrixFSIAMG::Vcycle(const int level,
+                                               const int nlevel,
+                                               MLAPI::MultiVector& z,
+                                               const MLAPI::MultiVector& b,
+                                               const vector<MLAPI::Operator>& A,
+                                               const vector<MLAPI::InverseOperator>& S,
+                                               const vector<MLAPI::Operator>& P,
+                                               const vector<MLAPI::Operator>& R) const
+{
+  // coarse solve
+  if (level==nlevel-1)
+  {
+    z = S[level] * b;
+    return;
+  }
+  
+  // presmoothing
+  //z = 0.0;
+  S[level].Apply(b,z);
+  
+  // coarse level residual and correction
+  MLAPI::MultiVector bc(P[level].GetDomainSpace(),1,false);
+  MLAPI::MultiVector zc(P[level].GetDomainSpace(),1,true);
+  
+  // compute residual and restrict to coarser level
+  bc = R[level] * ( b - A[level] * z );
+  
+  // solve coarse problem
+  Vcycle(level+1,nlevel,zc,bc,A,S,P,R);
+  
+  // prolongate correction
+  z = z + P[level] * zc;
+  
+  // postsmoothing
+  S[level].Apply(b,z);
+
+  return;
+}                
 
 #if 1
 /*----------------------------------------------------------------------*
+MLAPI version of the identical Kuettler Block Gauss Seidel monolithic precond,
+mainly for proof of concept of using extracted MG hierarchies and to achieve
+same convergence / timings through MLAPI
  *----------------------------------------------------------------------*/
 void FSI::OverlappingBlockMatrixFSIAMG::SGS(
                  const Epetra_MultiVector &X, Epetra_MultiVector &Y) const
 {
   if (!structuresplit_) dserror("FSIAMG for structuresplit monoFSI only");
   if (symmetric_)       dserror("FSIAMG symmetric Block Gauss-Seidel not impl.");
-  
+
+/*  explains naming conventions only, not a piece of code to be used 
   const LINALG::SparseMatrix& structInnerOp = Matrix(0,0); // Ass
   const LINALG::SparseMatrix& structBoundOp = Matrix(0,1); // Asf
   const LINALG::SparseMatrix& fluidBoundOp  = Matrix(1,0); // Afs
@@ -391,10 +439,7 @@ void FSI::OverlappingBlockMatrixFSIAMG::SGS(
   const LINALG::SparseMatrix& fluidMeshOp   = Matrix(1,2); // Afa
   const LINALG::SparseMatrix& aleBoundOp    = Matrix(2,1); // Aaf
   const LINALG::SparseMatrix& aleInnerOp    = Matrix(2,2); // Aaa
-
-  const MLAPI::Operator& Ass = Ass_[0];
-  const MLAPI::Operator& Aff = Aff_[0];
-  const MLAPI::Operator& Aaa = Aaa_[0];
+*/
 
   // Extract vector blocks
   // RHS
@@ -403,17 +448,28 @@ void FSI::OverlappingBlockMatrixFSIAMG::SGS(
   // initial guess
   Epetra_Vector& y = Teuchos::dyn_cast<Epetra_Vector>(Y);
 
+  // various range and domain spaces
+  MLAPI::Space rsspace(Matrix(0,0).RangeMap());
+  MLAPI::Space rfspace(Matrix(1,1).RangeMap());
+  MLAPI::Space raspace(Matrix(2,2).RangeMap());
+
+  MLAPI::Space dsspace(Matrix(0,0).DomainMap());
+  MLAPI::Space dfspace(Matrix(1,1).DomainMap());
+  MLAPI::Space daspace(Matrix(2,2).DomainMap());
+
+  // these wrap RCP<Epetra_Vector> objects
   Teuchos::RCP<Epetra_Vector> sy = RangeExtractor().ExtractVector(y,0);
   Teuchos::RCP<Epetra_Vector> fy = RangeExtractor().ExtractVector(y,1);
   Teuchos::RCP<Epetra_Vector> ay = RangeExtractor().ExtractVector(y,2);
-
-  Teuchos::RCP<Epetra_Vector> sz = Teuchos::rcp(new Epetra_Vector(sy->Map()));
-  Teuchos::RCP<Epetra_Vector> fz = Teuchos::rcp(new Epetra_Vector(fy->Map()));
-  Teuchos::RCP<Epetra_Vector> az = Teuchos::rcp(new Epetra_Vector(ay->Map()));
-
-  Teuchos::RCP<Epetra_Vector> tmpsx = Teuchos::rcp(new Epetra_Vector(DomainMap(0)));
-  Teuchos::RCP<Epetra_Vector> tmpfx = Teuchos::rcp(new Epetra_Vector(DomainMap(1)));
-  Teuchos::RCP<Epetra_Vector> tmpax = Teuchos::rcp(new Epetra_Vector(DomainMap(2)));
+  MLAPI::MultiVector mlsy(rsspace,sy->Pointers());
+  MLAPI::MultiVector mlfy(rfspace,fy->Pointers());
+  MLAPI::MultiVector mlay(raspace,ay->Pointers());
+  
+  // these are original MLAPI objects
+  MLAPI::MultiVector mlsz(rsspace,1,true);
+  MLAPI::MultiVector mlfz(rfspace,1,true);
+  MLAPI::MultiVector mlaz(raspace,1,true);
+  
 
   // outer Richardson loop
   for (int run=0; run<iterations_; ++run)
@@ -422,68 +478,72 @@ void FSI::OverlappingBlockMatrixFSIAMG::SGS(
     Teuchos::RCP<Epetra_Vector> sx = DomainExtractor().ExtractVector(x,0);
     Teuchos::RCP<Epetra_Vector> fx = DomainExtractor().ExtractVector(x,1);
     Teuchos::RCP<Epetra_Vector> ax = DomainExtractor().ExtractVector(x,2);
+    // these wrap the above RCP<Epetra_Vector> objects
+    MLAPI::MultiVector mlsx(dsspace,sx->Pointers());
+    MLAPI::MultiVector mlfx(dfspace,fx->Pointers());
+    MLAPI::MultiVector mlax(daspace,ax->Pointers());
 
     // ----------------------------------------------------------------
-    // lower GS
+    // lower Gauss-Seidel using AMG for main diag block inverses
     {
-      if (run>0)
+      if (run)
       {
-        structInnerOp.Multiply(false,*sy,*tmpsx);
-        sx->Update(-1.0,*tmpsx,1.0);
-        structBoundOp.Multiply(false,*fy,*tmpsx);
-        sx->Update(-1.0,*tmpsx,1.0);
+        MLAPI::MultiVector tmp;
+        tmp = Ass_[0] * mlsy;
+        mlsx.Update(-1.0,tmp,1.0);
+        tmp = Asf_ * mlfy;
+        mlsx.Update(-1.0,tmp,1.0);
       }
 
       // Solve structure equations for sy with the rhs sx
-      structuresolver_->Solve(structInnerOp.EpetraMatrix(),sz,sx,true);
+      Vcycle(0,snlevel_,mlsz,mlsx,Ass_,Sss_,Pss_,Rss_);
 
-      // do Richardson iteration
-      LocalBlockRichardson(structuresolver_,structInnerOp,sx,sz,tmpsx,siterations_,somega_,err_,Comm());
-
-      if (!run) sy->Update(omega_,*sz,0.0);
-      else      sy->Update(omega_,*sz,1.0);
+      if (!run) mlsy.Update(omega_,mlsz);
+      else      mlsy.Update(omega_,mlsz,1.0);
     }
 
     {
       // Solve ale equations for ay with the rhs ax - A(I,Gamma) sy
-      if (run>0)
+      if (run)
       {
-        aleInnerOp.Multiply(false,*ay,*tmpax);
-        ax->Update(-1.0,*tmpax,1.0);
-
-        aleBoundOp.Multiply(false,*fy,*tmpax);
-        ax->Update(-1.0,*tmpax,1.0);
+        MLAPI::MultiVector tmp;
+        tmp = Aaa_[0] * mlay;
+        mlax.Update(-1.0,tmp,1.0);
+        tmp = Aaf_ * mlfy;
+        mlax.Update(-1.0,tmp,1.0);
       }
 
-      alesolver_->Solve(aleInnerOp.EpetraMatrix(),az,ax,true);
+      // solve ale block
+      Vcycle(0,anlevel_,mlaz,mlax,Aaa_,Saa_,Paa_,Raa_);
 
-      if (!run) ay->Update(omega_,*az,0.0);
-      else      ay->Update(omega_,*az,1.0);
+      if (!run) mlay.Update(omega_,mlaz);
+      else      mlay.Update(omega_,mlaz,1.0);
     }
 
     {
       // Solve fluid equations for fy with the rhs fx - F(I,Gamma) sy - F(Mesh) ay
-
-      if (run>0)
+      MLAPI::MultiVector tmp;
+      if (run)
       {
-        fluidInnerOp.Multiply(false,*fy,*tmpfx);
-        fx->Update(-1.0,*tmpfx,1.0);
+        tmp = Aff_[0] * mlfy;
+        mlfx.Update(-1.0,tmp,1.0);
       }
 
-      fluidBoundOp.Multiply(false,*sy,*tmpfx);
-      fx->Update(-1.0,*tmpfx,1.0);
-      fluidMeshOp.Multiply(false,*ay,*tmpfx);
-      fx->Update(-1.0,*tmpfx,1.0);
-      fluidsolver_->Solve(fluidInnerOp.EpetraMatrix(),fz,fx,true);
+      tmp = Afs_ * mlsy;
+      mlfx.Update(-1.0,tmp,1.0);
+      tmp = Afa_ * mlay;
+      mlfx.Update(-1.0,tmp,1.0);
 
-      LocalBlockRichardson(fluidsolver_,fluidInnerOp,fx,fz,tmpfx,fiterations_,fomega_,err_,Comm());
+      // solve fluid block
+      Vcycle(0,fnlevel_,mlfz,mlfx,Aff_,Sff_,Pff_,Rff_);
 
-      if (!run) fy->Update(omega_,*fz,0.0);
-      else      fy->Update(omega_,*fz,1.0);
+      if (!run) mlfy.Update(omega_,mlfz);
+      else      mlfy.Update(omega_,mlfz,1.0);
     }
 
   }
 
+  // Note that mlsy, mlfy, mlay are views of sy, fy, ay, respectively.
   RangeExtractor().InsertVector(*sy,0,y);
   RangeExtractor().InsertVector(*fy,1,y);
   RangeExtractor().InsertVector(*ay,2,y);
@@ -550,7 +610,7 @@ void FSI::OverlappingBlockMatrixFSIAMG::SGS(
       structuresolver_->Solve(structInnerOp.EpetraMatrix(),sz,sx,true);
 
       // do Richardson iteration
-      LocalBlockRichardson(structuresolver_,structInnerOp,sx,sz,tmpsx,siterations_,somega_,err_,Comm());
+      //LocalBlockRichardson(structuresolver_,structInnerOp,sx,sz,tmpsx,siterations_,somega_,err_,Comm());
 
       if (!run) sy->Update(omega_,*sz,0.0);
       else      sy->Update(omega_,*sz,1.0);
@@ -588,7 +648,7 @@ void FSI::OverlappingBlockMatrixFSIAMG::SGS(
       fx->Update(-1.0,*tmpfx,1.0);
       fluidsolver_->Solve(fluidInnerOp.EpetraMatrix(),fz,fx,true);
 
-      LocalBlockRichardson(fluidsolver_,fluidInnerOp,fx,fz,tmpfx,fiterations_,fomega_,err_,Comm());
+      //LocalBlockRichardson(fluidsolver_,fluidInnerOp,fx,fz,tmpfx,fiterations_,fomega_,err_,Comm());
 
       if (!run) fy->Update(omega_,*fz,0.0);
       else      fy->Update(omega_,*fz,1.0);
