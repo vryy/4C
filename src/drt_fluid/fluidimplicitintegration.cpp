044de9822da7a9ad2bb8bc89c085ee2f2741b0ca
Peter Gamnitzer <gamnitzer@lnm.mw.tum.de>
2009-04-23 14:16:18 +0000

    update krylov-space projection for MultiCorrector (coupled problems)
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@9358 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_fluid/fluidimplicitintegration.cpp
+++ src/drt_fluid/fluidimplicitintegration.cpp
@@ -1781,7 +1781,117 @@ void FLD::FluidImplicitTimeInt::MultiCorrector()
         currresidual = max(currresidual,incprenorm_L2/prenorm_L2);
         solver_.AdaptTolerance(ittol,currresidual,adaptolbetter);
       }
-      solver_.Solve(sysmat_->EpetraOperator(),incvel_,residual_,true,itnum==1);
+
+      if (project_)
+      {
+        DRT::Condition* KSPcond=discret_->GetCondition("KrylovSpaceProjection");
+
+        // in this case, we want to project out some zero pressure modes 
+        const string* definition = KSPcond->Get<string>("weight vector definition");
+        const int numdim = params_.get<int>("number of velocity degrees of freedom");
+        
+        if(*definition == "pointvalues")
+        {
+          // zero w and c
+          w_->PutScalar(0.0);
+          c_->PutScalar(0.0);
+          
+          // get pressure 
+          const vector<double>* mode = KSPcond->Get<vector<double> >("mode");
+
+          for(int rr=0;rr<numdim;++rr)
+          {
+            if(abs((*mode)[rr]>1e-14))
+            {
+              dserror("expecting only an undetermined pressure");
+            }
+          }
+
+          int predof = numdim;
+          
+          Teuchos::RCP<Epetra_Vector> presmode = velpressplitter_.ExtractCondVector(*w_);
+          
+          presmode->PutScalar((*mode)[predof]);
+
+          /* export to vector to normalize against
+          //
+          // Note that in the case of definition pointvalue based,
+          // the average pressure will vanish in a pointwise sense 
+          //
+          //    +---+
+          //     \   
+          //      +   p_i  = 0
+          //     / 
+          //    +---+
+          */
+          LINALG::Export(*presmode,*w_);
+
+          // export to vector of ones
+          presmode->PutScalar(1.0);
+          LINALG::Export(*presmode,*c_);
+        }
+        else if(*definition == "integration")
+        {
+          // zero w and c
+          w_->PutScalar(0.0);
+          c_->PutScalar(0.0);
+
+          ParameterList mode_params;
+
+          // set action for elements
+          mode_params.set("action","integrate_shape");
+
+          if (alefluid_)
+          {
+            discret_->SetState("dispnp",dispnp_);
+          }
+          
+          /* evaluate KrylovSpaceProjection condition in order to get
+          // integrated nodal basis functions w_
+          // Note that in the case of definition integration based,
+          // the average pressure will vanish in an integral sense 
+          //
+          //                    /              /                      /
+          //   /    \          |              |  /          \        |  /    \
+          //  | w_*p | = p_i * | N_i(x) dx =  | | N_i(x)*p_i | dx =  | | p(x) | dx = 0
+          //   \    /          |              |  \          /        |  \    / 
+          //                   /              /                      /  
+          */
+
+         
+          discret_->EvaluateCondition
+          (mode_params        ,
+           Teuchos::null      ,
+           Teuchos::null      ,
+           w_                 ,
+           Teuchos::null      ,
+           Teuchos::null      ,
+           "KrylovSpaceProjection");
+         
+          // get pressure 
+          const vector<double>* mode = KSPcond->Get<vector<double> >("mode");
+
+          for(int rr=0;rr<numdim;++rr)
+          {
+            if(abs((*mode)[rr]>1e-14))
+            {
+              dserror("expecting only an undetermined pressure");
+            }
+          }
+          
+          Teuchos::RCP<Epetra_Vector> presmode = velpressplitter_.ExtractCondVector(*w_);
+          
+          // export to vector of ones
+          presmode->PutScalar(1.0);
+          LINALG::Export(*presmode,*c_);
+        }
+        else
+        {
+          dserror("unknown definition of weight vector w for restriction of Krylov space");
+        }
+      }
+      
+      solver_.Solve(sysmat_->EpetraOperator(),incvel_,residual_,true,itnum==1, w_, c_, project_);
 
       solver_.ResetTolerance();
 
