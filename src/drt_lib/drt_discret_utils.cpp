15811c3bccdb04b63c0e3c08afa7e59dffecfd70
Christian Cyron <cyron@lnm.mw.tum.de>
2009-04-27 13:36:40 +0000

    updated DRT::Discretization::ComputeNullSpaceIfNecessary in order to deal with element types beam2, beam2r, torsion2, torsion3. For beam3 element only dummy implemented.
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@9369 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_lib/drt_discret_utils.cpp
+++ src/drt_lib/drt_discret_utils.cpp
@@ -140,26 +140,18 @@ void DRT::Discretization::ComputeNullSpaceIfNecessary(
   switch (ele->Type())
   {
     case DRT::Element::element_beam2:
-      numdf = 3;
-      dimns = 3;
-    break;
     case DRT::Element::element_beam2r:
       numdf = 3;
       dimns = 3;
     break;
+    case DRT::Element::element_shell8:
     case DRT::Element::element_beam3:
       numdf = 6;
       dimns = 6;
     break;
-    case DRT::Element::element_truss3:
-      numdf = 3;
-      dimns = 6;
-    break;
-    case DRT::Element::element_shell8:
-      numdf = 6;
-      dimns = 6;
-    break;
     case DRT::Element::element_wall1:
+    case DRT::Element::element_truss2:
+    case DRT::Element::element_torsion2:
       numdf = 2;
       dimns = 3;
     break;
@@ -172,6 +164,8 @@ void DRT::Discretization::ComputeNullSpaceIfNecessary(
     case DRT::Element::element_so_weg6:
     case DRT::Element::element_sodisp:
     case DRT::Element::element_so_shw6:
+    case DRT::Element::element_truss3:
+    case DRT::Element::element_torsion3:
       numdf = 3;
       dimns = 6;
     break;
@@ -286,7 +280,9 @@ void DRT::Discretization::ComputeNullSpaceIfNecessary(
       ele->Type() == DRT::Element::element_so_tet10 ||
       ele->Type() == DRT::Element::element_so_weg6 ||
       ele->Type() == DRT::Element::element_sodisp ||
-      ele->Type() == DRT::Element::element_so_shw6)
+      ele->Type() == DRT::Element::element_so_shw6 ||
+      ele->Type() == DRT::Element::element_truss3 ||
+      ele->Type() == DRT::Element::element_torsion3)
   {
     for (int i=0; i<NumMyRowNodes(); ++i)
     {
@@ -360,7 +356,8 @@ void DRT::Discretization::ComputeNullSpaceIfNecessary(
 
 
   else if (ele->Type() == DRT::Element::element_wall1 ||
-           ele->Type() == DRT::Element::element_ale2)
+           ele->Type() == DRT::Element::element_ale2 ||
+           ele->Type() == DRT::Element::element_torsion2)
   {
     for (int i=0; i<NumMyRowNodes(); ++i)
     {
@@ -393,40 +390,102 @@ void DRT::Discretization::ComputeNullSpaceIfNecessary(
   } // else if (ele->Type() == DRT::Element::element_wall1)
 
 
-   //there are three rigid body modes for beam2
+  /* the variable mode[i][j] describes how the position of a
+   * node changes with respect to the j-th degree of freedom
+   * in case that the i-th rigid body mode is applied to the
+   * structure; the structure altogether always has 3 rigid body
+   * modes in R^2 and 6 in R^3; these modes are translation in
+   * each coordinate direction, respectively, and translation
+   * around each axis, respectively. This is summed up in the
+   * following table where in the left column x,y,z denote
+   * translations in x-, y- and z-direction of a node due to
+   * the application of a rigid body mode, whereas dx,dy,dz
+   * denote increments of the node's rotational degrees of 
+   * freedom, which relate to a rotation around the x-,y-
+   * and z-axis.
+   *
+        xtrans   ytrans  ztrans   xrot       yrot       zrot
+        mode[0]  mode[1] mode[2]  mode[3]    mode[4]    mode[5]
+  -----------------------------------------------------------
+  x   |    1       0       0       0          z-z0      -y+y0
+  y   |    0       1       0      -z+z0       0          x-x0
+  z   |    0       0       1       y-y0      -x+x0       0
+  dx  |    0       0       0       1          0          0
+  dy  |    0       0       0       0          1          0
+  dz  |    0       0       0       0          0          1
+  
+  for example the first line means: a translation of a node in
+  x-direction may be caused either by a x-translation of the whole
+  structure (which is rigid body mode 0) or by a rotation either
+  around the y-axis or the z-axis. In case of a rotation dtheta around the
+  y-axis for example the resulting x-translation is dtheta times the
+  lever arm z - z0. Here z0 represents the z-coordinate of the point
+  around which the structure is rotated. This point may be chosen
+  arbitrarily and by the algorithms underlying to this method it is 
+  chosen automatically according to some mathematical considerations.
+  Note that this holds true for infinitesimal rotations dtehta, only, of
+  course.
+  On the other hand e.g. the fourth column means that a rigid body
+  rotation dtheta around the x-axis entails translations (-z+z0)*dtheta
+  in y-direction and (y-y0)*dtheta in z-direction and a rotation 
+  increment 1*dtheta of the rotational degree of freedom related to the
+  x-axis.
+  */
+  
+  /* for beam2 elements the above table reduces to
+   *
+        xtrans   ytrans    zrot
+        mode[0]  mode[1]   mode[2]
+  -----------------------------------------------------------
+  x   |    1       0       -y+y0
+  y   |    0       1       x-x0
+  dz  |    0       0       1
+  note: for the here employed Timoshenko beam elements a rigid body 
+  rotation entails also an increment of the rotation degree of freedom
+  dz which makes the director of the beam move accordingly; only then
+  a rotation does not lead to any shear stress and is truely a rigid
+  body rotation
+  */
 
-  else if (ele->Type() == DRT::Element::element_beam2)
+  /*two dimensional beam beam elements, where each node has 2 translational
+   * degrees of freedom and one rotational degree of freedom*/
+  else if (ele->Type() == DRT::Element::element_beam2 ||
+           ele->Type() == DRT::Element::element_beam2r)
   {
+    //looping through all nodes
     for (int i=0; i<NumMyRowNodes(); ++i)
     {
+      //getting pointer at current node
       DRT::Node* actnode = lRowNode(i);
+      
+      //getting coordinates of current node
       const double* x = actnode->X();
+      
+      //getting number of degrees of freedom of current node
       vector<int> dofs = Dof(actnode);
+      
+      //looping through all degrees of freedom of a node
       for (unsigned j=0; j<dofs.size(); ++j)
       {
         const int dof = dofs[j];
         const int lid = rowmap->LID(dof);
         if (lid<0) dserror("Cannot find dof");
-        switch (j) // j is degree of freedom
+        // j is degree of freedom; each case refers to one line in the above table
+        switch (j)
         {
-        //translation of element in x-direction
         case 0:
           mode[0][lid] = 1.0;
           mode[1][lid] = 0.0;
-          mode[2][lid] = 0.0;
+          mode[2][lid] = -x[1] + x0[1];
         break;
-        //translation of element in y-direction
         case 1:
           mode[0][lid] = 0.0;
           mode[1][lid] = 1.0;
-          mode[2][lid] = 0.0;
+          mode[2][lid] = x[0] - x0[0];
         break;
-        //rotation of element about z-axis
-        //(e.g. x[0]-x0[0] is the arm of lever for translation in y-direction in case of rotation about z-axis)
-        //(hence x[0]-x0[0] is the translation in y-direction in case of unit rotation about z-axis)
         case 2:
-          mode[0][lid] = -x[1] + x0[1];
-          mode[1][lid] =  x[0] - x0[0];
+          mode[0][lid] = 0.0;
+          mode[1][lid] = 0.0;
           mode[2][lid] = 1.0;
         break;
         default:
@@ -435,206 +494,24 @@ void DRT::Discretization::ComputeNullSpaceIfNecessary(
         } // switch (j)
       } // for (int j=0; j<actnode->Dof().NumDof(); ++j)
     } // for (int i=0; i<NumMyRowNodes(); ++i)
-  } // else if (ele->Type() == DRT::Element::element_beam2)
+  } // else if (ele->Type() == DRT::Element::element_beam2 || ele->Type() == DRT::Element::element_beam2r)
   
-  //there are three rigid body modes for beam2r
-
- else if (ele->Type() == DRT::Element::element_beam2r)
- {
-   for (int i=0; i<NumMyRowNodes(); ++i)
-   {
-     DRT::Node* actnode = lRowNode(i);
-     const double* x = actnode->X();
-     vector<int> dofs = Dof(actnode);
-     for (unsigned j=0; j<dofs.size(); ++j)
-     {
-       const int dof = dofs[j];
-       const int lid = rowmap->LID(dof);
-       if (lid<0) dserror("Cannot find dof");
-       switch (j) // j is degree of freedom
-       {
-       //translation of element in x-direction
-       case 0:
-         mode[0][lid] = 1.0;
-         mode[1][lid] = 0.0;
-         mode[2][lid] = 0.0;
-       break;
-       //translation of element in y-direction
-       case 1:
-         mode[0][lid] = 0.0;
-         mode[1][lid] = 1.0;
-         mode[2][lid] = 0.0;
-       break;
-       //rotation of element about z-axis
-       //(e.g. x[0]-x0[0] is the arm of lever for translation in y-direction in case of rotation about z-axis)
-       //(hence x[0]-x0[0] is the translation in y-direction in case of unit rotation about z-axis)
-       case 2:
-         mode[0][lid] = -x[1] + x0[1];
-         mode[1][lid] =  x[0] - x0[0];
-         mode[2][lid] = 1.0;
-       break;
-       default:
-         dserror("Only dofs 0 - 2 supported");
-       break;
-       } // switch (j)
-     } // for (int j=0; j<actnode->Dof().NumDof(); ++j)
-   } // for (int i=0; i<NumMyRowNodes(); ++i)
- } // else if (ele->Type() == DRT::Element::element_beam2r)
-
-  //there are six rigid body modes for beam3
+  /* for beam3 elements the relation between rigid body modes and
+   * increments on the degrees of freedom is non-trivial since
+   * rotational increments in 3D are non-additive in general. In 
+   * general this relation may require calling all the elements.
+   * However, in opposition to the SHELL8 element it is not
+   * sufficient to just call a director saved in the element.
+   * Rather for 3D non-linear beam elements a more complicated
+   * relation has to be employed. As no use of beam3 elements
+   * together with Algebraic Multigrid is supposed to be required
+   * this case has not been treated here and if support is called
+   * then just a dserror is returned*/
 
  else if (ele->Type() == DRT::Element::element_beam3)
  {
-   for (int i=0; i<NumMyRowNodes(); ++i)
-   {
-     DRT::Node* actnode = lRowNode(i);
-     const double* x = actnode->X();
-     vector<int> dofs = Dof(actnode);
-     for (unsigned j=0; j<dofs.size(); ++j)
-     {
-       const int dof = dofs[j];
-       const int lid = rowmap->LID(dof);
-       if (lid<0) dserror("Cannot find dof");
-       switch (j) // j is degree of freedom
-       {
-       //translation of element in x-direction
-       case 0:
-         mode[0][lid] = 1.0;
-         mode[1][lid] = 0.0;
-         mode[2][lid] = 0.0;
-         mode[3][lid] = 0.0;
-         mode[4][lid] = 0.0;
-         mode[5][lid] = 0.0;
-       break;
-       //translation of element in y-direction
-       case 1:
-         mode[0][lid] = 0.0;
-         mode[1][lid] = 1.0;
-         mode[2][lid] = 0.0;
-         mode[3][lid] = 0.0;
-         mode[4][lid] = 0.0;
-         mode[5][lid] = 0.0;
-       break;
-       //translation of element in z-direction
-       case 2:
-         mode[0][lid] = 0.0;
-         mode[1][lid] = 0.0;
-         mode[2][lid] = 1.0;
-         mode[3][lid] = 0.0;
-         mode[4][lid] = 0.0;
-         mode[5][lid] = 0.0;
-       break;
-       //rotation of element about x-axis
-       case 3:
-         mode[0][lid] = 0.0;
-         mode[1][lid] = -x[2] + x0[2];
-         mode[2][lid] =  x[1] - x0[1];
-         mode[3][lid] = 1.0;
-         mode[4][lid] = 0.0;
-         mode[5][lid] = 0.0;
-       break;
-       //rotation of element about y-axis
-       case 4:
-         mode[0][lid] =  x[2] - x0[2];
-         mode[1][lid] = 0.0;
-         mode[2][lid] = -x[0] + x0[0];
-         mode[3][lid] = 0.0;
-         mode[4][lid] = 1.0;
-         mode[5][lid] = 0.0;
-       break;
-       //rotation of element about z-axis
-       case 5:
-         mode[0][lid] = -x[1] + x0[1];
-         mode[1][lid] =  x[0] - x0[0];
-         mode[2][lid] = 0.0;
-         mode[3][lid] = 0.0;
-         mode[4][lid] = 0.0;
-         mode[5][lid] = 1.0;
-       break;
-       default:
-         dserror("Only dofs 0 - 5 supported");
-       break;
-       } // switch (j)
-     } // for (int j=0; j<actnode->Dof().NumDof(); ++j)
-   } // for (int i=0; i<NumMyRowNodes(); ++i)
+   dserror("No Algebraic Multigrid support by beam3 element");
  } // else if (ele->Type() == DRT::Element::element_beam3)
-
-   else if (ele->Type() == DRT::Element::element_truss3)
- {
-   for (int i=0; i<NumMyRowNodes(); ++i)
-   {
-     DRT::Node* actnode = lRowNode(i);
-     const double* x = actnode->X();
-     vector<int> dofs = Dof(actnode);
-     for (unsigned j=0; j<dofs.size(); ++j)
-     {
-       const int dof = dofs[j];
-       const int lid = rowmap->LID(dof);
-       if (lid<0) dserror("Cannot find dof");
-       switch (j) // j is degree of freedom
-       {
-       //translation of element in x-direction
-       case 0:
-         mode[0][lid] = 1.0;
-         mode[1][lid] = 0.0;
-         mode[2][lid] = 0.0;
-         mode[3][lid] = 0.0;
-         mode[4][lid] = 0.0;
-         mode[5][lid] = 0.0;
-       break;
-       //translation of element in y-direction
-       case 1:
-         mode[0][lid] = 0.0;
-         mode[1][lid] = 1.0;
-         mode[2][lid] = 0.0;
-         mode[3][lid] = 0.0;
-         mode[4][lid] = 0.0;
-         mode[5][lid] = 0.0;
-       break;
-       //translation of element in z-direction
-       case 2:
-         mode[0][lid] = 0.0;
-         mode[1][lid] = 0.0;
-         mode[2][lid] = 1.0;
-         mode[3][lid] = 0.0;
-         mode[4][lid] = 0.0;
-         mode[5][lid] = 0.0;
-       break;
-       //rotation of element about x-axis
-       case 3:
-         mode[0][lid] = 0.0;
-         mode[1][lid] = -x[2] + x0[2];
-         mode[2][lid] =  x[1] - x0[1];
-         mode[3][lid] = 1.0;
-         mode[4][lid] = 0.0;
-         mode[5][lid] = 0.0;
-       break;
-       //rotation of element about y-axis
-       case 4:
-         mode[0][lid] =  x[2] - x0[2];
-         mode[1][lid] = 0.0;
-         mode[2][lid] = -x[0] + x0[0];
-         mode[3][lid] = 0.0;
-         mode[4][lid] = 1.0;
-         mode[5][lid] = 0.0;
-       break;
-       //rotation of element about z-axis
-       case 5:
-         mode[0][lid] = -x[1] + x0[1];
-         mode[1][lid] =  x[0] - x0[0];
-         mode[2][lid] = 0.0;
-         mode[3][lid] = 0.0;
-         mode[4][lid] = 0.0;
-         mode[5][lid] = 1.0;
-       break;
-       default:
-         dserror("Only dofs 0 - 5 supported");
-       break;
-       } // switch (j)
-     } // for (int j=0; j<actnode->Dof().NumDof(); ++j)
-   } // for (int i=0; i<NumMyRowNodes(); ++i)
- } // else if (ele->Type() == DRT::Element::element_truss3)
-
 
 
   /* the rigid body modes for fluids are:
