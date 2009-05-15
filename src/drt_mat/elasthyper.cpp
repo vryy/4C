771432048ece976b934ddef926cd11d025f019da
Burkhard Borneman <borneman@lnm.mw.tum.de>
2009-05-15 12:38:19 +0000

    chiefly beauty: renamed a few variables and unified comment style etc
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@9507 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_mat/elasthyper.cpp
+++ src/drt_mat/elasthyper.cpp
@@ -42,6 +42,21 @@ MAT::PAR::ElastHyper::ElastHyper(
   }
 }
 
+/*----------------------------------------------------------------------*/
+/*----------------------------------------------------------------------*/
+Teuchos::RCP<const MAT::ELAST::Summand> MAT::PAR::ElastHyper::MaterialById(const int id) const
+{
+  std::map<int,Teuchos::RCP<MAT::ELAST::Summand> >::const_iterator m = potsum_.find(id);
+  if (m == potsum_.end())
+  {
+    dserror("Material %d could not be found", id);
+    return Teuchos::null;
+  }
+  else
+  {
+    return m->second;
+  }
+}
 
 /*----------------------------------------------------------------------*/
 /*----------------------------------------------------------------------*/
@@ -109,6 +124,21 @@ void MAT::ElastHyper::Unpack(const std::vector<char>& data)
 
 /*----------------------------------------------------------------------*/
 /*----------------------------------------------------------------------*/
+int MAT::ElastHyper::MatID(
+  const unsigned index
+) const
+{ 
+  if ((int)index < params_->nummat_)
+    return params_->matids_->at(index);
+  else
+  {
+    dserror("Index too large");
+    return -1;
+  }
+}
+
+/*----------------------------------------------------------------------*/
+/*----------------------------------------------------------------------*/
 void MAT::ElastHyper::InvariantsPrincipal(
   LINALG::Matrix<3,1>& prinv,
   const LINALG::Matrix<6,1>& rcg
@@ -156,15 +186,20 @@ void MAT::ElastHyper::Evaluate(
   LINALG::Matrix<6,1>& stress
   )
 {
-  // build Cartesian identity 2-tensor I
+  // build Cartesian identity 2-tensor I_{AB}
   LINALG::Matrix<6,1> id2(true);
   for (int i=0; i<3; i++) id2(i) = 1.0;
 
-  // right Cauchy-Green Tensor  C = 2 * E + I
+  // right Cauchy-Green Tensor  C_{AB} = 2 * E_{AB} + I_{AB}
   // REMARK: strain-like 6-Voigt vector
   LINALG::Matrix<6,1> rcg(glstrain);
   rcg.Scale(2.0);
   rcg.Update(1.0, id2, 1.0);
+
+  // 'contra-variant' right Cauchy-Green Tensor C^{AB}
+  // REMARK: stress-like 6-Voigt vector of right CG
+  LINALG::Matrix<6,1> scg(rcg);
+  for (int i=3; i<6; i++) scg(i) *= 0.5;
 
   // principal invariants of right Cauchy-Green strain
   LINALG::Matrix<3,1> prinv;
@@ -220,18 +255,20 @@ void MAT::ElastHyper::Evaluate(
   
   }
   
-  // we make right Cauchy-Green 6-Voigt vector _stress_-like
-  // needed for dyadic products underneath
-  for (int i=3; i<6; i++) rcg(i) *= 0.5;
-  
   // set Cartesian identity 4-tensor in 6-Voigt matrix notation
-  LINALG::Matrix<6,6> id4(true);
-  for (int i=0; i<3; i++) id4(i,i) = 1.0;
-  for (int i=3; i<6; i++) id4(i,i) = 0.5;
+  // this is fully 'contra-variant' identity tensor, ie I^{ABCD}
+  // REMARK: rows are stress-like 6-Voigt
+  //         columns are stress-like 6-Voigt
+  LINALG::Matrix<6,6> id4sharp(true);
+  for (int i=0; i<3; i++) id4sharp(i,i) = 1.0;
+  for (int i=3; i<6; i++) id4sharp(i,i) = 0.5;
   
   // set Cartesian identity 4-tensor in 6x6-matrix notation (stress-like)
-  LINALG::Matrix<6,6> id4a(true);
-  for (int i=0; i<6; i++) id4a(i,i) = 1.0;
+  // this is a 'mixed co- and contra-variant' identity 4-tensor, ie I^{AB}_{CD}
+  // REMARK: rows are stress-like 6-Voigt
+  //         columns are strain-like 6-Voigt
+  LINALG::Matrix<6,6> id4(true);
+  for (int i=0; i<6; i++) id4(i,i) = 1.0;
     
   // blank resulting quantities
   // ... even if it is an implicit law that cmat is zero upon input
@@ -244,36 +281,36 @@ void MAT::ElastHyper::Evaluate(
   {
     // 2nd Piola Kirchhoff stresses
     stress.Update(gamma(0), id2, 1.0);
-    stress.Update(gamma(1), rcg, 1.0);
+    stress.Update(gamma(1), scg, 1.0);
     stress.Update(gamma(2), icg, 1.0);
 
     // constitutive tensor
     // contribution: Id \otimes Id
     cmat.MultiplyNT(delta(0), id2, id2, 1.0);
     // contribution: Id \otimes C + C \otimes Id
-    cmat.MultiplyNT(delta(1), id2, rcg, 1.0);
-    cmat.MultiplyNT(delta(1), rcg, id2, 1.0);
+    cmat.MultiplyNT(delta(1), id2, scg, 1.0);
+    cmat.MultiplyNT(delta(1), scg, id2, 1.0);
     // contribution: Id \otimes Cinv + Cinv \otimes Id
     cmat.MultiplyNT(delta(2), id2, icg, 1.0);
     cmat.MultiplyNT(delta(2), icg, id2, 1.0);
     // contribution: C \otimes C
-    cmat.MultiplyNT(delta(3), rcg, rcg, 1.0);
+    cmat.MultiplyNT(delta(3), scg, scg, 1.0);
     // contribution: C \otimes Cinv + Cinv \otimes C
-    cmat.MultiplyNT(delta(4), rcg, icg, 1.0);
-    cmat.MultiplyNT(delta(4), icg, rcg, 1.0);
+    cmat.MultiplyNT(delta(4), scg, icg, 1.0);
+    cmat.MultiplyNT(delta(4), icg, scg, 1.0);
     // contribution: Cinv \otimes Cinv
     cmat.MultiplyNT(delta(5), icg, icg, 1.0);
     // contribution: Cinv \odot Cinv
     AddtoCmatHolzapfelProduct(cmat, icg, delta(6));
-    // contribution: Id4
-    cmat.Update(delta(7), id4, 1.0);
+    // contribution: Id4^#
+    cmat.Update(delta(7), id4sharp, 1.0);
   }
   
   if (havecoeffmodi)
   {
-    //define necessary variables
+    // define necessary variables
     const double modscale = std::pow(prinv(2),-1./3.);
-    //modified right Cauhy-Green
+    // modified right Cauchy-Green
     LINALG::Matrix<6,1> modrcg(true);
     modrcg.Update(modscale, rcg);
     
@@ -285,11 +322,8 @@ void MAT::ElastHyper::Evaluate(
     modstress.Update(modgamma(1), modrcg, 1.0);
     // build 4-tensor for projection as 6x6 tensor
     LINALG::Matrix<6,6> Projection;
-    // we make right Cauchy-Green 6-Voigt vector _strain_-like again
-    LINALG::Matrix<6,1> realrcg(rcg); 
-    for (int i=3; i<6; i++) realrcg(i) *= 2.0;
-    Projection.MultiplyNT(1./3., icg, realrcg);
-    Projection.Update(1.0,id4a,-1.0);
+    Projection.MultiplyNT(1./3., icg, rcg);
+    Projection.Update(1.0, id4, -1.0);
     // isochoric stress
     LINALG::Matrix<6,1> isostress(true);
     isostress.MultiplyNN(modscale,Projection,modstress,1.0);
@@ -312,8 +346,8 @@ void MAT::ElastHyper::Evaluate(
     modcmat.MultiplyNT(moddelta(1), rcg, id2, 1.0);
     // contribution: C \otimes C
     modcmat.MultiplyNT(moddelta(2), rcg, modrcg, 1.0);
-    // contribution: Id4
-    modcmat.Update(moddelta(3), id4, 1.0);
+    // contribution: Id4^#
+    modcmat.Update(moddelta(3), id4sharp, 1.0);
     //scaling
     modcmat.Scale(std::pow(modinv(2),-4./3.));
     //constribution: P:modC:P 
@@ -324,7 +358,7 @@ void MAT::ElastHyper::Evaluate(
     modcmat.MultiplyNT(-1.0/3.0,icg,icg);
     AddtoCmatHolzapfelProduct(modcmat, icg, 1.0);
     LINALG::Matrix<1,1> tracemat;
-    tracemat.MultiplyTN(2./3.*std::pow(modinv(2),-2./3.),modstress,realrcg);
+    tracemat.MultiplyTN(2./3.*std::pow(modinv(2),-2./3.),modstress,rcg);
     cmat.Update(tracemat(0,0),modcmat,1.0);
     //contribution: -2/3 (Cinv \otimes S_iso + S_iso \otimes Cinv)
     cmat.MultiplyNT(-2./3.,icg,isostress,1.0);
