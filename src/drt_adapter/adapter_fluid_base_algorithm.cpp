6689cf70488fe14739bd362ca56af2affe150e22
Florian Henke <henke@lnm.mw.tum.de>
2009-07-03 09:02:20 +0000

    added header for combustion input parameters
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@9706 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_adapter/adapter_fluid_base_algorithm.cpp
+++ src/drt_adapter/adapter_fluid_base_algorithm.cpp
@@ -20,6 +20,7 @@ Maintainer: Ulrich Kuettler
 #include "../drt_io/io_control.H"
 #include "../drt_inpar/drt_validparameters.H"
 #include "../drt_inpar/inpar_fsi.H"
+#include "../drt_inpar/inpar_combust.H"
 #include <Teuchos_StandardParameterEntryValidators.hpp>
 #include <Teuchos_TimeMonitor.hpp>
 #include <Teuchos_Time.hpp>
