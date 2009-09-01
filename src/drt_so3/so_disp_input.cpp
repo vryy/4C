4dd3835136e5a9f3762470addc45f0b745289bac
Ulrich Kuettler <kuettler@lnm.mw.tum.de>
2009-09-01 08:55:16 +0000

    SOLID3 input changes
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@10070 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_so3/so_disp_input.cpp
+++ src/drt_so3/so_disp_input.cpp
@@ -20,7 +20,125 @@ Maintainer: Axel Gerstenberger
 #endif
 
 #include "so_disp.H"
+#include "../drt_lib/drt_linedefinition.H"
 
+
+/*----------------------------------------------------------------------*/
+/*----------------------------------------------------------------------*/
+bool DRT::ELEMENTS::SoDisp::ReadElement(const std::string& eletype,
+                                        const std::string& distype,
+                                        DRT::INPUT::LineDefinition* linedef)
+{
+  // read number of material model
+  int material = 0;
+  linedef->ExtractInt("MAT",material);
+  SetMaterial(material);
+
+  DiscretizationType shape = StringToDistype(distype);
+
+  switch (shape)
+  {
+  case hex8: case hex20: case hex27:
+  {
+    std::vector<int> ngp;
+    linedef->ExtractIntVector("GP",ngp);
+
+    switch (ngp[0])
+    {
+    case 1:
+      gaussrule_ = DRT::UTILS::intrule_hex_1point;
+      break;
+    case 2:
+      gaussrule_ = DRT::UTILS::intrule_hex_8point;
+      break;
+    case 3:
+      gaussrule_ = DRT::UTILS::intrule_hex_27point;
+      break;
+    default:
+      dserror("Reading of SOLID3 element failed: Gaussrule for hexaeder not supported!");
+    }
+    break;
+  }
+  case pyramid5:
+  {
+    int ngp;
+    linedef->ExtractInt("GP_PYRAMID",ngp);
+    switch (ngp)
+    {
+    case 1:
+      gaussrule_ = DRT::UTILS::intrule_pyramid_1point;
+      break;
+    case 8:
+      gaussrule_ = DRT::UTILS::intrule_pyramid_8point;
+      break;
+    default:
+      dserror("Reading of SOLID3 element failed: Gaussrule for pyramid not supported!\n");
+    }
+    break;
+  }
+  case tet4: case tet10:
+  {
+    int ngp;
+    linedef->ExtractInt("GP_TET",ngp);
+
+    std::string buffer;
+    linedef->ExtractString("GP_ALT",buffer);
+
+    switch(ngp)
+    {
+    case 1:
+      if (buffer=="standard")
+        gaussrule_ = DRT::UTILS::intrule_tet_1point;
+      else
+        dserror("Reading of SOLID3 element failed: GP_ALT: gauss-radau not possible!\n");
+      break;
+    case 4:
+      if (buffer=="standard")
+        gaussrule_ = DRT::UTILS::intrule_tet_4point;
+      else if (buffer=="gaussrad")
+        gaussrule_ = DRT::UTILS::intrule_tet_4point_gauss_radau;
+      else
+        dserror("Reading of SOLID3 element failed: GP_ALT\n");
+      break;
+    case 10:
+      if (buffer=="standard")
+        gaussrule_ = DRT::UTILS::intrule_tet_5point;
+      else
+        dserror("Reading of SOLID3 element failed: GP_ALT: gauss-radau not possible!\n");
+      break;
+    default:
+      dserror("Reading of SOLID3 element failed: Gaussrule for tetraeder not supported!\n");
+    }
+    break;
+  } // end reading gaussian points for tetrahedral elements
+  default:
+    dserror("Reading of SOLID3 element failed: integration points\n");
+  } // end switch distype
+
+  std::string buffer;
+  linedef->ExtractString("KINEM",buffer);
+
+  // geometrically linear
+  if      (buffer=="Geolin")    kintype_ = sodisp_geolin;
+  // geometrically non-linear with Total Lagrangean approach
+  else if (buffer=="Totlag")    kintype_ = sodisp_totlag;
+  // geometrically non-linear with Updated Lagrangean approach
+  else if (buffer=="Updlag")
+  {
+    kintype_ = sodisp_updlag;
+    dserror("Updated Lagrange for SOLID3 is not implemented!");
+  }
+  else dserror("Reading of SOLID3 element failed");
+
+  numnod_disp_ = NumNode();      // number of nodes
+  numdof_disp_ = NumNode() * NODDOF_DISP;     // total dofs per element
+  const DRT::UTILS::IntegrationPoints3D  intpoints(gaussrule_);
+  numgpt_disp_ = intpoints.nquad;      // total gauss points per element
+
+  return true;
+}
+
+#if 0
 /*----------------------------------------------------------------------*
  |  read element input (public)                                maf 04/07|
  *----------------------------------------------------------------------*/
@@ -190,7 +308,7 @@ bool DRT::ELEMENTS::SoDisp::ReadElement()
   return true;
 
 } // SoDisp::ReadElement()
-
+#endif
 
 #endif  // #ifdef CCADISCRET
 #endif  // #ifdef D_SOLID3
