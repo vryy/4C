042d7036ecc86bab8f2063dcedad09289b7b5d0c
Christian Cyron <cyron@lnm.mw.tum.de>
2009-09-08 09:41:20 +0000

    merged beam2 with version of wolfgang hering
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@10112 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_beam2/beam2_input.cpp
+++ src/drt_beam2/beam2_input.cpp
@@ -16,9 +16,9 @@ Maintainer: Christian Cyron
 #include "beam2.H"
 #include "../drt_lib/drt_linedefinition.H"
 
-
-/*----------------------------------------------------------------------*/
-/*----------------------------------------------------------------------*/
+/*----------------------------------------------------------------------*
+ |  read element input (public)                              cyron 01/08|
+ *----------------------------------------------------------------------*/
 bool DRT::ELEMENTS::Beam2::ReadElement(const std::string& eletype,
                                        const std::string& distype,
                                        DRT::INPUT::LineDefinition* linedef)
@@ -27,78 +27,17 @@ bool DRT::ELEMENTS::Beam2::ReadElement(const std::string& eletype,
   int material = 0;
   linedef->ExtractInt("MAT",material);
   SetMaterial(material);
-
+  
   linedef->ExtractDouble("CROSS",crosssec_);
 
   double shear_correction = 0.0;
   linedef->ExtractDouble("SHEARCORR",shear_correction);
   crosssecshear_ = crosssec_ * shear_correction;
-
+  
   linedef->ExtractDouble("INERMOM",mominer_);
-  return true;
-}
-
-
-#if 0
-/*----------------------------------------------------------------------*
- |  read element input (public)                              cyron 01/08|
- *----------------------------------------------------------------------*/
-bool DRT::ELEMENTS::Beam2::ReadElement()
-{
-  // read element's nodes; in case of a beam element always line2 shape
-  int ierr=0;
-
-  //note: BACI intern type is LINE2, but gid input files work with LIN2
-  frchk("LIN2",&ierr);
-  // two figures have to be read by frint
-  int nnode=2;
-  // provide an array of length two in order to store the two figures read
-  int nodes[2];
-  frint_n("LIN2",nodes,nnode,&ierr);
-
-  // if that does not work try LINE2, in case .dat file was created with pre_exodus
-  if (ierr != 1)
-  {
-    ierr=0;
-    frchk("LINE2",&ierr);
-    frint_n("LINE2",nodes,nnode,&ierr);
-  }
-
-  if (ierr != 1) dserror("Reading of ELEMENT Topology failed");
-
-  // reduce node numbers by one
-  for (int i=0; i<nnode; ++i) nodes[i]--;
-
-  SetNodeIds(nnode,nodes);
-
-  // read material parameters using structure _MATERIAL which is defined by inclusion of
-  // "../drt_lib/drt_timecurve.H"
-  int material = 0;
-  frint("MAT",&material,&ierr);
-  if (ierr!=1) dserror("Reading of Beam2 element failed");
-  SetMaterial(material);
-
-  // read beam cross section
-  crosssec_ = 1.0;
-  frdouble("CROSS",&crosssec_,&ierr);
-  if (ierr!=1) dserror("Reading of Beam2 element failed");
-
-   // read beam cross section
-  double shear_correction = 0.0;
-  frdouble("SHEARCORR",&shear_correction,&ierr);
-  if (ierr!=1) dserror("Reading of Beam2 element failed");
-  crosssecshear_ = crosssec_ * shear_correction;
-
-  // read beam moment of inertia of area
-  mominer_ = 1.0;
-  frdouble("INERMOM",&mominer_,&ierr);
-  if (ierr!=1) dserror("Reading of Beam2 element failed");
-
-
+   
   return true;
 } // Beam2::ReadElement()
-#endif
-
 
 
 #endif  // #ifdef CCADISCRET
