ce0d0a85706d942f46cba5baa9839cff2a254a72
Ulrich Kuettler <kuettler@lnm.mw.tum.de>
2009-09-15 12:27:55 +0000

    experimental enabling of amg block preconditioning in fluid split monolithic FSI
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@10138 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_fsi/fsi_monolithicoverlap.cpp
+++ src/drt_fsi/fsi_monolithicoverlap.cpp
@@ -9,6 +9,7 @@
 
 #include "../drt_io/io_control.H"
 
+#define FLUIDSPLITAMG
 
 /*----------------------------------------------------------------------*/
 /*----------------------------------------------------------------------*/
@@ -74,7 +75,11 @@ void FSI::MonolithicOverlap::SetupSystem()
 
   std::vector<Teuchos::RCP<const Epetra_Map> > vecSpaces;
   vecSpaces.push_back(StructureField().DofRowMap());
+#ifdef FLUIDSPLITAMG
+  vecSpaces.push_back(FluidField()    .DofRowMap());
+#else
   vecSpaces.push_back(FluidField()    .Interface().OtherMap());
+#endif
   vecSpaces.push_back(AleField()      .Interface().OtherMap());
 
   if (vecSpaces[1]->NumGlobalElements()==0)
@@ -132,7 +137,6 @@ void FSI::MonolithicOverlap::SetupRHS(Epetra_Vector& f, bool firstcall)
               AleField().RHS(),
               FluidField().ResidualScaling());
 
-#if 1
   if (firstcall)
   {
     Teuchos::RCP<LINALG::BlockSparseMatrixBase> blockf = FluidField().BlockSystemMatrix();
@@ -149,6 +153,9 @@ void FSI::MonolithicOverlap::SetupRHS(Epetra_Vector& f, bool firstcall)
     fig.Apply(*fveln,*rhs);
     rhs->Scale(timescale*Dt());
 
+#ifdef FLUIDSPLITAMG
+    rhs = FluidField().Interface().InsertOtherVector(rhs);
+#endif
     Extractor().AddVector(*rhs,1,f);
 
     rhs = Teuchos::rcp(new Epetra_Vector(fgg.RowMap()));
@@ -160,7 +167,6 @@ void FSI::MonolithicOverlap::SetupRHS(Epetra_Vector& f, bool firstcall)
     rhs = StructureField().Interface().InsertCondVector(rhs);
     Extractor().AddVector(*rhs,0,f);
   }
-#endif
 
   // NOX expects a different sign here.
   f.Scale(-1.);
@@ -234,7 +240,14 @@ void FSI::MonolithicOverlap::SetupSystemMatrix(LINALG::BlockSparseMatrixBase& ma
                 timescale,
                 ADAPTER::Coupling::SlaveConverter(coupsf),
                 mat.Matrix(1,0));
+
+#ifdef FLUIDSPLITAMG
+  mat.Matrix(1,1).Add(fii,false,1.,0.0);
+  Teuchos::RCP<LINALG::SparseMatrix> eye = LINALG::Eye(*FluidField().Interface().FSICondMap());
+  mat.Matrix(1,1).Add(*eye,false,1.,1.0);
+#else
   mat.Assign(1,1,View,fii);
+#endif
 
   aigtransform_(*a,
                 aig,
@@ -250,9 +263,28 @@ void FSI::MonolithicOverlap::SetupSystemMatrix(LINALG::BlockSparseMatrixBase& ma
   if (mmm!=Teuchos::null)
   {
     LINALG::SparseMatrix& fmii = mmm->Matrix(0,0);
-    //LINALG::SparseMatrix& fmig = mmm->Matrix(0,1);
     LINALG::SparseMatrix& fmgi = mmm->Matrix(1,0);
-    //LINALG::SparseMatrix& fmgg = mmm->Matrix(1,1);
+
+#if 0
+    LINALG::SparseMatrix& fmig = mmm->Matrix(0,1);
+    LINALG::SparseMatrix& fmgg = mmm->Matrix(1,1);
+
+    // reuse transform objects to add shape derivative matrices to structural blocks
+
+    figtransform_(*blockf,
+                  fmig,
+                  1.,
+                  ADAPTER::Coupling::SlaveConverter(coupsf),
+                  mat.Matrix(1,0));
+
+    fggtransform_(fmgg,
+                  scale,
+                  ADAPTER::Coupling::SlaveConverter(coupsf),
+                  ADAPTER::Coupling::SlaveConverter(coupsf),
+                  mat.Matrix(0,0),
+                  false,
+                  true);
+#endif
 
     // We cannot copy the pressure value. It is not used anyway. So no exact
     // match here.
@@ -394,8 +426,50 @@ void FSI::MonolithicOverlap::UnscaleSolution(LINALG::BlockSparseMatrixBase& mat,
         mat.Matrix(0,2).EpetraMatrix()->RightScale(*acolsum_) or
         mat.Matrix(1,2).EpetraMatrix()->RightScale(*acolsum_))
       dserror("ale scaling failed");
-
   }
+
+#if 1
+  // very simple hack just to see the linear solution
+
+  Epetra_Vector r(b.Map());
+  mat.Apply(x,r);
+  r.Update(1.,b,1.);
+
+  Teuchos::RCP<Epetra_Vector> sr = Extractor().ExtractVector(r,0);
+  Teuchos::RCP<Epetra_Vector> fr = Extractor().ExtractVector(r,1);
+  Teuchos::RCP<Epetra_Vector> ar = Extractor().ExtractVector(r,2);
+
+  // increment additional ale residual
+  //aleresidual_->Update(-1.,*ar,0.);
+
+  ios_base::fmtflags flags = Utils()->out().flags();
+
+  double n,ns,nf,na;
+  r.Norm2(&n);
+  sr->Norm2(&ns);
+  fr->Norm2(&nf);
+  ar->Norm2(&na);
+  Utils()->out() << std::scientific
+                 << "\nlinear solver quality:\n"
+                 << "L_2-norms:\n"
+                 << END_COLOR "   |r|=" YELLOW << n
+                 << END_COLOR "   |rs|=" YELLOW << ns
+                 << END_COLOR "   |rf|=" YELLOW << nf
+                 << END_COLOR "   |ra|=" YELLOW << na
+                 << END_COLOR "\n";
+  r.NormInf(&n);
+  sr->NormInf(&ns);
+  fr->NormInf(&nf);
+  ar->NormInf(&na);
+  Utils()->out() << "L_inf-norms:\n"
+                 << END_COLOR "   |r|=" YELLOW << n
+                 << END_COLOR "   |rs|=" YELLOW << ns
+                 << END_COLOR "   |rf|=" YELLOW << nf
+                 << END_COLOR "   |ra|=" YELLOW << na
+                 << END_COLOR "\n";
+
+  Utils()->out().flags(flags);
+#endif
 }
 
 
@@ -411,6 +485,9 @@ void FSI::MonolithicOverlap::SetupVector(Epetra_Vector &f,
   // extract the inner and boundary dofs of all three fields
 
   Teuchos::RCP<Epetra_Vector> fov = FluidField()    .Interface().ExtractOtherVector(fv);
+#ifdef FLUIDSPLITAMG
+  fov = FluidField().Interface().InsertOtherVector(fov);
+#endif
   Teuchos::RCP<Epetra_Vector> aov = AleField()      .Interface().ExtractOtherVector(av);
 
   if (fluidscale!=0)
@@ -605,6 +682,9 @@ void FSI::MonolithicOverlap::ExtractFieldVectors(Teuchos::RCP<const Epetra_Vecto
   // process fluid unknowns
 
   Teuchos::RCP<const Epetra_Vector> fox = Extractor().ExtractVector(x,1);
+#ifdef FLUIDSPLITAMG
+  fox = FluidField().Interface().ExtractOtherVector(fox);
+#endif
   Teuchos::RCP<Epetra_Vector> fcx = StructToFluid(scx);
 
   FluidField().DisplacementToVelocity(fcx);
