f73de375bd7a6a728ba8e46f4263d376aaa621c5
Michael Gee <gee@lnm.mw.tum.de>
2009-10-28 17:09:51 +0000

    replaced the implementation of neohooke that used to be tensor style with an implementation in voigt notation. this is very recommendable in general as its significantly faster
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@10363 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_mat/neohooke.cpp
+++ src/drt_mat/neohooke.cpp
@@ -100,7 +100,7 @@ void MAT::NeoHooke::Unpack(const vector<char>& data)
 
 
 /*----------------------------------------------------------------------*
- |  Calculate stress and constitutive tensor (Neo Hookean law) gee 10/08|
+ |  Calculate stress and constitutive tensor (Neo Hookean law) gee 10/09|
  *----------------------------------------------------------------------*/
 void MAT::NeoHooke::Evaluate(
             const LINALG::Matrix<6,1>& glstrain,
@@ -111,119 +111,60 @@ void MAT::NeoHooke::Evaluate(
   const double ym = params_->youngs_;    // Young's modulus
   const double nu = params_->poissonratio_; // Poisson's ratio
 
-  // Green-Lagrange Strain Tensor
-  LINALG::Matrix<3,3> E(false);
-  E(0,0) = glstrain(0);
-  E(1,1) = glstrain(1);
-  E(2,2) = glstrain(2);
-  E(0,1) = 0.5 * glstrain(3);  E(1,0) = 0.5 * glstrain(3);
-  E(1,2) = 0.5 * glstrain(4);  E(2,1) = 0.5 * glstrain(4);
-  E(0,2) = 0.5 * glstrain(5);  E(2,0) = 0.5 * glstrain(5);
+  // right Cauchy-Green Tensor  C = 2 * E + I
+  LINALG::Matrix<6,1> rcg(glstrain);
+  rcg.Scale(2.0);
+  rcg(0) += 1.0;
+  rcg(1) += 1.0;
+  rcg(2) += 1.0;
 
-  // Right Cauchy-Green Tensor  C = 2 * E + I
-  LINALG::Matrix<3,3> C(E);
-  C.Scale(2.0);
-  C(0,0) += 1.0;
-  C(1,1) += 1.0;
-  C(2,2) += 1.0;
+  // invariants
+  //double I1 = rcg(0) + rcg(1) + rcg(2);  // 1st invariant, trace
+  const double I3 = rcg(0)*rcg(1)*rcg(2)
+           + 0.25 * rcg(3)*rcg(4)*rcg(5)
+           - 0.25 * rcg(1)*rcg(5)*rcg(5)
+           - 0.25 * rcg(2)*rcg(3)*rcg(3)
+           - 0.25 * rcg(0)*rcg(4)*rcg(4);    // 3rd invariant, determinante
+  
+  //--------------------------------------------------------------------------------------
+  // invert C
+  LINALG::Matrix<6,1> invc(false);
 
-  // Principal Invariants I1 = tr(C) and I3 = det(C)
-  //double I1 = C(0,0)+C(1,1)+C(2,2); // Needed only for energy
-  const double I3 = C(0,0)*C(1,1)*C(2,2) + C(0,1)*C(1,2)*C(2,0)
-                  + C(0,2)*C(1,0)*C(2,1) - (C(0,2)*C(1,1)*C(2,0)
-                  + C(0,1)*C(1,0)*C(2,2) + C(0,0)*C(1,2)*C(2,1));
+  double invdet = 1./I3;
 
-  // Calculation of C^-1 (Cinv)
-  // C no longer needed, so we destroy it by inplace inversion
-  C.Invert();
+  invc(0) = rcg(1)*rcg(2) - 0.25*rcg(4)*rcg(4);
+  invc(1) = rcg(0)*rcg(2) - 0.25*rcg(5)*rcg(5);
+  invc(2) = rcg(0)*rcg(1) - 0.25*rcg(3)*rcg(3);
+  invc(3) = 0.25*rcg(5)*rcg(4) - 0.5*rcg(3)*rcg(2);
+  invc(4) = 0.25*rcg(3)*rcg(5) - 0.5*rcg(0)*rcg(4);
+  invc(5) = 0.25*rcg(3)*rcg(4) - 0.5*rcg(5)*rcg(1);
+
+  invc.Scale(invdet);
 
   // Material Constants c1 and beta
   const double c1 = 0.5 * ym/(2*(1+nu));
   const double beta = nu/(1-2*nu);
 
-  // Energy
-  // double W = c1/beta * (pow(J,-beta) - 1) + c1 (I1-3);
-
-  // PK2 Stresses
-  LINALG::Matrix<3,3> PK2(false);
-  for (int i=0; i<3; i++)
-    for (int j=0; j<3; j++)
-    {
-      PK2(i,j)= 2.0 * c1 * ( - pow(I3,-beta) * C(i,j) );
-    }
-  PK2(0,0) += (2.0 * c1);
-  PK2(1,1) += (2.0 * c1);
-  PK2(2,2) += (2.0 * c1);
-
-
-  // Transfer PK2 tensor to stress vector
-  stress(0) = PK2(0,0);
-  stress(1) = PK2(1,1);
-  stress(2) = PK2(2,2);
-  stress(3) = PK2(0,1);
-  stress(4) = PK2(1,2);
-  stress(5) = PK2(0,2);
-
-  // Elasticity Tensor
+  // energy function
+  // Psi = c1/beta (I3^{-beta} - 1) + c1 ( I1-3 )
+  // S = -2 c1 I3^{-beta} C^{-1} + 2 c1 Identity
+  
+  stress = invc;
+  stress.Scale(-2.0*c1*pow(I3,-beta)); // volumetric part
+  const double iso = 2.0*c1;           // isochoric part
+  stress(0) += iso;
+  stress(1) += iso;
+  stress(2) += iso;
+  
+  // material tensor:
+  // C = 4 c1 beta I3^{-beta} C^{-1} dyad C^{-1} + 4 c1 I3^{-beta} C^{-1} boeppel C^{-1}
+  // where `boeppel' is called `Holzapfelproduct' below
   const double delta6 = 4. * c1 * beta * pow(I3,-beta);
   const double delta7 = 4. * c1 * pow(I3,-beta);
-
-  LINALG::Matrix<9,9> ET(false);
-
-
-  for (int k=0; k<3; k++)
-  for (int l=0; l<3; l++)
-  {
-    ET(k,l)    = delta6 * (C(0,0) * C(k,l)) + delta7 * 0.5 *(C(0,k) * C(0,l) + C(0,l) * C(0,k));
-    ET(k+3,l)  = delta6 * (C(1,0) * C(k,l)) + delta7 * 0.5 *(C(1,k) * C(0,l) + C(1,l) * C(0,k));
-    ET(k+3,l+3)= delta6 * (C(1,1) * C(k,l)) + delta7 * 0.5 *(C(1,k) * C(1,l) + C(1,l) * C(1,k));
-    ET(k+6,l)  = delta6 * (C(2,0) * C(k,l)) + delta7 * 0.5 *(C(2,k) * C(0,l) + C(2,l) * C(0,k));
-    ET(k+6,l+3)= delta6 * (C(2,1) * C(k,l)) + delta7 * 0.5 *(C(2,k) * C(1,l) + C(2,l) * C(1,k));
-    ET(k+6,l+6)= delta6 * (C(2,2) * C(k,l)) + delta7 * 0.5 *(C(2,k) * C(2,l) + C(2,l) * C(2,k));
-  }
-
-  cmat(0,0)=ET(0,0);
-  cmat(0,1)=ET(1,1);
-  cmat(0,2)=ET(2,2);
-  cmat(0,3)=ET(1,0);
-  cmat(0,4)=ET(2,1);
-  cmat(0,5)=ET(2,0);
-
-  cmat(1,0)=ET(3,3);
-  cmat(1,1)=ET(4,4);
-  cmat(1,2)=ET(5,5);
-  cmat(1,3)=ET(4,3);
-  cmat(1,4)=ET(5,4);
-  cmat(1,5)=ET(5,3);
-
-  cmat(2,0)=ET(6,6);
-  cmat(2,1)=ET(7,7);
-  cmat(2,2)=ET(8,8);
-  cmat(2,3)=ET(7,6);
-  cmat(2,4)=ET(8,7);
-  cmat(2,5)=ET(8,6);
-
-  cmat(3,0)=ET(3,0);
-  cmat(3,1)=ET(4,1);
-  cmat(3,2)=ET(5,2);
-  cmat(3,3)=ET(4,0);
-  cmat(3,4)=ET(5,1);
-  cmat(3,5)=ET(5,0);
-
-  cmat(4,0)=ET(6,3);
-  cmat(4,1)=ET(7,4);
-  cmat(4,2)=ET(8,5);
-  cmat(4,3)=ET(7,3);
-  cmat(4,4)=ET(8,4);
-  cmat(4,5)=ET(8,3);
-
-  cmat(5,0)=ET(6,0);
-  cmat(5,1)=ET(7,1);
-  cmat(5,2)=ET(8,2);
-  cmat(5,3)=ET(7,0);
-  cmat(5,4)=ET(8,1);
-  cmat(5,5)=ET(8,0);
-
+  for (int i=0; i<6; ++i)
+    for (int j=0; j<6; ++j)
+      cmat(i,j) = delta6 * invc(i)*invc(j);
+  AddtoCmatHolzapfelProduct(cmat,invc,delta7);
   return;
 } // end of neohooke evaluate
 
