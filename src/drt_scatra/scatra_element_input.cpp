909547268a0bb697250c214ab756d7d8369bde2a
Georg Bauer <bauer@lnm.mw.tum.de>
2009-12-03 09:51:39 +0000

    removed obsolete code from element read-in
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@10549 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_scatra/scatra_element_input.cpp
+++ src/drt_scatra/scatra_element_input.cpp
@@ -19,8 +19,9 @@ Maintainer: Georg Bauer
 #include "../drt_lib/drt_linedefinition.H"
 
 
-/*----------------------------------------------------------------------*/
-/*----------------------------------------------------------------------*/
+/*----------------------------------------------------------------------*
+ | read element input (public)                                          |
+ *----------------------------------------------------------------------*/
 bool DRT::ELEMENTS::Transport::ReadElement(const std::string& eletype,
                                            const std::string& distype,
                                            DRT::INPUT::LineDefinition* linedef)
@@ -35,73 +36,6 @@ bool DRT::ELEMENTS::Transport::ReadElement(const std::string& eletype,
   return true;
 }
 
-#if 0
-/*----------------------------------------------------------------------*
- |  read element input (public)                                gjb 01/08|
- *----------------------------------------------------------------------*/
-bool DRT::ELEMENTS::Transport::ReadElement()
-{
-    map<string, DiscretizationType> gid2distype;
-    gid2distype["HEX8"]     = hex8;
-    gid2distype["HEX20"]    = hex20;
-    gid2distype["HEX27"]    = hex27;
-    gid2distype["TET4"]     = tet4;
-    gid2distype["TET10"]    = tet10;
-    gid2distype["WEDGE6"]   = wedge6;
-    gid2distype["WEDGE15"]  = wedge15;
-    gid2distype["PYRAMID5"] = pyramid5;
-    gid2distype["QUAD4"]    = quad4;
-    gid2distype["QUAD8"]    = quad8;
-    gid2distype["QUAD9"]    = quad9;
-    gid2distype["TRI3"]     = tri3;
-    gid2distype["TRI6"]     = tri6;
-    gid2distype["NURBS4"]   = nurbs4;
-    gid2distype["NURBS9"]   = nurbs9;
-    gid2distype["LINE2"]    = line2;
-    gid2distype["LINE3"]    = line3;
-
-    // read element's nodes
-    int   ierr = 0;
-    int   nnode = 0;
-    int   nodes[27];
-    DiscretizationType distype = dis_none;
-
-    map<string, DiscretizationType>::const_iterator iter;
-    for( iter = gid2distype.begin(); iter != gid2distype.end(); iter++ )
-    {
-        const string eletext = iter->first;
-        frchk(eletext.c_str(), &ierr);
-        if (ierr == 1)
-        {
-            distype = gid2distype[eletext];
-            // set the discretization type
-            SetDisType(distype);
-            nnode = DRT::UTILS::getNumberOfElementNodes(distype);
-            frint_n(eletext.c_str(), nodes, nnode, &ierr);
-            dsassert(ierr==1, "Reading of ELEMENT Topology failed\n");
-            break;
-        }
-    }
-
-    // reduce node numbers by one
-    for (int i=0; i<nnode; ++i) nodes[i]--;
-
-    // apply the node ids
-    SetNodeIds(nnode,nodes);
-
-    // read number of material model
-    int material = 0;
-    frint("MAT",&material,&ierr);
-    dsassert(ierr==1, "Reading of material for Transport element failed\n");
-    dsassert(material!=0, "No material defined for Transport element\n");
-    // set material (and numdofpernode_) of transport element
-    SetMaterial(material);
-
-
-  return true;
-
-} // Transport::ReadElement()
-#endif
 
 #endif  // #ifdef CCADISCRET
 #endif  // #if defined(D_FLUID2) || defined(D_FLUID3)
