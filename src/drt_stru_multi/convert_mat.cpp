9cab4e621f2c249bfce70601aa99c06e29a933e3
Lena Yoshihara <yoshihara@lnm.mw.tum.de>
2010-03-16 14:53:19 +0000

    Multi-scale (structure) =======================
    To ensure minor symmetries (which are exploited in Voigt notation), the constitutive tensor relating
    S and C (which is obtained via conversion of the constitutive tensor relating P and F) has to be 
    symmetrized explicitly, i.e. we consider
    1/4*(c_{ABCD}+c_{ABDC}+c_{BACD}+c_{BADC}).
    
    This is really awkward! 
    
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@11109 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_stru_multi/convert_mat.cpp
+++ src/drt_stru_multi/convert_mat.cpp
@@ -26,44 +26,139 @@ void STRUMULTI::MicroStatic::ConvertMat(const Epetra_MultiVector& cmatpf,
   // Marsden and Hughes, Mathematical Foundations of Elasticity,
   // Dover, pg. 215.
 
-  int StressIndex[6][6] =
+  int StressIndex[24][6] =
     {
       {0,3,5,3,5,0},
       {3,1,4,1,4,3},
       {5,4,2,4,2,5},
       {3,1,4,1,4,3},
       {5,4,2,4,2,5},
-      {0,3,5,3,5,0}
+      {0,3,5,3,5,0},
+
+      {0,3,5,0,3,5},
+      {3,1,4,3,1,4},
+      {5,4,2,5,4,2},
+      {3,1,4,3,1,4},
+      {5,4,2,5,4,2},
+      {0,3,5,0,3,5},
+
+      {0,3,5,3,5,0},
+      {3,1,4,1,4,3},
+      {5,4,2,4,2,5},
+      {0,3,5,3,5,0},
+      {3,1,4,1,4,3},
+      {5,4,2,4,2,5},
+
+      {0,3,5,0,3,5},
+      {3,1,4,3,1,4},
+      {5,4,2,5,4,2},
+      {0,3,5,0,3,5},
+      {3,1,4,3,1,4},
+      {5,4,2,5,4,2}
     };
 
-  int cmatpfIndex[6][6] =
+
+  int cmatpfIndex1[24][3] =
     {
       {0,7,5},
       {3,1,8},
       {6,4,2},
       {3,1,8},
       {6,4,2},
-      {0,7,5}
+      {0,7,5},
+
+      {0,7,5},
+      {3,1,8},
+      {6,4,2},
+      {3,1,8},
+      {6,4,2},
+      {0,7,5},
+
+      {0,7,5},
+      {3,1,8},
+      {6,4,2},
+      {0,7,5},
+      {3,1,8},
+      {6,4,2},
+
+      {0,7,5},
+      {3,1,8},
+      {6,4,2},
+      {0,7,5},
+      {3,1,8},
+      {6,4,2},
     };
 
-  int FinvIndex[6] = {0,1,2,0,1,2};
+  int cmatpfIndex2[24][3] =
+    {
+      {0,7,5},
+      {3,1,8},
+      {6,4,2},
+      {3,1,8},
+      {6,4,2},
+      {0,7,5},
 
+      {0,7,5},
+      {3,1,8},
+      {6,4,2},
+      {0,7,5},
+      {3,1,8},
+      {6,4,2},
+
+      {0,7,5},
+      {3,1,8},
+      {6,4,2},
+      {3,1,8},
+      {6,4,2},
+      {0,7,5},
+
+      {0,7,5},
+      {3,1,8},
+      {6,4,2},
+      {0,7,5},
+      {3,1,8},
+      {6,4,2},
+    };
+
+  int FinvIndex1[4][6] =
+    {
+      {0,1,2,0,1,2},
+      {0,1,2,0,1,2},
+      {0,1,2,1,2,0},
+      {0,1,2,1,2,0}
+    };
+
+  int FinvIndex2[4][6] =
+    {
+      {0,1,2,0,1,2},
+      {0,1,2,1,2,0},
+      {0,1,2,0,1,2},
+      {0,1,2,1,2,0}
+    };
+
+
+  cmat.PutScalar(0.0);
 
   for (int i=0; i<6; ++i)
   {
     for (int j=0; j<6; ++j)
     {
-      cmat(i,j) = ((*cmatpf(cmatpfIndex[j][0]))[cmatpfIndex[i][0]]-S(StressIndex[i][j]))*F_inv(FinvIndex[i],0)*F_inv(FinvIndex[j],0)
-                  +(*cmatpf(cmatpfIndex[j][1]))[cmatpfIndex[i][0]]                      *F_inv(FinvIndex[i],0)*F_inv(FinvIndex[j],1)
-                  +(*cmatpf(cmatpfIndex[j][2]))[cmatpfIndex[i][0]]                      *F_inv(FinvIndex[i],0)*F_inv(FinvIndex[j],2)
-                  +(*cmatpf(cmatpfIndex[j][0]))[cmatpfIndex[i][1]]                      *F_inv(FinvIndex[i],1)*F_inv(FinvIndex[j],0)
-                 +((*cmatpf(cmatpfIndex[j][1]))[cmatpfIndex[i][1]]-S(StressIndex[i][j]))*F_inv(FinvIndex[i],1)*F_inv(FinvIndex[j],1)
-                  +(*cmatpf(cmatpfIndex[j][2]))[cmatpfIndex[i][1]]                      *F_inv(FinvIndex[i],1)*F_inv(FinvIndex[j],2)
-                  +(*cmatpf(cmatpfIndex[j][0]))[cmatpfIndex[i][2]]                      *F_inv(FinvIndex[i],2)*F_inv(FinvIndex[j],0)
-                  +(*cmatpf(cmatpfIndex[j][1]))[cmatpfIndex[i][2]]                      *F_inv(FinvIndex[i],2)*F_inv(FinvIndex[j],1)
-                 +((*cmatpf(cmatpfIndex[j][2]))[cmatpfIndex[i][2]]-S(StressIndex[i][j]))*F_inv(FinvIndex[i],2)*F_inv(FinvIndex[j],2);
+      for (int k=0; k<4; ++k)
+      {
+        cmat(i,j) += ((*cmatpf(cmatpfIndex2[j+k*6][0]))[cmatpfIndex1[i+k*6][0]]-S(StressIndex[i+k*6][j]))*F_inv(FinvIndex1[k][i],0)*F_inv(FinvIndex2[k][j],0)
+                     +(*cmatpf(cmatpfIndex2[j+k*6][1]))[cmatpfIndex1[i+k*6][0]]                          *F_inv(FinvIndex1[k][i],0)*F_inv(FinvIndex2[k][j],1)
+                     +(*cmatpf(cmatpfIndex2[j+k*6][2]))[cmatpfIndex1[i+k*6][0]]                          *F_inv(FinvIndex1[k][i],0)*F_inv(FinvIndex2[k][j],2)
+                     +(*cmatpf(cmatpfIndex2[j+k*6][0]))[cmatpfIndex1[i+k*6][1]]                          *F_inv(FinvIndex1[k][i],1)*F_inv(FinvIndex2[k][j],0)
+                    +((*cmatpf(cmatpfIndex2[j+k*6][1]))[cmatpfIndex1[i+k*6][1]]-S(StressIndex[i+k*6][j]))*F_inv(FinvIndex1[k][i],1)*F_inv(FinvIndex2[k][j],1)
+                     +(*cmatpf(cmatpfIndex2[j+k*6][2]))[cmatpfIndex1[i+k*6][1]]                          *F_inv(FinvIndex1[k][i],1)*F_inv(FinvIndex2[k][j],2)
+                     +(*cmatpf(cmatpfIndex2[j+k*6][0]))[cmatpfIndex1[i+k*6][2]]                          *F_inv(FinvIndex1[k][i],2)*F_inv(FinvIndex2[k][j],0)
+                     +(*cmatpf(cmatpfIndex2[j+k*6][1]))[cmatpfIndex1[i+k*6][2]]                          *F_inv(FinvIndex1[k][i],2)*F_inv(FinvIndex2[k][j],1)
+                    +((*cmatpf(cmatpfIndex2[j+k*6][2]))[cmatpfIndex1[i+k*6][2]]-S(StressIndex[i+k*6][j]))*F_inv(FinvIndex1[k][i],2)*F_inv(FinvIndex2[k][j],2);
+      }
     }
   }
+
+  cmat.Scale(0.25);
 
   return;
 }
