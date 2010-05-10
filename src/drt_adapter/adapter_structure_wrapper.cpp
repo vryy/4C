d747f119aaa7bca8980c18cba7b587e42fe1443c
Axel Gerstenberger <gerstenberger@lnm.mw.tum.de>
2010-05-10 09:51:54 +0000

    improved variable naming for monolithic FSI to better identify the meaning of "increment" -- approved by most (all?) monolithic users
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@11618 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_adapter/adapter_structure_wrapper.cpp
+++ src/drt_adapter/adapter_structure_wrapper.cpp
@@ -9,40 +9,44 @@
 void ADAPTER::StructureNOXCorrectionWrapper::PrepareTimeStep()
 {
   StructureWrapper::PrepareTimeStep();
-  if (disinc_!=Teuchos::null)
-    disinc_->PutScalar(0.);
+  if (disstepinc_!=Teuchos::null)
+    disstepinc_->PutScalar(0.);
 }
 
 
 /*----------------------------------------------------------------------*/
 /*----------------------------------------------------------------------*/
-void ADAPTER::StructureNOXCorrectionWrapper::Evaluate(Teuchos::RCP<const Epetra_Vector> disp)
+void ADAPTER::StructureNOXCorrectionWrapper::Evaluate(Teuchos::RCP<const Epetra_Vector> disstepinc)
 {
-  // Yes, this is complicated. But we have to be very careful
-  // here. The field solver always expects an increment only. And
+  // The field solver always expects an iteration increment only. And
   // there are Dirichlet conditions that need to be preserved. So take
-  // the sum of increments we get from NOX and apply the latest
+  // the sum of increments we get from NOX and apply the latest iteration
   // increment only.
-  if (disp!=Teuchos::null)
+  // Naming:
+  //
+  // x^n+1_i+1 = x^n+1_i + disiterinc  (sometimes referred to as residual increment), and
+  //
+  // x^n+1_i+1 = x^n     + disstepinc
+
+  if (disstepinc!=Teuchos::null)
   {
-    // residual displacements (or iteration increments or iteratively
-    // incremental displacements)
-    Teuchos::RCP<Epetra_Vector> disi = Teuchos::rcp(new Epetra_Vector(*disp));
-    if (disinc_!=Teuchos::null)
+    // iteration increments
+    Teuchos::RCP<Epetra_Vector> disiterinc = Teuchos::rcp(new Epetra_Vector(*disstepinc));
+    if (disstepinc_!=Teuchos::null)
     {
-      disi->Update(-1.0,*disinc_,1.0);
+      disiterinc->Update(-1.0,*disstepinc_,1.0);
 
       // update incremental displacement member to provided step increments
       // shortly: disinc_^<i> := disp^<i+1>
-      disinc_->Update(1.0,*disp,0.0);
+      disstepinc_->Update(1.0,*disstepinc,0.0);
     }
     else
     {
-      disinc_ = Teuchos::rcp(new Epetra_Vector(*disp));
+      disstepinc_ = Teuchos::rcp(new Epetra_Vector(*disstepinc));
     }
 
-    // do structural update with provided residual displacements
-    StructureWrapper::Evaluate(disi);
+    // do structural update with provided residual displacements - iteration increment
+    StructureWrapper::Evaluate(disiterinc);
   }
   else
   {
