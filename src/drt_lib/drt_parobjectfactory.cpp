6b53f5791827a97df792ef475efc3999ed20c462
Ulrich Kuettler <kuettler@lnm.mw.tum.de>
2010-07-30 08:17:23 +0000

    Lena demands her multi-scale examples to keep running. I don't know why. But well. ;) (This also solves the meshtying3D_structure failures.)
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@12256 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_lib/drt_parobjectfactory.cpp
+++ src/drt_lib/drt_parobjectfactory.cpp
@@ -241,12 +241,9 @@ void DRT::ParObjectFactory::InitializeElements( DRT::Discretization & dis )
     ElementType * eot = dynamic_cast<ElementType*>( pot );
     if ( eot!=NULL )
     {
-      if ( active_elements_.count( eot )==0 )
-      {
-        active_elements_.insert( eot );
-        int err = eot->Initialize( dis );
-        if (err) dserror("Element Initialize returned err=%d",err);
-      }
+      active_elements_.insert( eot );
+      int err = eot->Initialize( dis );
+      if (err) dserror("Element Initialize returned err=%d",err);
     }
     else
     {
