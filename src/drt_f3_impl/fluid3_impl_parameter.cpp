70b6721b41e1ca431c1201e3c2be206f0a0b0611
Ulrich Kuettler <kuettler@lnm.mw.tum.de>
2010-08-09 15:43:15 +0000

    extract fluid parameter class
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@12352 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/drt_f3_impl/fluid3_impl_parameter.cpp
@@ -0,0 +1,257 @@
+
+#include <string>
+
+#include "fluid3_impl_parameter.H"
+#include "../drt_lib/drt_dserror.H"
+
+//----------------------------------------------------------------------*/
+//    definition of the instance
+//----------------------------------------------------------------------*/
+DRT::ELEMENTS::Fluid3ImplParameter* DRT::ELEMENTS::Fluid3ImplParameter::instance_;
+
+
+//----------------------------------------------------------------------*/
+//    definition of the instance
+//----------------------------------------------------------------------*/
+DRT::ELEMENTS::Fluid3ImplParameter* DRT::ELEMENTS::Fluid3ImplParameter::Instance()
+{
+  if (instance_==NULL)
+    instance_ = new Fluid3ImplParameter();
+  return instance_;
+}
+
+
+//----------------------------------------------------------------------*/
+// private constructor of Fluid3ImplParameter
+//----------------------------------------------------------------------*/
+DRT::ELEMENTS::Fluid3ImplParameter::Fluid3ImplParameter()
+  :
+  is_genalpha_(false),
+  is_conservative_(false),
+  is_stationary_(false),
+  is_newton_(false),
+  is_inconsistent_(false),
+  physicaltype_(INPAR::FLUID::incompressible),
+  tds_(INPAR::FLUID::subscales_none),
+  transient_(INPAR::FLUID::inertia_stab_drop),
+  pspg_(INPAR::FLUID::pstab_use_pspg),
+  supg_(INPAR::FLUID::convective_stab_supg),
+  vstab_(INPAR::FLUID::viscous_stab_none),
+  cstab_(INPAR::FLUID::continuity_stab_yes),
+  cross_(INPAR::FLUID::cross_stress_stab_none),
+  reynolds_(INPAR::FLUID::reynolds_stress_stab_none),
+  whichtau_(INPAR::FLUID::tau_not_defined),
+  fssgv_(INPAR::FLUID::no_fssgv),
+  turb_mod_action_(INPAR::FLUID::no_model),
+  mat_gp_(false),     // standard evaluation of the material at the element center
+  tau_gp_(false),     // standard evaluation of tau at the element center
+  timefac_(0.0),
+  Cs_(0.0),
+  l_tau_(0.0)
+{
+}
+
+
+//----------------------------------------------------------------------*
+//  set general parameters                                   ehrl 04/10 |
+//---------------------------------------------------------------------*/
+void DRT::ELEMENTS::Fluid3ImplParameter::SetParameter( Teuchos::ParameterList& params )
+{
+//----------------------------------------------------------------------
+// get flags to switch on/off different fluid formulations
+//----------------------------------------------------------------------
+
+  //TODO: check, when is_stationary must be set
+  // set flag, if it is a stationary fluid system
+  is_stationary_ = params.get<bool>("is stationary");
+
+  // check whether we have a generalized-alpha time-integration scheme
+  is_genalpha_ = params.get<bool>("using generalized-alpha time integration");
+
+  // set flag for type of linearization (fixed-point-like or Newton)
+  std::string newtonstr   = params.get<std::string>("Linearisation");
+  if (newtonstr=="Newton")          is_newton_       = true;
+
+  // set flags for formuation of the convective velocity term (conservative or convective)
+  std::string convformstr = params.get<std::string>("form of convective term");
+  if (convformstr =="conservative") is_conservative_ = true;
+
+  // set flag for physical type of fluid flow
+  physicaltype_ = params.get<INPAR::FLUID::PhysicalType>("Physical Type");
+  if (((physicaltype_ != INPAR::FLUID::boussinesq) and (physicaltype_ != INPAR::FLUID::incompressible))
+      and (is_stationary_ == true))
+    dserror("physical type is not supported in stationary FLUID implementation.");
+
+//----------------------------------------------------------------------
+// get control parameters for time integration
+//----------------------------------------------------------------------
+
+  // get current time: n+alpha_F for generalized-alpha scheme, n+1 otherwise
+  time_ = params.get<double>("total time",-1.0);
+
+  // set global variable timefac to zero
+  timefac_ = 0.0;
+
+  if (is_stationary_ == false)
+  {
+    // get time-step length and time-integration parameters
+    dt_                = params.get<double>("dt");
+    const double theta = params.get<double>("theta",-1.0);
+    omtheta_           = params.get<double>("omtheta",-1.0);
+
+    // compute timefactor for left-hand side
+    // One-step-Theta:    timefac = theta*dt
+    // BDF2:              timefac = 2/3 * dt
+    // generalized-alpha: timefac = (alpha_F/alpha_M) * gamma * dt
+    timefac_ = theta*dt_;
+
+    if(is_genalpha_)
+    {
+      gamma_ =params.get<double>("gamma");
+      alphaF_=params.get<double>("alphaF");
+      alphaM_=params.get<double>("alphaM");
+    }
+    else
+    {
+      gamma_ =theta;
+      alphaF_=1.0;
+      alphaM_=1.0;
+    }
+
+    // in the case of not genalpha: afgdt = theta * dt_
+    afgdt_=alphaF_*gamma_*dt_;
+  }
+  else
+  {
+    // timefac stationary = 1.0
+    timefac_ = 1.0;
+  }
+
+  if (timefac_ < 0.0 or time_ < 0.0)
+    dserror("Negative time-integration parameter or time-step length supplied");
+
+// ---------------------------------------------------------------------
+// get control parameters for stabilization and higher-order elements
+//----------------------------------------------------------------------
+  Teuchos::ParameterList& stablist = params.sublist("STABILIZATION");
+
+  // no safety check necessary since all options are used
+  tds_      = Teuchos::getIntegralValue<INPAR::FLUID::SubscalesTD>(stablist,"TDS");
+  transient_= Teuchos::getIntegralValue<INPAR::FLUID::Transient>(stablist,"TRANSIENT");
+  pspg_     = Teuchos::getIntegralValue<INPAR::FLUID::PSPG>(stablist,"PSPG");
+  supg_     = Teuchos::getIntegralValue<INPAR::FLUID::SUPG>(stablist,"SUPG");
+  vstab_    = Teuchos::getIntegralValue<INPAR::FLUID::VStab>(stablist,"VSTAB");
+  cstab_    = Teuchos::getIntegralValue<INPAR::FLUID::CStab>(stablist,"CSTAB");
+  cross_    = Teuchos::getIntegralValue<INPAR::FLUID::CrossStress>(stablist,"CROSS-STRESS");
+  reynolds_ = Teuchos::getIntegralValue<INPAR::FLUID::ReynoldsStress>(stablist,"REYNOLDS-STRESS");
+
+//-------------------------------
+// get tau definition
+//-------------------------------
+
+  whichtau_ =  Teuchos::getIntegralValue<INPAR::FLUID::TauType>(stablist,"DEFINITION_TAU");
+  // check if tau can be handled
+  if (not(whichtau_ == INPAR::FLUID::tautype_franca_barrenechea_valentin_wall or
+      INPAR::FLUID::tautype_bazilevs or
+      INPAR::FLUID::tautype_franca_barrenechea_valentin_codina or
+      INPAR::FLUID::tautype_oberai))
+    dserror("Definition of Tau cannot be handled by the element");
+
+  // TODO: stationary tau definition cannot be choosen in the dat.-file
+  if (is_stationary_ == true)
+    whichtau_ = INPAR::FLUID::tautype_stationary;
+
+  // overrule higher_order_ele if input-parameter is set
+  // this might be interesting for fast (but slightly
+  // less accurate) computations
+  if (stablist.get<std::string>("STABTYPE") == "inconsistent") is_inconsistent_ = true;
+
+  // set flags for potential evaluation of tau and material law at int. point
+  // default value: evaluation at element center
+  const std::string tauloc = stablist.get<std::string>("EVALUATION_TAU");
+  if (tauloc == "integration_point") tau_gp_ = true;
+  else                               tau_gp_ = false;
+  const std::string matloc = stablist.get<std::string>("EVALUATION_MAT");
+  if (matloc == "integration_point") mat_gp_ = true;
+  else                               mat_gp_ = false;
+
+//---------------------------------------------------------------------------------
+// parameter for turbulence and subgrid-viscosity approach
+//---------------------------------------------------------------------------------
+
+  //TODO: change Parameter in ValidParameter
+  // get flag for fine-scale subgrid-viscosity approach
+  {
+    const std::string fssgvdef = params.get<std::string>("fs subgrid viscosity","No");
+
+    if (fssgvdef == "Smagorinsky_all")        fssgv_ = INPAR::FLUID::smagorinsky_all;
+    else if (fssgvdef == "Smagorinsky_small") fssgv_ = INPAR::FLUID::smagorinsky_small;
+  }
+
+  Teuchos::ParameterList& turbmodelparams    = params.sublist("TURBULENCE MODEL");
+
+  if (fssgv_ != INPAR::FLUID::no_fssgv) Cs_ = turbmodelparams.get<double>("C_SMAGORINSKY",0.0);
+
+  // the default action is no model
+  turb_mod_action_ = INPAR::FLUID::no_model;
+
+  // No turbulent flow: TURBULENCE_APPROACH = DNS
+  if (turbmodelparams.get<std::string>("TURBULENCE_APPROACH", "none") == "CLASSICAL_LES")
+  {
+    if (is_stationary_ == true)
+      dserror("Stationary turbulent flow does not make any sense");
+
+    //TODO: Check parameter for 2D and 3D case
+
+    // get Smagorinsky model parameter for fine-scale subgrid viscosity
+    // (Since either all-scale Smagorinsky model (i.e., classical LES model
+    // as will be inititalized below) or fine-scale Smagorinsky model is
+    // used (and never both), the same input parameter can be exploited.)
+
+    std::string& physical_turbulence_model = turbmodelparams.get<std::string>("PHYSICAL_MODEL");
+
+    // --------------------------------------------------
+    // standard constant coefficient Smagorinsky model
+    if (physical_turbulence_model == "Smagorinsky")
+    {
+      // the classic Smagorinsky model only requires one constant parameter
+      turb_mod_action_ = INPAR::FLUID::smagorinsky;
+      Cs_              = turbmodelparams.get<double>("C_SMAGORINSKY");
+    }
+    // --------------------------------------------------
+    // Smagorinsky model with van Driest damping
+    else if (physical_turbulence_model == "Smagorinsky_with_van_Driest_damping")
+    {
+      // that's only implemented for turbulent channel flow
+      // wall function length is hard implemented
+      if (turbmodelparams.get<std::string>("CANONICAL_FLOW","no")
+          !=
+          "channel_flow_of_height_2")
+          dserror("van_Driest_damping only for channel_flow_of_height_2\n");
+
+      // for the Smagorinsky model with van Driest damping, we need
+      // a viscous length to determine the y+ (heigth in wall units)
+      turb_mod_action_ = INPAR::FLUID::smagorinsky_with_van_Driest_damping;
+
+      // get parameters of model
+      Cs_              = turbmodelparams.get<double>("C_SMAGORINSKY");
+      l_tau_           = turbmodelparams.get<double>("CHANNEL_L_TAU");
+    }
+
+    // --------------------------------------------------
+    // Smagorinsky model with dynamic Computation of Cs
+    else if (physical_turbulence_model == "Dynamic_Smagorinsky")
+    {
+      turb_mod_action_ = INPAR::FLUID::dynamic_smagorinsky;
+
+      // In the case of dynamic Smagorinsky:
+      // Cs_ is calculated from Cs_sqrt_delta to compare it with the standard
+      // it is stored in Cs_ after its calculation in CalcSubgrVisc
+      Cs_ = 0.0;
+    }
+    else
+    {
+      dserror("Up to now, only Smagorinsky (constant coefficient with and without wall function as well as dynamic) is available");
+    }
+  } // end if(Classical LES)
+}
