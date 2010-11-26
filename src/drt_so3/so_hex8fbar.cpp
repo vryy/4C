0f923b3082ee98d2cb1851fbc60a2cc8de3a9539
Susanna Tinkl <tinkl@lnm.mw.tum.de>
2010-11-26 13:52:33 +0000

    ComputeNullSpace for FBAR element
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@12996 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_so3/so_hex8fbar.cpp
+++ src/drt_so3/so_hex8fbar.cpp
@@ -53,15 +53,15 @@ Teuchos::RCP<DRT::Element> DRT::ELEMENTS::So_hex8fbarType::Create( const int id,
 
 void DRT::ELEMENTS::So_hex8fbarType::NodalBlockInformation( DRT::Element * dwele, int & numdf, int & dimns, int & nv, int & np )
 {
-   numdf = 3;
-   dimns = 6;
-   nv = 3;
-   np = 0;
+  numdf = 3;
+  dimns = 6;
+  nv = 3;
+  np = 0;
 }
 
 void DRT::ELEMENTS::So_hex8fbarType::ComputeNullSpace( DRT::Discretization & dis, std::vector<double> & ns, const double * x0, int numdf, int dimns )
 {
-//   DRT::UTILS::ComputeStructure3DNullSpace( dis, ns, x0, numdf, dimns );
+  DRT::UTILS::ComputeStructure3DNullSpace( dis, ns, x0, numdf, dimns );
 }
 
 void DRT::ELEMENTS::So_hex8fbarType::SetupElementDefinition( std::map<std::string,std::map<std::string,DRT::INPUT::LineDefinition> > & definitions )
