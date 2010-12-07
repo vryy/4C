9d6a6e1b3b5698616cc9d0ea9d64e01dc5007c5d
Christian Cyron <cyron@lnm.mw.tum.de>
2010-12-07 14:39:40 +0000

    changed default value of artificial damping moment to artificial = 4000
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@13103 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_beam3/beam3_evaluate.cpp
+++ src/drt_beam3/beam3_evaluate.cpp
@@ -1169,7 +1169,7 @@ inline void DRT::ELEMENTS::Beam3::MyDampingConstants(ParameterList& params,LINAL
   /*damping coefficient of rigid straight rod spinning around its own axis according to Howard, p. 107, table 6.2;
    *as this coefficient is very small for thin rods it is increased artificially by a factor for numerical convencience*/
   double rsquare = pow((4*Iyy_/PI),0.5);
-  double artificial = 1000;//1000;  //1000 not bad for standard Actin3D_10.dat files; for 40 elements also 1 seems to work really well
+  double artificial = 4000;//1000;  //1000 not bad for standard Actin3D_10.dat files; for 40 elements also 1 seems to work really well
   gamma(2) = 4*PI*params.get<double>("ETA",0.0)*rsquare*artificial;
 
 
