03afe3aeaf0cbe40af223c08b37fc2ddc22f7f49
Ulrich Kuettler <kuettler@lnm.mw.tum.de>
2010-12-21 20:46:22 +0000

    Try to fix my periodic boundary bug before buildbot complains. Lets see if it worked.
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@13223 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_fluid/drt_pbcdofset.cpp
+++ src/drt_fluid/drt_pbcdofset.cpp
@@ -44,4 +44,45 @@ PBCDofSet::~PBCDofSet()
 }
 
 
+int PBCDofSet::AssignDegreesOfFreedom(const DRT::Discretization& dis, const unsigned dspos, const int start)
+{
+  DRT::DofSet::AssignDegreesOfFreedom(dis,dspos,start);
+
+  // loop all master nodes and set the degrees of freedom of
+  // the slaves to the degrees of freedom of the master
+  for(map<int,vector<int> >::iterator master = perbndcouples_->begin();
+      master != perbndcouples_->end();
+      ++master )
+  {
+    int master_lid=dis.NodeColMap()->LID(master->first);
+
+    if (master_lid<0)
+    {
+      dserror("master not on proc");
+    }
+
+    for (vector<int>::iterator slave=master->second.begin();
+         slave!=master->second.end();
+         ++slave)
+    {
+      int slave_lid=dis.NodeColMap()->LID(*slave);
+
+      if (slave_lid>-1)
+      {
+        (*numdfcolnodes_)[slave_lid] = (*numdfcolnodes_)[master_lid];
+        (*idxcolnodes_)  [slave_lid] = (*idxcolnodes_)  [master_lid];
+      }
+      else
+      {
+#ifdef DEBUG
+        if(dis.NodeRowMap()->MyGID(master->first))
+        {
+          dserror("slave not on proc but master owned by proc\n");
+        }
+#endif
+      }
+    }
+  }
+}
+
 #endif  // #ifdef CCADISCRET
