b36f3fb1931d78bd2f2a6bdd09dd4a92e25aba50
Sophie Rausch <rausch@lnm.mw.tum.de>
2011-02-08 14:53:57 +0000

    Deleted unused versions of the inverse analysis.
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@13453 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_structure/str_invanalysis.cpp
+++ src/drt_structure/str_invanalysis.cpp
@@ -33,8 +33,6 @@ Maintainer: Sophie Rausch
 #include "../drt_inpar/drt_validparameters.H"
 #include "stru_resulttest.H"
 #include "../drt_inv_analysis/inv_analysis.H"
-#include "../drt_inv_analysis/airways_inv_analysis.H"
-#include "../drt_inv_analysis/coll_inv_analysis.H"
 #include "../drt_inv_analysis/gen_inv_analysis.H"
 #include "../drt_inpar/inpar_invanalysis.H"
 
@@ -101,17 +99,6 @@ void STR::invanalysis()
     case INPAR::STR::inv_lung:
     {
       STR::InvAnalysis ia(actdis,solver,output);
-      ia.Integrate();
-    }
-    break;
-    case INPAR::STR::inv_airways:
-    {
-      STR::AirwaysInvAnalysis ia(actdis,solver,output);
-      ia.Integrate();
-    }
-    case INPAR::STR::inv_coll:
-    {
-      STR::CollInvAnalysis ia(actdis,solver,output);
       ia.Integrate();
     }
     break;
