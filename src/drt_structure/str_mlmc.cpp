1bc35deb91b976d3724ff7e3f07c18f294f259c0
Jonas Biehler <biehler@lnm.mw.tum.de>
2011-02-25 15:05:30 +0000

    forgot to add to svn
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@13645 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/drt_structure/str_mlmc.cpp
@@ -0,0 +1,111 @@
+/*----------------------------------------------------------------------*/
+/*!
+\file str_mlmc.cpp
+\brief Multilevel Monte Carlo analysis for structures
+
+<pre>
+Maintainer: Jonas Biehler
+            biehler@lnm.mw.tum.de
+            http://www.lnm.mw.tum.de
+</pre>
+*/
+
+/*----------------------------------------------------------------------*/
+/* macros */
+#ifdef CCADISCRET
+
+/*----------------------------------------------------------------------*/
+/* headers */
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+
+#include <Teuchos_StandardParameterEntryValidators.hpp>
+
+#ifdef PARALLEL
+#include <mpi.h>
+#endif
+
+#include "str_mlmc.H"
+#include "../drt_io/io.H"
+#include "../drt_lib/drt_globalproblem.H"
+#include "../drt_inpar/drt_validparameters.H"
+#include "stru_resulttest.H"
+#include "../drt_mlmc/mlmc.H"
+#include "../drt_inpar/inpar_mlmc.H"
+
+#include "../drt_adapter/adapter_structure_timint.H"
+
+#include "strtimint.H"
+#include "strtimint_impl.H"
+#include "strtimint_expl.H"
+#include "strtimint_genalpha.H"
+#include "strtimint_ost.H"
+#include "strtimint_gemm.H"
+#include "strtimint_ab2.H"
+
+#include "strtimada.H"
+#include "strtimada_zienxie.H"
+#include "strtimada_joint.H"
+
+#include "strtimint_create.H"
+#include "strtimada_create.H"
+
+
+/*----------------------------------------------------------------------*/
+//! General problem data
+//!
+//! global variable GENPROB genprob is defined in global_control.c
+extern GENPROB genprob;
+
+/*======================================================================*/
+/* Multi Level Monte Carlo analysis of structures */
+void STR::mlmc()
+{
+  // get input lists
+  const Teuchos::ParameterList& mlmcp = DRT::Problem::Instance()->MultiLevelMonteCarloParams();
+
+  // access the discretization
+  Teuchos::RCP<DRT::Discretization> actdis = Teuchos::null;
+  actdis = DRT::Problem::Instance()->Dis(genprob.numsf, 0);
+
+  // set degrees of freedom in the discretization
+  if (not actdis->Filled()) actdis->FillComplete();
+
+  // context for output and restart
+  Teuchos::RCP<IO::DiscretizationWriter> output
+    = Teuchos::rcp(new IO::DiscretizationWriter(actdis));
+
+  // input parameters for structural dynamics
+  const Teuchos::ParameterList& sdyn
+    = DRT::Problem::Instance()->StructuralDynamicParams();
+
+  // show default parameters
+  if (actdis->Comm().MyPID() == 0)
+    DRT::INPUT::PrintDefaultParameters(std::cout, sdyn);
+
+  // create a solver
+  Teuchos::RCP<LINALG::Solver> solver
+    = Teuchos::rcp(new LINALG::Solver(DRT::Problem::Instance()->StructSolverParams(),
+                                      actdis->Comm(),
+                                      DRT::Problem::Instance()->ErrorFile()->Handle()));
+  actdis->ComputeNullSpaceIfNecessary(solver->Params());
+  bool perform_mlmc = Teuchos::getIntegralValue<int>(mlmcp,"MLMC");
+  //cout << perform_mlmc << "see if we come in here" << __LINE__ << __FILE__ <<endl;
+  if (perform_mlmc==true)
+  {
+    STR::MLMC mc(actdis,solver,output);
+    mc.Integrate();
+  }
+  else
+  {
+    dserror("Unknown type of Multi Level Monte Carlo Analysis");
+  }
+
+  // done
+  return;
+} // end str_mlmc()
+
+
+/*----------------------------------------------------------------------*/
+#endif  // #ifdef CCADISCRET
