3df323e33972317044d5e27a2f7b7d9d9da222ae
Ulrich Kuettler <kuettler@lnm.mw.tum.de>
2011-05-25 15:34:53 +0000

    remove old code
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@14172 060e22d8-9d1c-0410-868a-e3a950d63d95

--- tests/cut_test/cut_test_graph.cpp
+++ tests/cut_test/cut_test_graph.cpp
@@ -5,131 +5,14 @@
 #include <iterator>
 
 #include "../../src/drt_cut/cut_coloredgraph.H"
-#include "../../src/drt_cut/cut_graph.H"
 #include "cut_test_utils.H"
 
 void test_graph()
 {
-  GEO::CUT::GRAPH::Graph g;
-
-  g.Add( 1, 14 );
-  g.Add( 1, 20 );
-  g.Add( 1, 36 );
-
-  g.Add( 14, 19 );
-  g.Add( 14, 28 );
-  g.Add( 14, 35 );
-
-  g.Add( 19, 20 );
-  g.Add( 19, 35 );
-  g.Add( 19, 37 );
-
-  g.Add( 20, 27 );
-  g.Add( 20, 36 );
-  g.Add( 20, 37 );
-
-  g.Add( 26, 27 );
-  g.Add( 26, 28 );
-
-  g.Add( 35, 37 );
-
-  g.Add( 36, 37 );
-
-  g.Print();
-
-  std::vector<int> cycle;
-  cycle.push_back( 27 );
-  cycle.push_back( 20 );
-  cycle.push_back( 1 );
-  cycle.push_back( 36 );
-  cycle.push_back( 37 );
-  cycle.push_back( 35 );
-  cycle.push_back( 14 );
-  cycle.push_back( 28 );
-  cycle.push_back( 26 );
-
-  std::set<int> free;
-  g.GetAll( free );
-
-  for ( std::vector<int>::iterator i=cycle.begin(); i!=cycle.end(); ++i )
-  {
-    int p = *i;
-    free.erase( p );
-  }
-
-  GEO::CUT::GRAPH::Graph used;
-  used.AddCycle( cycle );
-
-  GEO::CUT::GRAPH::CycleList facet_cycles;
-
-  facet_cycles.AddPoints( g, used, cycle, free );
-
-  for ( GEO::CUT::GRAPH::CycleListIterator i=facet_cycles.begin();
-        i!=facet_cycles.end();
-        ++i )
-  {
-    const std::vector<int> & c = *i;
-    std::copy( c.begin(), c.end(), std::ostream_iterator<int>( std::cout, " " ) );
-    std::cout << "\n";
-  }
 }
 
 void test_graph2()
 {
-  GEO::CUT::GRAPH::Graph g;
-
-  g.Add( 0, 10 );
-  g.Add( 0, 11 );
-  g.Add( 0, 23 );
-
-  g.Add( 9, 10 );
-  g.Add( 9, 11 );
-  g.Add( 9, 23 );
-
-  g.Add( 10, 20 );
-  g.Add( 10, 23 );
-
-  g.Add( 11, 21 );
-  g.Add( 11, 23 );
-
-  g.Add( 20, 22 );
-
-  g.Add( 21, 22 );
-
-  g.Print();
-
-  std::vector<int> cycle;
-  cycle.push_back( 10 );
-  cycle.push_back(  0 );
-  cycle.push_back( 11 );
-  cycle.push_back( 21 );
-  cycle.push_back( 22 );
-  cycle.push_back( 20 );
-
-  std::set<int> free;
-  g.GetAll( free );
-
-  for ( std::vector<int>::iterator i=cycle.begin(); i!=cycle.end(); ++i )
-  {
-    int p = *i;
-    free.erase( p );
-  }
-
-  GEO::CUT::GRAPH::Graph used;
-  used.AddCycle( cycle );
-
-  GEO::CUT::GRAPH::CycleList facet_cycles;
-
-  facet_cycles.AddPoints( g, used, cycle, free );
-
-  for ( GEO::CUT::GRAPH::CycleListIterator i=facet_cycles.begin();
-        i!=facet_cycles.end();
-        ++i )
-  {
-    const std::vector<int> & c = *i;
-    std::copy( c.begin(), c.end(), std::ostream_iterator<int>( std::cout, " " ) );
-    std::cout << "\n";
-  }
 }
 
 void test_colored_graph()
