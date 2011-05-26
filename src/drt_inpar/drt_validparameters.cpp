a1c5b87e4f1c7053c8bf4fa8211240d8f165d0d7
Michael Gee <gee@lnm.mw.tum.de>
2011-05-26 11:29:06 +0000

    added a flag STRUCT_SE in the IO section that allows output of strain energy for every single element separately.
    - The feature is hooked up in old strugenalpha only right now
    - The feature is hooked up with st. Venent-Kirchhoff material only.
    - strain energy is written in every step where stresses are also written
    - se is automatically filtered by the ensight filter if present in the files
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@14175 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_inpar/drt_validparameters.cpp
+++ src/drt_inpar/drt_validparameters.cpp
@@ -642,6 +642,7 @@ Teuchos::RCP<const Teuchos::ParameterList> DRT::INPUT::ValidParameters()
   setStringToIntegralParameter<int>("OUTPUT_BIN","No","",yesnotuple,yesnovalue,&io);
 
   setStringToIntegralParameter<int>("STRUCT_DISP","Yes","",yesnotuple,yesnovalue,&io);
+  setStringToIntegralParameter<int>("STRUCT_SE","No","output of strain energy",yesnotuple,yesnovalue,&io);
   setStringToIntegralParameter<int>("STRUCT_STRESS","No","",
                                tuple<std::string>("No","no","NO",
                                                   "Yes","yes","YES",
