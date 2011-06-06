346cf5f4b4a30283719b2d9d6c16e4ecf13a4cf7
Georg Bauer <bauer@lnm.mw.tum.de>
2011-06-06 12:48:21 +0000

    fixed BACI:
    added missing "fluid_fluid_ale" to PROBLEMNAMES
    Now, problem types and problem names seem to be in sync again.
    
    
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@14227 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/headers/enums.h
+++ src/headers/enums.h
@@ -27,7 +27,7 @@ typedef enum _PROBLEM_TYP
                        prb_fluid_xfem,   /*  fluid problem including XFEM interfaces */
                        prb_fluid_xfem2,  /*  fluid problem including XFEM interfaces */
                        prb_fluid_fluid_ale, /*  fluid_fluid_ale problem */
-		       prb_fluid_fluid,  /*  fluid_fluid problem */
+		               prb_fluid_fluid,  /*  fluid_fluid problem */
                        prb_fluid_dgfem,  /*  fluid problem including XFEM interfaces */
                        prb_fluid_ale,    /*  fluid on an ale mesh (no structure) */
                        prb_freesurf,     /*  free surface fluid */
@@ -48,7 +48,7 @@ typedef enum _PROBLEM_TYP
  * initialize static variables. Keep in sync!
  * The trailing NULL is essential for the filters to read the problem
  * type! */
-#define PROBLEMNAMES { "none","fsi","fsi_xfem","fsi_lung","fsi_lung_gas","ssi","structure","structure_ale","fluid","fluid_xfem","fluid_xfem2","fluid_fluid","fluid_dgfem","fluid_ale","freesurf","opt","ale","tsi","thermo","fluid_pm","scatra","pfsi","loma","elch","combustion","art_net","red_airways",NULL }
+#define PROBLEMNAMES {"none","fsi","fsi_xfem","fsi_lung","fsi_lung_gas","ssi","structure","structure_ale","fluid","fluid_xfem","fluid_xfem2","fluid_fluid_ale","fluid_fluid","fluid_dgfem","fluid_ale","freesurf","opt","ale","tsi","thermo","fluid_pm","scatra","pfsi","loma","elch","combustion","art_net","red_airways",NULL }
 /*----------------------------------------------------------------------*
  | TIME TYPES                                             m.gee 7/01    |
  *----------------------------------------------------------------------*/
