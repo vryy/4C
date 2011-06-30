fc823c29d2182de4832d42a50ee696fa64cc55c3
Ulrich Kuettler <kuettler@lnm.mw.tum.de>
2011-06-30 18:03:59 +0000

    Make the test hard ... and get a failure. Ready to debug! :)
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@14411 060e22d8-9d1c-0410-868a-e3a950d63d95

--- tests/cut_test/cut_test_volume.cpp
+++ tests/cut_test/cut_test_volume.cpp
@@ -39,35 +39,42 @@ void test_cut_volumes()
 
 void test_cut_volumes2()
 {
-  GEO::CUT::Options options;
+  for ( int i=2; i<5; ++i )
+  {
+    for ( int j=2; j<5; ++j )
+    {
+      for ( int k=2; k<5; ++k )
+      {
+        GEO::CUT::Options options;
 
-  // this is meant to be used with matching boundaries. Thus, no
-  // inside/outside positions.
-  options.SetFindPositions( false );
+        // this is meant to be used with matching boundaries. Thus, no
+        // inside/outside positions.
+        options.SetFindPositions( false );
 
-  GEO::CUT::Mesh mesh1( options );
-  GEO::CUT::Mesh mesh2( options, 1, mesh1.Points() );
+        GEO::CUT::Mesh mesh1( options );
+        GEO::CUT::Mesh mesh2( options, 1, mesh1.Points() );
 
 #if 1
-  create_hex8_mesh( mesh1, 1, 1, 1 );
-  create_hex8_mesh( mesh2, 3, 3, 3 );
-//   create_hex8_mesh( mesh1, 1, 1, 1 );
-//   create_hex8_mesh( mesh2, 2, 2, 3 );
+        create_hex8_mesh( mesh1, 1, 1, 1 );
+        create_hex8_mesh( mesh2, i, j, k );
 #else
-  create_hex8_mesh( mesh1, 4, 4, 4 );
-  create_hex8_mesh( mesh2, 8, 9, 10 );
+        create_hex8_mesh( mesh1, 4, 4, 4 );
+        create_hex8_mesh( mesh2, 8, 9, 10 );
 #endif
 
-  mesh2.CreateSideIds();
+        mesh2.CreateSideIds();
 
-  mesh1.Status();
-  mesh2.Status();
+        mesh1.Status();
+        mesh2.Status();
 
-  GEO::CUT::plain_element_set elements_done;
+        GEO::CUT::plain_element_set elements_done;
 
-  mesh2.Cut( mesh1, elements_done );
+        mesh2.Cut( mesh1, elements_done );
 
-  cutmesh( mesh1 );
+        cutmesh( mesh1 );
 
-  mesh2.AssignOtherVolumeCells( mesh1 );
+        mesh2.AssignOtherVolumeCells( mesh1 );
+      }
+    }
+  }
 }
