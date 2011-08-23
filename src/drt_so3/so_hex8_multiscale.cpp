933de001990198d593a635b90c537448e670c344
Lena Yoshihara <yoshihara@lnm.mw.tum.de>
2011-08-23 14:53:57 +0000

    Beautified calculation of homogenized reference density
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@14694 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_so3/so_hex8_multiscale.cpp
+++ src/drt_so3/so_hex8_multiscale.cpp
@@ -29,84 +29,13 @@ using namespace std; // cout etc.
 void DRT::ELEMENTS::So_hex8::soh8_homog(ParameterList&  params)
 {
   double homogdens = 0.;
+  const static std::vector<double> weights = soh8_weights();
+  const double density = Material()->Density();
 
-/* ============================================================================*
-** CONST SHAPE FUNCTIONS, DERIVATIVES and WEIGHTS for HEX_8 with 8 GAUSS POINTS*
-** ============================================================================*/
-/* pointer to (static) shape function array
- * for each node, evaluated at each gp*/
-  LINALG::Matrix<NUMNOD_SOH8,NUMGPT_SOH8>* shapefct; //[NUMNOD_SOH8][NUMGPT_SOH8]
-/* pointer to (static) shape function derivatives array
- * for each node wrt to each direction, evaluated at each gp*/
-  LINALG::Matrix<NUMGPT_SOH8*NUMDIM_SOH8,NUMNOD_SOH8>* deriv;    //[NUMGPT_SOH8*NUMDIM][NUMNOD_SOH8]
-/* pointer to (static) weight factors at each gp */
-  LINALG::Matrix<NUMGPT_SOH8,1>* weights;  //[NUMGPT_SOH8]
-  soh8_shapederiv(&shapefct,&deriv,&weights);   // call to evaluate
-/* ============================================================================*/
-
-  // update element geometry
-  LINALG::Matrix<NUMNOD_SOH8,NUMDIM_SOH8> xrefe;  // material coord. of element
-  DRT::Node** nodes = Nodes();
-  for (int i=0; i<NUMNOD_SOH8; ++i){
-    const double* x = nodes[i]->X();
-    xrefe(i,0) = x[0];
-    xrefe(i,1) = x[1];
-    xrefe(i,2) = x[2];
+  for (int gp=0; gp<NUMGPT_SOH8; ++gp)
+  {
+    homogdens += detJ_[gp] * weights[gp] * density;
   }
-
-  /* =========================================================================*/
-  /* ================================================= Loop over Gauss Points */
-  /* =========================================================================*/
-  LINALG::Matrix<NUMDIM_SOH8,NUMGPT_SOH8> deriv_gp;
-  for (int gp=0; gp<NUMGPT_SOH8; ++gp) {
-
-    // get submatrix of deriv at actual gp
-    for (int m=0; m<NUMDIM_SOH8; ++m) {
-      for (int n=0; n<NUMGPT_SOH8; ++n) {
-        deriv_gp(m,n)=(*deriv)(NUMDIM_SOH8*gp+m,n);
-      }
-    }
-
-    /* compute the Jacobian matrix which looks like:
-    **         [ x_,r  y_,r  z_,r ]
-    **     J = [ x_,s  y_,s  z_,s ]
-    **         [ x_,t  y_,t  z_,t ]
-    */
-    LINALG::Matrix<NUMDIM_SOH8,NUMDIM_SOH8> jac;
-    //jac.Multiply('N','N',1.0,deriv_gp,xrefe,1.0);
-    jac.Multiply(deriv_gp, xrefe);
-
-    // compute determinant of Jacobian
-    double detJ = jac.Determinant();
-    if (detJ == 0.0) dserror("ZERO JACOBIAN DETERMINANT");
-    else if (detJ < 0.0) dserror("NEGATIVE JACOBIAN DETERMINANT");
-
-    // (material) deformation gradient F (=I in reference configuration)
-    LINALG::Matrix<NUMDIM_SOH8,NUMDIM_SOH8> defgrd(true);
-    for (unsigned int i = 0;i<3; ++i) defgrd(i,i) = 1.;
-
-    // Green-Lagrange strains matrix (=0 in reference configuration)
-    LINALG::Matrix<NUMSTR_SOH8,1> glstrain(true);
-
-    /* call material law cccccccccccccccccccccccccccccccccccccccccccccccccccccc
-    ** Here all possible material laws need to be incorporated,
-    ** the stress vector, a C-matrix, and a density must be retrieved,
-    ** every necessary data must be passed.
-    */
-    LINALG::Matrix<NUMSTR_SOH8,NUMSTR_SOH8> cmat(true);
-    LINALG::Matrix<NUMSTR_SOH8,1> stress(true);
-    LINALG::Matrix<NUMSTR_SOH8,1> plglstrain(true);
-    double density;
-    soh8_mat_sel(&stress,&cmat,&density,&glstrain,&plglstrain,&defgrd,gp,params);
-    // end of call material law ccccccccccccccccccccccccccccccccccccccccccccccc
-
-    double integrationfactor = detJ * (*weights)(gp);
-
-    homogdens += integrationfactor*density;
-
-   /* =========================================================================*/
-  }/* ==================================================== end of Loop over GP */
-   /* =========================================================================*/
 
   double homogdensity = params.get<double>("homogdens", 0.0);
   params.set("homogdens", homogdensity+homogdens);
