78d36e39f0758b69283283e248673fa700cf41bd
Shadan Shahmiri <shahmiri@lnm.mw.tum.de>
2012-02-01 10:40:10 +0000

    Added the function ComputeErrorXFEM and removed the earlier error calculation of jeffery hamel flow.
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@15559 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_f3_impl/fluid3_impl.cpp
+++ src/drt_f3_impl/fluid3_impl.cpp
@@ -548,6 +548,232 @@ int DRT::ELEMENTS::Fluid3Impl<distype>::ComputeError(
 }
 
 /*----------------------------------------------------------------------*
+ * Action type: Compute Error                              shahmiri 01/12
+ *----------------------------------------------------------------------*/
+template <DRT::Element::DiscretizationType distype>
+int DRT::ELEMENTS::Fluid3Impl<distype>::ComputeErrorXFEM(
+    DRT::ELEMENTS::Fluid3*          ele,
+    ParameterList&                  params,
+    Teuchos::RCP<MAT::Material>&    mat,
+    DRT::Discretization&            discretization,
+    vector<int>&                    lm,
+    Epetra_SerialDenseVector&       elevec1
+    )
+{
+  // integrations points and weights
+  // more GP than usual due to (possible) cos/exp fcts in analytical solutions
+  // degree 5
+  const DRT::UTILS::GaussIntegration intpoints(distype, 5);
+  return ComputeErrorXFEM( ele, params, mat,
+                            discretization, lm,
+                            elevec1, intpoints);
+}
+/*----------------------------------------------------------------------*
+ * Action type: Compute Error                              shahmiri 01/12
+ *----------------------------------------------------------------------*/
+template <DRT::Element::DiscretizationType distype>
+int DRT::ELEMENTS::Fluid3Impl<distype>::ComputeErrorXFEM(
+    DRT::ELEMENTS::Fluid3*          ele,
+    ParameterList&                  params,
+    Teuchos::RCP<MAT::Material>&    mat,
+    DRT::Discretization&            discretization,
+    vector<int>&                    lm,
+    Epetra_SerialDenseVector&       elevec1,
+    const DRT::UTILS::GaussIntegration & intpoints
+    )
+{
+  // analytical solution
+  LINALG::Matrix<nsd_,1>  u(true);
+  double p = 0.0;
+
+  // error
+  LINALG::Matrix<nsd_,1> deltavel(true);
+  double         deltap=0.0;
+
+  const int calcerr = DRT::INPUT::get<INPAR::FLUID::CalcError>(params,"calculate error");
+
+  //----------------------------------------------------------------------------
+  //   Extract velocity/pressure from global vectors
+  //----------------------------------------------------------------------------
+
+  // fill the local element vector/matrix with the global values
+  LINALG::Matrix<nsd_,nen_> evelaf(true);
+  LINALG::Matrix<nen_,1> epreaf(true);
+  ExtractValuesFromGlobalVector(discretization,lm, *rotsymmpbc_, &evelaf, &epreaf,"u and p at time n+1 (converged)");
+
+  //----------------------------------------------------------------------------
+  //                         ELEMENT GEOMETRY
+  //----------------------------------------------------------------------------
+
+  // get node coordinates
+  GEO::fillInitialPositionArray<distype,nsd_, LINALG::Matrix<nsd_,nen_> >(ele,xyze_);
+
+  if (ele->IsAle())
+  {
+    LINALG::Matrix<nsd_,nen_>       edispnp(true);
+    ExtractValuesFromGlobalVector(discretization,lm, *rotsymmpbc_, &edispnp, NULL,"dispnp");
+
+    // get new node positions for isale
+     xyze_ += edispnp;
+  }
+
+//------------------------------------------------------------------
+//                       INTEGRATION LOOP
+//------------------------------------------------------------------
+
+  for ( DRT::UTILS::GaussIntegration::iterator iquad=intpoints.begin(); iquad!=intpoints.end(); ++iquad )
+  {
+    // evaluate shape functions and derivatives at integration point
+    EvalShapeFuncAndDerivsAtIntPoint(iquad,ele->Id());
+
+    // get velocity at integration point
+    // (values at n+alpha_F for generalized-alpha scheme, n+1 otherwise)
+    velint_.Multiply(evelaf,funct_);
+
+    // get pressure at integration point
+    // (value at n+alpha_F for generalized-alpha scheme, n+1 otherwise)
+    double preint = funct_.Dot(epreaf);
+
+    // get coordinates at integration point
+    LINALG::Matrix<nsd_,1> xyzint(true);
+    xyzint.Multiply(xyze_,funct_);
+
+    // Compute analytical solution
+    switch(calcerr)
+    {
+    case INPAR::FLUID::beltrami_flow:
+    {
+      if (nsd_ == 3)
+      {
+         // get viscosity
+        if (mat->MaterialType() == INPAR::MAT::m_fluid)
+        {
+          const MAT::NewtonianFluid* actmat = static_cast<const MAT::NewtonianFluid*>(mat.get());
+
+          // get constant kinematic viscosity
+          visc_ = actmat->Viscosity()/actmat->Density();
+        }
+        else dserror("Material is not Newtonian Fluid");
+
+         const double a      = M_PI/4.0;
+         const double d      = M_PI/2.0;
+
+         const double t = f3Parameter_->time_;
+
+         // compute analytical pressure
+         p = -a*a/2.0 *
+             ( exp(2.0*a*xyzint(0))
+             + exp(2.0*a*xyzint(1))
+             + exp(2.0*a*xyzint(2))
+             + 2.0 * sin(a*xyzint(0) + d*xyzint(1)) * cos(a*xyzint(2) + d*xyzint(0)) * exp(a*(xyzint(1)+xyzint(2)))
+             + 2.0 * sin(a*xyzint(1) + d*xyzint(2)) * cos(a*xyzint(0) + d*xyzint(1)) * exp(a*(xyzint(2)+xyzint(0)))
+             + 2.0 * sin(a*xyzint(2) + d*xyzint(0)) * cos(a*xyzint(1) + d*xyzint(2)) * exp(a*(xyzint(0)+xyzint(1)))
+             )* exp(-2.0*visc_*d*d*t);
+
+          // compute analytical velocities
+          u(0) = -a * ( exp(a*xyzint(0)) * sin(a*xyzint(1) + d*xyzint(2)) +
+                       exp(a*xyzint(2)) * cos(a*xyzint(0) + d*xyzint(1)) ) * exp(-visc_*d*d*t);
+          u(1) = -a * ( exp(a*xyzint(1)) * sin(a*xyzint(2) + d*xyzint(0)) +
+                       exp(a*xyzint(0)) * cos(a*xyzint(1) + d*xyzint(2)) ) * exp(-visc_*d*d*t);
+          u(2) = -a * ( exp(a*xyzint(2)) * sin(a*xyzint(0) + d*xyzint(1)) +
+                       exp(a*xyzint(1)) * cos(a*xyzint(2) + d*xyzint(0)) ) * exp(-visc_*d*d*t);
+        }
+        else dserror("action 'calc_fluid_beltrami_error' is a 3D specific action");
+      }
+      break;
+    case INPAR::FLUID::shear_flow:
+      {
+        const double maxvel = 1.0;
+        const double hight = 1.0;
+
+        // y=0 is located in the middle of the domain
+        if (nsd_ == 2)
+        {
+          p = 1.0;
+          u(0) = xyzint(1)*maxvel + hight/2*maxvel;
+          u(1) = 0.0;
+        }
+        if (nsd_ == 3)
+        {
+          p = 0.0;
+          u(0) = xyzint(1)*maxvel + hight/2*maxvel;
+          u(1) = 0.0;
+          u(2) = 0.0;
+        }
+      }
+      break;
+    case INPAR::FLUID::channel2D:
+      {
+        const double maxvel=1.25;
+        const double hight = 1.0;
+        const double visc = 1.0;
+        const double pressure_gradient = 10.0;
+
+        // u_max = 1.25
+        // y=0 is located in the middle of the channel
+        if (nsd_ == 2)
+        {
+          p = 1.0;
+          //p = -10*xyzint(0)+20;
+          u(0) = maxvel -((hight*hight)/(2.0*visc)*pressure_gradient*(xyzint(1)/hight)*(xyzint(1)/hight));
+          u(1) = 0.0;
+        }
+        else
+          dserror("3D analytical solution is not implemented yet");
+      }
+      break;
+    case INPAR::FLUID::jeffery_hamel_flow:
+    {
+      //LINALG::Matrix<3,1> physpos(true);
+      //GEO::elementToCurrentCoordinates(distype, xyzint, xsi_, physpos);
+
+      double position[2];
+      position[0] = xyzint(0);
+      position[1] = xyzint(1);
+
+      if (1.0 < position[0] and position[0] < 2.0 and 0.0 < position[1] and position[1] < position[0])
+      {
+        const double u_exact_x = DRT::Problem::Instance()->Funct(0).Evaluate(0,position,0.0,NULL);
+        const double u_exact_y = DRT::Problem::Instance()->Funct(0).Evaluate(1,position,0.0,NULL);
+        u(0) = u_exact_x;
+        u(1) = u_exact_y;
+      }
+
+    }
+      break;
+    default:
+      dserror("analytical solution is not defined");
+    }
+
+    // compute difference between analytical solution and numerical solution
+    deltap    = preint - p;
+    deltavel.Update(1.0, velint_, -1.0, u);
+
+    // L2 error
+    // 0: vel_mag
+    // 1: p
+    // 2: vel_mag,analytical
+    // 3: p_analytic
+    // (4: vel_x)
+    // (5: vel_y)
+    // (6: vel_z)
+    for (int isd=0;isd<nsd_;isd++)
+    {
+      elevec1[0] += deltavel(isd)*deltavel(isd)*fac_;
+      //integrate analytical velocity (computation of relative error)
+      elevec1[2] += u(isd)*u(isd)*fac_;
+      // velocity components
+      //elevec1[isd+4] += deltavel(isd)*deltavel(isd)*fac_;
+    }
+    elevec1[1] += deltap*deltap*fac_;
+    //integrate analytical pressure (computation of relative error)
+    elevec1[3] += p*p*fac_;
+  }
+
+  return 0;
+}
+
+/*----------------------------------------------------------------------*
  * Action type: Evaluate
  *----------------------------------------------------------------------*/
 template <DRT::Element::DiscretizationType distype>
@@ -829,10 +1055,6 @@ int DRT::ELEMENTS::Fluid3Impl<distype>::Evaluate(
   const double thermpressdtaf = params.get<double>("thermpressderiv at n+alpha_F/n+1");
   const double thermpressdtam = params.get<double>("thermpressderiv at n+alpha_M/n+1");
 
-#ifdef JEFFERYHAMELFLOW
-  L2_ = params.get<double>("L2");
-#endif
-
 
   // ---------------------------------------------------------------------
   // set parameters for classical turbulence models
@@ -886,9 +1108,6 @@ int DRT::ELEMENTS::Fluid3Impl<distype>::Evaluate(
          svelnp,
          intpoints);
 
-#ifdef JEFFERYHAMELFLOW
-  params.set("L2",L2_);
-#endif
 
   // ---------------------------------------------------------------------
   // output values of Cs, visceff and Cs_delta_sq
@@ -1599,25 +1818,6 @@ void DRT::ELEMENTS::Fluid3Impl<distype>::Sysmat(
                          timefacfac);
       else
         dserror("Linearization of the mesh motion is not available in 1D");
-
-#ifdef JEFFERYHAMELFLOW
-      LINALG::Matrix<3,1> physpos(true);
-      GEO::elementToCurrentCoordinates(distype, xyze_, xsi_, physpos);
-
-      double position[2];
-      position[0] = physpos(0);
-      position[1] = physpos(1);
-      const double u_exact_x = DRT::Problem::Instance()->Funct(0).Evaluate(0,position,0.0,NULL);
-      const double u_exact_y = DRT::Problem::Instance()->Funct(0).Evaluate(1,position,0.0,NULL);
-
-      if (1.0 < position[0] and position[0] < 2.0 and 0.0 < position[1] and position[1] < position[0])
-      {
-        const double epsilon_x = (velint_(0) - u_exact_x);
-        const double epsilon_y = (velint_(1) - u_exact_y);
-
-        L2_ += (epsilon_x*epsilon_x + epsilon_y*epsilon_y)*fac_;
-      }
-#endif
     }
   }
   //------------------------------------------------------------------------
@@ -8557,9 +8757,9 @@ namespace DRT
             dserror("define start side xi-coordinates for unsupported cell type");
           }
 
-          const double absTolIncr = 1.0e-10;   // rel tolerance for the local coordinates increment
-          const double relTolRes  = 1.0e-10;   // rel tolerance for the whole residual
-          const double absTOLdist = 1.0e-10;   // abs tolerance for distance
+          const double absTolIncr = 1.0e-9;   // rel tolerance for the local coordinates increment
+          const double relTolRes  = 1.0e-9;   // rel tolerance for the whole residual
+          const double absTOLdist = 1.0e-9;   // abs tolerance for distance
 
           int iter=0;
           const int maxiter = 7;
@@ -11523,7 +11723,6 @@ void Fluid3Impl<distype>::ElementXfemInterface(
   Epetra_SerialDenseMatrix&  Cuiui
   )
 {
-
   const Teuchos::RCP<Epetra_Vector> iforcecol = params.get<Teuchos::RCP<Epetra_Vector> >("iforcenp", Teuchos::null);
 
 
@@ -12707,7 +12906,6 @@ void Fluid3Impl<distype>::ElementXfemInterfaceNitsche(
   LINALG::Matrix<nsd_,nen_> evelaf(true);
   LINALG::Matrix<nen_,1> epreaf(true);
   ExtractValuesFromGlobalVector(dis, lm, *rotsymmpbc_, &evelaf, &epreaf, "velaf");
-
 
   // integrate surface
 
