fc134ebfb555730ef0c7075ee6f54df8cfa2ee25
Karl-Robert Wichmann <wichmann@lnm.mw.tum.de>
2012-04-13 13:51:57 +0000

    switch to cdash on jacobi
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@16145 060e22d8-9d1c-0410-868a-e3a950d63d95

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,7 @@
 set(CTEST_PROJECT_NAME "baci")
-set(CTEST_NIGHTLY_START_TIME "21:59:59 GMT")
+set(CTEST_NIGHTLY_START_TIME "00:01:00 GMT")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "navier.lnm.mw.tum.de")
+set(CTEST_DROP_SITE "jacobi.lnm.mw.tum.de")
 set(CTEST_DROP_LOCATION "/cdash/submit.php?project=baci")
 set(CTEST_DROP_SITE_CDASH TRUE)
