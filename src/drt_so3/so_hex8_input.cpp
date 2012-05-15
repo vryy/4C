5bbc1d4d4ba54d6d72f324831e08d51f0717463b
Jonas Biehler <biehler@lnm.mw.tum.de>
2012-05-15 11:47:26 +0000

    beauty! leftover from Baci cleaning week
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@16279 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_so3/so_hex8_input.cpp
+++ src/drt_so3/so_hex8_input.cpp
@@ -42,57 +42,103 @@ bool DRT::ELEMENTS::So_hex8::ReadElement(const std::string& eletype,
 
   SetMaterial(material);
 
+  switch (Material()->MaterialType()){
   // special element-dependent input of material parameters
-  if (Material()->MaterialType() == INPAR::MAT::m_artwallremod)
-  {
-    MAT::ArtWallRemod* remo = static_cast <MAT::ArtWallRemod*>(Material().get());
-    remo->Setup(NUMGPT_SOH8, this->Id(), linedef);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_viscoanisotropic){
-    MAT::ViscoAnisotropic* visco = static_cast <MAT::ViscoAnisotropic*>(Material().get());
-    visco->Setup(NUMGPT_SOH8, linedef);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_visconeohooke){
-    MAT::ViscoNeoHooke* visco = static_cast <MAT::ViscoNeoHooke*>(Material().get());
-    visco->Setup(NUMGPT_SOH8);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_viscogenmax){
-    MAT::ViscoGenMax* viscogenmax = static_cast <MAT::ViscoGenMax*>(Material().get());
-    viscogenmax->Setup(NUMGPT_SOH8,linedef);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_charmm){
-    MAT::CHARMM* charmm = static_cast <MAT::CHARMM*>(Material().get());
-    charmm->Setup(data_);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_elasthyper){
-    MAT::ElastHyper* elahy = static_cast <MAT::ElastHyper*>(Material().get());
-    elahy->Setup(linedef);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_aaaraghavanvorp_damage){
-    double strength = 0.0; // section for extracting the element strength
-    linedef->ExtractDouble("STRENGTH",strength);
-    MAT::AAAraghavanvorp_damage* aaadamage = static_cast <MAT::AAAraghavanvorp_damage*>(Material().get());
-    aaadamage->Setup(NUMGPT_SOH8,strength);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_holzapfelcardiovascular){
-    MAT::HolzapfelCardio* holzcard = static_cast <MAT::HolzapfelCardio*>(Material().get());
-    holzcard->Setup(NUMGPT_SOH8, linedef);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_plneohooke){
-    MAT::PlasticNeoHooke* plastic = static_cast <MAT::PlasticNeoHooke*>(Material().get());
-    plastic->Setup(NUMGPT_SOH8);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_pllinelast){
-    MAT::PlasticLinElast* plastic = static_cast <MAT::PlasticLinElast*>(Material().get());
-    plastic->Setup(NUMGPT_SOH8);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_thermopllinelast){
-    MAT::ThermoPlasticLinElast* plastic = static_cast <MAT::ThermoPlasticLinElast*>(Material().get());
-    plastic->Setup(NUMGPT_SOH8);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_vp_robinson){
-    MAT::Robinson* robinson = static_cast <MAT::Robinson*>(Material().get());
-    robinson->Setup(NUMGPT_SOH8, linedef);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_humphreycardiovascular){
-    MAT::HumphreyCardio* humcard = static_cast <MAT::HumphreyCardio*>(Material().get());
-    humcard->Setup(NUMGPT_SOH8, linedef);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_growth){
-    MAT::Growth* grow = static_cast <MAT::Growth*>(Material().get());
-    grow->Setup(NUMGPT_SOH8, linedef);
-  } else if (Material()->MaterialType() == INPAR::MAT::m_constraintmixture){
-    MAT::ConstraintMixture* comix = static_cast <MAT::ConstraintMixture*>(Material().get());
-    comix->Setup(NUMGPT_SOH8, linedef);
+    case  INPAR::MAT::m_artwallremod:
+    {
+      MAT::ArtWallRemod* remo = static_cast <MAT::ArtWallRemod*>(Material().get());
+      remo->Setup(NUMGPT_SOH8, this->Id(), linedef);
+    }
+      break;
+    case INPAR::MAT::m_viscoanisotropic:
+    {
+      MAT::ViscoAnisotropic* visco = static_cast <MAT::ViscoAnisotropic*>(Material().get());
+      visco->Setup(NUMGPT_SOH8, linedef);
+    }
+      break;
+    case INPAR::MAT::m_visconeohooke:
+    {
+      MAT::ViscoNeoHooke* visco = static_cast <MAT::ViscoNeoHooke*>(Material().get());
+      visco->Setup(NUMGPT_SOH8);
+    }
+      break;
+    case  INPAR::MAT::m_viscogenmax:
+    {
+      MAT::ViscoGenMax* viscogenmax = static_cast <MAT::ViscoGenMax*>(Material().get());
+      viscogenmax->Setup(NUMGPT_SOH8,linedef);
+    }
+      break;
+    case INPAR::MAT::m_charmm:
+    {
+      MAT::CHARMM* charmm = static_cast <MAT::CHARMM*>(Material().get());
+      charmm->Setup(data_);
+    }
+      break;
+    case INPAR::MAT::m_elasthyper:
+    {
+      MAT::ElastHyper* elahy = static_cast <MAT::ElastHyper*>(Material().get());
+      elahy->Setup(linedef);
+    }
+      break;
+    case  INPAR::MAT::m_aaaraghavanvorp_damage:
+    {
+      double strength = 0.0; // section for extracting the element strength
+      linedef->ExtractDouble("STRENGTH",strength);
+      MAT::AAAraghavanvorp_damage* aaadamage = static_cast <MAT::AAAraghavanvorp_damage*>(Material().get());
+      aaadamage->Setup(NUMGPT_SOH8,strength);
+    }
+      break;
+    case INPAR::MAT::m_holzapfelcardiovascular:
+    {
+      MAT::HolzapfelCardio* holzcard = static_cast <MAT::HolzapfelCardio*>(Material().get());
+      holzcard->Setup(NUMGPT_SOH8, linedef);
+    }
+      break;
+    case INPAR::MAT::m_plneohooke:
+    {
+      MAT::PlasticNeoHooke* plastic = static_cast <MAT::PlasticNeoHooke*>(Material().get());
+      plastic->Setup(NUMGPT_SOH8);
+    }
+      break;
+    case  INPAR::MAT::m_pllinelast:
+    {
+      MAT::PlasticLinElast* plastic = static_cast <MAT::PlasticLinElast*>(Material().get());
+      plastic->Setup(NUMGPT_SOH8);
+    }
+      break;
+    case  INPAR::MAT::m_thermopllinelast:
+    {
+      MAT::ThermoPlasticLinElast* plastic = static_cast <MAT::ThermoPlasticLinElast*>(Material().get());
+      plastic->Setup(NUMGPT_SOH8);
+    }
+      break;
+    case  INPAR::MAT::m_vp_robinson:
+    {
+      MAT::Robinson* robinson = static_cast <MAT::Robinson*>(Material().get());
+      robinson->Setup(NUMGPT_SOH8, linedef);
+    }
+      break;
+    case  INPAR::MAT::m_humphreycardiovascular:
+    {
+      MAT::HumphreyCardio* humcard = static_cast <MAT::HumphreyCardio*>(Material().get());
+      humcard->Setup(NUMGPT_SOH8, linedef);
+    }
+      break;
+    case INPAR::MAT::m_growth:
+    {
+      MAT::Growth* grow = static_cast <MAT::Growth*>(Material().get());
+      grow->Setup(NUMGPT_SOH8, linedef);
+    }
+      break;
+    case INPAR::MAT::m_constraintmixture:
+    {
+      MAT::ConstraintMixture* comix = static_cast <MAT::ConstraintMixture*>(Material().get());
+      comix->Setup(NUMGPT_SOH8, linedef);
+    }
+      break;
+    default:
+      break;
   }
-
   // temporary variable for read-in
    std::string buffer;
 
