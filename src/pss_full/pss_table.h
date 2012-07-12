ca619679d32c073d0efedd38e5bda054eeb124fa
Georg Bauer <bauer@lnm.mw.tum.de>
2012-07-12 13:19:56 +0000

    forgot to commit two files.
    By the way: 
    I also removed the defines flag WIN_MUENCH from the io routines
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@16510 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/pss_full/pss_table.h
+++ src/pss_full/pss_table.h
@@ -47,105 +47,10 @@ table. This table can be queried for those values quite easily.
 
 #include "../headers/standardtypes.h"
 #include "../headers/am.h"
+#include "../pss_full/pss_types.h"
 #ifdef PARALLEL
-#include <mpi.h>
+#include <mpi.h>  /* Be careful: this is the C mpi header */
 #endif
-
-
-/*----------------------------------------------------------------------*/
-/*!
-  \brief Types of the symbols we store in a map.
-
-  We can have strings, integer and doubles as well as a subgroup.
-
-  \author u.kue
-  \date 08/04
-*/
-/*----------------------------------------------------------------------*/
-typedef enum _SYMBOL_TYPES {
-  sym_undefined,
-  sym_string,
-  sym_int,
-  sym_real,
-  sym_map
-} SYMBOL_TYPES;
-
-
-/*----------------------------------------------------------------------*/
-/*!
-  \brief Data struct of a map's symbol.
-
-  Depending on its type a symbol contains different values. You don't
-  need to deal with these explicitly, there are access
-  functions. However, you need to access the ``next`` pointer.
-
-  Inside a map all symbols with the same key are linked by their next
-  pointers. Thus a map node gives the first symbol and from there you
-  can follow the next pointer until you reach a NULL.
-
-  \warning String and map values are assumed to be allocated
-  dynamically and are owned by the symbol. That is when a symbol is
-  destroyed its string or map value will be free'd, too.
-
-  \author u.kue
-  \date 08/04
-*/
-/*----------------------------------------------------------------------*/
-typedef struct _SYMBOL {
-  /* put the union first to have the double value properly aligned */
-  union {
-    CHAR* string;
-    INT integer;
-    DOUBLE real;
-    struct _MAP* dir;
-  } s;
-  SYMBOL_TYPES type;
-  struct _SYMBOL* next;
-} SYMBOL;
-
-
-/*----------------------------------------------------------------------*/
-/*!
-  \brief A map node that contains a key and a symbol.
-
-  The backbone structure of a map is the map node. Map nodes
-  constitute a binary tree (unbalanced by now), so there are left and
-  right subnodes. The symbol is the start of a list of symbols that
-  share one key.
-
-  The node knows how many symbols there are.
-
-  \warning The map node owns its key and symbols as well as any
-  subnodes. It'll destroy them when it is destroyed itself.
-
-  \author u.kue
-  \date 08/04
-*/
-/*----------------------------------------------------------------------*/
-typedef struct _MAP_NODE {
-  CHAR* key;
-  SYMBOL* symbol;
-  INT count;
-  struct _MAP_NODE* lhs;
-  struct _MAP_NODE* rhs;
-} MAP_NODE;
-
-
-/*----------------------------------------------------------------------*/
-/*!
-  \brief The central map structure.
-
-  You need to initialize an object of this structure in order to have
-  a map. Internally it's nothing but a map node and a total count.
-
-  \author u.kue
-  \date 08/04
-*/
-/*----------------------------------------------------------------------*/
-typedef struct _MAP {
-  MAP_NODE root;
-  INT count;
-} MAP;
 
 
 /*----------------------------------------------------------------------*/
