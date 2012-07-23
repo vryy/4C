f3d0d6791bff9befff1740d7d3f7dee277ac17ec
Tobias Wiesner <wiesner@lnm.mw.tum.de>
2012-07-23 06:41:57 +0000

    BACI clean-up outdated solver options. removed corresponding fortran files
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@16549 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/headers/compiler_definitions.h
+++ src/headers/compiler_definitions.h
@@ -50,35 +50,9 @@ Maintainer: Malte Neumann
 #define dsytrs              dsytrs_
 #define dveczero            dveczero_
 
-#define fortranpow          fortranpow_
-#define iluk                iluk_
-#define iveczero            iveczero_
-#define lusol               lusol_
-#define mlpcgupdupdvec      mlpcgupdupdvec_
-#define mlpcgupdvec         mlpcgupdvec_
-#define mlpcgvecvec         mlpcgvecvec_
-#define mlpcgveczero        mlpcgveczero_
-#define mumps_interface     mumps_interface_
-#define qat2v2              qat2v2_
 #define s8jacb              s8jacb_
-#define v2call              v2call_
-#define v3call              v3call_
-#define v3call_struct       v3call_struct_
 
 #endif
-
-/* methods located in src/fortran and (theoretically) still in usable  (gjb 7/12) */
-/* however, the corresponding source files are currently not in CmakeMakeLists and hence not compiled */
-void colsol(double *a, double *v, int *maxa, int *nn, int *nrr, int *nrc, int *nwa, int *nqm, int *nr1, int *nr2, int *kkk, double *det, int *isc, int *nsch, int *ipr, int *info);
-void iluk(int *n, double *a, int *ja, int *ia, int *lfil, double *alu, int *jlu, int *ju, int *levs, int *iwk, double *w, int *jw, int *ierr);
-void lusol(int *n, double *y, double *x, double *alu, int *jlu, int *ju);
-void mlpcgveczero(double *x, int *n);
-void mlpcgvecvec(double *x, double *y, double *sum, int *n);
-void mlpcgupdupdvec(double *a, double *y, double *facy, double *x, double *facx, int *init, int *n);
-void mlpcgupdvec(double *y, double *x, double *fac, int *init, int *n);
-void dveczero(double *x, int *n);
-void iveczero(int *x, int *n);
-void fortranpow(double *V,double *R,double *RE);
 
 /* fortran routines from the lapack package, used in src/linalg/linalg_utils.cpp */
 #ifdef __cplusplus
