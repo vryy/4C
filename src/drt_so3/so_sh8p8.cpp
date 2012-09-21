eab1f8a33173a93c84a96e6850bcae90d18d79e4
Lena Yoshihara <yoshihara@lnm.mw.tum.de>
2012-09-21 12:26:22 +0000

    forgot this
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@16750 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_so3/so_sh8p8.cpp
+++ src/drt_so3/so_sh8p8.cpp
@@ -291,11 +291,14 @@ void DRT::ELEMENTS::So_sh8p8::sosh8p8_expol
   // "assembly" of extrapolated nodal stresses
   for (int i=0;i<NUMNOD_;++i)
   {
-    int adjele = Nodes()[i]->NumElement();
-    int gnid = NodeIds()[i];
-    int lnid = expolstresses.Map().LID(gnid);
-    for (int j=0;j<6;j++)
-      (*(expolstresses(j)))[lnid] += nodalstresses(i,j)/adjele;
+    int gid = NodeIds()[i];
+    if (expolstresses.Map().MyGID(NodeIds()[i])) // rownode
+    {
+      int lid = expolstresses.Map().LID(gid);
+      int myadjele = Nodes()[i]->NumElement();
+      for (int j=0;j<6;j++)
+        (*(expolstresses(j)))[lid] += nodalstresses(i,j)/myadjele;
+    }
   }
 
 }
