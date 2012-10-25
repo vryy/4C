28cb47991c07250a56e0fb48d692a140a9998fed
Matthias Mayr <mayr@mhpc.mw.tum.de>
2012-10-25 14:26:09 +0000

    added separate convergence test for interface in NOX Status test
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@16931 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_fsi/fsi_monolithicfluidsplit.cpp
+++ src/drt_fsi/fsi_monolithicfluidsplit.cpp
@@ -1151,6 +1151,34 @@ FSI::MonolithicFluidSplit::CreateStatusTest(Teuchos::ParameterList& nlParams,
 
   converged->addStatusTest(structcombo);
 
+  // setup tests for interface
+
+  std::vector<Teuchos::RCP<const Epetra_Map> > interface;
+  interface.push_back(StructureField()->Interface()->FSICondMap());
+  interface.push_back(Teuchos::null);
+  LINALG::MultiMapExtractor interfaceextract(*DofRowMap(),interface);
+
+  Teuchos::RCP<NOX::StatusTest::Combo> interfacecombo =
+    Teuchos::rcp(new NOX::StatusTest::Combo(NOX::StatusTest::Combo::OR));
+
+  Teuchos::RCP<NOX::FSI::PartialNormF> interfaceTest =
+    Teuchos::rcp(new NOX::FSI::PartialNormF("interface",
+                                            interfaceextract,0,
+                                            nlParams.get("Norm abs vel", 1.0e-6),
+                                            NOX::Abstract::Vector::TwoNorm,
+                                            NOX::FSI::PartialNormF::Scaled));
+  Teuchos::RCP<NOX::FSI::PartialNormUpdate> interfaceTestUpdate =
+    Teuchos::rcp(new NOX::FSI::PartialNormUpdate("interface update",
+                                                 interfaceextract,0,
+                                                 nlParams.get("Norm abs vel", 1.0e-6),
+                                                 NOX::FSI::PartialNormUpdate::Scaled));
+
+  AddStatusTest(interfaceTest);
+  interfacecombo->addStatusTest(interfaceTest);
+  //interfacecombo->addStatusTest(interfaceTestUpdate);
+
+  converged->addStatusTest(interfacecombo);
+
   // setup tests for fluid velocities
 
   std::vector<Teuchos::RCP<const Epetra_Map> > fluidvel;
