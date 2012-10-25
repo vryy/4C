63e0292255b9c41e60873c4ef37b318078b92706
Andreas Nagler <nagler@lnm.mw.tum.de>
2012-10-25 09:37:18 +0000

    Adaption of the post_processing Test due to suggestion of Georg
    Mainly beauty and user convenience. 
    
    - created post_processing files start with xxx, so the remove tool can stay the same
    - different folder for script comparison.py
    - implementation of global call of this script. So user doesn't have to copy this script in the baci-executible folder
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@16926 060e22d8-9d1c-0410-868a-e3a950d63d95

--- tests/post_processing_test/comparison.py
+++ tests/post_processing_test/comparison.py
@@ -1,3 +1,7 @@
+# Script for comparing result of the parellel and serial post processing by the post_drt_ensight filter
+# 
+# Maintainer: A. Nagler
+
 from paraview import servermanager as sm
 import sys
 import csv
@@ -14,7 +18,7 @@ p3 = ['nodal_cauchy_stresses_xyz', '1']
 c1 = ['element_EA_strains_xyz', '0'] 
 c2 = ['element_cauchy_stresses_xyz', '0']
 
-ens_reader1.CaseFileName = './PAR_structure.case'
+ens_reader1.CaseFileName = './xxx_PAR_structure.case'
 ens_reader1.PointArrayStatus = p1+p2+p3
 ens_reader1.CellArrayStatus = c1+c2
 
@@ -23,11 +27,11 @@ ens_reader1.UpdatePipelineInformation()
 converter1 = sm.filters.AttributeDataToTableFilter(Input=ens_reader1, FieldAssociation="Points", AddMetaData=1)
 
 csvWriter1 = sm.writers.CSVWriter(Input=converter1)
-csvWriter1.FileName = './par.csv'
+csvWriter1.FileName = './xxx_par.csv'
 csvWriter1.UpdatePipeline()
 
 
-csv_reader1 = csv.reader(open('./par0.csv','r'), delimiter = ',')
+csv_reader1 = csv.reader(open('./xxx_par0.csv','r'), delimiter = ',')
 head1   = csv_reader1.next()
 index1  = head1.index('Points:0')
 
@@ -35,7 +39,7 @@ index1  = head1.index('Points:0')
 # read in of serial calculatet case file
 ens_reader2 = sm.sources.ensight()
 
-ens_reader2.CaseFileName = './SER_structure.case'
+ens_reader2.CaseFileName = './xxx_SER_structure.case'
 ens_reader2.PointArrayStatus = p1+p2+p3
 ens_reader2.CellArrayStatus = c1+c2
 
@@ -44,10 +48,10 @@ ens_reader2.UpdatePipelineInformation()
 converter2 = sm.filters.AttributeDataToTableFilter(Input=ens_reader2, FieldAssociation="Points", AddMetaData=1)
 
 csvWriter2 = sm.writers.CSVWriter(Input=converter2)
-csvWriter2.FileName = './ser.csv'
+csvWriter2.FileName = './xxx_ser.csv'
 csvWriter2.UpdatePipeline()
 
-csv_reader2 = csv.reader(open('./ser0.csv', 'r'), delimiter = ',')
+csv_reader2 = csv.reader(open('./xxx_ser0.csv', 'r'), delimiter = ',')
 head2   = csv_reader2.next()
 index2  = head1.index('Points:0')
 
@@ -61,5 +65,5 @@ for ele1, ele2 in zip(sortlist1, sortlist2):
         print 'csv lists are not equal for: '
         print ele1
         print ele2
-        sys.exit(1)
+        sys.exit(1)      
 print 'files are identical'
