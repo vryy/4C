24094ece1d19a3b9599ca64fb9f1642c4c947ec9
Tobias Wiesner <wiesner@lnm.mw.tum.de>
2012-10-26 12:42:17 +0000

    just beauty
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@16941 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_structure/strtimint_impl.cpp
+++ src/drt_structure/strtimint_impl.cpp
@@ -418,22 +418,14 @@ void STR::TimIntImpl::PredictTangDisConsistVelAcc()
   stiff_->Complete();
   LINALG::ApplyDirichlettoSystem(stiff_, disi_, fres_,
                                  GetLocSysTrafo(), zeros_, *(dbcmaps_->CondMap()));
-#if 0
-  // solve for disi_
-  // Solve K_Teffdyn . IncD = -R  ===>  IncD_{n+1}
-  solver_->Reset();
-  solver_->Solve(stiff_->EpetraOperator(), disi_, fres_, true, true);
-  solver_->Reset();
-#else
-  // solve for disi_
-  // Solve K_Teffdyn . IncD = -R  ===>  IncD_{n+1}
 
-  // linear solver call (contact / meshtying case or default)
+  // solve for disi_
+  // Solve K_Teffdyn . IncD = -R  ===>  IncD_{n+1}
   if (HaveContactMeshtying())
-    CmtLinearSolve();
+    CmtLinearSolve();  // use contact/meshtying solver
   else
     solver_->Solve(stiff_->EpetraOperator(), disi_, fres_, true, true);
-#endif
+
   // extract norm of disi_
   if (pressure_ != Teuchos::null)
   {
