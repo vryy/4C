2e0da02bbbafc5bbb0f697c6a01d1479484a30a1
Tobias Wiesner <wiesner@lnm.mw.tum.de>
2012-11-14 10:01:38 +0000

    prepare BACI for AMD libM replacement of poor system libM (Fedora 16 only)
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@17079 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ cmake/Modules/FindAMDLIBM.cmake
@@ -0,0 +1,38 @@
+ 
+# include(FindLibraryWithDebug)
+
+if (AMDLIBM_INCLUDES AND AMDLIBM_LIBRARIES)
+  set(AMDLIBM_FIND_QUIETLY TRUE)
+endif (AMDLIBM_INCLUDES AND AMDLIBM_LIBRARIES)
+find_path(AMDLIBM_INCLUDES
+  NAMES
+  amdlibm.h
+  PATHS
+  ${AMDLIBMDIR}/include
+  ${AMDLIBM_DIR}/include
+  ${INCLUDE_INSTALL_DIR}
+)
+
+find_library(AMDLIBM_LIBRARIES
+  NAMES
+  amdlibm
+  PATHS
+  ${AMDLIBMDIR}/lib/static
+  ${AMDLIBM_DIR}/lib/static
+  ${LIB_INSTALL_DIR}
+)
+
+find_file(AMDLIBM_LIBRARIES
+  NAMES
+  libamdlibm.so
+  PATHS
+  /usr/lib
+  ${AMDLIBMDIR}/lib/dynamic
+  ${LIB_INSTALL_DIR}
+)
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(AMDLIBM DEFAULT_MSG
+                                  AMDLIBM_INCLUDES AMDLIBM_LIBRARIES)
+mark_as_advanced(AMDLIBM_INCLUDES AMDLIBM_LIBRARIES)
