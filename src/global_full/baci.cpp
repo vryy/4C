7ad3eeb1c92d2529e573d4a7fdf74323909a2e99
Tobias Wiesner <wiesner@lnm.mw.tum.de>
2013-04-22 08:13:06 +0000

    - introduce new guard flag for Q2/2013 Trilinos - print out Trilinos version description in baci header.
    The frequency of official and internal Trilinos releases is
    too low to follow all the detailed changes (especially in Epetra
    and EpetraExt). Printing the git SHA1 hash of the Trilinos repository
    allows to determine the exact version that has been used.
    This helps to give the Sandia guys more detailed feedback about
    possible bugs and problems.
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@17898 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/global_full/baci.cpp
+++ src/global_full/baci.cpp
@@ -116,6 +116,7 @@ int main(int argc, char *argv[])
            "*                                      *\n"
            "****************************************\n\n",
            CHANGEDREVISION+0);
+    printf("Trilinos SHA1: %s\n",Trilinos_SHA1);
     printf("Total number of processors: %d\n",gcomm->NumProc());
   }
 
