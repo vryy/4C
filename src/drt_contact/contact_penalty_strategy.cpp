bd74b4d6e1332729cc8a222f34c983261ee36853
Alexander Popp <popp@lnm.mw.tum.de>
2013-10-17 11:08:54 +0000

    removed unused header
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@18487 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_contact/contact_penalty_strategy.cpp
+++ src/drt_contact/contact_penalty_strategy.cpp
@@ -44,7 +44,6 @@ Maintainer: Alexander Popp
 #include "../drt_inpar/inpar_contact.H"
 #include "../drt_mortar/mortar_defines.H"
 #include "../drt_mortar/mortar_utils.H"
-#include "../drt_io/io.H"
 #include "../linalg/linalg_solver.H"
 #include "../linalg/linalg_utils.H"
 
