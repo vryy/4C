980e7d659e5d8da83fee3c5d9ec758a6fce45777
Andreas Ehrl <ehrl@lnm.mw.tum.de>
2013-11-06 15:03:03 +0000

    - fixed for TRAP_FE flag: resolved ln(0)
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@18617 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_scatra/scatra_timint_genalpha.cpp
+++ src/drt_scatra/scatra_timint_genalpha.cpp
@@ -78,14 +78,16 @@ SCATRA::TimIntGenAlpha::TimIntGenAlpha(
   if (IsElch(scatratype_))
     ComputeTimeDerivPot0(true);
 
+  // for initializing phiaf_, phiam based on the initial field that was
+  // set for phinp_, phin_ in the TimInt base class constructor
+  // this method has to be called before method OutputElectrodeInfo() is called
+  // otherwise phi_af is initialized with zeros instead of the initial field
+  ComputeIntermediateValues();
+
   // Important: this adds the required ConditionID's to the single conditions.
   // It is necessary to do this BEFORE ReadRestart() is called!
   // Output to screen and file is suppressed
   OutputElectrodeInfo(false,false);
-
-  // for initializing phiaf_, phiam based on the initial field that was
-  // set for phinp_, phin_ in the TimInt base class constructor
-  ComputeIntermediateValues();
 
   return;
 }
