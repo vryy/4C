6af341e1b88062cfb0f25efeab2165e95acfc35e
Susanna Tinkl <tinkl@lnm.mw.tum.de>
2013-11-18 14:11:06 +0000

    forgot these files
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@18675 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_patspec/patspec.cpp
+++ src/drt_patspec/patspec.cpp
@@ -76,6 +76,7 @@ void PATSPEC::PatientSpecificGeometry(Teuchos::RCP<DRT::Discretization> dis,
   }
 
   int calc_strength  = DRT::INPUT::IntegralValue<int>(pslist,"CALCSTRENGTH");
+  bool calc_shear = DRT::INPUT::IntegralValue<bool>(pslist,"CALCINNERRADIUS");
 
   if (gfoundit or calc_strength)
   {
@@ -91,6 +92,13 @@ void PATSPEC::PatientSpecificGeometry(Teuchos::RCP<DRT::Discretization> dis,
     if (!dis->Comm().MyPID())
       IO::cout << "Computing strength model...\n";
     PATSPEC::ComputeEleStrength(dis, params);
+  }
+
+  if (calc_shear)
+  {
+    if (!dis->Comm().MyPID())
+      IO::cout << "Computing inner radius...\n";
+    PATSPEC::InitializeMappingInnerSurface(dis);
   }
   //-------------------------------------------------------------------------
 
@@ -410,8 +418,6 @@ void PATSPEC::ComputeEleNormalizedLumenDistance(Teuchos::RCP<DRT::Discretization
   return;
 }
 
-
-
 /*----------------------------------------------------------------------*
  |                                                           maier 11/10|
  *----------------------------------------------------------------------*/
@@ -514,6 +520,295 @@ void PATSPEC::ComputeEleLocalRadius(Teuchos::RCP<DRT::Discretization> dis)
 }
 
 /*----------------------------------------------------------------------*
+ |                                                           tinkl 11/13|
+ *----------------------------------------------------------------------*/
+void PATSPEC::InitializeMappingInnerSurface(Teuchos::RCP<DRT::Discretization> dis)
+{
+  // find out whether we have a orthopressure or FSI condition
+  std::vector<DRT::Condition*> conds;
+  std::vector<DRT::Condition*> ortho(0);
+  std::vector<DRT::Condition*> fsi(0);
+  dis->GetCondition("SurfaceNeumann",ortho);
+  for (int i=0; i<(int)ortho.size(); ++i)
+  {
+    if (ortho[i]->GType() != DRT::Condition::Surface) continue;
+    const std::string* type = ortho[i]->Get<std::string>("type");
+    if (*type == "neum_orthopressure" || *type == "neum_pseudo_orthopressure")
+      conds.push_back(ortho[i]);
+  }
+
+  dis->GetCondition("FSICoupling",fsi);
+  for (int i=0; i<(int)fsi.size(); ++i)
+  {
+    if (fsi[i]->GType() != DRT::Condition::Surface) continue;
+    conds.push_back(fsi[i]);
+  }
+
+  if (!conds.size())
+    dserror("There is no orthopressure nor FSI condition in this discretization");
+
+  // collect all nodes
+  std::set<int> allnodes;
+  for (int i=0; i<(int)conds.size(); ++i)
+  {
+    const std::vector<int>* nodes = conds[i]->Nodes();
+    if (!nodes) dserror("Cannot find node ids in condition");
+    for (int j=0; j<(int)nodes->size(); ++j)
+      allnodes.insert((*nodes)[j]);
+  }
+
+  // create coordinates for all these nodes
+  const int nnodes = (int)allnodes.size();
+  std::vector<double> lcoords(nnodes*3,0.0);
+  std::vector<double> gcoords(nnodes*3,0.0);
+  std::set<int>::iterator fool;
+  int count=0;
+  for (fool=allnodes.begin(); fool != allnodes.end(); ++fool)
+  {
+    if (!dis->NodeRowMap()->MyGID(*fool))
+    {
+      count++;
+      continue;
+    }
+    DRT::Node* node = dis->gNode(*fool);
+    lcoords[count*3]   = node->X()[0];
+    lcoords[count*3+1] = node->X()[1];
+    lcoords[count*3+2] = node->X()[2];
+    count++;
+  }
+  dis->Comm().SumAll(&lcoords[0],&gcoords[0],nnodes*3);
+  lcoords.clear();
+  allnodes.clear();
+
+  // compute distance of all of my elements to these nodes
+  // WARNING: This is a brute force expensive minimum distance search!
+  RCP<Epetra_Vector> innerid = LINALG::CreateVector(*(dis->NodeRowMap()),true);
+  for (int i=0; i<dis->NodeRowMap()->NumMyElements(); ++i)
+  {
+    const double* x = dis->gNode(dis->NodeRowMap()->GID(i))->X();
+    // loop nodes from the condition and find minimum distance
+    double mindist = 1.0e12;
+    int inneridloc = 0;
+    for (int j=0; j<nnodes; ++j)
+    {
+      double* xorth = &gcoords[j*3];
+      double dist = sqrt( (x[0]-xorth[0])*(x[0]-xorth[0]) +
+                          (x[1]-xorth[1])*(x[1]-xorth[1]) +
+                          (x[2]-xorth[2])*(x[2]-xorth[2]) );
+      if (dist<mindist)
+      {
+        mindist = dist;
+        inneridloc = j;
+      }
+    }
+    (*innerid)[i] = inneridloc;
+  }
+  gcoords.clear();
+
+  // export nodal distances to column map
+  RCP<Epetra_Vector> tmpnode = LINALG::CreateVector(*(dis->NodeColMap()),true);
+  LINALG::Export(*innerid,*tmpnode);
+  innerid = tmpnode;
+
+  // discretization
+  Teuchos::RCP<DRT::Condition> cond = Teuchos::rcp(
+    new DRT::Condition(0,DRT::Condition::PatientSpecificData,false,DRT::Condition::Volume));
+  cond->Add("inner surface node id",*innerid);
+
+  // check whether discretization has been filled before putting condition to dis
+  bool filled = dis->Filled();
+  dis->SetCondition("PatientSpecificData",cond);
+  if (filled && !dis->Filled()) dis->FillComplete();
+
+  return;
+}
+
+/*----------------------------------------------------------------------*
+ |                                                           tinkl 11/13|
+ *----------------------------------------------------------------------*/
+void PATSPEC::ComputeEleInnerRadius(Teuchos::RCP<DRT::Discretization> dis)
+{
+  // calculation only needed when PatientSpecificData available
+  std::vector<DRT::Condition*> mypatspeccond;
+  dis->GetCondition("PatientSpecificData", mypatspeccond);
+  if (!mypatspeccond.size()) return;
+  // check if inner id has been computed otherwise leave
+  for(unsigned int i=0; i<mypatspeccond.size(); ++i)
+  {
+    const Epetra_Vector* fool = mypatspeccond[i]->Get<Epetra_Vector>("inner surface node id");
+    if (!fool) return;
+  }
+
+  // get centerline
+  const Teuchos::ParameterList& pslist = DRT::Problem::Instance()->PatSpecParams();
+  bool lin_centerline = DRT::INPUT::IntegralValue<bool>(pslist,"LINEARCENTERLINE");
+  std::vector<double> clcoords;
+  std::vector<double> direction;
+  double normdirection = 0.0;
+  std::vector<double> support;
+  if (lin_centerline)
+  {
+    // centerline is straight line
+    double word;
+    std::istringstream directionstream(Teuchos::getNumericStringParameter(pslist,"CENTERLINEDIRECTION"));
+    while (directionstream >> word)
+      direction.push_back(word);
+    if ((int)direction.size() != 3) dserror("Length of direction is wrong %f!\n",(int)direction.size());
+
+    normdirection = sqrt(direction[0]*direction[0] + direction[1]*direction[1] + direction[2]*direction[2]);
+
+    std::istringstream supportstream(Teuchos::getNumericStringParameter(pslist,"CENTERLINEPOINT"));
+    while (supportstream >> word)
+      support.push_back(word);
+    if ((int)support.size() != 3) dserror("Length of direction is wrong!\n");
+  }
+  else
+  {
+    std::string filename = pslist.get<std::string>("CENTERLINEFILE");
+    if (filename=="name.txt")
+    {
+      dserror("no centerline file provided");
+      // this function makes no sense without centerline
+    }
+    clcoords = PATSPEC::GetCenterline(filename);
+  }
+
+  // find out whether we have a orthopressure or FSI condition
+  std::vector<DRT::Condition*> conds;
+  std::vector<DRT::Condition*> ortho(0);
+  std::vector<DRT::Condition*> fsi(0);
+  dis->GetCondition("SurfaceNeumann",ortho);
+  for (int i=0; i<(int)ortho.size(); ++i)
+  {
+    if (ortho[i]->GType() != DRT::Condition::Surface) continue;
+    const std::string* type = ortho[i]->Get<std::string>("type");
+    if (*type == "neum_orthopressure" || *type == "neum_pseudo_orthopressure")
+      conds.push_back(ortho[i]);
+  }
+
+  dis->GetCondition("FSICoupling",fsi);
+  for (int i=0; i<(int)fsi.size(); ++i)
+  {
+    if (fsi[i]->GType() != DRT::Condition::Surface) continue;
+    conds.push_back(fsi[i]);
+  }
+
+  if (!conds.size())
+    dserror("There is no orthopressure nor FSI condition in this discretization");
+
+  // collect all nodes
+  std::set<int> allnodes;
+  for (int i=0; i<(int)conds.size(); ++i)
+  {
+    const std::vector<int>* nodes = conds[i]->Nodes();
+    if (!nodes) dserror("Cannot find node ids in condition");
+    for (int j=0; j<(int)nodes->size(); ++j)
+      allnodes.insert((*nodes)[j]);
+  }
+
+  // compute current coordinates and inner radius for all these nodes
+  RCP<const Epetra_Vector> disp = dis->GetState("displacement");
+  if (disp==Teuchos::null) dserror("Cannot get state vectors 'displacement'");
+  // compute inner radius for all these nodes
+  const int nnodes = (int)allnodes.size();
+  std::vector<double> linnerradius(nnodes,0.0);
+  std::vector<double> ginnerradius(nnodes,0.0);
+  std::set<int>::iterator fool;
+  int count=0;
+  for (fool=allnodes.begin(); fool != allnodes.end(); ++fool)
+  {
+    if (!dis->NodeRowMap()->MyGID(*fool))
+    {
+      count++;
+      continue;
+    }
+    DRT::Node* node = dis->gNode(*fool);
+    std::vector<double> mydisp(1);
+    std::vector<int> lm;
+    dis->Dof(node, lm);
+    DRT::UTILS::ExtractMyValues(*disp,mydisp,lm);
+    const double* x = node->X();
+    std::vector<double> xcurr(3);
+    xcurr[0] = x[0] + mydisp[0];
+    xcurr[1] = x[1] + mydisp[1];
+    xcurr[2] = x[2] + mydisp[2];
+    // inner radius
+    double mindist = 1.0e12;
+    if (normdirection == 0.0)
+    {
+      // centerline from file
+      for (int j=0; j<(int)(clcoords.size()/3); ++j)
+      {
+        double* xcl = &clcoords[j*3];
+        double dist = sqrt( (xcurr[0]-xcl[0])*(xcurr[0]-xcl[0]) +
+                            (xcurr[1]-xcl[1])*(xcurr[1]-xcl[1]) +
+                            (xcurr[2]-xcl[2])*(xcurr[2]-xcl[2]) );
+        if (dist<mindist) mindist = dist;
+      }
+    }
+    else
+    {
+      // centerline is a straight line
+      std::vector<double> diff(3);
+      diff[0] = xcurr[0] - support[0];
+      diff[1] = xcurr[1] - support[1];
+      diff[2] = xcurr[2] - support[2];
+      std::vector<double> product(3);
+      product[0] = direction[1]*diff[2] - direction[2]*diff[1];
+      product[1] = direction[2]*diff[0] - direction[0]*diff[2];
+      product[2] = direction[0]*diff[1] - direction[1]*diff[0];
+      mindist = sqrt(product[0]*product[0] + product[1]*product[1] + product[2]*product[2])/normdirection;
+    }
+
+    linnerradius[count] = mindist;
+    count++;
+  }
+  dis->Comm().SumAll(&linnerradius[0],&ginnerradius[0],nnodes);
+
+  // add inner radius to condition
+  for(unsigned int j=0; j<mypatspeccond.size(); ++j)
+  {
+    const Epetra_Vector* foolnode = mypatspeccond[j]->Get<Epetra_Vector>("inner surface node id");
+    if (foolnode)
+    {
+      RCP<Epetra_Vector> innerradius = LINALG::CreateVector(*(dis->NodeRowMap()),true);
+      for (int i=0; i<dis->NodeRowMap()->NumMyElements(); ++i)
+      {
+        if (!foolnode->Map().MyGID(dis->NodeRowMap()->GID(i))) dserror("I do not have this node");
+
+        int innerid = (*foolnode)[foolnode->Map().LID(dis->NodeRowMap()->GID(i))];
+        (*innerradius)[i] = ginnerradius[innerid];
+      }
+      RCP<Epetra_Vector> tmp = LINALG::CreateVector(*(dis->NodeColMap()),true);
+      LINALG::Export(*innerradius,*tmp);
+      innerradius = tmp;
+
+      // compute element-wise mean distance from nodal distance
+      RCP<Epetra_Vector> innerradiusele = LINALG::CreateVector(*(dis->ElementRowMap()),true);
+      for (int i=0; i<dis->ElementRowMap()->NumMyElements(); ++i)
+      {
+        DRT::Element* actele = dis->gElement(dis->ElementRowMap()->GID(i));
+        double mean = 0.0;
+        for (int k=0; k<actele->NumNode(); ++k)
+          mean += (*innerradius)[innerradius->Map().LID(actele->Nodes()[k]->Id())];
+        mean /= actele->NumNode();
+        (*innerradiusele)[i] = mean;
+      }
+
+      tmp = LINALG::CreateVector(*(dis->ElementColMap()),true);
+      LINALG::Export(*innerradiusele,*tmp);
+      innerradiusele = tmp;
+      // add this column vector of inner radius to the condition and store in
+      // discretization
+      mypatspeccond[j]->Add("inner radius",*innerradiusele);
+    }
+  }
+  ginnerradius.clear();
+
+  return;
+}
+
+/*----------------------------------------------------------------------*
  |                                                           maier 06/11|
  *----------------------------------------------------------------------*/
 std::vector<double> PATSPEC::GetCenterline(std::string filename)
@@ -522,21 +817,18 @@ std::vector<double> PATSPEC::GetCenterline(std::string filename)
   std::ifstream file (filename.c_str());
   if (file == NULL) dserror ("Error opening centerline file");
   std::string sLine;
-  char buffer[1000];
   std::vector<double> clcoords;
   while (std::getline(file, sLine))
   {
     if (sLine.size() != 0)
     {
-      strcpy(buffer,sLine.c_str());
-      char * pEnd;
-    //only get values if first occurence of line is double number
-      if (strtod(buffer, &pEnd) != 0.0)
+      std::stringstream ss( sLine );
+      double myDouble = 0;
+      int count = 0;
+      while( ss >> myDouble && count < 3)
       {
-	//store x,y,z corrdinates
-	clcoords.push_back(strtod(buffer, &pEnd));
-	clcoords.push_back(strtod(pEnd, &pEnd));
-	clcoords.push_back(strtod(pEnd, NULL));
+        clcoords.push_back(myDouble);
+        count ++;
       }
     }
   }
@@ -612,6 +904,36 @@ void PATSPEC::GetLocalRadius(const int eleid,
 }
 
 /*----------------------------------------------------------------------*
+ |                                                           tinkl 11/13|
+ *----------------------------------------------------------------------*/
+void PATSPEC::GetInnerRadius(const int eleid,
+                             Teuchos::ParameterList& params,
+                             DRT::Discretization& dis)
+{
+  std::vector<DRT::Condition*> mypatspeccond;
+  dis.GetCondition("PatientSpecificData", mypatspeccond);
+  if (!mypatspeccond.size()) return;
+
+  for(unsigned int i=0; i<mypatspeccond.size(); ++i)
+  {
+    const Epetra_Vector* fool = mypatspeccond[i]->Get<Epetra_Vector>("inner radius");
+    if (fool)
+    {
+      if (!fool->Map().MyGID(eleid)) dserror("I do not have this element");
+
+      double innerradius = (*fool)[fool->Map().LID(eleid)];
+      params.set("inner radius",innerradius);
+
+      return;
+    }
+  }
+
+  // if inner radius not found in condition just return
+  return;
+}
+
+
+/*----------------------------------------------------------------------*
  |                                                           kehl 03/12|
  *----------------------------------------------------------------------*/
 void PATSPEC::PatspecOutput(Teuchos::RCP<IO::DiscretizationWriter> output_,
@@ -620,7 +942,6 @@ void PATSPEC::PatspecOutput(Teuchos::RCP<IO::DiscretizationWriter> output_,
 {
 
     std::vector<DRT::Condition*> mypatspeccond;
-    double maxiltthick = params->get<double>("max ilt thick");
     discret_->GetCondition("PatientSpecificData", mypatspeccond);
     IO::DiscretizationWriter::VectorType vt= IO::DiscretizationWriter::elementvector;
 
@@ -633,6 +954,7 @@ void PATSPEC::PatspecOutput(Teuchos::RCP<IO::DiscretizationWriter> output_,
         const Epetra_Vector* actcond = mypatspeccond[i]->Get<Epetra_Vector>("normalized ilt thickness");
         if (actcond)
         {
+          double maxiltthick = params->get<double>("max ilt thick");
           for (int j=0; j<patspecstuff->MyLength(); ++j)
             (*patspecstuff)[j] = (*actcond)[actcond->Map().LID(discret_->ElementRowMap()->GID(j))];
           patspecstuff->Scale(maxiltthick);
@@ -653,6 +975,14 @@ void PATSPEC::PatspecOutput(Teuchos::RCP<IO::DiscretizationWriter> output_,
           for (int j=0; j<patspecstuff->MyLength(); ++j)
             (*patspecstuff)[j] = (*actcond)[actcond->Map().LID(discret_->ElementRowMap()->GID(j))];
           output_->WriteVector("strength", patspecstuff, vt);
+        }
+
+        actcond = mypatspeccond[i]->Get<Epetra_Vector>("inner radius");
+        if (actcond)
+        {
+          for (int j=0; j<patspecstuff->MyLength(); ++j)
+            (*patspecstuff)[j] = (*actcond)[actcond->Map().LID(discret_->ElementRowMap()->GID(j))];
+          output_->WriteVector("inner_radius", patspecstuff, vt);
         }
       }
       RCP<Epetra_Vector> eleID = LINALG::CreateVector(*(discret_->ElementRowMap()),true);
