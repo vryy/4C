4bbb7fdb929553ee8f8852a6ece46a697be1b0f5
Martin Kronbichler <kronbichler@lnm.mw.tum.de>
2013-11-19 12:47:57 +0000

    Add more information to stack trace: If the system supports it (detected by a cmake script), demangle function names and add line numbers. As by-product of this change, the error messages in dsassert are no longer converted implicitly to std::string, which happens also in release mode when the actual operation is a no-op. Let me know if there are problems.
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@18678 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ cmake/checks/check_advanced_features.cmake
@@ -0,0 +1,61 @@
+# check for advanced features that give rise to extra preprocessor flags 
+
+CHECK_CXX_SOURCE_RUNS(
+  "
+  #include <cxxabi.h>
+  #include <stdio.h>
+  #include <execinfo.h>
+  #include <stdlib.h>
+  #include <string>
+  #include <stdexcept>
+  template <int dummy>
+  bool f()
+  {
+  int nptrs;
+  void *buffer[100];
+  char **strings;
+  nptrs   = backtrace(buffer, 100);
+  strings = backtrace_symbols(buffer, nptrs);
+  std::string entry (strings[0]);
+  std::string applicationname = entry.substr(0,entry.find('('));
+  std::string filename;
+  const std::size_t address0 = entry.find(\" [\");
+  const std::size_t address1 = entry.find(\"]\", address0);
+  std::string progname = \"addr2line \" + entry.substr(address0+2,address1-2-address0) + \" -e \" + applicationname;
+  FILE *stream = popen( progname.c_str(), \"r\");
+  if (stream != 0) {
+  char path[4096];
+  if (fgets(path, sizeof(path)-1, stream) != 0) {
+  std::string pathname (path);
+  if (pathname.find('?') == std::string::npos)
+  filename = \"  (\" +
+  pathname.substr(pathname.find_last_of('/')+1,
+  pathname.length()-pathname.find_last_of('/')-2) + ')';
+  }
+  }
+  pclose(stream);
+  const std::size_t start = entry.find('('), end = entry.find('+');
+  std::string functionname = entry.substr(start+1,end-start-1);
+  int status;
+  char *p = abi::__cxa_demangle(functionname.c_str(), 0, 0, &status);
+  bool completed = false;
+  if (status == 0)
+  {
+  std::string demangled(p);
+  if (demangled.find(\"bool f<1>()\") != std::string::npos)
+  completed = true;
+  }
+  free(p);
+  free(strings);
+  return !completed;
+  }
+  int main(int argc, char **argv)
+  {
+  return f<1>();
+  }
+  "
+  ADVANCED_STACKTR)
+
+if(ADVANCED_STACKTR)
+  add_definitions("-DENABLE_ADVANCED_STACKTR")
+endif(ADVANCED_STACKTR)
--- src/drt_lib/drt_dserror.cpp
+++ src/drt_lib/drt_dserror.cpp
@@ -49,25 +49,12 @@ Maintainer: Michael Gee
 #include <execinfo.h>
 #include <unistd.h>
 
+#include <cxxabi.h>
+#include <stdio.h>
+
 
 static int         latest_line = -1;
 static std::string latest_file = "{dserror_func call without prototype}";
-/*----------------------------------------------------------------------*
- |  assert function                                          mwgee 11/06|
- | used by macro dsassert in dserror.H                                  |
- *----------------------------------------------------------------------*/
-void cpp_dsassert_func(const std::string file, const int line, const bool test, const std::string text)
-{
-#ifdef DEBUG
-  if (!test)
-  {
-    latest_file = file;
-    latest_line = line;
-    cpp_dserror_func(text);
-  }
-#endif
-  return;
-} /* end of dsassert_func */
 
 /*----------------------------------------------------------------------*
  |  assert function                                          mwgee 11/06|
@@ -115,7 +102,8 @@ void cpp_dslatest(const char* file, const int line)
 extern "C"
 void cpp_dserror_func(const char* text, ...)
 {
-  int myrank;
+  int myrank, nprocs;
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
   char errbuf[4096];
@@ -136,15 +124,108 @@ void cpp_dserror_func(const char* text, ...)
   strings = backtrace_symbols(buffer, nptrs);
 
   sprintf(&errbuf[strlen(errbuf)], "\n\n--- stacktrace ---");
-  for (int j = 0; j < nptrs; ++j)
-    sprintf(&errbuf[strlen(errbuf)], "\n%s", strings[j]);
+
+  // start stack trace where we actually got the error, not this function
+  int frame = 0;
+  while ((frame < nptrs)
+         &&
+         ((std::string(strings[frame]).find ("cpp_dserror_func") != std::string::npos)
+          ||
+          (std::string(strings[frame]).find ("cpp_dsassert_func") != std::string::npos)))
+    ++frame;
+
+  // find name of application for getting the code location of the error
+#ifdef ENABLE_ADVANCED_STACKTR
+  std::string applicationname;
+  for (int frame2 = nptrs-1; frame2 > 0; --frame2)
+    {
+      std::string entry (strings[frame2]);
+      const std::size_t start = entry.find('('),
+                        end   = entry.find('+');
+      std::string functionname = entry.substr (start+1,end-start-1);
+      if (functionname == "main")
+        {
+          applicationname = entry.substr(0, start);
+          break;
+        }
+    }
+#endif
+
+
+  int startframe = frame;
+  for ( ; frame < nptrs; ++frame)
+  {
+    std::string entry (strings[frame]);
+
+#ifdef ENABLE_ADVANCED_STACKTR
+    // get code location of error in call stack by running the program addr2line
+    // (on linux, only when debug symbols are present)
+    std::string filename;
+    const std::size_t address0 = entry.find(" [");
+    const std::size_t address1 = entry.find("]", address0);
+
+    if (not applicationname.empty() &&
+        address0 != std::string::npos) {
+      std::string progname = "addr2line " + entry.substr(address0+2,address1-2-address0) + " -e " + applicationname;
+      FILE *stream = popen( progname.c_str(), "r");
+      if (stream != 0) {
+        char path[4096];
+        if (fgets(path, sizeof(path)-1, stream) != 0) {
+          std::string pathname (path);
+          if (pathname.find("?") == std::string::npos)
+            filename = "  (" +
+              pathname.substr(pathname.find_last_of('/')+1,
+                              pathname.find('\n')-pathname.find_last_of('/')-1) + ")";
+        }
+      }
+      pclose(stream);
+    }
+    else
+    if (address0 != std::string::npos)
+      filename = entry.substr(address0,address1-address0+1);
+
+    // demangle class names if possible
+    const std::size_t start = entry.find('('),
+                      end   = entry.find('+');
+    std::string functionname = entry.substr(start+1,end-start-1);
+    int status;
+    char *p = abi::__cxa_demangle(functionname.c_str(), 0, 0, &status);
+    if (status == 0)
+      sprintf(&errbuf[strlen(errbuf)], "\n[%2d]: %s%s", frame-startframe, p, filename.c_str());
+    else
+      sprintf(&errbuf[strlen(errbuf)], "\n[%2d]: %s%s", frame-startframe, functionname.c_str(), filename.c_str());
+    free(p);
+
+    if (functionname == "main")
+      break;
+
+#else
+    sprintf(&errbuf[strlen(errbuf)], "\n[%2d]: %s", frame-startframe, entry.c_str());
+#endif
+  }
+  sprintf(&errbuf[strlen(errbuf)], "\n------------------\n");
 
   free(strings);
 #endif
 
   va_end(ap);
 
-  throw std::runtime_error(errbuf);
+  if (nprocs > 1)
+    throw std::runtime_error(errbuf);
+  else {
+    // if only one processor is running, do not throw because that destroys the call
+    // history in debuggers like gdb. Instead, call abort which preserves the location
+    // (but do not do it in parallel because otherwise an error leads to a call stack
+    // from every processor with OpenMPI, which has a severe impact in readability).
+    char line[] = "=========================================================================\n";
+    std::cout << "\n\n"
+              << line
+              << errbuf
+              << "\n"
+              << line
+              << "\n" << std::endl;
+    std::abort();
+  }
 } /* end of dserror_func */
 
 /*----------------------------------------------------------------------*
@@ -153,36 +234,7 @@ void cpp_dserror_func(const char* text, ...)
  *----------------------------------------------------------------------*/
 void cpp_dserror_func(const std::string text, ...)
 {
-  int myrank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-
-  char errbuf[4096];
-
-  va_list ap;
-  va_start(ap, text);
-
-  sprintf(errbuf,"PROC %d ERROR in %s, line %i:\n",myrank,latest_file.c_str(),latest_line);
-  vsprintf(&errbuf[strlen(errbuf)],text.c_str(),ap);
-
-#ifdef ENABLE_STACKTR
-  // print stacktrace
-  int nptrs;
-  void *buffer[100];
-  char **strings;
-
-  nptrs   = backtrace(buffer, 100);
-  strings = backtrace_symbols(buffer, nptrs);
-
-  sprintf(&errbuf[strlen(errbuf)], "\n\n--- stacktrace ---");
-  for (int j = 0; j < nptrs; ++j)
-    sprintf(&errbuf[strlen(errbuf)], "\n%s", strings[j]);
-
-  free(strings);
-#endif
-
-  va_end(ap);
-
-  throw std::runtime_error(errbuf);
+  cpp_dserror_func(text.c_str());
 } /* end of dserror_func */
 
 
