e2dc70fada5706ce60d7a5164736cb57fbd8d2d9
Tobias Wiesner <wiesner@lnm.mw.tum.de>
2013-12-06 13:56:22 +0000

    Adapted configuration script to prohibit "in-source-builds" of BACI.
    Due to confusion with concurrent build types of BACI (Debug, Release) and
    massive performance penalties in Eclipse (when using the Indexer) we now
    switch to a more systematic out-of-source-build strategy.
    I.e. your "build" folder for BACI must not lie in or beneath your "baci" folder.
    
    HOWTO:
      mkdir -p "/home/<USER>/build-debug"
      cd "/home/<USER>/build-debug"
      /home/<USER>/baci/do-configure [--debug] [--experimental] [--useACML] [...]
      make -j 6 baci
    
    cleaned up "adapt_cproject.py"
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@18795 060e22d8-9d1c-0410-868a-e3a950d63d95

--- adapt_cproject.py
+++ adapt_cproject.py
@@ -74,64 +74,49 @@ def getPaths(build_folder):
     
     return pathlist
 
-def getSymbols(fname,build_type):
-    """Get all defines flags from do-configure file"""
-    f = open(fname,"r")
-    symbollist = set()
-    for l in f.readlines():
-        words = l.split()
-        if len(words) > 1:
-            if words[0] == "-D":
-                flags = words[1].split("=")
-                flag = flags[0]
-                status = flags[1]
-                #valid_flag = flag[0:2] == "D_"
-                #if valid_flag == True and status == "ON":
-                #    symbollist.add(flag.split(":")[0])
-                if status == "ON":
-                    symbollist.add(flag.split(":")[0])
-
-    if build_type == "DEBUG":
-    	symbollist.add("DEBUG")
-    # flags that are set in CMakeLists.txt
-    symbollist.add("PARALLEL")
-    symbollist.add("PARMETIS")
-    symbollist.add("BINIO")
-    symbollist.add("HAVE_FFTW")
-    symbollist.add("D_SHELL8")
-    return symbollist
-
 def getDefineValue(build_folder):
     """Get all define flags that specify a value from the CMakeFiles folder"""
-    f = open(build_folder+"/CMakeFiles/drt_lib.dir/flags.make","r")
-    definevaluelist = set()
-    for l in f.readlines():
-        for w in l.split():
-            if w.startswith("-DBOOST_MAJOR_VERSION") or w.startswith("-DBOOST_MINOR_VERSION"):
-                flag = w[2:] #loose the -D
-                definevaluelist.add(flag)
-    return definevaluelist
+    definevalueset = set()
+    symbolset = set()
+    
+    with open(build_folder+"/CMakeFiles/drt_lib.dir/flags.make","r") as f:
+      for l in f.readlines():
+	if l.startswith("CXX_DEFINES"):  
+	  for w in l.split():
+	    if w.startswith("-D"):
+	      flag = w[2:]
+	      if "=" in flag:
+		defineflag,val = flag.split("=",1)
+		definevalueset.add((defineflag,val))
+	      else:
+		symbolset.add(flag)           
+    
+    definevaluelist = []
+    symbollist = []
+    
+    definevaluelist = [x for x in definevalueset]
+    definevaluelist.sort()
+    
+    symbollist = [x for x in symbolset]
+    symbollist.sort()
 
+    return (definevaluelist, symbollist)
+    
 def adapt(do_configure_file,build_folder,build_type):
     """update .cproject file if existing"""
 
+    print build_folder
+    
     if os.path.isfile(".cproject"): # if file exists
         f = open(".cproject","r")
-
+       
         project = etree.fromstring(f.read())
 
         pathset = getPaths(build_folder)
         pathlist = [x for x in pathset]
         pathlist.sort()
-        #print pathlist
-        symbolset=getSymbols(do_configure_file,build_type)
-        symbollist = [x for x in symbolset]
-        symbollist.sort()
-        #print symbollist
-        definevalueset = getDefineValue(build_folder)
-        definevaluelist = [x for x in definevalueset]
-        definevaluelist.sort()
-        #print definevaluelist
+
+        definevaluelist,symbollist = getDefineValue(build_folder)
 
         # iterate over all entries named 'option'
         found_path = False
@@ -148,14 +133,14 @@ def adapt(do_configure_file,build_folder,build_type):
                 #print(etree.tostring(option, pretty_print=True))
                 found_path = True
 
-            if superClass == "gnu.cpp.compiler.option.preprocessor.def" or superClass == "gnu.c.compiler.option.preprocessor.def.symbols":
+            if superClass.endswith("gnu.cpp.compiler.option.preprocessor.def") or superClass.endswith("gnu.c.compiler.option.preprocessor.def.symbols"):
                 #print(etree.tostring(option, pretty_print=True))
                 for entry in option:
                     option.remove(entry)
                 for entry in symbollist:
                     option.append(etree.Element("listOptionValue", builtIn="false", value=entry ))
                 for entry in definevaluelist:
-                    option.append(etree.Element("listOptionValue", builtIn="false", value=entry ))
+                    option.append(etree.Element("listOptionValue", builtIn="false", value="{}={}".format(entry[0],entry[1]) ))
                 #print(etree.tostring(option, pretty_print=True))
                 found_symbol = True
                 
