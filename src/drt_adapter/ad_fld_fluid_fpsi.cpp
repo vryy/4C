a502f6bb2edbd2eb8eb3028a2c9c670aab5948ec
Andreas Rauch <rauch@lnm.mw.tum.de>
2013-12-16 10:31:08 +0000

    forgot to check these two --> sorry
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@18824 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/drt_adapter/ad_fld_fluid_fpsi.cpp
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------*/
+/*!
+\file ad_fld_fluid_fsi.cpp
+
+\brief Fluid field adapter for fpsi
+
+Can only be used in conjunction with #FluidImplicitTimeInt
+
+<pre>
+Maintainer: Andreas Rauch
+            rauch@lnm.mw.tum.de
+            http://www.lnm.mw.tum.de
+            089 - 289-15240
+</pre>
+*/
+/*----------------------------------------------------------------------*/
+#include "ad_fld_fluid_fpsi.H"
+#include "ad_fld_fluid_fsi.H"
+
+#include "../drt_fluid/fluid_utils_mapextractor.H"
+
+/* constructor */
+ADAPTER::FluidFPSI::FluidFPSI(Teuchos::RCP<Fluid> fluid,
+    Teuchos::RCP<DRT::Discretization> dis,
+    Teuchos::RCP<LINALG::Solver> solver,
+    Teuchos::RCP<Teuchos::ParameterList> params,
+    Teuchos::RCP<IO::DiscretizationWriter> output,
+    bool isale,
+    bool dirichletcond)
+: FluidFSI(fluid, dis, solver, params, output, isale, dirichletcond),
+  fpsiinterface_(Teuchos::rcp(new FLD::UTILS::MapExtractor()))
+{
+  fpsiinterface_->Setup(*dis,true);
+
+} // constructor
+
+
