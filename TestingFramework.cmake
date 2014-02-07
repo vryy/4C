5f2fe29f699806d719603418447e2a7713d909ca
Tobias Wiesner <wiesner@lnm.mw.tum.de>
2014-02-07 15:51:49 +0000

    move nightly tests with external xml files to Q1/2014 Were tested with Q3/2013 with does not support this feature
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@19095 060e22d8-9d1c-0410-868a-e3a950d63d95

--- TestingFramework.cmake
+++ TestingFramework.cmake
@@ -1502,15 +1502,16 @@ baci_test(meshtying3D_contactsp_stdlagr_braesssarazin 2 "")
 baci_test(meshtying3D_contactsp_duallagr_braesssarazin 1 "")
 baci_test(meshtying3D_contactsp_duallagr_braesssarazin 2 "")
 
-baci_test(f2_drivencavity20x20_muelu_xml_PGAMG 1 "")
-baci_test(f2_drivencavity20x20_muelu_xml_PGAMG 2 "")
-
 # 2 block contact (saddlepoint formulation)
 baci_test(contact3D_lin_duallagr_SP 2 "")
 
 endif (HAVE_Trilinos_Q3_2013)
 
 if (HAVE_Trilinos_Q1_2014)
+
+baci_test(f2_drivencavity20x20_muelu_xml_PGAMG 1 "")
+baci_test(f2_drivencavity20x20_muelu_xml_PGAMG 2 "")
+
 endif (HAVE_Trilinos_Q1_2014)
 
 endif (HAVE_MueLu)
