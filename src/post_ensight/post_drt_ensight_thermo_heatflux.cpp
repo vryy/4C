vryy/4C

commit fac3f0b52b50b4f7948d3fd6e3cbfaab9e213800

Volume mortar with TSI (working in serial) ========================================= * providing an element with more than one material (thanks to Georg)   - an element now holds a vector of materials instead of one material   - for volume coupled problems, the element will get a pointer to the material of the element its coupled with as second material   - this way, there is no need for accessing the other discretization to get the material on element level   -> both fields can evaluate their term indenpendent from each other, which makes setting up the coupling easier   - still the element is only responsible for its own material - the first one in the material vector - and therefore will still only pack/unpack one material object * slight change when applying states from other fields on structure and thermo discretization (again, thanks to Georg)    - states are written directly on the respective discretization instead of passing them to the time integrator first    - no need for saving those state vectors in the time integrator -> deleted respective variables and methods in structure and thermo    - added bool to method ClearState() in discretization class, to decide whether coupling states are to be cleared * moved setup for periodic boundary conditions from time integrator to global algorithm (i.e. tsi algorithm) for tsi problems * used enums instead of strings for element action in evaluate of thermo element * applied coupling states before calling predictors in TSI -> slight change in results of a view test cases * added two bools in cut algorithm (thanks to Sudhakar)    - one to suppress screen output    - one to force cut cells to be tets only * added test case for monolithic TSI with volume mortar (so far only working on one processor) * Todo:    - strategy for parallel distribution and ghosting    - faster strategy for search algorithm of integration cells when building projection matrizes (brute force, so far)    - find one to one mapping for materials in volume mortar

src/post_ensight/post_drt_ensight_thermo_heatflux.cpp
@@ -25,6 +25,7 @@
 #include "post_drt_ensight_single_field_writers.H"
 #include "../linalg/linalg_utils.H"
 #include "../pss_full/pss_cpp.h"
+#include "../drt_thermo/thermo_ele_action.H"
 
 /*----------------------------------------------------------------------*
  | constructor                                               dano 11/09 |
@@ -210,7 +211,7 @@ void ThermoEnsightWriter::WriteNodalHeatfluxStep(
   // create the parameters for the discretization
   Teuchos::ParameterList p;
   // other parameters that might be needed by the elements
-  p.set("action","postproc_thermo_heatflux");
+  p.set<int>("action",THR::postproc_thermo_heatflux);
   p.set("heatfluxtype","ndxyz");
   p.set("gpheatfluxmap", data);
   p.set("total time", -1.0);
@@ -451,7 +452,7 @@ void ThermoEnsightWriter::WriteElementCenterHeatfluxStep(
   // create the parameters for the discretization
   Teuchos::ParameterList p;
   // other parameters that might be needed by the elements
-  p.set("action","postproc_thermo_heatflux");
+  p.set("action",THR::postproc_thermo_heatflux);
   p.set("heatfluxtype","cxyz");
   p.set("gpheatfluxmap",data);
   p.set("total time", -1.0);

Input/tsi_geonln_necking_fbar_thrplast.dat
@@ -705,10 +705,10 @@ NODE 171 COORD 1.9770744258786601e+00 4.5874197209974401e+00 1.3333500000000001e
 95 SOLIDH8FBARTHERMO HEX8 163 167 12 24 130 132 18 27 MAT 1 KINEM nonlinear
 96 SOLIDH8FBARTHERMO HEX8 167 171 10 12 132 134 17 18 MAT 1 KINEM nonlinear
 ----------------------------------------------------------RESULT DESCRIPTION
-STRUCTURE DIS structure NODE 115 QUANTITY dispx VALUE -1.02681030882259980e-01 TOLERANCE 1.0e-13 NAME ux_top_out
+STRUCTURE DIS structure NODE 115 QUANTITY dispx VALUE -1.02681030882259980e-01 TOLERANCE 1.0e-9 NAME ux_top_out
 STRUCTURE DIS structure NODE 115 QUANTITY dispy VALUE 0.0 TOLERANCE 1.0e-13 NAME uy_top_out
 STRUCTURE DIS structure NODE 115 QUANTITY dispz VALUE 1.2 TOLERANCE 1.0e-13 NAME uz_top_out
-STRUCTURE DIS structure NODE 82 QUANTITY dispx VALUE -1.07382354731585111e-01 TOLERANCE 1.0e-13 NAME ux_neck
+STRUCTURE DIS structure NODE 82 QUANTITY dispx VALUE -1.07382354731585111e-01 TOLERANCE 1.0e-9 NAME ux_neck
 STRUCTURE DIS structure NODE 82 QUANTITY dispy VALUE 0.0 TOLERANCE 1.0e-13 NAME uy_neck
 STRUCTURE DIS structure NODE 82 QUANTITY dispz VALUE 0.0 TOLERANCE 1.0e-13 NAME uz_neck
 -------------------------------------------------------------------------END

Input/tsi_geonln_plexichannel_varyE_OST_monolithic.dat
@@ -33,7 +33,7 @@ STRUCTURE DIS structure NODE 421 QUANTITY dispx VALUE 3.00175106779087544e-04 TO
 STRUCTURE DIS structure NODE 141 QUANTITY dispx VALUE -2.00898687345515845e-04 TOLERANCE 1e-9 NAME ux(8.5,35,0)
 THERMAL DIS thermo NODE 433 QUANTITY temp VALUE 79.9973213105214143 TOLERANCE 1.0e-8 NAME T(-3.5,0,0)
 THERMAL DIS thermo NODE 421 QUANTITY temp VALUE 79.9836533624902302 TOLERANCE 1.0e-8 NAME T(3.5,30,0)
-THERMAL DIS thermo NODE 141 QUANTITY temp VALUE 72.4028784951147486 TOLERANCE 1.0e-8 NAME T(8.5,35,0)
+THERMAL DIS thermo NODE 141 QUANTITY temp VALUE 72.1374787600902448 TOLERANCE 1.0e-8 NAME T(8.5,35,0)
 
 ----------------------------------------------------------------PROBLEM SIZE
 //ELEMENTS                      385
@@ -3005,6 +3005,6 @@ STRUCTURE DIS structure NODE 421 QUANTITY dispx VALUE 3.00135464855054337e-04 TO
 STRUCTURE DIS structure NODE 141 QUANTITY dispx VALUE -1.99015248196995187e-04 TOLERANCE 1e-9 NAME ux(8.5,35,0) //lin: -1.84705088155790055e-04
 THERMAL DIS thermo NODE 433 QUANTITY temp VALUE 79.9979468989792082 TOLERANCE 1.0e-9 NAME T(-3.5,0,0) //lin: 79.9968774850210735
 THERMAL DIS thermo NODE 421 QUANTITY temp VALUE 79.9843533036719379 TOLERANCE 1.0e-9 NAME T(3.5,30,0) //lin: 79.9821761564873412
-THERMAL DIS thermo NODE 141 QUANTITY temp VALUE 72.1374787607902448 TOLERANCE 1.0e-9 NAME T(8.5,35,0) //lin: 72.3629248976951800
+THERMAL DIS thermo NODE 141 QUANTITY temp VALUE 72.1374787600902448 TOLERANCE 1.0e-9 NAME T(8.5,35,0) //lin: 72.3629248976951800
 -------------------------------------------------------------------------END
 //  END

Input/tsi_geonln_plexichannel_varyE_OST_monolithic_mergeTSImatrix.dat
@@ -33,7 +33,7 @@ STRUCTURE DIS structure NODE 421 QUANTITY dispx VALUE 3.00175106779087544e-04 TO
 STRUCTURE DIS structure NODE 141 QUANTITY dispx VALUE -2.00898687345515845e-04 TOLERANCE 1e-9 NAME ux(8.5,35,0)
 THERMAL DIS thermo NODE 433 QUANTITY temp VALUE 79.9973213105214143 TOLERANCE 1.0e-8 NAME T(-3.5,0,0)
 THERMAL DIS thermo NODE 421 QUANTITY temp VALUE 79.9836533624902302 TOLERANCE 1.0e-8 NAME T(3.5,30,0)
-THERMAL DIS thermo NODE 141 QUANTITY temp VALUE 72.4028784951147486 TOLERANCE 1.0e-8 NAME T(8.5,35,0)
+THERMAL DIS thermo NODE 141 QUANTITY temp VALUE 72.1374787600902448 TOLERANCE 1.0e-8 NAME T(8.5,35,0)
 
 The TSI block matrix is merged to enable the use of a direct solver for the
 monolithic                      system.
@@ -2965,6 +2965,6 @@ STRUCTURE DIS structure NODE 421 QUANTITY dispx VALUE 3.00135464855518105e-04 TO
 STRUCTURE DIS structure NODE 141 QUANTITY dispx VALUE -1.99015248196594358e-04 TOLERANCE 1e-9 NAME ux(8.5,35,0) //lin: -1.84705088155790055e-04
 THERMAL DIS thermo NODE 433 QUANTITY temp VALUE 79.9979468989792224 TOLERANCE 1.0e-9 NAME T(-3.5,0,0) //lin: 79.9968774850210735
 THERMAL DIS thermo NODE 421 QUANTITY temp VALUE 79.9843533036719521 TOLERANCE 1.0e-9 NAME T(3.5,30,0) //lin: 79.9821761564873412
-THERMAL DIS thermo NODE 141 QUANTITY temp VALUE 72.1374787607902448 TOLERANCE 1.0e-9 NAME T(8.5,35,0) //lin: 72.3629248976951800
+THERMAL DIS thermo NODE 141 QUANTITY temp VALUE 72.1374787600902448 TOLERANCE 1.0e-9 NAME T(8.5,35,0) //lin: 72.3629248976951800
 -------------------------------------------------------------------------END
 //  END

TestingFramework.cmake
@@ -1110,7 +1110,7 @@ baci_test(tsi_lincompression_sequstaggdisp 1 "")
 baci_test(tsi_lincompression_sequstaggtemp 1 "")
 baci_test(tsi_lincompression_iterstaggdisp 1 "")
 baci_test(tsi_lincompression_iterstaggtemp 1 "")
-baci_test(tsi_lincompression_iterstaggaitken 1 "")
+baci_test(tsi_lincompression_iterstaggaitken 1 8)
 baci_test(tsi_lincompression_monolithic 1 "")
 baci_test(tsi_lincompression_thrplastic_monolithic 1 "")
 baci_test(tsi_lincompression_monolithic_mergeTSImatrix 1 "")
@@ -1124,7 +1124,7 @@ baci_test(tsi_heatconvection_thrplastic_monolithic 2 "")
 baci_test(tsi_heatconvection_thrplastic_genalpha_monolithic 2 "")
 baci_test(tsi_heatflux_iterstagg 1 "")
 baci_test(tsi_heatflux_iterstaggaitken 1 "")
-baci_test(tsi_heatflux_monolithic 1 "")
+baci_test(tsi_heatflux_monolithic 1 2)
 baci_test(tsi_heatflux_monolithic_pbc 1 15)
 baci_test(tsi_heatflux_monolithic_pbc 2 15)
 baci_test(tsi_heatflux_flexoutsurf_monolithic 1 "")
@@ -1210,6 +1210,7 @@ baci_test(viscoelasthyper_isoratedep 1 "")
 baci_test(viscogenmax 1 "")
 baci_test(viscoNeoHooke_creep 1 60)
 baci_test(viscoNeoHooke_creep 2 60)
+baci_test(volmortar3D_tsi 1 "")
 baci_test(w1_dyn_dirich_drt 1 "")
 baci_test(w1_dyn_dirich_drt 2 "")
 baci_test(w1t6_patch_linear 1 "")

src/drt_adapter/ad_str_structure.H
@@ -405,11 +405,6 @@ namespace ADAPTER {
 
     //! @name volume coupled specific methods
     //@{
-    /// set volume coupling state (e.g. temperatures for tsi, fluid velocities for poro) in structural discretization
-    virtual void SetCouplingState() = 0;
-
-    /// apply volume coupling state (e.g. temperatures for tsi, fluid velocities for poro) in structural time integrator
-    virtual void ApplyCouplingState(Teuchos::RCP<const Epetra_Vector> velnp, const std::string& name, unsigned dofset=1) = 0;
 
     /// Set forces due to interface with fluid, the force is expected external-force-like
     virtual void SetForceInterface(Teuchos::RCP<Epetra_MultiVector> iforce) = 0;

src/drt_adapter/ad_str_wrapper.H
@@ -332,14 +332,6 @@ namespace ADAPTER
     //! @name volume coupled specific methods
     //@{
 
-    //! set volume coupling state (e.g. temperatures for tsi, fluid velocities for poro) in structural discretization
-    virtual void SetCouplingState()
-    { structure_->SetCouplingState(); }
-
-    //! apply volume coupling state (e.g. temperatures for tsi, fluid velocities for poro) in structural time integrator
-    virtual void ApplyCouplingState(Teuchos::RCP<const Epetra_Vector> velnp,   const std::string& name, unsigned dofset=1)
-    { structure_->ApplyCouplingState(velnp,name,dofset); }
-
     /// set forces due to interface with fluid, the force is expected external-force-like
     virtual void SetForceInterface(Teuchos::RCP<Epetra_MultiVector> iforce)
     { structure_->SetForceInterface(iforce); }

commit 85c2218d6d71ba4fb300778054492d0365943956

fixed bug I introduced in r19102 concerning filtering of element based heatflux data
Thanks and sorry to Caroline

git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@19168 060e22d8-9d1c-0410-868a-e3a950d63d95

src/post_ensight/post_drt_ensight_thermo_heatflux.cpp
@@ -452,7 +452,7 @@ void ThermoEnsightWriter::WriteElementCenterHeatfluxStep(
   // create the parameters for the discretization
   Teuchos::ParameterList p;
   // other parameters that might be needed by the elements
-  p.set("action",THR::postproc_thermo_heatflux);
+  p.set<int>("action",THR::postproc_thermo_heatflux);
   p.set("heatfluxtype","cxyz");
   p.set("gpheatfluxmap",data);
   p.set("total time", -1.0);