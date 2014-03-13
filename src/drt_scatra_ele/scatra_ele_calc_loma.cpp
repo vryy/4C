b87048e84d93e6536682c216fba2ab6c7bafcb14
Georg Hammerl <hammerl@lnm.mw.tum.de>
2014-03-13 11:03:00 +0000

    resolved some more e-mail inconsistencies
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@19249 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_scatra_ele/scatra_ele_calc_loma.cpp
+++ src/drt_scatra_ele/scatra_ele_calc_loma.cpp
@@ -6,7 +6,7 @@
 
 <pre>
 Maintainer: Ursula Rasthofer/Volker Gravemeier
-            rasthofer/vgravem@lnm.mw.tum.de
+            {rasthofer,vgravem}@lnm.mw.tum.de
             http://www.lnm.mw.tum.de
             089-289-15236/45
 </pre>
