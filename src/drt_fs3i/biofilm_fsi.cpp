ca3d6f30e36e40ede4dbbea5fb287814693d57ec
Christoph Ager <ager@lnm.mw.tum.de>
2014-06-05 11:41:48 +0000

    - Fix of paralell bug for biofilm growth!  - Comments!
     Thx to Mirella & Andreas E.
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@19629 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_fs3i/biofilm_fsi.cpp
+++ src/drt_fs3i/biofilm_fsi.cpp
@@ -791,14 +791,21 @@ Teuchos::RCP<Epetra_Vector> FS3I::BiofilmFSI::StructToAle(Teuchos::RCP<const Epe
 /*----------------------------------------------------------------------*/
 void FS3I::BiofilmFSI::ChangeConfig(Teuchos::RCP<DRT::Discretization> dis, Teuchos::RCP<Epetra_Vector> disp)
 {
-  const int numnode = (dis->NodeRowMap())->NumMyElements();
-  const Epetra_Vector& gvector =*disp;
+  const int numnode = (dis->NodeColMap())->NumMyElements();
+
+  //Create Vector which holds all col-displacments of processor
+  Teuchos::RCP<Epetra_Vector> coldisp = Teuchos::rcp(new Epetra_Vector(*(dis->DofColMap())));
+
+  //Export row-displacments to col-displacements
+  LINALG::Export(*disp, *coldisp);
+
+  const Epetra_Vector& gvector =*coldisp;
 
   // loop over all nodes
   for (int index = 0; index < numnode; ++index)
   {
     // get current node
-    int gid = (dis->NodeRowMap())->GID(index);
+    int gid = (dis->NodeColMap())->GID(index);
     DRT::Node* mynode = dis->gNode(gid);
 
     std::vector<int> globaldofs = dis->Dof(0,mynode);
@@ -828,18 +835,26 @@ void FS3I::BiofilmFSI::ScatraChangeConfig(Teuchos::RCP<DRT::Discretization> scat
                                           Teuchos::RCP<DRT::Discretization> dis,
                                           Teuchos::RCP<Epetra_Vector> disp)
 {
-  const int numnode = (scatradis->NodeRowMap())->NumMyElements();
-  const Epetra_Vector& gvector =*disp;
+  const int numnode = (scatradis->NodeColMap())->NumMyElements();
+
+  //Create Vector which holds all col-displacments of processor
+  Teuchos::RCP<Epetra_Vector> coldisp = Teuchos::rcp(new Epetra_Vector(*(dis->DofColMap())));
+
+  //Export row-displacments to col-displacements
+  LINALG::Export(*disp, *coldisp);
+
+
+  const Epetra_Vector& gvector =*coldisp;
 
   // loop over all nodes
   for (int index = 0; index < numnode; ++index)
   {
     // get current node
-    int gid = (scatradis->NodeRowMap())->GID(index);
+    int gid = (scatradis->NodeColMap())->GID(index);
     DRT::Node* mynode = scatradis->gNode(gid);
 
     // get local fluid/structure node with the same local node id
-    DRT::Node* lnode = dis->lRowNode(index);
+    DRT::Node* lnode = dis->lColNode(index);
 
     // get degrees of freedom associated with this fluid/structure node
     std::vector<int> nodedofs = dis->Dof(0,lnode);
