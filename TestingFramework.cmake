d31a69ec54052c930f28f151f0495f433fac8dae
Francesc Verdugo <verdugo@lnm.mw.tum.de>
2014-07-09 09:30:49 +0000

    Added nightly test associated with the SIMPLE block smoother
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@19760 060e22d8-9d1c-0410-868a-e3a950d63d95

--- TestingFramework.cmake
+++ TestingFramework.cmake
@@ -1664,6 +1664,7 @@ baci_test(f2_drivencavity20x20_muelu_xml_PGAMG 2 "")
 
 # testing the AMGnxn with a TSI problem
 baci_test(tsi_simplrocketnozzle_monolithic_amgnxn 2 "")
+baci_test(tsi_simplrocketnozzle_monolithic_amgnxn_simple 2 "")
 
 endif (HAVE_Trilinos_Q1_2014)
 
