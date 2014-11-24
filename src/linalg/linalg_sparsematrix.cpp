49e8625522702de59f7455e728305e017f6b933b
Tobias Wiesner <wiesner@lnm.mw.tum.de>
2014-11-24 13:58:18 +0000

    some more header clean up
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@20336 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/linalg/linalg_sparsematrix.cpp
+++ src/linalg/linalg_sparsematrix.cpp
@@ -12,7 +12,6 @@ Maintainer: Michael Gee
 
 #include "linalg_sparsematrix.H"
 #include "linalg_utils.H"
-#include "linalg_solver.H"
 #include "../drt_lib/drt_dserror.H"
 
 #include <EpetraExt_Transpose_RowMatrix.h>
