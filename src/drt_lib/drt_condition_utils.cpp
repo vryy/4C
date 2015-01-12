29597532e9fe282d7e51105205092f394ee05792
Christian Roth <roth@lnm.mw.tum.de>
2015-01-12 16:30:15 +0000

    Reverted to version 20528; as discussed with Alex P. and Tobias, contactmodule will be fixed differently.
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@20536 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_lib/drt_condition_utils.cpp
+++ src/drt_lib/drt_condition_utils.cpp
@@ -22,9 +22,11 @@ Maintainer: Martin Kronbichler
 #include "drt_condition_utils.H"
 #include "drt_condition_selector.H"
 #include "drt_discret_iterator.H"
+#include "drt_globalproblem.H"
 
 #include "../drt_lib/drt_utils_parallel.H"
 #include "../linalg/linalg_utils.H"
+#include "../drt_io/io_control.H"
 
 /*----------------------------------------------------------------------*
  *----------------------------------------------------------------------*/
@@ -667,3 +669,44 @@ void DRT::UTILS::CollectElementsByConditionLabel(
   if(discret.NumMyColElements() != numOfCollectedIds)
     dserror("not all elements collected.");
 }
+
+/*-----------------------------------------------------------------------*
+ * Writes boundary surfaces of a volumetrically coupled problem to file  *
+ * 'boundarysurfaces.log' storing the condition-Id as surface-Id. For    *
+ * visualisation in gmsh and checking for tetrahedra whose four surfaces *
+ * are wrongly contained in the boundary surface of the volumetric       *
+ * coupling this file can be used.                         (croth 01/15) *
+ *-----------------------------------------------------------------------*/
+void DRT::UTILS::WriteBoundarySurfacesVolumeCoupling(
+    std::map< std::vector<int>, Teuchos::RCP<DRT::Element> > surfmap,
+    int condID,
+    int numproc,
+    int mypid)
+{
+  if (numproc==1)
+  {
+    //Get output prefix
+    std::string outputprefix = DRT::Problem::Instance()->OutputControlFile()->NewOutputFileName();
+    //Create boundary surface file
+    std::ostringstream sf;
+    sf << outputprefix << "_boundarysurfaces.log";
+    std::string boundarysurffilename;
+    boundarysurffilename = sf.str();
+
+    std::ofstream myfile;
+    myfile.open (boundarysurffilename.c_str(), std::ios_base::app);
+    myfile << "Surfaces in Surfmap for Coupling Condition No. " << condID+1 << ":\n";
+    myfile << "Format: [Node1, Node2, Node3, CondID] \n";
+    for(std::map< std::vector<int>, Teuchos::RCP<DRT::Element> >::const_iterator iterat=surfmap.begin(); iterat!=surfmap.end(); ++iterat)
+    {
+      myfile << iterat->first[0] << " " << iterat->first[1] << " " << iterat->first[2] << " " << condID+1 <<"\n";
+    }
+    myfile << "End \n";
+    myfile.close();
+  }
+  else if (mypid==0)
+  {
+    std::cout << " No 'boundarysurfaces.log' written as number of procs = "<< numproc <<" is bigger than 1." << std::endl;
+  }
+}
+
