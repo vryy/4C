0cbb4cbecf8e4c3dca0d0c613959e667b9d631ea
Alexander Seitz <seitz@lnm.mw.tum.de>
2015-01-14 08:00:22 +0000

    - added safety check that INTEGER material parameters are   actually given as whole numbers. Up to now, a given double   was simply be cast to an int - deleted some unused parameters from my test case
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@20541 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_lib/drt_materialdefinition.cpp
+++ src/drt_lib/drt_materialdefinition.cpp
@@ -292,11 +292,20 @@ Teuchos::RCP<std::stringstream> DRT::INPUT::IntMaterialComponent::Read(
 
     // try to convert to integer
     char *check;
-    integer = strtol(sinteger.c_str(),&check,10);
+
+    // check if it actually is an integer
+    double double_val =strtod(sinteger.c_str(),&check);
+    double int_val;
+    if (modf(double_val, &int_val) == 0.0)
+      integer = int_val;
+    else
+      dserror("Value of parameter '%s' for material '%s' expected INT but got DOUBLE!", Name().c_str(), def->Name().c_str());
 
     // return error in case the conversion was not successful
     if(sinteger == check)
       dserror("Value of parameter '%s' for material '%s' not properly specified in input file!", Name().c_str(), def->Name().c_str());
+
+    integer = strtol(sinteger.c_str(),&check,10);
 
     // remove double parameter value from stringstream "condline"
     condline->str(condline->str().erase((size_t)condline->tellg()-sinteger.size(),sinteger.size()));
