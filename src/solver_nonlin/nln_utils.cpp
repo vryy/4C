878c92a4ef24eededd9660efcc607a9397431248
Matthias Mayr <mayr@mhpc.mw.tum.de>
2015-01-19 12:23:26 +0000

    Enhanced reading of XML-files into Teuchos::ParameterList: the XML-file doesn't have to model the entire hierarchical structure of the Teuchos::ParameterList anymore. Sublists can be replaced by XML-files.
    To replace a sublist by an XML-file, introduce the line 
    
    <Parameter name="Name of the sublist to be inserted here" type="string" value="sublistfile: path/filename.xml"/>
    
    into the XML-file. During XML-input, this line will be replaced by the sublist that is in 'path/filename.xml'. The prefix 'sublistfile: ' is necessary to detect the parameter as place holder for an additional sublist.
    
    Note: not 100% bug-free, yet.
    
    
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@20562 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/solver_nonlin/nln_utils.cpp
+++ src/solver_nonlin/nln_utils.cpp
@@ -18,8 +18,10 @@ Maintainer: Matthias Mayr
 #include <string>
 
 // Teuchos
+#include <Teuchos_ParameterEntry.hpp>
 #include <Teuchos_ParameterList.hpp>
 #include <Teuchos_RCP.hpp>
+#include <Teuchos_TimeMonitor.hpp>
 #include <Teuchos_XMLParameterListCoreHelpers.hpp>
 
 // baci
@@ -31,30 +33,97 @@ Maintainer: Matthias Mayr
 /*----------------------------------------------------------------------------*/
 
 /*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
 Teuchos::RCP<Teuchos::ParameterList>
 NLNSOL::UTILS::CreateParamListFromXML()
 {
-  Teuchos::RCP<Teuchos::ParameterList> params =
-      Teuchos::rcp(new Teuchos::ParameterList());
-
-  std::string xmlfilename =
+  std::string filename =
       DRT::Problem::Instance()->NonlinearSolverParams().get<std::string>(
           "XML_FILE");
 
   // check for reasonable xml file
-  if (xmlfilename == "none")
+  if (filename == "none")
     dserror("Seems like you forgot to set the XML file for configuration of "
         "the nonlinear solver.");
 
-  if(xmlfilename.length())
-  {
-    Teuchos::updateParametersFromXmlFile(xmlfilename, inoutArg(*params));
-  }
-  else
-  {
-    dserror("The file name '%s' is not a valid XML file name.",
-        xmlfilename.c_str());
-  }
+  return CreateParamListFromXML(filename);
+}
+
+/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+Teuchos::RCP<Teuchos::ParameterList>
+NLNSOL::UTILS::CreateParamListFromXML(const std::string filename)
+{
+  // create a new parameter list to be filled
+  Teuchos::RCP<Teuchos::ParameterList> params =
+      Teuchos::rcp(new Teuchos::ParameterList());
+
+  // fill the parameter list with the content of the given XML-file
+  CreateParamListFromXML(filename, *params);
 
   return params;
+}
+
+/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+void NLNSOL::UTILS::CreateParamListFromXML(const std::string filename,
+    Teuchos::ParameterList& params)
+{
+  // time measurements
+  Teuchos::RCP<Teuchos::Time> time = Teuchos::TimeMonitor::getNewCounter(
+      "NLNSOL::UTILS::CreateParamListFromXML");
+  Teuchos::TimeMonitor monitor(*time);
+
+  // check for reasonable filename of xml-file to be read
+  if (filename.length() && filename.rfind(".xml"))
+  {
+    // fill parameter list with content of xml-file
+    params = *(Teuchos::getParametersFromXmlFile(filename));
+  }
+  else
+    dserror("The file name '%s' is not a valid XML file name.",
+        filename.c_str());
+
+  // loop over all entries (parameters and sublists) of parameter list 'params'
+  for (Teuchos::ParameterList::ConstIterator it = params.begin(); it != params.end(); ++it)
+  {
+    // Is this entry of type "std::string"?
+    if (it->second.isType<std::string>())
+    {
+      // get the string
+      std::string str;
+      str = it->second.getValue(&str);
+
+      /* Is this string a filename to an xml-file for a sublist?
+       * Note: We introduced a naming convention to identify those cases (see
+       * header file).
+       */
+      if (not str.find("sublistfile:") && str.rfind(".xml") != std::string::npos)
+      {
+        // extract filename from string by removing the flag 'sublistfile: '
+        std::string sublistfilename;
+        sublistfilename.assign(str.begin()+13, str.end());
+
+        /* remove the place holder Teuchos::ParameterEntry and replace it by a
+         * sublist with the same name.
+         */
+        const std::string sublistname = it->first;
+        params.remove(it->first);
+        Teuchos::ParameterList& subparams = params.sublist(sublistname);
+
+        // check for reasonable filename of xml-file to be read
+        if (filename.length() && filename.rfind(".xml"))
+        {
+          // fill sublist with content of xml-file
+          CreateParamListFromXML(sublistfilename, subparams);
+          subparams.print(std::cout);
+        }
+        else
+          dserror("The file name '%s' is not a valid XML file name.",
+              filename.c_str());
+      }
+    }
+  }
+
+  return;
 }
