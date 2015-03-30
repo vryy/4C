e55a6fc84b78f93caf69f0300fad68c912c9ab08
Andreas Rauch <rauch@lnm.mw.tum.de>
2015-03-30 11:44:26 +0000

    o fixes duplicate case value
    I'm sorry
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@20887 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_adapter/ad_fld_moving_boundary.cpp
+++ src/drt_adapter/ad_fld_moving_boundary.cpp
@@ -64,7 +64,6 @@ ADAPTER::FluidMovingBoundaryBaseAlgorithm::FluidMovingBoundaryBaseAlgorithm(
     }
     case prb_immersed_fsi:
     case prb_immersed_ale_fsi:
-    case prb_immersed_ale_fsi:
     {
       //std::cout << "using FluidImmersed as FluidMovingBoundary" << std::endl;
       fluid_ = Teuchos::rcp(new FluidImmersed(prbdyn,condname));
