1e7647b00f29449f906201811e9ca5156bc92754
Karl-Robert Wichmann <wichmann@lnm.mw.tum.de>
2015-09-16 09:49:35 +0000

    Fixed the post-processing comparison test script for F22, which uses a newer ParaView.
    Brought to you by HiWi Stefan Legat.
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@21340 060e22d8-9d1c-0410-868a-e3a950d63d95

--- tests/post_processing_test/comparison.py
+++ tests/post_processing_test/comparison.py
@@ -7,63 +7,126 @@ import sys
 import csv
 import operator
 
-sm.Connect()
+if sm.vtkSMProxyManager.GetVersionMajor() <= 3:
+  sm.Connect()
 
-# read in of parallel calculatet case file
-ens_reader1 = sm.sources.ensight()
-#p1 = ['displacement', '1']
-#p2 = ['nodal_EA_strains_xyz', '1']
-#p3 = ['nodal_cauchy_stresses_xyz', '1']
+  # read in of parallel calculatet case file
+  ens_reader1 = sm.sources.ensight()
+  #p1 = ['displacement', '1']
+  #p2 = ['nodal_EA_strains_xyz', '1']
+  #p3 = ['nodal_cauchy_stresses_xyz', '1']
 
-c1 = ['element_EA_strains_xyz', '0'] 
-c2 = ['element_cauchy_stresses_xyz', '0']
+  c1 = ['element_EA_strains_xyz', '0'] 
+  c2 = ['element_cauchy_stresses_xyz', '0']
 
-ens_reader1.CaseFileName = sys.argv[1]
-#ens_reader1.PointArrayStatus = p1+p2+p3
-ens_reader1.CellArrayStatus = c1+c2
+  ens_reader1.CaseFileName = sys.argv[1]
+  #ens_reader1.PointArrayStatus = p1+p2+p3
+  ens_reader1.CellArrayStatus = c1+c2
 
-ens_reader1.UpdatePipelineInformation()
+  ens_reader1.UpdatePipelineInformation()
 
-converter1 = sm.filters.AttributeDataToTableFilter(Input=ens_reader1, FieldAssociation="Points", AddMetaData=1)
+  converter1 = sm.filters.AttributeDataToTableFilter(Input=ens_reader1, FieldAssociation="Points", AddMetaData=1)
 
-csvWriter1 = sm.writers.CSVWriter(Input=converter1)
-csvWriter1.FileName = './xxx_par.csv'
-csvWriter1.UpdatePipeline()
-
-
-csv_reader1 = csv.reader(open('./xxx_par0.csv','r'), delimiter = ',')
-head1   = csv_reader1.next()
-index1  = head1.index('Points:0')
+  csvWriter1 = sm.writers.CSVWriter(Input=converter1)
+  csvWriter1.FileName = './xxx_par.csv'
+  csvWriter1.UpdatePipeline()
 
 
-# read in of serial calculatet case file
-ens_reader2 = sm.sources.ensight()
+  csv_reader1 = csv.reader(open('./xxx_par0.csv','r'), delimiter = ',')
+  head1   = csv_reader1.next()
+  index1  = head1.index('Points:0')
 
-ens_reader2.CaseFileName = sys.argv[2]
-#ens_reader2.PointArrayStatus = p1+p2+p3
-ens_reader2.CellArrayStatus = c1+c2
 
-ens_reader2.UpdatePipelineInformation()
+  # read in of serial calculatet case file
+  ens_reader2 = sm.sources.ensight()
 
-converter2 = sm.filters.AttributeDataToTableFilter(Input=ens_reader2, FieldAssociation="Points", AddMetaData=1)
+  ens_reader2.CaseFileName = sys.argv[2]
+  #ens_reader2.PointArrayStatus = p1+p2+p3
+  ens_reader2.CellArrayStatus = c1+c2
 
-csvWriter2 = sm.writers.CSVWriter(Input=converter2)
-csvWriter2.FileName = './xxx_ser.csv'
-csvWriter2.UpdatePipeline()
+  ens_reader2.UpdatePipelineInformation()
 
-csv_reader2 = csv.reader(open('./xxx_ser0.csv', 'r'), delimiter = ',')
-head2   = csv_reader2.next()
-index2  = head1.index('Points:0')
+  converter2 = sm.filters.AttributeDataToTableFilter(Input=ens_reader2, FieldAssociation="Points", AddMetaData=1)
 
-# sorting of data according to xyz coodinates of the Points
-sortlist1 = sorted(csv_reader1, key = operator.itemgetter(index1, index1 + 1, index1 + 2))
-sortlist2 = sorted(csv_reader2, key = operator.itemgetter(index2, index2 + 1, index2 + 2))
+  csvWriter2 = sm.writers.CSVWriter(Input=converter2)
+  csvWriter2.FileName = './xxx_ser.csv'
+  csvWriter2.UpdatePipeline()
 
-# comparison
-for ele1, ele2 in zip(sortlist1, sortlist2):
-    if ele1 != ele2:
-        print 'csv lists are not equal for: '
-        print ele1
-        print ele2
-        sys.exit(1)      
-print 'files are identical'
+  csv_reader2 = csv.reader(open('./xxx_ser0.csv', 'r'), delimiter = ',')
+  head2   = csv_reader2.next()
+  index2  = head1.index('Points:0')
+
+  # sorting of data according to xyz coodinates of the Points
+  sortlist1 = sorted(csv_reader1, key = operator.itemgetter(index1, index1 + 1, index1 + 2))
+  sortlist2 = sorted(csv_reader2, key = operator.itemgetter(index2, index2 + 1, index2 + 2))
+
+  # comparison
+  for ele1, ele2 in zip(sortlist1, sortlist2):
+      if ele1 != ele2:
+	  print 'csv lists are not equal for: '
+	  print ele1
+	  print ele2
+	  sys.exit(1)      
+  print 'files are identical'
+
+else:
+  sm.Connect()
+
+  # read in of parallel calculatet case file
+  ens_reader1 = sm.sources.EnSightReader()
+  #p1 = ['displacement', '1']
+  #p2 = ['nodal_EA_strains_xyz', '1']
+  #p3 = ['nodal_cauchy_stresses_xyz', '1']
+
+  c1 = ['element_EA_strains_xyz', '0'] 
+  c2 = ['element_cauchy_stresses_xyz', '0']
+
+  ens_reader1.CaseFileName = sys.argv[1]
+  #ens_reader1.PointArrayStatus = p1+p2+p3
+  ens_reader1.CellArrays = c1+c2
+
+  ens_reader1.UpdatePipelineInformation()
+
+  converter1 = sm.filters.ExtractAttributes(Input=ens_reader1, FieldAssociation="Points", AddMetaData=1)
+
+  csvWriter1 = sm.writers.CSVWriter(Input=converter1)
+  csvWriter1.FileName = './xxx_par.csv'
+  csvWriter1.UpdatePipeline()
+
+
+  csv_reader1 = csv.reader(open('./xxx_par0.csv','r'), delimiter = ',')
+  head1   = csv_reader1.next()
+  index1  = head1.index('Points:0')
+
+
+  # read in of serial calculatet case file
+  ens_reader2 = sm.sources.EnSightReader()
+
+  ens_reader2.CaseFileName = sys.argv[2]
+  #ens_reader2.PointArrayStatus = p1+p2+p3
+  ens_reader2.CellArrays = c1+c2
+
+  ens_reader2.UpdatePipelineInformation()
+
+  converter2 = sm.filters.ExtractAttributes(Input=ens_reader2, FieldAssociation="Points", AddMetaData=1)
+
+  csvWriter2 = sm.writers.CSVWriter(Input=converter2)
+  csvWriter2.FileName = './xxx_ser.csv'
+  csvWriter2.UpdatePipeline()
+
+  csv_reader2 = csv.reader(open('./xxx_ser0.csv', 'r'), delimiter = ',')
+  head2   = csv_reader2.next()
+  index2  = head1.index('Points:0')
+
+  # sorting of data according to xyz coodinates of the Points
+  sortlist1 = sorted(csv_reader1, key = operator.itemgetter(index1, index1 + 1, index1 + 2))
+  sortlist2 = sorted(csv_reader2, key = operator.itemgetter(index2, index2 + 1, index2 + 2))
+
+  # comparison
+  for ele1, ele2 in zip(sortlist1, sortlist2):
+      if ele1 != ele2:
+	  print 'csv lists are not equal for: '
+	  print ele1
+	  print ele2
+	  sys.exit(1)      
+  print 'files are identical'
