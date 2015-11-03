30e6fc4f76ed7d9c406540661736ec11b88103d0
Andy Wirtz <wirtz@lnm.mw.tum.de>
2015-11-03 07:02:23 +0000

    The Reynolds problem implementation ***********************************
    Step 2 – Reynolds dynamic
    *************************
    
    * introduction of the new Reynolds dynamic (part 2)
    
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@21491 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/drt_reynolds/reynolds_dyn.cpp
@@ -0,0 +1,104 @@
+/*--------------------------------------------------------------------------*/
+/*!
+\file reynolds_dyn.cpp
+
+\brief entry point for the solution of Reynolds problems
+
+<pre>
+Maintainer: Andy Wirtz
+            wirtz@lnm.mw.tum.de
+            http://www.lnm.mw.tum.de
+            089-289-15270
+</pre>
+*/
+/*--------------------------------------------------------------------------*/
+
+#include "../drt_adapter/adapter_scatra_base_algorithm.H"
+
+//#include "../drt_lib/drt_globalproblem.H"
+#include "../drt_lib/drt_utils_createdis.H"
+
+#include "../drt_scatra/scatra_timint_implicit.H"
+
+//#include "../drt_scatra_ele/scatra_ele.H"
+
+//#include <iostream>
+//#include <Teuchos_StandardParameterEntryValidators.hpp>
+//#include <Teuchos_TimeMonitor.hpp>
+
+//#include "scatra_algorithm.H"
+//#include "scatra_resulttest.H"
+//#include "scatra_utils_clonestrategy.H"
+#include "reynolds_dyn.H"
+
+/*----------------------------------------------------------------------*
+ * Main control routine for Reynolds problems
+ *----------------------------------------------------------------------*/
+void reynolds_dyn(int restart)
+{
+  // access the communicator
+  const Epetra_Comm& comm = DRT::Problem::Instance()->GetDis("scatra")->Comm();
+
+  // print problem type
+  if (comm.MyPID() == 0)
+  {
+    std::cout << "###################################################"
+        << std::endl;
+    std::cout << "# YOUR PROBLEM TYPE: "
+        << DRT::Problem::Instance()->ProblemName() << std::endl;
+    std::cout << "###################################################"
+        << std::endl;
+  }
+
+  // access the problem-specific parameter list
+  const Teuchos::ParameterList& scatradyn =
+      DRT::Problem::Instance()->ScalarTransportDynamicParams();
+
+  // access the scatra discretization
+  Teuchos::RCP<DRT::Discretization> scatradis =
+      DRT::Problem::Instance()->GetDis("scatra");
+
+  scatradis->FillComplete();
+
+  // we directly use the elements from the scalar transport elements section
+  if (scatradis->NumGlobalNodes() == 0)
+    dserror("No elements in the ---TRANSPORT ELEMENTS section");
+
+  // add proxy of velocity related degrees of freedom to scatra discretization
+  if (scatradis->BuildDofSetAuxProxy(DRT::Problem::Instance()->NDim() + 1, 0, 0,
+      true) != 1)
+    dserror("Scatra discretization has illegal number of dofsets!");
+
+  // finalize discretization
+  scatradis->FillComplete(true, false, false);
+
+  // get linear solver id from REYNOLDS DYNAMIC
+  const int linsolvernumber = scatradyn.get<int>("LINEAR_SOLVER");
+  if (linsolvernumber == (-1))
+    dserror(
+        "no linear solver defined for REYNOLDS problem. Please set LINEAR_SOLVER in REYNOLDS DYNAMIC to a valid number!");
+
+  // create instance of scalar transport basis algorithm (empty fluid discretization)
+  Teuchos::RCP<ADAPTER::ScaTraBaseAlgorithm> scatraonly = Teuchos::rcp(
+      new ADAPTER::ScaTraBaseAlgorithm(scatradyn, scatradyn,
+          DRT::Problem::Instance()->SolverParams(linsolvernumber)));
+
+  // set initial velocity field
+  // note: The order ReadRestart() before SetVelocityField() is important here!!
+  // for time-dependent velocity fields, SetVelocityField() is additionally called in each PrepareTimeStep()-call
+  (scatraonly->ScaTraField())->SetVelocityField(1);
+
+  // read the restart information, set vectors and variables
+  if (restart)
+    scatraonly->ScaTraField()->ReadRestart(restart);
+
+  // enter time loop to solve problem
+  (scatraonly->ScaTraField())->TimeLoop();
+
+  // perform the result test if required
+  DRT::Problem::Instance()->AddFieldTest(scatraonly->CreateScaTraFieldTest());
+  DRT::Problem::Instance()->TestAll(comm);
+
+return;
+
+} // end of reynolds_dyn()
