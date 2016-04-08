426626d81bb4481debdad454394990b7476538c4
Alexander Seitz <seitz@lnm.mw.tum.de>
2016-04-08 13:08:56 +0000

    increased integration rule in mortar element integrator for 3D 2nd order approximations - 'til now we used 7gp triangles for triangles, which was not sufficient to integrate undistorted tri6 mass matrices   and 3x3 gp for quad8, quad9, nurbs9 which are only sufficient for undistorted elements - changed to 16gp for tri6 and 5x5gp for quads - slightly adapted results of nurbs/tet test cases due to reduced integration error
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@21966 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_mortar/mortar_element_integrator.cpp
+++ src/drt_mortar/mortar_element_integrator.cpp
@@ -1,14 +1,9 @@
 /*!----------------------------------------------------------------------
 \file mortar_element_integrator.cpp
 
-\brief A class to perform Gaussian integration on a mortar element
+\maintainer Philipp Farah, Alexander Seitz
 
-<pre>
-Maintainer: Alexander Popp
-            popp@lnm.mw.tum.de
-            http://www.lnm.mw.tum.de
-            089 - 289-15238
-</pre>
+\brief A class to perform Gaussian integration on a mortar element
 
 *----------------------------------------------------------------------*/
 
@@ -23,10 +18,14 @@ MORTAR::ElementIntegrator::ElementIntegrator(DRT::Element::DiscretizationType el
   //*********************************************************************
   // Create integration points according to eletype!
   // Note that our standard Gauss rules are:
-  // 5 points: for integrals on 1D lines                 (1,2,3,4,5)
-  // 7 points: for integrals on 2D triangles             (1,3,6,7,12,37)
-  // 9 points: for integrals on 2D quadrilaterals        (1,4,9)
+  // 5  points: for integrals on 1D lines                 (1,2,3,4,5)
+  // 7  points: for integrals on 2D 1st triangles         (1,3,6,7,12,16,37)
+  // 16 points: for integrals on 2D 1st triangles         (1,3,6,7,12,16,37)
+  // 9  points: for integrals on 2D 1st order quadrilaterals
+  // 25 points: for integrals on 2D 2nd order quadrilaterals
   //**********************************************************************
+  Teuchos::RCP<DRT::UTILS::IntegrationPoints2D> rule2d;
+
   switch(eletype)
   {
   case DRT::Element::line2:
@@ -47,42 +46,37 @@ MORTAR::ElementIntegrator::ElementIntegrator(DRT::Element::DiscretizationType el
     break;
   }
   case DRT::Element::tri3:
-  case DRT::Element::tri6:
-  {
-    const DRT::UTILS::IntegrationPoints2D intpoints(DRT::UTILS::intrule_tri_7point);
-    ngp_ = intpoints.nquad;
-    coords_.Reshape(nGP(),2);
-    weights_.resize(nGP());
-    for (int i=0;i<nGP();++i)
-    {
-      coords_(i,0)=intpoints.qxg[i][0];
-      coords_(i,1)=intpoints.qxg[i][1];
-      weights_[i]=intpoints.qwgt[i];
-    }
+    rule2d = Teuchos::rcp(new DRT::UTILS::IntegrationPoints2D(DRT::UTILS::intrule_tri_7point));
     break;
-  }
+  case DRT::Element::tri6:
+    rule2d = Teuchos::rcp(new DRT::UTILS::IntegrationPoints2D(DRT::UTILS::intrule_tri_16point));
+    break;
   case DRT::Element::quad4:
+    rule2d = Teuchos::rcp(new DRT::UTILS::IntegrationPoints2D(DRT::UTILS::intrule_quad_9point));
+    break;
   case DRT::Element::quad8:
   case DRT::Element::quad9:
   case DRT::Element::nurbs4:
-  case DRT::Element::nurbs8:
   case DRT::Element::nurbs9:
+    rule2d = Teuchos::rcp(new DRT::UTILS::IntegrationPoints2D(DRT::UTILS::intrule_quad_25point));
+    break;
+  default:
+    dserror("ERROR: ElementIntegrator: This contact element type is not implemented!");
+  } // switch(eletype)
+
+  // save Gauss points for all 2D rules
+  if (rule2d!=Teuchos::null)
   {
-    const DRT::UTILS::IntegrationPoints2D intpoints(DRT::UTILS::intrule_quad_9point);
-    ngp_ = intpoints.nquad;
+    ngp_ = rule2d->nquad;
     coords_.Reshape(nGP(),2);
     weights_.resize(nGP());
     for (int i=0;i<nGP();++i)
     {
-      coords_(i,0)=intpoints.qxg[i][0];
-      coords_(i,1)=intpoints.qxg[i][1];
-      weights_[i]=intpoints.qwgt[i];
+      coords_(i,0)=rule2d->qxg[i][0];
+      coords_(i,1)=rule2d->qxg[i][1];
+      weights_[i]=rule2d->qwgt[i];
     }
-    break;
   }
-  default:
-    dserror("ERROR: ElementIntegrator: This contact element type is not implemented!");
-  } // switch(eletype)
 
   return;
 }
