6f09f47ec1d9cbe2c5e42ad596d402abac4d8ee0
Moritz Thon <thon@mhpc.mw.tum.de>
2016-06-17 12:55:30 +0000

    * fixed bug in the implementation of the derivatives of the pyramid5 shape functions.
    * implemented the case t=1 (corresponds to the top of the pyramid), which has to be dealt separately due to the unintuitive choice of shape functions (N_1,2,3,4 = 1.0/4.0*((1.0+-r)*(1.0+-s)-t+-r*s*t/(1-t), N_5=t).
    
    * adapted test cases and added new one to further test pyramid5 elements in the structure.
    
    * made files level 0.
    
    Approved by Martin.
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@22107 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_fem_general/drt_utils_fem_shapefunctions.cpp
+++ src/drt_fem_general/drt_utils_fem_shapefunctions.cpp
@@ -7,7 +7,8 @@ The corresponding graphics and a detailed description can be found in
 the Baci guide in the Convention chapter.
 
 <pre>
-\level 1
+\level 0
+
 \maintainer Martin Kronbichler
             http://www.lnm.mw.tum.de
             089 - 289-15235
