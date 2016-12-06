091752d39f526068b27909edcb8f8d207d0eb8c4
Andreas Rauch <rauch@lnm.mw.tum.de>
2016-12-06 10:20:42 +0000

    Obviously these files have to be committed separately.
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@22655 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ src/drt_struct_ale/struct_ale_str_model_evaluator.cpp
@@ -0,0 +1,53 @@
+/*-----------------------------------------------------------*/
+/*!
+\file struct_ale_str_model_evaluator.cpp
+
+\brief Model evaluator for Structure-ALE problems.
+
+\maintainer Andreas Rauch
+
+\date Dec 05, 2016
+
+\level 3
+
+*/
+/*-----------------------------------------------------------*/
+
+
+#include "struct_ale_str_model_evaluator.H"
+
+#include "../drt_lib/drt_discret.H"
+#include "../drt_structure_new/str_timint_basedataglobalstate.H"
+
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+STR::MODELEVALUATOR::StructAle::StructAle()
+    : material_displacements_np_(Teuchos::null)
+{
+  // empty
+}
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+void STR::MODELEVALUATOR::StructAle::
+    Setup()
+{
+  // call Setup() in base class
+  STR::MODELEVALUATOR::Structure::Setup();
+
+  // construct material displacements at \f$t_{n+1}\f$
+  material_displacements_np_ =
+      Teuchos::rcp(new Epetra_Vector(*GState().DofRowMapView(),true));
+
+  return;
+}
+
+/*----------------------------------------------------------------------------*
+ *----------------------------------------------------------------------------*/
+void STR::MODELEVALUATOR::StructAle::PreEvaluateInternal()
+{
+  // set state
+  DiscretPtr()->SetState(0, "material_displacement",material_displacements_np_);
+  return;
+}
