0a5f17f61eb837269eda4863aaee3a858b7435bb
Rui Fang <fang@lnm.mw.tum.de>
2017-03-28 16:29:34 +0000

    added scatra-thermo interaction (STI) test case involving AMG(BGS) preconditioning (only BGS(AMG) tested so far...)
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@23002 060e22d8-9d1c-0410-868a-e3a950d63d95

--- TestingFramework.cmake
+++ TestingFramework.cmake
@@ -1790,6 +1790,7 @@ baci_test(elch_3D_tet4_s2i_butlervolmer_mortar_standard_BGS-AMG_3x3 3 "")
 
 # scatra-thermo interaction using the AMGnxn framework
 baci_test(sti_2D_quad4_elch_s2i_butlervolmerpeltier_diabatic_BGS-AMG_2x2 3 15)
+baci_test(sti_3D_tet4_elch_s2i_butlervolmerpeltier_adiabatic_AMG-BGS_4x4 3 "")
 baci_test(sti_3D_tet4_elch_s2i_butlervolmerpeltier_adiabatic_BGS-AMG_4x4 3 "")
 baci_test(sti_3D_tet4_elch_s2i_butlervolmerpeltier_adiabatic_mortar_standard_BGS-AMG_2x2 3 "")
 baci_test(sti_3D_tet4_hex8_elch_s2i_butlervolmerpeltier_adiabatic_mortar_standard_BGS-AMG_4x4 3 15)
