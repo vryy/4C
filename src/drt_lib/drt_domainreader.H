46f2038dea12d03b441ab9d0cd8056100da25e22
Christoph Ager <ager@lnm.mw.tum.de>
2018-01-18 13:31:48 +0000

    ==| Change of maintainer in domainreader |==
    - took over maintainer from Magnus in domainreader (box geometry)
    
    
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@23485 060e22d8-9d1c-0410-868a-e3a950d63d95

--- src/drt_lib/drt_domainreader.H
+++ src/drt_lib/drt_domainreader.H
@@ -6,7 +6,12 @@
 
 \level 0
 
-\maintainer Magnus Winter
+<pre>
+\maintainer  Ager Christoph
+             ager@lnm.mw.tum.de
+             http://www.lnm.mw.tum.de
+             089 - 289-15249
+</pre>
 */
 /*----------------------------------------------------------------------*/
 
