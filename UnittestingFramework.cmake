7dde353209d5f35900443edc2f801aec4a69b29e
Sebastian Fuchs <fuchs@lnm.mw.tum.de>
2018-02-14 09:13:23 +0000

    reactivate unittests: refer to commit 22586 from Karl Wichmann for detailed information
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@23529 060e22d8-9d1c-0410-868a-e3a950d63d95

--- UnittestingFramework.cmake
+++ UnittestingFramework.cmake
@@ -1,9 +1,8 @@
 # This is the base of all unittests
 find_package( CxxTest 4 )
-if ( CxxTest_FOUND )
+if ( CXXTEST_FOUND )
   enable_testing()
   add_subdirectory ( Unittests )
 else ()
-  message(" WW Warning: Unittests are unavailable, as cxxtest is missing. Try: sudo dnf install cxxtest")
-endif ( CxxTest_FOUND )
-
+  message(" WW Warning: Unittests are unavailable, as cxxtest is missing. Try: sudo yum install cxxtest")
+endif ( CXXTEST_FOUND )
