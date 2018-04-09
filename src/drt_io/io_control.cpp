vryy/4C

commit 9c20ba3fc5d1eea876f968ea47afe321a4fba828

+++ every_iteration_writer +++ - Added a function to remove the restart counter from the directory name. - Added a routine which writes LineSearch steps as well.
+++ io_control +++
- Fixed a bug in the output control which caused problems (e.g. in case of the EveryIter
  output) for spatial approximations which differ from polynomials (e.g. NURBS). The
  spatial approximation string is now consistently considered in all cases.
- Since there was a lot of redundant code, I did a small clean-up as well.

+++ io +++
- Added the spatial approximation string as member for easy access. Changed some
  function calls due to the changes in io_control.

git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@23494 060e22d8-9d1c-0410-868a-e3a950d63d95

src/drt_io/io_control.H
@@ -98,17 +98,28 @@ namespace IO
     int BinIO() const { return create_controlfile_; }
 
     /// overwrites result files for the inverse analysis
-    void OverwriteResultFile();
+    void OverwriteResultFile( const std::string& spatial_approx );
     /// creates new result files for the inverse analysis
-    void NewResultFile(int numb_run);
+    void NewResultFile( int numb_run, const std::string& spatial_approx);
     /// creates new result files for the mlmc
-    void NewResultFile(std::string name_appendix, int numb_run);
+    void NewResultFile(
+        const std::string& name_appendix,
+        int numb_run,
+        const std::string& spatial_approx );
 
     /// creates new result files
-    void NewResultFile(std::string name );
+    void NewResultFile(
+        std::string name,
+        const std::string& spatial_approx );
 
     /// return my processor ID
     inline int MyRank() const { return myrank_; };
+
+  private:
+
+    void WriteHeader(
+        const std::string& control_file_name,
+        const std::string& spatial_approx );
 
   private:
     std::string problemtype_;

src/drt_io/io_control.cpp
@@ -104,30 +104,7 @@ IO::OutputControl::OutputControl(const Epetra_Comm& comm,
   {
     std::stringstream name;
     name << filename_ << ".control";
-    if (controlfile_.is_open())
-      controlfile_.close();
-    controlfile_.open(name.str().c_str(),std::ios_base::out);
-    if (not controlfile_)
-      dserror("could not open control file '%s' for writing", name.str().c_str());
-
-    time_t time_value;
-    time_value = time(NULL);
-
-    char hostname[31];
-    struct passwd *user_entry;
-    user_entry = getpwuid(getuid());
-    gethostname(hostname, 30);
-
-    controlfile_ << "# baci output control file\n"
-                 << "# created by "
-                 << user_entry->pw_name
-                 << " on " << hostname << " at " << ctime(&time_value)
-                 << "# using code revision " << (CHANGEDREVISION+0) << " \n\n"
-                 << "input_file = \"" << inputfile << "\"\n"
-                 << "problem_type = \"" << problemtype << "\"\n"
-                 << "spatial_approximation = \"" << spatial_approx << "\"\n"
-                 << "ndim = " << ndim << "\n"
-                 << "\n";
+    WriteHeader( name.str(), spatial_approx );
 
     // insert back reference
     if (restart)
@@ -228,48 +205,12 @@ IO::OutputControl::OutputControl(const Epetra_Comm& comm,
     }
   }
 
-  if (myrank_==0)
+  if ( create_controlfile_ )
   {
     std::stringstream name;
     name << filename_ << ".control";
-    if(create_controlfile_)
-    {
-    controlfile_.open(name.str().c_str(),std::ios_base::out);
-    if (not controlfile_)
-      dserror("could not open control file '%s' for writing", name.str().c_str());
-    }
 
-    time_t time_value;
-    time_value = time(NULL);
-
-    char hostname[31];
-    struct passwd *user_entry;
-    user_entry = getpwuid(getuid());
-    gethostname(hostname, 30);
-    if(create_controlfile_)
-    {
-      controlfile_ << "# baci output control file\n"
-                   << "# created by "
-                   << user_entry->pw_name
-                   << " on " << hostname << " at " << ctime(&time_value)
-                   << "# using code revision " << (CHANGEDREVISION+0) << " \n\n"
-                   << "input_file = \"" << inputfile << "\"\n"
-                   << "problem_type = \"" << problemtype << "\"\n"
-                   << "spatial_approximation = \"" << spatial_approx << "\"\n"
-                   << "ndim = " << ndim << "\n"
-                   << "\n";
-
-      // insert back reference
-      if (restart && create_controlfile_)
-      {
-        size_t pos = restartname_.rfind('/');
-        controlfile_ << "restarted_run = \""
-                     << ((pos!=std::string::npos) ? restartname_.substr(pos+1) : restartname_)
-                     << "\"\n\n";
-      }
-
-      controlfile_ << std::flush;
-    }
+    WriteHeader( name.str(), spatial_approx );
   }
 }
 
@@ -319,44 +260,20 @@ IO::OutputControl::OutputControl( const OutputControl& ocontrol,
 
 /*----------------------------------------------------------------------*/
 /*----------------------------------------------------------------------*/
-void IO::OutputControl::OverwriteResultFile()
+void IO::OutputControl::OverwriteResultFile( const std::string& spatial_approx )
 {
-  if (myrank_==0)
-  {
-    controlfile_.close();
-    std::stringstream name;
-    name << filename_ << ".control";
-    controlfile_.open(name.str().c_str(),std::ios_base::out);
-    if (not controlfile_)
-      dserror("could not open control file '%s' for writing", name.str().c_str());
+  std::stringstream name;
+  name << filename_ << ".control";
 
-    time_t time_value;
-    time_value = time(NULL);
-
-    char hostname[31];
-    struct passwd *user_entry;
-    user_entry = getpwuid(getuid());
-    gethostname(hostname, 30);
-
-    controlfile_ << "# baci output control file\n"
-                 << "# created by "
-                 << user_entry->pw_name
-                 << " on " << hostname << " at " << ctime(&time_value)
-                 << "# using code revision " << (CHANGEDREVISION+0) << " \n\n"
-                 << "input_file = \"" << inputfile_ << "\"\n"
-                 << "problem_type = \"" << problemtype_ << "\"\n"
-                 << "spatial_approximation = \"" << "Polynomial" << "\"\n"
-                 << "ndim = " << ndim_ << "\n"
-                 << "\n";
-
-    controlfile_ << std::flush;
-  }
+  WriteHeader( name.str(), spatial_approx );
 }
 
 
 /*----------------------------------------------------------------------*/
 /*----------------------------------------------------------------------*/
-void IO::OutputControl::NewResultFile(int numb_run)
+void IO::OutputControl::NewResultFile(
+    int numb_run,
+    const std::string& spatial_approx )
 {
   if (filename_.rfind("_run_")!=std::string::npos)
   {
@@ -371,63 +288,51 @@ void IO::OutputControl::NewResultFile(int numb_run)
   filename_ = name.str();
   name << ".control";
 
-  if (myrank_==0)
-  {
-    controlfile_.close();
-    controlfile_.open(name.str().c_str(),std::ios_base::out);
-    if (not controlfile_)
-      dserror("could not open control file '%s' for writing", name.str().c_str());
 
-    time_t time_value;
-    time_value = time(NULL);
-
-    char hostname[31];
-    struct passwd *user_entry;
-    user_entry = getpwuid(getuid());
-    gethostname(hostname, 30);
-
-    controlfile_ << "# baci output control file\n"
-                 << "# created by "
-                 << user_entry->pw_name
-                 << " on " << hostname << " at " << ctime(&time_value)
-                 << "# using code revision " << (CHANGEDREVISION+0) << " \n\n"
-                 << "input_file = \"" << inputfile_ << "\"\n"
-                 << "problem_type = \"" << problemtype_ << "\"\n"
-                 << "spatial_approximation = \"" << "Polynomial" << "\"\n"
-                 << "ndim = " << ndim_ << "\n"
-                 << "\n";
-
-    controlfile_ << std::flush;
-  }
+  WriteHeader( name.str(), spatial_approx );
 }
 
 /*----------------------------------------------------------------------*/
 /*----------------------------------------------------------------------*/
-void IO::OutputControl::NewResultFile(std::string name_appendix, int numb_run)
+void IO::OutputControl::NewResultFile(
+    const std::string& name_appendix,
+    int numb_run,
+    const std::string& spatial_approx )
 {
   std::stringstream name;
   name  << name_appendix;
   name << "_run_"<< numb_run;
 
-  NewResultFile( name.str() );
+  NewResultFile( name.str(), spatial_approx );
 }
 
 /*----------------------------------------------------------------------*/
 /*----------------------------------------------------------------------*/
-void IO::OutputControl::NewResultFile(std::string name)
+void IO::OutputControl::NewResultFile(
+    std::string name,
+    const std::string& spatial_approx )
 {
   filename_ = name;
   name += ".control";
 
+  WriteHeader( name, spatial_approx );
+}
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+void IO::OutputControl::WriteHeader(
+    const std::string& control_file_name,
+    const std::string& spatial_approx )
+{
   if (myrank_==0)
   {
-    controlfile_.close();
-    // bool b = controlfile_.fail();
-    // IO::cout << b << IO::endl;
+    if (controlfile_.is_open())
+      controlfile_.close();
 
-    controlfile_.open(name.c_str(),std::ios_base::out);
+    controlfile_.open(control_file_name.c_str(),std::ios_base::out);
     if (not controlfile_)
-      dserror("could not open control file '%s' for writing", name.c_str());
+      dserror("Could not open control file '%s' for writing",
+          control_file_name.c_str());
 
     time_t time_value;
     time_value = time(NULL);
@@ -444,7 +349,7 @@ void IO::OutputControl::NewResultFile(std::string name)
                  << "# using code revision " << (CHANGEDREVISION+0) << " \n\n"
                  << "input_file = \"" << inputfile_ << "\"\n"
                  << "problem_type = \"" << problemtype_ << "\"\n"
-                 << "spatial_approximation = \"" << "Polynomial" << "\"\n"
+                 << "spatial_approximation = \"" << spatial_approx << "\"\n"
                  << "ndim = " << ndim_ << "\n"
                  << "\n";
 

src/drt_io/every_iteration_writer.H
@@ -9,7 +9,7 @@
 
 \date Aug 10, 2017
 
-\level 3
+\level 1
 
 */
 /*----------------------------------------------------------------------------*/
@@ -61,8 +61,9 @@ namespace IO {
 
     /** @brief Get the current time/load step number
      *
-     *  Np stands for n+1, i.e. the first step is equal to one. This just a
-     *  definition thing, you can return any kind of meaningful number.
+     *  Np stands for n+1, i.e. the first step is equal to one. This is just a
+     *  definition thing, you can return any kind of meaningful unique
+     *  identification number.
      *
      *  @author hiermeier @date 08/17 */
     virtual int GetStepNp() const = 0;
@@ -76,10 +77,10 @@ namespace IO {
    *  input parameters are summarized in the section "---IO/EVERY ITERATION".
    *  The implementation is meant to work as plug-in solution for all kinds of
    *  problems. Anyway, the first implemented use can be found in the structural
-   *  time integration, such that it can be seen as a first working example.
+   *  time integration, such that it can be seen as a blue print.
    *
    *  In general, if you consider to use this class, you will have to construct
-   *  a object and call Init() and Setup() in consecutive order. Pass the
+   *  an object and call Init() and Setup() in consecutive order. Pass the
    *  output-writer of your problem to the Init() function. This output
    *  writer will be copied, such that the every iteration output should generate
    *  the same output each Newton step just as the parent writer does in each
@@ -103,10 +104,23 @@ namespace IO {
    *  "../o/fancy_sim_every_iter/fancy_sim_step_<CURRENT_STEP_NUMBER>". Each
    *  step gets an extra control, result and mesh file. It is also possible to
    *  restrict the output to one single time/load step. If you perform different
-   *  runs of the same simulation and the same load/time step, this is also
-   *  supported. Just use the input parameter RUN_NUMBER. If a positive number
-   *  is provided the result of each run can be found in the directory
-   *  "../o/fancy_sim_every_iter/run_<RUN_NUMBER>/".
+   *  runs of the same simulation and the same load/time step, just use the
+   *  input parameter RUN_NUMBER. If a positive number is provided the result of
+   *  each run can be found in the directory
+   *
+   *            "../o/fancy_sim_every_iter/run_<RUN_NUMBER>/".
+   *
+   *  Finally, a short note related to restarted steps: Think of a scenario
+   *  where you want to restart step 2 of your simulation "fancy_sim" and you
+   *  want to write the corresponding Newton iteration output as well. In this
+   *  case the directory path stays untouched, while the final file name is
+   *  extended by the restart counter number. Therefore, in the first restart
+   *  run the entire path will be
+   *
+   *      "../o/fancy_sim_every_iter/run_<RUN_NUMBER>/fancy_sim-1_step_2".
+   *
+   *  This makes sense since you will not overwrite any previous (debugging)
+   *  results which were generated by a possible completely different head file.
    *
    *  For more information see the mentioned input section.
    *
@@ -155,6 +169,18 @@ namespace IO {
      *  @author hiermeier @date 08/17 */
     void AddNewtonIteration( const int newton_iteration );
 
+    /** @brief Add a line search iteration to the current output
+     *
+     *  Call this method before the step length will be modified.
+     *
+     *  @param[in] newton_iteration  Number of the current Newton iteration.
+     *  @param[in] linesearch_iteration  Number of the current line search iteration.
+     *
+     *  @author hiermeier @date 11/17 */
+    void AddLineSearchIteration(
+        const int newton_iteration,
+        const int linesearch_iteration );
+
   private:
     /// Throw if Init() has not been called.
     inline void ThrowIfNotInitialized( const int line ) const
@@ -180,9 +206,6 @@ namespace IO {
 
     /// Create a new directory.
     void CreateDirectory( const std::string& dir_path ) const;
-
-    /// Extract the path of a full filename.
-    std::string ExtractPath( const std::string& full_filename ) const;
 
     /// Adjust the specified steps per file.
     void AdjustStepsPerFile( IO::OutputControl& control ) const;
@@ -226,6 +249,20 @@ namespace IO {
 
     constexpr static unsigned MAX_NUMBER_LINE_SEARCH_ITERATIONS_ = 100;
   };
+
+  /// Extract the path of a full filename.
+  std::string ExtractPath( const std::string& full_filename );
+
+  std::string ExtractFileName( const std::string& full_filename );
+
+  /// remove the restart counter from the file name
+  std::string RemoveRestartCountFromFileName( const std::string& filename );
+
+  /// Create a new directory.
+  void CreateDirectory( const std::string& dir_path, const int myrank );
+
+  /// Return number of lines in the given file
+  int CountLinesInFile( const std::string& filepath );
 }
 
 

src/drt_io/every_iteration_writer.cpp
@@ -19,6 +19,8 @@
 #include "io_control.H"
 #include "io_pstream.H"
 
+#include "../drt_lib/drt_globalproblem.H"
+
 #include "../drt_inpar/inpar_parameterlist_utils.H"
 
 #include <boost/filesystem.hpp>
@@ -70,16 +72,16 @@ void IO::EveryIterationWriter::Setup()
 {
   ThrowIfNotInitialized( __LINE__ );
 
-  const std::string dir_name( ParentWriter().Output()->FileNameOnlyPrefix() +
-      "_every_iter");
+  /* Remove the restart counter from the folder name. Note that the restart
+   * counter stays a part of the final file name of the corresponding step. */
+  const std::string filename_without_restart = RemoveRestartCountFromFileName(
+      ParentWriter().Output()->FileNameOnlyPrefix() );
+
+  const std::string dir_name( filename_without_restart + "_every_iter");
 
   std::string file_dir_path = ExtractPath( ParentWriter().Output()->FileName() );
   file_dir_path += dir_name;
   CreateDirectory( file_dir_path );
-
-  std::string restart_dir_path = ExtractPath( ParentWriter().Output()->RestartName() );
-  restart_dir_path += dir_name;
-  CreateDirectory( restart_dir_path );
 
   // modify the prefix and copy possible restart number
   std::string prefix;
@@ -121,9 +123,17 @@ std::string IO::EveryIterationWriter::CreateRunDirectory(
 
 /*----------------------------------------------------------------------------*
  *----------------------------------------------------------------------------*/
-void IO::EveryIterationWriter::CreateDirectory( const std::string& dir_path ) const
+void IO::EveryIterationWriter::CreateDirectory(
+    const std::string& dir_path ) const
 {
-  if ( myrank_ != 0)
+  IO::CreateDirectory( dir_path, myrank_ );
+}
+
+/*----------------------------------------------------------------------------*
+ *----------------------------------------------------------------------------*/
+void IO::CreateDirectory( const std::string& dir_path, const int myrank )
+{
+  if ( myrank != 0)
     return;
 
   boost::filesystem::path dir( dir_path );
@@ -134,8 +144,7 @@ void IO::EveryIterationWriter::CreateDirectory( const std::string& dir_path ) co
 
 /*----------------------------------------------------------------------------*
  *----------------------------------------------------------------------------*/
-std::string IO::EveryIterationWriter::ExtractPath(
-    const std::string& full_filename ) const
+std::string IO::ExtractPath( const std::string& full_filename )
 {
   std::string filename_path;
   size_t pos = full_filename.rfind('/');
@@ -144,6 +153,36 @@ std::string IO::EveryIterationWriter::ExtractPath(
     filename_path = full_filename.substr(0,pos+1);
 
   return filename_path;
+}
+
+/*----------------------------------------------------------------------------*
+ *----------------------------------------------------------------------------*/
+std::string IO::ExtractFileName( const std::string& full_filename )
+{
+  std::string filenameonly = full_filename;
+
+  size_t pos = full_filename.rfind('/');
+  if (pos!=std::string::npos)
+  {
+    filenameonly = full_filename.substr(pos+1);
+  }
+
+  return filenameonly;
+}
+
+/*----------------------------------------------------------------------------*
+ *----------------------------------------------------------------------------*/
+std::string IO::RemoveRestartCountFromFileName( const std::string& filename )
+{
+  const int restart_count = DRT::Problem::Instance()->Restart();
+  if ( restart_count == 0 )
+    return filename;
+
+  size_t pos = filename.rfind('-');
+  if (pos==std::string::npos)
+    dserror("Couldn't find the restart separator \"-\"!");
+
+  return filename.substr( 0, pos );
 }
 
 /*----------------------------------------------------------------------------*
@@ -218,4 +257,55 @@ void IO::EveryIterationWriter::AddNewtonIteration( const int newton_iteration )
 
   interface_->PrepareOutput();
   interface_->OutputState( *every_iter_writer_, write_owner_each_newton_iteration_ );
+}
+
+/*----------------------------------------------------------------------------*
+ *----------------------------------------------------------------------------*/
+void IO::EveryIterationWriter::AddLineSearchIteration(
+    const int newton_iteration,
+    const int linesearch_iteration )
+{
+  ThrowIfNotSetup( __LINE__ );
+
+  if ( not WriteThisStep() )
+    return;
+
+  if ( not isnewton_initialized_ )
+    dserror( "Call InitNewtonIteration() first!" );
+
+  if ( linesearch_iteration >= static_cast<int>(MAX_NUMBER_LINE_SEARCH_ITERATIONS_) )
+    dserror("The EveryIterationWriter does not support more than %d line search"
+        " steps. If this number is exceeded, the counters will get mixed up.",
+        MAX_NUMBER_LINE_SEARCH_ITERATIONS_-1);
+
+  const int counter = MAX_NUMBER_LINE_SEARCH_ITERATIONS_ * newton_iteration
+      + linesearch_iteration;
+  every_iter_writer_->WriteMesh( counter, counter );
+  every_iter_writer_->NewStep( counter, counter );
+
+  interface_->PrepareOutput();
+  interface_->OutputState( *every_iter_writer_, write_owner_each_newton_iteration_ );
+}
+
+/*----------------------------------------------------------------------------*
+ *----------------------------------------------------------------------------*/
+int IO::CountLinesInFile( const std::string& filepath )
+{
+  std::ifstream myfile( filepath );
+
+  if ( not myfile.is_open() )
+    return -1;
+
+  // new lines will be skipped unless we stop it from happening:
+  myfile.unsetf(std::ios_base::skipws);
+
+  // count the newlines with an algorithm specialized for counting:
+  int line_count = std::count(
+      std::istream_iterator<char>(myfile),
+      std::istream_iterator<char>(),
+      '\n');
+
+  myfile.close();
+
+  return line_count;
 }

src/drt_io/io.H
@@ -421,6 +421,8 @@ protected:
 
   // do we want binary output
   bool binio_;
+
+  std::string spatial_approx_;
 };
 
 

src/drt_io/io.cpp
@@ -493,7 +493,8 @@ IO::DiscretizationWriter::DiscretizationWriter()           /* PROTECTED */
       resultfile_changed_(-1),
       meshfile_changed_(-1),
       output_(Teuchos::null),
-      binio_(false)
+      binio_(false),
+      spatial_approx_("")
 {
   // intentionally left blank
 }
@@ -512,7 +513,8 @@ IO::DiscretizationWriter::DiscretizationWriter(Teuchos::RCP<DRT::Discretization>
       resultgroup_(-1),
       resultfile_changed_(-1),
       meshfile_changed_(-1),
-      output_(DRT::Problem::Instance()->OutputControlFile())
+      output_(DRT::Problem::Instance()->OutputControlFile()),
+      spatial_approx_(DRT::Problem::Instance()->SpatialApproximation())
 {
   if (output_!=Teuchos::null)
     binio_ = output_->BinIO();
@@ -539,7 +541,8 @@ IO::DiscretizationWriter::DiscretizationWriter(
       resultfile_changed_(-1),
       meshfile_changed_(-1),
       output_( Teuchos::null ),
-      binio_( false )
+      binio_( false ),
+      spatial_approx_( writer.spatial_approx_ )
 {
   output_ = ( control.is_null() ? writer.output_ : control );
   if ( not output_.is_null() )
@@ -698,7 +701,7 @@ void IO::DiscretizationWriter::NewResultFile(int numb_run)
   if(binio_)
   {
     CreateNewResultAndMeshFile();
-    output_->NewResultFile(numb_run);
+    output_->NewResultFile(numb_run,spatial_approx_);
   }
 }
 
@@ -711,7 +714,7 @@ void IO::DiscretizationWriter::NewResultFile(std::string name_appendix,
   if(binio_)
   {
     CreateNewResultAndMeshFile();
-    output_->NewResultFile(name_appendix, numb_run);
+    output_->NewResultFile(name_appendix, numb_run, spatial_approx_);
   }
 }
 
@@ -722,7 +725,7 @@ void IO::DiscretizationWriter::NewResultFile(std::string name)
   if(binio_)
   {
     CreateNewResultAndMeshFile();
-    output_->NewResultFile(name);
+    output_->NewResultFile(name, spatial_approx_);
   }
 }
 
@@ -733,7 +736,7 @@ void IO::DiscretizationWriter::OverwriteResultFile()
   if(binio_)
   {
     CreateNewResultAndMeshFile();
-    output_->OverwriteResultFile();
+    output_->OverwriteResultFile(spatial_approx_);
   }
 }
 

commit 71a1fde10ec6d264c4c4a8d2b746991798ce64c9

Fixed a bug in io_control. The back reference to former (restart) control files was missing. This caused an incomplete post-processing result. The bug has been introduced by myself in r23494. Sorry for that.
Thx to Alex for the hint and short discussion.

git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@23596 060e22d8-9d1c-0410-868a-e3a950d63d95

src/drt_io/io_control.H
@@ -121,6 +121,10 @@ namespace IO
         const std::string& control_file_name,
         const std::string& spatial_approx );
 
+    void InsertRestartBackReference(
+        int restart,
+        const std::string& outputname );
+
   private:
     std::string problemtype_;
     std::string inputfile_;  ///< input file name

src/drt_io/io_control.cpp
@@ -100,23 +100,11 @@ IO::OutputControl::OutputControl(const Epetra_Comm& comm,
     }
   }
 
-  if (myrank_==0)
-  {
-    std::stringstream name;
-    name << filename_ << ".control";
-    WriteHeader( name.str(), spatial_approx );
+  std::stringstream name;
+  name << filename_ << ".control";
+  WriteHeader( name.str(), spatial_approx );
 
-    // insert back reference
-    if (restart)
-    {
-      size_t pos = outputname.rfind('/');
-      controlfile_ << "restarted_run = \""
-                   << ((pos!=std::string::npos) ? outputname.substr(pos+1) : outputname)
-                   << "\"\n\n";
-    }
-
-    controlfile_ << std::flush;
-  }
+  InsertRestartBackReference( restart, outputname );
 }
 
 
@@ -211,6 +199,8 @@ IO::OutputControl::OutputControl(const Epetra_Comm& comm,
     name << filename_ << ".control";
 
     WriteHeader( name.str(), spatial_approx );
+
+    InsertRestartBackReference( restart, outputname );
   }
 }
 
@@ -352,6 +342,27 @@ void IO::OutputControl::WriteHeader(
                  << "spatial_approximation = \"" << spatial_approx << "\"\n"
                  << "ndim = " << ndim_ << "\n"
                  << "\n";
+
+    controlfile_ << std::flush;
+  }
+}
+
+/*----------------------------------------------------------------------*/
+/*----------------------------------------------------------------------*/
+void IO::OutputControl::InsertRestartBackReference(
+    int restart,
+    const std::string& outputname )
+{
+  if ( myrank_!=0 )
+    return;
+
+  // insert back reference
+  if (restart)
+  {
+    size_t pos = outputname.rfind('/');
+    controlfile_ << "restarted_run = \""
+                 << ((pos!=std::string::npos) ? outputname.substr(pos+1) : outputname)
+                 << "\"\n\n";
 
     controlfile_ << std::flush;
   }