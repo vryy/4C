f85690bf7a2871c7b03d732b9770d5ec1ea1714f
Christoph Ager <ager@lnm.mw.tum.de>
2018-08-06 11:58:15 +0000

    ==| Preparation for Testing in GitLab |==
    - new folder tests/testconfig includes the configuration files for the different tests
            o release
            o debug
            o doxygen
            therein environmental variables are used to specify the actual test.
            
            o additionally the file tests/testconfig/.gitlab-ci.yml includes the setup of all testing piplines and 
            
    - add specific config file to test the debug version (since we need now everything for testing in the repository)
    
    - adapt CTestConfig.cmake to be specified by environmental variables (if not set we get the default behavior --> submit to dashboard on jacobi)
            o this allows to use different configurations for different test machines/gitlab-runners
            
            
    Ivo & Christoph
    
    git-svn-id: svn://jacobi.lnm.mw.tum.de/baci/trunk/ccarat@23652 060e22d8-9d1c-0410-868a-e3a950d63d95

---
+++ tests/testconfig/test_debug.cmake
@@ -0,0 +1,41 @@
+# parameters
+set(CTEST_SOURCE_DIRECTORY "$ENV{CI_PROJECT_DIR}")
+set(CTEST_BINARY_DIRECTORY "$ENV{CI_PROJECT_DIR}/../baci-build")
+set(CTEST_TIMEOUT          "2000")       # ctest timeout 500s
+set($ENV{LC_MESSAGES}      "en_EN" )    # set output to english such that ctest can analyze it
+
+set(CTEST_SITE "$ENV{HOSTNAME}")
+set(CTEST_BUILD_NAME "$ENV{CTEST_BUILD_NAME_GITLAB}")
+
+# prepare environment
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(WITH_MEMCHECK TRUE)
+set(WITH_COVERAGE TRUE)
+
+ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+
+# prepare commands
+set(CTEST_CONFIGURE_COMMAND "${CTEST_SOURCE_DIRECTORY}/do-configure -f --debug --trilinos-debug --config=${CTEST_SOURCE_DIRECTORY}/buildconfig/$ENV{CTEST_BUILD_CONFIG_GITLAB} --useDEAL")
+set(CTEST_BUILD_COMMAND     "$ENV{CTEST_MAKE}")
+set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS "5000")
+set(CTEST_COMMAND "ctest -D Nightly")
+
+# do the testing
+ctest_start("${HOSTNAME}")
+#ctest_update()
+ctest_configure()
+ctest_build(RETURN_VALUE testBuild)
+ctest_test(RETURN_VALUE testRes)
+
+if ($ENV{CTEST_DROP_SITE_CDASH_GITLAB} EQUAL 1)
+    ctest_submit()
+endif ($ENV{CTEST_DROP_SITE_CDASH_GITLAB} EQUAL 1)
+
+if (NOT ${testBuild} EQUAL 0)
+  message( SEND_ERROR "Baci build failed!" )
+endif (NOT ${testBuild} EQUAL 0)
+
+if (NOT ${testRes} EQUAL 0)
+  message( SEND_ERROR "Baci tests failed!" )
+endif (NOT ${testRes} EQUAL 0)
+
