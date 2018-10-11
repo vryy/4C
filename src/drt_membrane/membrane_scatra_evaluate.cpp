ac7a0f2ab985f3111fff97fbf0516cdbdc90b434
Sebastian Fuchs <fuchs@lnm.mw.tum.de>
2018-10-11 14:18:09 +0200

    Add safety check in membrane scatra evaluation
    in membrane scatra preevaluate check if scalar state is set before
    finally accessing it
    
    part of #97

--- src/drt_membrane/membrane_scatra_evaluate.cpp
+++ src/drt_membrane/membrane_scatra_evaluate.cpp
@@ -39,61 +39,64 @@ void DRT::ELEMENTS::MembraneScatra<distype>::PreEvaluate(Teuchos::ParameterList&
     // name of scalarfield
     std::string scalarfield = "scalarfield";
 
-    // get the scalar state
-    Teuchos::RCP<const Epetra_Vector> scalarnp = discretization.GetState(1, scalarfield);
-
-    if (scalarnp == Teuchos::null) dserror("can not get state vector %s", scalarfield.c_str());
-
-    // extract local values of the global vectors
-    Teuchos::RCP<std::vector<double>> myscalar =
-        Teuchos::rcp(new std::vector<double>(la[1].lm_.size(), 0.0));
-
-    DRT::UTILS::ExtractMyValues(*scalarnp, *myscalar, la[1].lm_);
-
-    // element vector for k-th scalar
-    std::vector<LINALG::Matrix<Membrane<distype>::numnod_, 1>> elescalar(numscal);
-    for (int k = 0; k < numscal; ++k)
+    if (discretization.HasState(1, scalarfield))
     {
-      for (int i = 0; i < Membrane<distype>::numnod_; ++i)
-      {
-        (elescalar.at(k))(i, 0) = myscalar->at(numscal * i + k);
-      }
-    }
+      // get the scalar state
+      Teuchos::RCP<const Epetra_Vector> scalarnp = discretization.GetState(1, scalarfield);
 
-    // number of gauss points
-    const int numgp = (Membrane<distype>::intpoints_).nquad;
+      if (scalarnp == Teuchos::null) dserror("can not get state vector %s", scalarfield.c_str());
 
-    // create vector of gauss point values to be set in params list
-    Teuchos::RCP<std::vector<std::vector<double>>> gpscalar = Teuchos::rcp(
-        new std::vector<std::vector<double>>(numgp, std::vector<double>(numscal, 0.0)));
+      // extract local values of the global vectors
+      Teuchos::RCP<std::vector<double>> myscalar =
+          Teuchos::rcp(new std::vector<double>(la[1].lm_.size(), 0.0));
 
-    // allocate vector for shape functions and matrix for derivatives at gp
-    LINALG::Matrix<Membrane<distype>::numnod_, 1> shapefcts(true);
+      DRT::UTILS::ExtractMyValues(*scalarnp, *myscalar, la[1].lm_);
 
-    // loop over gauss points
-    for (int gp = 0; gp < numgp; ++gp)
-    {
-      // get gauss points from integration rule
-      double xi_gp = (Membrane<distype>::intpoints_).qxg[gp][0];
-      double eta_gp = (Membrane<distype>::intpoints_).qxg[gp][1];
-
-      // get shape functions and derivatives in the plane of the element
-      DRT::UTILS::shape_function_2D(shapefcts, xi_gp, eta_gp, Shape());
-
-      // scalar at current gp
-      std::vector<double> scalar_curr_gp(numscal, 0.0);
-
+      // element vector for k-th scalar
+      std::vector<LINALG::Matrix<Membrane<distype>::numnod_, 1>> elescalar(numscal);
       for (int k = 0; k < numscal; ++k)
       {
-        // identical shapefunctions for displacements and scalar fields
-        scalar_curr_gp.at(k) = shapefcts.Dot(elescalar.at(k));
+        for (int i = 0; i < Membrane<distype>::numnod_; ++i)
+        {
+          (elescalar.at(k))(i, 0) = myscalar->at(numscal * i + k);
+        }
       }
 
-      gpscalar->at(gp) = scalar_curr_gp;
-    }
+      // number of gauss points
+      const int numgp = (Membrane<distype>::intpoints_).nquad;
 
-    // set scalar states at gp to params list
-    params.set<Teuchos::RCP<std::vector<std::vector<double>>>>("gp_scalar", gpscalar);
+      // create vector of gauss point values to be set in params list
+      Teuchos::RCP<std::vector<std::vector<double>>> gpscalar = Teuchos::rcp(
+          new std::vector<std::vector<double>>(numgp, std::vector<double>(numscal, 0.0)));
+
+      // allocate vector for shape functions and matrix for derivatives at gp
+      LINALG::Matrix<Membrane<distype>::numnod_, 1> shapefcts(true);
+
+      // loop over gauss points
+      for (int gp = 0; gp < numgp; ++gp)
+      {
+        // get gauss points from integration rule
+        double xi_gp = (Membrane<distype>::intpoints_).qxg[gp][0];
+        double eta_gp = (Membrane<distype>::intpoints_).qxg[gp][1];
+
+        // get shape functions and derivatives in the plane of the element
+        DRT::UTILS::shape_function_2D(shapefcts, xi_gp, eta_gp, Shape());
+
+        // scalar at current gp
+        std::vector<double> scalar_curr_gp(numscal, 0.0);
+
+        for (int k = 0; k < numscal; ++k)
+        {
+          // identical shapefunctions for displacements and scalar fields
+          scalar_curr_gp.at(k) = shapefcts.Dot(elescalar.at(k));
+        }
+
+        gpscalar->at(gp) = scalar_curr_gp;
+      }
+
+      // set scalar states at gp to params list
+      params.set<Teuchos::RCP<std::vector<std::vector<double>>>>("gp_scalar", gpscalar);
+    }
   }
 
   return;
