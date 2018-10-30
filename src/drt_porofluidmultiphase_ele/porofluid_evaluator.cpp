2b640977d0fc1d800e41d849490aeccf51002511
Johannes Kremheller <kremheller@lnm.mw.tum.de>
2018-10-30 17:53:11 +0100

    Change maintainer of Tumor-Framework
    part of #118
    
    maintainer of PoromultiphaseScatra-Framework is changed from
    @lyoshihara to @jkremheller

--- src/drt_porofluidmultiphase_ele/porofluid_evaluator.cpp
+++ src/drt_porofluidmultiphase_ele/porofluid_evaluator.cpp
@@ -6,8 +6,8 @@
 
    \level 3
 
-   \maintainer  Lena Yoshihara
-                yoshihara@lnm.mw.tum.de
+   \maintainer  Johannes Kremheller
+                kremheller@lnm.mw.tum.de
                 http://www.lnm.mw.tum.de251
  *----------------------------------------------------------------------*/
 
