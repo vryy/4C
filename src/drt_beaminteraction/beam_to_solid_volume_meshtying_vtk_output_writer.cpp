c61f8cdb05504acda482bb646164a236c2fdc045
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2019-04-12 11:35:14 +0200

    Add nodal contact forces output #158

--- src/drt_beaminteraction/beam_to_solid_volume_meshtying_vtk_output_writer.cpp
+++ src/drt_beaminteraction/beam_to_solid_volume_meshtying_vtk_output_writer.cpp
@@ -15,8 +15,13 @@
 #include "beam_to_solid_vtu_output_writer_base.H"
 #include "beam_to_solid_vtu_output_writer_visualization.H"
 #include "beaminteraction_submodel_evaluator_beamcontact.H"
-
+#include "beaminteraction_calc_utils.H"
+#include "str_model_evaluator_beaminteraction_datastate.H"
 #include "../drt_structure_new/str_timint_basedataglobalstate.H"
+#include "../drt_lib/drt_discret.H"
+#include "../linalg/linalg_utils.H"
+
+#include <Epetra_FEVector.h>
 
 
 /**
@@ -141,6 +146,54 @@ void BEAMINTERACTION::BeamToSolidVolumeMeshtyingVtkOutputWriter::
         const BEAMINTERACTION::SUBMODELEVALUATOR::BeamContact* beam_contact, int i_step,
         double time) const
 {
+  // Parameter list that will be passed to all contact pairs when they create their visualization.
+  Teuchos::ParameterList visualization_params;
+
+
+  // Add the nodal forces resulting from beam contact. The forces are split up into beam and solid
+  // nodes.
+  Teuchos::RCP<BEAMINTERACTION::BeamToSolidVtuOutputWriterVisualization> visualization =
+      output_writer_base_ptr_->GetVisualizationWriter("nodal-forces");
+  if (visualization != Teuchos::null)
+  {
+    // Add the reference geometry and displacement to the visualization.
+    visualization->AddDiscretizationNodalReferencePosition(beam_contact->DiscretPtr());
+    visualization->AddDiscretizationNodalData(
+        "displacement", beam_contact->BeamInteractionDataState().GetDisNp());
+
+    // Create maps with the GIDs of beam and solid nodes.
+    std::vector<int> gid_beam_dof;
+    std::vector<int> gid_solid_dof;
+    std::vector<int> gid_node;
+    for (int i_lid = 0; i_lid < beam_contact->DiscretPtr()->NumMyRowNodes(); i_lid++)
+    {
+      gid_node.clear();
+      DRT::Node* current_node = beam_contact->DiscretPtr()->lRowNode(i_lid);
+      beam_contact->DiscretPtr()->Dof(current_node, gid_node);
+      if (BEAMINTERACTION::UTILS::IsBeamNode(*current_node))
+        for (unsigned int dim = 0; dim < 3; ++dim) gid_beam_dof.push_back(gid_node[dim]);
+      else
+        for (unsigned int dim = 0; dim < 3; ++dim) gid_solid_dof.push_back(gid_node[dim]);
+    }
+    Epetra_Map beam_dof_map(
+        -1, gid_beam_dof.size(), &gid_beam_dof[0], 0, beam_contact->DiscretPtr()->Comm());
+    Epetra_Map solid_dof_map(
+        -1, gid_solid_dof.size(), &gid_solid_dof[0], 0, beam_contact->DiscretPtr()->Comm());
+
+    // Extract the forces and add them to the discretization.
+    Teuchos::RCP<Epetra_Vector> force_beam =
+        Teuchos::rcp<Epetra_Vector>(new Epetra_Vector(beam_dof_map, true));
+    LINALG::Export(*beam_contact->BeamInteractionDataState().GetForceNp(), *force_beam);
+    Teuchos::RCP<Epetra_Vector> force_solid =
+        Teuchos::rcp<Epetra_Vector>(new Epetra_Vector(solid_dof_map, true));
+    LINALG::Export(*beam_contact->BeamInteractionDataState().GetForceNp(), *force_solid);
+    visualization->AddDiscretizationNodalData("force_beam", force_beam);
+    visualization->AddDiscretizationNodalData("force_solid", force_solid);
+  }
+
+
+  // Write the data to disc. The data will be cleared in this method.
+  output_writer_base_ptr_->Write(i_step, time);
 }
 
 
