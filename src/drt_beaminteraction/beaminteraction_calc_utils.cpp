2a2e7bb006986c251235e973b5e66790abbc749a
Nora Hagmeyer <nora.hagmeyer@unibw.de>
2019-05-14 14:38:38 +0200

    Implement beaminteraction util functions for fbi
    for the coupling of flui and beams, many
    necessary operations are similar to the ones
    for solid beam to beam coupling,  but need both,
    fluid and structure, discretizations.
    This commit mainly adds util functions that can
    receive two seperate discretizations.i #194

--- src/drt_beaminteraction/beaminteraction_calc_utils.cpp
+++ src/drt_beaminteraction/beaminteraction_calc_utils.cpp
@@ -614,6 +614,68 @@ namespace BEAMINTERACTION
         fe_sysmat->FEAssemble(elemat[1][1], lmrow2, lmrow2);
       }
     }
+    /*----------------------------------------------------------------------------*
+     *----------------------------------------------------------------------------*/
+    void FEAssembleEleForceStiffIntoSystemVectorMatrices(
+        const Teuchos::RCP<const std::vector<Teuchos::RCP<DRT::Discretization>>>& discretizations,
+        std::vector<int> const& elegid, std::vector<LINALG::SerialDenseVector> const& elevec,
+        std::vector<std::vector<LINALG::SerialDenseMatrix>> const& elemat,
+        Teuchos::RCP<Epetra_FEVector>& ff, Teuchos::RCP<Epetra_FEVector>& fb,
+        Teuchos::RCP<LINALG::SparseMatrix>& cff, Teuchos::RCP<LINALG::SparseMatrix>& cbb,
+        Teuchos::RCP<LINALG::SparseMatrix>& cfb, Teuchos::RCP<LINALG::SparseMatrix>& cbf)
+    {
+      if (discretizations->size() < 2)
+      {
+        dserror(
+            "This function expects at least 2 discretizations. Please check that you are calling "
+            "the correct function!\n");
+      }
+      // the entries of elevecX  belong to the Dofs of the element with GID elegidX
+      // the rows    of elematXY belong to the Dofs of the element with GID elegidX
+      // the columns of elematXY belong to the Dofs of the element with GID elegidY
+      const DRT::Element* ele1 = (*discretizations)[0]->gElement(elegid[0]);
+      const DRT::Element* ele2 = (*discretizations)[1]->gElement(elegid[1]);
+
+      // get element location vector and ownerships
+      std::vector<int> lmrow1;
+      std::vector<int> lmrow2;
+      std::vector<int> lmrowowner1;
+      std::vector<int> lmrowowner2;
+      std::vector<int> lmstride;
+
+      ele1->LocationVector(*(*discretizations)[0], lmrow1, lmrowowner1, lmstride);
+      ele2->LocationVector(*(*discretizations)[1], lmrow2, lmrowowner2, lmstride);
+
+      // assemble both element vectors into global system vector
+      if (fb != Teuchos::null)
+      {
+        fb->SumIntoGlobalValues(elevec[0].Length(), &lmrow1[0], elevec[0].Values());
+      }
+      if (ff != Teuchos::null)
+      {
+        ff->SumIntoGlobalValues(elevec[1].Length(), &lmrow2[0], elevec[1].Values());
+      }
+
+      // and finally also assemble stiffness contributions
+      if (cbb != Teuchos::null)
+      {
+        cbb->FEAssemble(elemat[0][0], lmrow1, lmrow1);
+      }
+      if (cbf != Teuchos::null)
+      {
+        cbf->FEAssemble(elemat[0][1], lmrow1, lmrow2);
+      }
+      if (cfb != Teuchos::null)
+      {
+        cfb->FEAssemble(elemat[1][0], lmrow2, lmrow1);
+      }
+      if (cff != Teuchos::null)
+      {
+        cff->FEAssemble(elemat[1][1], lmrow2, lmrow2);
+      }
+    }
+    /*----------------------------------------------------------------------------*
+     *----------------------------------------------------------------------------*/
 
     /**
      *
@@ -641,7 +703,128 @@ namespace BEAMINTERACTION
         for (unsigned int i = 0; i < num_dof; ++i) ele_centerline_dof_indices[i] = i;
       }
     }
+    /*----------------------------------------------------------------------------*
+     *----------------------------------------------------------------------------*/
 
+    /**
+     *
+     */
+    void GetFBIElementCenterlineDOFIndices(DRT::Discretization const& discret,
+        const DRT::Element* ele, std::vector<unsigned int>& ele_centerline_dof_indices,
+        unsigned int& num_dof)
+    {
+      // Todo implement method in DRT::Element or find alternative way of doing this
+      // find out the elements' number of Dofs (=dimension of element vector/matrices)
+      std::vector<int> lmrow;
+      std::vector<int> dummy1, dummy2;
+
+      ele->LocationVector(discret, lmrow, dummy1, dummy2);
+      num_dof = lmrow.size();
+
+      const DRT::ELEMENTS::Beam3Base* beamele = dynamic_cast<const DRT::ELEMENTS::Beam3Base*>(ele);
+
+      if (beamele != NULL)
+      {
+        beamele->CenterlineDofIndicesOfElement(ele_centerline_dof_indices);
+      }
+      else
+      {
+        ele_centerline_dof_indices.resize(num_dof * 3 / 4);
+        int j = 0;
+        for (unsigned int i = 0; i < num_dof; ++i)
+        {
+          if (!((i + 1) % 4)) ++i;
+          ele_centerline_dof_indices[j] = i;
+          j++;
+        }
+      }
+    }
+
+    /*----------------------------------------------------------------------------*
+     *----------------------------------------------------------------------------*/
+    void AssembleCenterlineDofForceStiffIntoFBIElementForceStiff(
+        const Teuchos::RCP<const std::vector<Teuchos::RCP<DRT::Discretization>>>& discretizations,
+        std::vector<int> const& elegid,
+        std::vector<LINALG::SerialDenseVector> const& eleforce_centerlineDOFs,
+        std::vector<std::vector<LINALG::SerialDenseMatrix>> const& elestiff_centerlineDOFs,
+        std::vector<LINALG::SerialDenseVector>* eleforce,
+        std::vector<std::vector<LINALG::SerialDenseMatrix>>* elestiff)
+    {
+      if (discretizations->size() < 2)
+      {
+        dserror(
+            "This function expects at least 2 discretizations. Please check that you are calling "
+            "the correct function!\n");
+      }
+      std::vector<unsigned int> numdof_ele(2);
+      std::vector<std::vector<unsigned int>> ele_centerlinedofindices(2);
+
+      for (unsigned int iele = 0; iele < 2; ++iele)
+      {
+        DRT::Element* ele = (*discretizations)[iele]->gElement(elegid[iele]);
+        GetFBIElementCenterlineDOFIndices(
+            *(*discretizations)[iele], ele, ele_centerlinedofindices[iele], numdof_ele[iele]);
+      }
+
+
+      // assemble centerline DOF values correctly into element DOFvec vectors/matrices
+      if (eleforce != NULL)
+      {
+        for (unsigned int iele = 0; iele < 2; ++iele)
+        {
+          // resize and clear variable
+          ((*eleforce)[iele]).Size(numdof_ele[iele]);
+
+          // safety check: dimensions
+          if ((unsigned int)eleforce_centerlineDOFs[iele].RowDim() !=
+              ele_centerlinedofindices[iele].size())
+            dserror(
+                "size mismatch! need to assemble %d values of centerline-Dof based "
+                "force vector into element vector but only got %d element-local Dof indices",
+                eleforce_centerlineDOFs[iele].RowDim(), ele_centerlinedofindices[iele].size());
+
+          // Todo maybe use a more general 'SerialDenseAssemble' method here
+          for (unsigned int idof = 0; idof < ele_centerlinedofindices[iele].size(); ++idof)
+            ((*eleforce)[iele])(ele_centerlinedofindices[iele][idof]) =
+                eleforce_centerlineDOFs[iele](idof);
+        }
+      }
+
+      if (elestiff != NULL)
+      {
+        for (unsigned int iele = 0; iele < 2; ++iele)
+        {
+          for (unsigned int jele = 0; jele < 2; ++jele)
+          {
+            // resize and clear variable
+            ((*elestiff)[iele][jele]).Shape(numdof_ele[iele], numdof_ele[jele]);
+
+            // safety check: dimensions
+            if ((unsigned int)elestiff_centerlineDOFs[iele][jele].RowDim() !=
+                ele_centerlinedofindices[iele].size())
+              dserror(
+                  "size mismatch! need to assemble %d row values of centerline-Dof based "
+                  "stiffness matrix into element matrix but only got %d element-local Dof indices",
+                  elestiff_centerlineDOFs[iele][jele].RowDim(),
+                  ele_centerlinedofindices[iele].size());
+
+            if ((unsigned int)elestiff_centerlineDOFs[iele][jele].ColDim() !=
+                ele_centerlinedofindices[jele].size())
+              dserror(
+                  "size mismatch! need to assemble %d column values of centerline-Dof based "
+                  "stiffness matrix into element matrix but only got %d element-local Dof indices",
+                  elestiff_centerlineDOFs[iele][jele].ColDim(),
+                  ele_centerlinedofindices[jele].size());
+
+            for (unsigned int idof = 0; idof < ele_centerlinedofindices[iele].size(); ++idof)
+              for (unsigned int jdof = 0; jdof < ele_centerlinedofindices[jele].size(); ++jdof)
+                ((*elestiff)[iele][jele])(
+                    ele_centerlinedofindices[iele][idof], ele_centerlinedofindices[jele][jdof]) =
+                    elestiff_centerlineDOFs[iele][jele](idof, jdof);
+          }
+        }
+      }
+    }
     /*----------------------------------------------------------------------------*
      *----------------------------------------------------------------------------*/
     void AssembleCenterlineDofForceStiffIntoElementForceStiff(DRT::Discretization const& discret,
@@ -754,7 +937,7 @@ namespace BEAMINTERACTION
     /*-----------------------------------------------------------------------------*
      *-----------------------------------------------------------------------------*/
     void ExtractPosDofVecAbsoluteValues(DRT::Discretization const& discret, DRT::Element const* ele,
-        Teuchos::RCP<Epetra_Vector> const& ia_discolnp,
+        Teuchos::RCP<const Epetra_Vector> const& ia_discolnp,
         std::vector<double>& element_posdofvec_absolutevalues)
     {
       std::vector<double> eledispvec;
@@ -771,6 +954,36 @@ namespace BEAMINTERACTION
         // initial values are added by element itself
         beam_element_ptr->ExtractCenterlineDofValuesFromElementStateVector(
             eledispvec, element_posdofvec_absolutevalues, true);
+      }
+      else
+      {
+        element_posdofvec_absolutevalues = eledispvec;
+        for (unsigned int dim = 0; dim < 3; ++dim)
+          for (int node = 0; node < ele->NumNode(); ++node)
+            element_posdofvec_absolutevalues[3 * node + dim] += ele->Nodes()[node]->X()[dim];
+      }
+    }
+
+    /*-----------------------------------------------------------------------------*
+     *-----------------------------------------------------------------------------*/
+    void ExtractPosDofVecValues(DRT::Discretization const& discret, DRT::Element const* ele,
+        Teuchos::RCP<const Epetra_Vector> const& ia_discolnp,
+        std::vector<double>& element_posdofvec_absolutevalues)
+    {
+      std::vector<double> eledispvec;
+
+      // extract the Dof values of this element from displacement vector
+      GetCurrentElementDis(discret, ele, ia_discolnp, eledispvec);
+
+      DRT::ELEMENTS::Beam3Base const* beam_element_ptr =
+          dynamic_cast<const DRT::ELEMENTS::Beam3Base*>(ele);
+
+      if (beam_element_ptr != NULL)
+      {
+        // get the current absolute values for those Dofs relevant for centerline interpolation
+        // initial values are added by element itself
+        beam_element_ptr->ExtractCenterlineDofValuesFromElementStateVector(
+            eledispvec, element_posdofvec_absolutevalues, false);
       }
       else
       {
