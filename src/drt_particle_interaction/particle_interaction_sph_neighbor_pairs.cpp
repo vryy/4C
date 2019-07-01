4f28423eba5cb0bd90cf6211e36678241bb58889
Sebastian Fuchs <fuchs@lnm.mw.tum.de>
2019-07-01 10:52:20 +0200

    Check for neighboring particle-wall pairs in SPH
    In the smoothed particle hydrodynamics (SPH) framework a method is added
    that checks for particle to wall neighbor pairs, meaning a contact point
    on the wall element is within the interaction distance of the particle.
    
    This method is very similar to the corresponding method in the discrete
    element method (DEM) framework being added with commit 39401f17 as part
    of #238.
    
    Note:
    - In some cases the closest contact point of a particle to a wall
      element is not located inside the element but on an edge or a node. If
      several wall elements share an edge or a node this contact point
      should only be detected once!
    - Contact points on an edge or a node inside the penetration volume of a
      surface contact point are not evaluated. The same accounts for contact
      points on a node inside the penetration volume of a line contact
      point.
    
    part of #285

--- src/drt_particle_interaction/particle_interaction_sph_neighbor_pairs.cpp
+++ src/drt_particle_interaction/particle_interaction_sph_neighbor_pairs.cpp
@@ -20,7 +20,11 @@
 #include "../drt_particle_engine/particle_engine_interface.H"
 #include "../drt_particle_engine/particle_container.H"
 
-#include "../drt_lib/drt_dserror.H"
+#include "../drt_particle_wall/particle_wall_interface.H"
+
+#include "../drt_geometry/searchtree_geometry_service.H"
+#include "../drt_geometry/position_array.H"
+#include "../drt_geometry/element_coordtrafo.H"
 
 #include <Teuchos_TimeMonitor.hpp>
 
@@ -176,6 +180,7 @@ void PARTICLEINTERACTION::SPHNeighborPairs::EvaluateParticlePairs()
     // absolute distance between particles
     const double absdist = UTILS::vec_norm2(r_ji);
 
+    // neighboring particles within interaction distance
     if (absdist < rad_i[0] or (absdist < rad_j[0] and status_j == PARTICLEENGINE::Owned))
     {
       // initialize particle pair
@@ -245,4 +250,196 @@ void PARTICLEINTERACTION::SPHNeighborPairs::EvaluateParticleWallPairs()
 
   // clear particle-wall pair data
   particlewallpairdata_.clear();
+
+  // relate particles to index of particle-wall pairs (considering object type of contact point)
+  std::unordered_map<int, std::vector<std::pair<GEO::ObjectType, int>>>
+      particletoindexofparticlewallpairs;
+
+  // index of particle-wall pairs
+  int particlewallpairindex = 0;
+
+  // iterate over potential wall neighbors
+  for (const auto& potentialneighbors : particlewallinterface_->GetPotentialWallNeighbors())
+  {
+    // access values of local index tuple of particle i
+    PARTICLEENGINE::TypeEnum type_i;
+    PARTICLEENGINE::StatusEnum status_i;
+    int particle_i;
+    std::tie(type_i, status_i, particle_i) = potentialneighbors.first;
+
+    // get corresponding particle container
+    PARTICLEENGINE::ParticleContainer* container_i =
+        particlecontainerbundle_->GetSpecificContainer(type_i, status_i);
+
+    // get global id of particle i
+    const int* globalid_i = container_i->GetPtrToParticleGlobalID(particle_i);
+
+    // declare pointer variables for particle i
+    const double* rad_i;
+
+    // get pointer to particle states
+    rad_i = container_i->GetPtrToParticleState(PARTICLEENGINE::Radius, particle_i);
+
+    // get position of particle i
+    const LINALG::Matrix<3, 1> pos_i(
+        container_i->GetPtrToParticleState(PARTICLEENGINE::Position, particle_i));
+
+    // get pointer to column wall element
+    DRT::Element* ele = potentialneighbors.second;
+
+    // determine nodal positions of column wall element
+    std::map<int, LINALG::Matrix<3, 1>> colelenodalpos;
+    particlewallinterface_->DetermineColWallEleNodalPos(ele, colelenodalpos);
+
+    // get coordinates of closest point on current column wall element to particle
+    LINALG::Matrix<3, 1> closestpos;
+    GEO::ObjectType objecttype =
+        GEO::nearest3DObjectOnElement(ele, colelenodalpos, pos_i, closestpos);
+
+    // vector from particle i to wall contact point j
+    double r_ji[3];
+    for (int i = 0; i < 3; i++) r_ji[i] = closestpos(i) - pos_i(i);
+
+    // absolute distance between particle and wall contact point
+    const double absdist = UTILS::vec_norm2(r_ji);
+
+    // neighboring particle and wall element within interaction distance
+    if (absdist < rad_i[0])
+    {
+      // initialize particle-wall pair
+      particlewallpairdata_.push_back(SPHParticleWallPair());
+
+      // get reference to current particle-wall pair
+      SPHParticleWallPair& particlewallpair = particlewallpairdata_[particlewallpairindex];
+
+      // store index of particle-wall pair
+      particletoindexofparticlewallpairs[globalid_i[0]].push_back(
+          std::make_pair(objecttype, particlewallpairindex));
+
+      // increase index
+      ++particlewallpairindex;
+
+      // set local index tuple of particle i
+      particlewallpair.tuple_i_ = potentialneighbors.first;
+
+      // set pointer to column wall element
+      particlewallpair.ele_ = potentialneighbors.second;
+
+      // set absolute distance between particle and wall contact point
+      particlewallpair.absdist_ = absdist;
+
+      // versor from wall contact point j to particle i
+      UTILS::vec_setscale(particlewallpair.e_ij_, -1.0 / absdist, r_ji);
+
+      // get coordinates of wall contact point in element parameter space
+      LINALG::Matrix<2, 1> elecoords(true);
+      const LINALG::SerialDenseMatrix xyze(GEO::getCurrentNodalPositions(ele, colelenodalpos));
+      GEO::CurrentToSurfaceElementCoordinates(ele->Shape(), xyze, closestpos, elecoords);
+
+      // set parameter space coordinates of wall contact point
+      particlewallpair.elecoords_[0] = elecoords(0, 0);
+      particlewallpair.elecoords_[1] = elecoords(1, 0);
+    }
+  }
+
+  // set of particle-wall pairs to remove
+  std::set<int> particlewallpairstoremove;
+
+  // iterate over particles with neighboring wall contact points
+  for (auto& particleIt : particletoindexofparticlewallpairs)
+  {
+    // get reference to index of particle-wall pairs for current particle
+    std::vector<std::pair<GEO::ObjectType, int>>& indexofparticlewallpairs = particleIt.second;
+
+    // only one particle-wall pair for current particle
+    if (indexofparticlewallpairs.size() == 1) continue;
+
+    // get local index tuple of current particle
+    PARTICLEENGINE::LocalIndexTuple tuple_i =
+        particlewallpairdata_[indexofparticlewallpairs[0].second].tuple_i_;
+
+    // access values of local index tuple of particle i
+    PARTICLEENGINE::TypeEnum type_i;
+    PARTICLEENGINE::StatusEnum status_i;
+    int particle_i;
+    std::tie(type_i, status_i, particle_i) = tuple_i;
+
+    // get corresponding particle container
+    PARTICLEENGINE::ParticleContainer* container_i =
+        particlecontainerbundle_->GetSpecificContainer(type_i, status_i);
+
+    // declare pointer variables for particle i
+    const double* rad_i;
+
+    // get pointer to particle states
+    rad_i = container_i->GetPtrToParticleState(PARTICLEENGINE::Radius, particle_i);
+
+    // define tolerance dependent on the particle radius
+    const double adaptedtol = 1.0e-7 * rad_i[0];
+
+    // iterate over particle-wall pairs (master)
+    for (std::pair<GEO::ObjectType, int>& master : indexofparticlewallpairs)
+    {
+      // get reference to particle-wall pair (master)
+      SPHParticleWallPair& masterpair = particlewallpairdata_[master.second];
+
+      // intersection radius of particle with column wall element in wall contact point
+      const double intersectionradius =
+          std::sqrt(UTILS::pow<2>(rad_i[0]) - UTILS::pow<2>(masterpair.absdist_));
+
+      // check with other particle-wall pairs (slave)
+      for (std::pair<GEO::ObjectType, int>& slave : indexofparticlewallpairs)
+      {
+        // no-self checking
+        if (master.second == slave.second) continue;
+
+        // get reference to particle-wall pair (slave)
+        SPHParticleWallPair& slavepair = particlewallpairdata_[slave.second];
+
+        // vector between detected wall contact points
+        double dist[3];
+        UTILS::vec_setscale(dist, masterpair.absdist_, masterpair.e_ij_);
+        UTILS::vec_addscale(dist, -slavepair.absdist_, slavepair.e_ij_);
+
+        // absolute distance between wall contact points
+        const double absdist = UTILS::vec_norm2(dist);
+
+        bool removeslavepair = false;
+
+        // check for coincident contact points of same type (e.g. on line between two surfaces)
+        if (master.first == slave.first)
+        {
+          // contact point already detected (e.g. on line between two surfaces)
+          if (absdist <= adaptedtol)
+          {
+            if (master.second < slave.second) removeslavepair = true;
+          }
+        }
+        // check for line/node contact points within penetration volume of a surface contact point
+        else if (master.first == GEO::SURFACE_OBJECT)
+        {
+          if (absdist <= intersectionradius) removeslavepair = true;
+        }
+        // check for node contact points within penetration volume of a line contact point
+        else if (master.first == GEO::LINE_OBJECT and slave.first == GEO::NODE_OBJECT)
+        {
+          if (absdist <= intersectionradius) removeslavepair = true;
+        }
+
+        // mark particle-wall pair (slave) to be removed
+        if (removeslavepair) particlewallpairstoremove.insert(slave.second);
+      }
+    }
+  }
+
+  // erase particle-wall pairs to be removed
+  {
+    int numparticlewallpairs = particlewallpairdata_.size();
+
+    std::set<int>::reverse_iterator rit;
+    for (rit = particlewallpairstoremove.rbegin(); rit != particlewallpairstoremove.rend(); ++rit)
+      particlewallpairdata_[*rit] = particlewallpairdata_[--numparticlewallpairs];
+
+    particlewallpairdata_.resize(numparticlewallpairs);
+  }
 }
