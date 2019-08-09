fdb367ed489322b59b9d3354feb68ea324ba1e1d
Barbara Wirthl <wirthl@lnm.mw.tum.de>
2019-08-09 10:48:34 +0200

    Restart: Set correct name in control-file
    Set correct name of restarted simulation in the control-file

--- src/drt_io/io_control.cpp
+++ src/drt_io/io_control.cpp
@@ -176,7 +176,7 @@ IO::OutputControl::OutputControl(const Epetra_Comm& comm, std::string problemtyp
 
     WriteHeader(name.str(), spatial_approx);
 
-    InsertRestartBackReference(restart, outputname);
+    InsertRestartBackReference(restart, restartname);
   }
 }
 
