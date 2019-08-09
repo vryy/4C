acaeadfc809861d9ba8ff1b81d23320c8a2bd694
Sebastian Fuchs <fuchs@lnm.mw.tum.de>
2019-08-09 10:01:50 +0200

    Set correct name hamaker constant in DEM adhesion
    follows #252, !279

--- src/drt_inpar/inpar_particle.cpp
+++ src/drt_inpar/inpar_particle.cpp
@@ -363,8 +363,7 @@ void INPAR::PARTICLE::SetValidParameters(Teuchos::RCP<Teuchos::ParameterList> li
   BoolParameter(
       "ADHESION_VDW_CURVE_SHIFT", "no", "shifts van-der-Waals-curve to g = 0", &particledyndem);
 
-  DoubleParameter("ADHESION_NORMAL_EPS", -1.0, "depth of Lennard-Jones adhesion potential well",
-      &particledyndem);
+  DoubleParameter("ADHESION_HAMAKER", -1.0, "hamaker constant of vdW interaction", &particledyndem);
 
   DoubleParameter("ADHESION_SURFACE_ENERGY", -1.0,
       "adhesion surface energy for the calculation of the pull-out force", &particledyndem);
