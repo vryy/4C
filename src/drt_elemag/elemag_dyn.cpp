280dcf39ca28a6d2a197bebc7b216454ec3a3e50
Luca Berardocco <berardocco@lnm.mw.tum.de>
2019-08-14 10:19:47 +0200

    Add some printing functionalities

--- src/drt_elemag/elemag_dyn.cpp
+++ src/drt_elemag/elemag_dyn.cpp
@@ -166,6 +166,8 @@ void electromagnetics_drt()
   // call time-integration scheme
   elemagalgo->Integrate();
 
+  elemagalgo->SpySysmat("matrix_file.mat");
+
   // Computing the error at the las time step (the conditional stateme nt is inside for now)
   if (DRT::INPUT::IntegralValue<bool>(elemagparams, "CALCERR"))
   {
