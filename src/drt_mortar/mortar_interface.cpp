5c4681616367eb016c0dc2180aa9722d29421d6d
Matthias Mayr <matthias.mayr@unibw.de>
2019-08-21 09:27:27 +0200

    Mortar: move setup of binning strategy to own func
    - encapsulate setup of binning strategy into SetupBinningStrategy()
    - add comments
    - use 'unsigned int' where possible
    
    Part of #295.

--- src/drt_mortar/mortar_interface.cpp
+++ src/drt_mortar/mortar_interface.cpp
@@ -1092,78 +1092,10 @@ void MORTAR::MortarInterface::InitializeDataContainer()
  |  Parallel Strategy based on bin distribution (public)     farah 11/13|
  *----------------------------------------------------------------------*/
 void MORTAR::MortarInterface::BinningStrategy(
-    Teuchos::RCP<Epetra_Map> initial_elecolmap, double vel)
+    Teuchos::RCP<Epetra_Map> initial_elecolmap, const double vel)
 {
-  // init XAABB
-  LINALG::Matrix<3, 2> XAABB(false);
-  for (int dim = 0; dim < 3; ++dim)
-  {
-    XAABB(dim, 0) = +1.0e12;
-    XAABB(dim, 1) = -1.0e12;
-  }
-
-  // loop all slave nodes and merge XAABB with their eXtendedAxisAlignedBoundingBox
-  for (int lid = 0; lid < SlaveColNodes()->NumMyElements(); ++lid)
-  {
-    int gid = SlaveColNodes()->GID(lid);
-    DRT::Node* node = Discret().gNode(gid);
-    if (!node) dserror("ERROR: Cannot find node with gid %i", gid);
-    MORTAR::MortarNode* mtrnode = dynamic_cast<MORTAR::MortarNode*>(node);
-
-    for (int dim = 0; dim < 3; ++dim)
-    {
-      XAABB(dim, 0) = std::min(XAABB(dim, 0), mtrnode->xspatial()[dim] - MORTARPROJLIM);
-      XAABB(dim, 1) = std::max(XAABB(dim, 1), mtrnode->xspatial()[dim] + MORTARPROJLIM);
-    }
-  }
-
-  // local bounding box
-  double locmin[3] = {XAABB(0, 0), XAABB(1, 0), XAABB(2, 0)};
-  double locmax[3] = {XAABB(0, 1), XAABB(1, 1), XAABB(2, 1)};
-
-  // global bounding box
-  double globmin[3];
-  double globmax[3];
-
-  // do the necessary communication
-  Comm().MinAll(&locmin[0], &globmin[0], 3);
-  Comm().MaxAll(&locmax[0], &globmax[0], 3);
-
-  // compute cutoff radius:
-  double totalsme = -1.0;
-  for (int lid = 0; lid < SlaveColElements()->NumMyElements(); ++lid)
-  {
-    int gid = SlaveColElements()->GID(lid);
-    DRT::Element* ele = Discret().gElement(gid);
-    if (!ele) dserror("ERROR: Cannot find element with gid %i", gid);
-    MORTAR::MortarElement* mtrele = dynamic_cast<MORTAR::MortarElement*>(ele);
-
-    // to be thought about, whether this is enough (safety = 2??)
-    double sme = mtrele->MaxEdgeSize();
-    totalsme = std::max(sme, totalsme);
-  }
-
-  double cutoff = -1.0;
-  Comm().MaxAll(&totalsme, &cutoff, 1);
-
-  // extend cutoff based on problem interface velocity
-  // --> only for contact problems
-  if (vel >= 1e-12)
-  {
-    double dt = InterfaceParams().get<double>("TIMESTEP");
-    cutoff = cutoff + 2 * dt * vel;
-  }
-
-  // increase XAABB by 2xcutoff radius
-  for (int dim = 0; dim < 3; ++dim)
-  {
-    XAABB(dim, 0) = globmin[dim] - cutoff;
-    XAABB(dim, 1) = globmax[dim] + cutoff;
-  }
-
-  /// binning strategy is created
-  Teuchos::RCP<BINSTRATEGY::BinningStrategy> binningstrategy =
-      Teuchos::rcp(new BINSTRATEGY::BinningStrategy(Comm(), cutoff, XAABB));
+  // Create the binning strategy
+  Teuchos::RCP<BINSTRATEGY::BinningStrategy> binningstrategy = SetupBinningStrategy(vel);
 
   // fill master and slave elements into bins
   std::map<int, std::set<int>> slavebinelemap;
@@ -1200,6 +1132,85 @@ void MORTAR::MortarInterface::BinningStrategy(
   FillComplete();
 
   return;
+}
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+Teuchos::RCP<BINSTRATEGY::BinningStrategy> MORTAR::MortarInterface::SetupBinningStrategy(
+    const double vel)
+{
+  // Initialize eXtendedAxisAlignedBoundingBox (XAABB)
+  LINALG::Matrix<3, 2> XAABB(false);
+  for (unsigned int dim = 0; dim < 3; ++dim)
+  {
+    XAABB(dim, 0) = +1.0e12;
+    XAABB(dim, 1) = -1.0e12;
+  }
+
+  // loop all slave nodes and merge XAABB with their eXtendedAxisAlignedBoundingBox
+  for (int lid = 0; lid < SlaveColNodes()->NumMyElements(); ++lid)
+  {
+    int gid = SlaveColNodes()->GID(lid);
+    DRT::Node* node = Discret().gNode(gid);
+    if (!node)
+      dserror(
+          "Cannot find node with gid %i in discretization '%s'.", gid, Discret().Name().c_str());
+    MORTAR::MortarNode* mtrnode = dynamic_cast<MORTAR::MortarNode*>(node);
+
+    for (unsigned int dim = 0; dim < 3; ++dim)
+    {
+      XAABB(dim, 0) = std::min(XAABB(dim, 0), mtrnode->xspatial()[dim] - MORTARPROJLIM);
+      XAABB(dim, 1) = std::max(XAABB(dim, 1), mtrnode->xspatial()[dim] + MORTARPROJLIM);
+    }
+  }
+
+  // local bounding box
+  double locmin[3] = {XAABB(0, 0), XAABB(1, 0), XAABB(2, 0)};
+  double locmax[3] = {XAABB(0, 1), XAABB(1, 1), XAABB(2, 1)};
+
+  // global bounding box
+  double globmin[3];
+  double globmax[3];
+
+  // do the necessary communication
+  Comm().MinAll(&locmin[0], &globmin[0], 3);
+  Comm().MaxAll(&locmax[0], &globmax[0], 3);
+
+  // compute cutoff radius:
+  double totalsme = -1.0;
+  for (int lid = 0; lid < SlaveColElements()->NumMyElements(); ++lid)
+  {
+    int gid = SlaveColElements()->GID(lid);
+    DRT::Element* ele = Discret().gElement(gid);
+    if (!ele)
+      dserror(
+          "Cannot find element with gid %i in discretization '%s'.", gid, Discret().Name().c_str());
+    MORTAR::MortarElement* mtrele = dynamic_cast<MORTAR::MortarElement*>(ele);
+
+    // to be thought about, whether this is enough (safety = 2??)
+    double sme = mtrele->MaxEdgeSize();
+    totalsme = std::max(sme, totalsme);
+  }
+
+  double cutoff = -1.0;
+  Comm().MaxAll(&totalsme, &cutoff, 1);
+
+  // extend cutoff based on problem interface velocity
+  // --> only for contact problems
+  if (vel >= EPS12)
+  {
+    const double dt = InterfaceParams().get<double>("TIMESTEP");
+    cutoff = cutoff + 2 * dt * vel;
+  }
+
+  // increase XAABB by 2x cutoff radius
+  for (unsigned int dim = 0; dim < 3; ++dim)
+  {
+    XAABB(dim, 0) = globmin[dim] - cutoff;
+    XAABB(dim, 1) = globmax[dim] + cutoff;
+  }
+
+  return Teuchos::rcp(new BINSTRATEGY::BinningStrategy(Comm(), cutoff, XAABB));
 }
 
 
