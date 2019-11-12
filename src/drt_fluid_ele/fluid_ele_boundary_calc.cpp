b4ea8c51cf87dd312d85709eaa5862d317f3cf9a
Nora Hagmeyer <nora.hagmeyer@unibw.de>
2019-11-12 16:32:45 +0100

    Include guards in fluid Neumann boundary calc
    Bugfix: In the calculation of the fluid Neumann
    boundary conditions, several values are
    calculated but never used unless the "new" one
    step theta time integration is used. This
    calculations can lead to errors thrown in the
    fucntin class, because a megative time is handed
    in for some parameter sets of e.g. the
    generelized alpha method. This sommit simple
    guards these fucntion calls by an if clause. #215

--- src/drt_fluid_ele/fluid_ele_boundary_calc.cpp
+++ src/drt_fluid_ele/fluid_ele_boundary_calc.cpp
@@ -465,9 +465,10 @@ int DRT::ELEMENTS::FluidBoundaryImpl<distype>::EvaluateNeumann(DRT::ELEMENTS::Fl
             // evaluate function at current gauss point
             functfac =
                 DRT::Problem::Instance()->Funct(functnum - 1).Evaluate(idim, coordgpref, time);
-            functfacn = DRT::Problem::Instance()
-                            ->Funct(functnum - 1)
-                            .Evaluate(idim, coordgpref, time - fldparatimint_->Dt());
+            if (fldparatimint_->IsNewOSTImplementation())
+              functfacn = DRT::Problem::Instance()
+                              ->Funct(functnum - 1)
+                              .Evaluate(idim, coordgpref, time - fldparatimint_->Dt());
           }
           else
           {
@@ -476,14 +477,16 @@ int DRT::ELEMENTS::FluidBoundaryImpl<distype>::EvaluateNeumann(DRT::ELEMENTS::Fl
           }
 
           const double valfac = (*val)[idim] * fac_time_dens * functfac;
-          const double valfacn = (*val)[idim] * fac_time_densn * functfacn;
           for (int inode = 0; inode < bdrynen_; ++inode)
           {
             elevec1_epetra[inode * numdofpernode_ + idim] += funct_(inode) * valfac;
             if (fldparatimint_->IsNewOSTImplementation())
             {
               if (fldparatimint_->IsOneStepTheta())
+              {
+                const double valfacn = (*val)[idim] * fac_time_densn * functfacn;
                 elevec1_epetra[inode * numdofpernode_ + idim] += funct_(inode) * valfacn;
+              }
             }
           }  // end IsNewOSTImplementation
         }    // if (*onoff)
@@ -503,9 +506,10 @@ int DRT::ELEMENTS::FluidBoundaryImpl<distype>::EvaluateNeumann(DRT::ELEMENTS::Fl
             // evaluate function at current gauss point
             functfac =
                 DRT::Problem::Instance()->Funct(functnum - 1).Evaluate(idim, coordgpref, time);
-            functfacn = DRT::Problem::Instance()
-                            ->Funct(functnum - 1)
-                            .Evaluate(idim, coordgpref, time - fldparatimint_->Dt());
+            if (fldparatimint_->IsNewOSTImplementation())
+              functfacn = DRT::Problem::Instance()
+                              ->Funct(functnum - 1)
+                              .Evaluate(idim, coordgpref, time - fldparatimint_->Dt());
           }
           else
           {
@@ -514,7 +518,6 @@ int DRT::ELEMENTS::FluidBoundaryImpl<distype>::EvaluateNeumann(DRT::ELEMENTS::Fl
           }
 
           const double valfac = (*val)[0] * fac_time_dens * functfac;
-          const double valfacn = (*val)[0] * fac_time_densn * functfacn;
           for (int inode = 0; inode < bdrynen_; ++inode)
           {
             elevec1_epetra[inode * numdofpernode_ + idim] +=
@@ -523,8 +526,11 @@ int DRT::ELEMENTS::FluidBoundaryImpl<distype>::EvaluateNeumann(DRT::ELEMENTS::Fl
             if (fldparatimint_->IsNewOSTImplementation())
             {
               if (fldparatimint_->IsOneStepTheta())
+              {
+                const double valfacn = (*val)[0] * fac_time_densn * functfacn;
                 elevec1_epetra[inode * numdofpernode_ + idim] +=
                     funct_(inode) * valfacn * (-unitnormal_(idim));
+              }
             }
           }  // end IsNewOSTImplementation
         }    // if (*onoff)
