ae73e370ff5936990e3d2ed1ca59eb3342a8d72b
Sebastian Fuchs <fuchs@lnm.mw.tum.de>
2020-06-25 13:17:51 +0200

    Skeleton for trilinos version information
    part of #321

---
+++ cmake/Modules/GetTrilinosVersion.cmake
@@ -0,0 +1,8 @@
+# get trilinos version
+function(get_trilinos_version)
+
+  set(TrilinosVersion "${Trilinos_VERSION}")
+  
+  set(TrilinosGitHash "Unable to determine Trilinos git hash!")
+
+endfunction()
