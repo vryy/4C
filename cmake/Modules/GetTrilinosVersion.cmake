fc57c842fd92d26f8e47dcbf8915298bef85a9be
Sebastian Fuchs <fuchs@lnm.mw.tum.de>
2020-06-29 14:46:23 +0200

    Extract Trilinos version and git sha
    part of #321

--- cmake/Modules/GetTrilinosVersion.cmake
+++ cmake/Modules/GetTrilinosVersion.cmake
@@ -1,8 +1,24 @@
 # get trilinos version
 function(get_trilinos_version)
 
-  set(TrilinosVersion "${Trilinos_VERSION}")
-  
-  set(TrilinosGitHash "Unable to determine Trilinos git hash!")
+  # get Trilinos version
+  set(TrilinosVersion "${Trilinos_VERSION}" PARENT_SCOPE)
+  #message(STATUS "found Trilinos Version: ${TrilinosVersion}")
+
+  # get Trilinos git hash
+  if(EXISTS "${Trilinos_DIR}/../../../TrilinosRepoVersion.txt")
+    file(STRINGS "${Trilinos_DIR}/../../../TrilinosRepoVersion.txt" TrilinosRepoVersionFile)
+    #message(STATUS "found Trilinos repo version file: ${TrilinosRepoVersionFile}")
+
+    list(GET TrilinosRepoVersionFile 1 TrilinosRepoVersionFileLine2)
+    separate_arguments(TrilinosRepoVersionFileLine2)
+    list(GET TrilinosRepoVersionFileLine2 0 TrilinosSHA)
+
+    set(TrilinosGitHash "${TrilinosSHA}" PARENT_SCOPE)
+    #message(STATUS "found Trilinos SHA: ${TrilinosSHA}")
+  else()
+    set(TrilinosGitHash "Unable to determine Trilinos git hash!" PARENT_SCOPE)
+    message(WARNING "Trilinos repo version file not found! Build will not contain Trilinos git revision information.")
+  endif()
 
 endfunction()
