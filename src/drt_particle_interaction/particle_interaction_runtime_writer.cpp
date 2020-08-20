fa8fcff9dec9ea26fe37322178b7d74730cad2a7
Sebastian Fuchs <fuchs@lnm.mw.tum.de>
2020-08-20 14:04:40 +0200

    Extend particle interaction writer for csv output
    The particle interaction runtime writer can in additioo hold multiple
    runtime csv writers, that are registered in each specific interaction
    routine.

--- src/drt_particle_interaction/particle_interaction_runtime_writer.cpp
+++ src/drt_particle_interaction/particle_interaction_runtime_writer.cpp
@@ -17,6 +17,7 @@
 #include "../drt_io/io.H"
 #include "../drt_io/io_control.H"
 #include "../drt_io/runtime_vtp_writer.H"
+#include "../drt_io/runtime_csv_writer.H"
 
 /*---------------------------------------------------------------------------*
  | definitions                                                               |
@@ -74,23 +75,39 @@ void PARTICLEINTERACTION::InteractionWriter::RegisterSpecificRuntimeVtpWriter(
   bool write_binary_output = (DRT::INPUT::IntegralValue<INPAR::PARTICLE::OutputDataFormat>(
                                   params_, "OUTPUT_DATA_FORMAT") == INPAR::PARTICLE::binary);
 
-  // construct the writer object
+  // construct and init the vtp writer object
   std::shared_ptr<RuntimeVtpWriter> runtime_vtpwriter = std::make_shared<RuntimeVtpWriter>();
 
-  // initialize the writer object
   runtime_vtpwriter->Initialize(comm_.MyPID(), comm_.NumProc(), max_number_timesteps_to_be_written,
       output_directory_path, DRT::Problem::Instance()->OutputControlFile()->FileNameOnlyPrefix(),
       fieldname, DRT::Problem::Instance()->OutputControlFile()->RestartName(), setuptime_,
       write_binary_output);
 
-  // set the writer object
+  // set the vtp writer object
   runtime_vtpwriters_[fieldname] = runtime_vtpwriter;
+}
+
+void PARTICLEINTERACTION::InteractionWriter::RegisterSpecificRuntimeCsvWriter(
+    const std::string& fieldname)
+{
+  // safety check
+  if (runtime_csvwriters_.count(fieldname))
+    dserror("a runtime csv writer for field '%s' is already stored!", fieldname.c_str());
+
+  // construct and init the csv writer object
+  std::shared_ptr<RuntimeCsvWriter> runtime_csvwriter =
+      std::make_shared<RuntimeCsvWriter>(comm_.MyPID());
+
+  runtime_csvwriter->Init(fieldname);
+
+  // set the csv writer object
+  runtime_csvwriters_[fieldname] = runtime_csvwriter;
 }
 
 void PARTICLEINTERACTION::InteractionWriter::WriteParticleInteractionRuntimeOutput(
     const int step, const double time) const
 {
-  // iterate over writer objects
+  // iterate over vtp writer objects
   for (auto& writerIt : runtime_vtpwriters_)
   {
     const std::string& fieldname = writerIt.first;
@@ -99,11 +116,25 @@ void PARTICLEINTERACTION::InteractionWriter::WriteParticleInteractionRuntimeOutp
     // reset time and time step of the writer object
     runtime_vtpwriter->SetupForNewTimeStepAndGeometry(time, step, fieldname);
 
-    // position and states preset in particle interaction evaluation
+    // data to be written preset in particle interaction evaluation
 
     // finalize everything and write all required files to filesystem
     runtime_vtpwriter->WriteFiles();
     runtime_vtpwriter->WriteCollectionFileOfAllWrittenFiles(
         DRT::Problem::Instance()->OutputControlFile()->FileNameOnlyPrefix() + "-" + fieldname);
+  }
+
+  // iterate over csv writer objects
+  for (auto& writerIt : runtime_csvwriters_)
+  {
+    std::shared_ptr<RuntimeCsvWriter> runtime_csvwriter = writerIt.second;
+
+    // reset time and time step of the writer object
+    runtime_csvwriter->ResetTimeAndTimeStep(time, step);
+
+    // data to be written preset in particle interaction evaluation
+
+    // write file to filesystem
+    runtime_csvwriter->WriteFile();
   }
 }
