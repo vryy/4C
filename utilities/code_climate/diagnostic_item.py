a306ed526b2c35871e421b513f376559bd2e32e1
Amadeus Gebauer <gebauer@lnm.mw.tum.de>
2020-09-10 14:23:20 +0200

    Implement first setup of codeclimate checks
    This commit adds a new job to the codecheck stage on GitLab that
    evaluates clang-tidy to determine all issues found in the codebase.
    The codeclimate report is then exported to GitLab so that it will be
    visualized in the Pipeline and durng a MR.
    
    Issue #541

---
+++ utilities/code_climate/diagnostic_item.py
@@ -0,0 +1,69 @@
+import os
+import hashlib
+import json
+
+
+class DiagnosticItem:
+    def __init__(self, src):
+        self.name = None
+        self.message = None
+        self.file = None
+        self.offset = None
+        self.srcdir = src
+
+        self.line_number = None
+        self.previous_line = None
+        self.line = None
+        self.line_offset = None
+
+    def hash(self):
+        hash_source = "{0}{1}{2}{3}".format(
+            self.name, self.message, self.get_relative_path(), self.offset
+        )
+        return str(hashlib.md5(hash_source.encode()).hexdigest())
+
+    def global_hash(self, hash_set):
+        for i in range(100):
+
+            hash_source = "{0}{1}{2}{3}{4}{5}".format(
+                self.name,
+                self.message,
+                self.get_relative_path(),
+                self.line,
+                self.previous_line,
+                i,
+            )
+            hash_str = str(hashlib.md5(hash_source.encode()).hexdigest())
+            if hash_str not in hash_set:
+                hash_set.add(hash_str)
+                return hash_str
+
+        return RuntimeError("Should never hend here")
+
+    def get_relative_path(self):
+        return os.path.relpath(self.file, self.srcdir)
+
+    def is_of_interest(self):
+        return self.get_relative_path().startswith("src/") and self.file != ""
+
+    def write(self, fout, hash_set):
+        item = {
+            "description": "{0} ({1})".format(self.message, self.name),
+            "fingerprint": self.global_hash(hash_set),
+            "location": {
+                "path": self.get_relative_path(),
+                "lines": {"begin": self.line_number},
+            },
+        }
+        fout.write(json.dumps(item))
+
+    @staticmethod
+    def parse(object, src):
+        diagnosticItem = DiagnosticItem(src)
+        diagnosticItem.name = object["DiagnosticName"]
+        diagnosticItem.message = object["DiagnosticMessage"]["Message"]
+
+        diagnosticItem.file = object["DiagnosticMessage"]["FilePath"]
+        diagnosticItem.offset = object["DiagnosticMessage"]["FileOffset"]
+
+        return diagnosticItem
