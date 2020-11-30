2aa8bb5c2603f60747f4acadb5f23301cdaa9d61
Sebastian Fuchs <fuchs@lnm.mw.tum.de>
2020-11-30 09:07:07 +0100

    Print warning on violation of critical time step
    Instead of throwing a dserror print a warning to the output in case the
    critical time step size is violated.
    
    In some applications or for initialization purposes, a larger time step
    than the (theoretical) critical time step can be meaningful and allows
    for faster evaluation.

--- src/drt_particle_interaction/particle_interaction_dem_contact.cpp
+++ src/drt_particle_interaction/particle_interaction_dem_contact.cpp
@@ -35,6 +35,7 @@
 #include "../drt_lib/drt_dserror.H"
 
 #include "../drt_io/runtime_vtp_writer.H"
+#include "../drt_io/io_pstream.H"
 
 #include <Teuchos_TimeMonitor.hpp>
 
@@ -158,7 +159,7 @@ double PARTICLEINTERACTION::DEMContact::GetNormalContactStiffness() const
 void PARTICLEINTERACTION::DEMContact::CheckCriticalTimeStep() const
 {
   // init value of minimum mass
-  double minmass = 0.0;
+  double minmass = std::numeric_limits<double>::max();
 
   // iterate over particle types
   for (const auto& type_i : particlecontainerbundle_->GetParticleTypes())
@@ -167,14 +168,18 @@ void PARTICLEINTERACTION::DEMContact::CheckCriticalTimeStep() const
     PARTICLEENGINE::ParticleContainer* container =
         particlecontainerbundle_->GetSpecificContainer(type_i, PARTICLEENGINE::Owned);
 
+    // get number of particles stored in container
+    const int particlestored = container->ParticlesStored();
+
+    // no owned particles of current particle type
+    if (particlestored <= 0) continue;
+
     // get minimum stored value of state
     double currminmass = container->GetMinValueOfState(PARTICLEENGINE::Mass);
 
-    if ((not(minmass > 0.0)) or (currminmass < minmass)) minmass = currminmass;
+    // update value of minimum mass
+    minmass = std::min(minmass, currminmass);
   }
-
-  // currently no particles in simulation domain
-  if (minmass == 0.0) return;
 
   // get critical normal contact stiffness
   const double k_normal_crit = contactnormal_->GetCriticalNormalContactStiffness();
@@ -185,7 +190,9 @@ void PARTICLEINTERACTION::DEMContact::CheckCriticalTimeStep() const
   const double dt_crit = safety * factor * std::sqrt(minmass / k_normal_crit);
 
   // checks time step
-  if (dt_ > dt_crit) dserror("time step %f larger than critical time step %f!", dt_, dt_crit);
+  if (dt_ > dt_crit)
+    IO::cout << "Warning: time step " << dt_ << " larger than critical time step " << dt_crit << "!"
+             << IO::endl;
 }
 
 void PARTICLEINTERACTION::DEMContact::AddForceAndMomentContribution()
