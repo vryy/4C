bf6a399eef0b4c8d41ddfdcbca34e3cab39f5875
nora <nora.hagmeyer@unibw.de>
2020-12-03 14:23:41 +0100

    Fix fbi output
    The coupling contributions were additionally
    scaled with the penalty parameter in the output
    method. This commit fixes this issue.

--- src/drt_fbi/ad_fbi_constraintbridge_penalty.cpp
+++ src/drt_fbi/ad_fbi_constraintbridge_penalty.cpp
@@ -55,6 +55,8 @@ void ADAPTER::FBIConstraintBridgePenalty::ResetBridge()
   fs_->PutScalar(0.0);
   Cff_->Reset();
   ff_->PutScalar(0.0);
+  fluid_scaled_ = false;
+  structure_scaled_ = false;
 }
 /*----------------------------------------------------------------------*/
 /*----------------------------------------------------------------------*/
@@ -73,15 +75,23 @@ void ADAPTER::FBIConstraintBridgePenalty::UnsetWeakDirichletFlag()
 /*----------------------------------------------------------------------*/
 void ADAPTER::FBIConstraintBridgePenalty::ScalePenaltyStructureContributions()
 {
-  if (fs_->Scale(GetParams()->GetPenaltyParameter()))
-    dserror("Scaling of the penalty force was unsuccessful!\n");
+  if (!structure_scaled_)
+  {
+    if (fs_->Scale(GetParams()->GetPenaltyParameter()))
+      dserror("Scaling of the penalty force was unsuccessful!\n");
+    structure_scaled_ = true;
+  }
 }
 
 /*----------------------------------------------------------------------*/
 /*----------------------------------------------------------------------*/
 void ADAPTER::FBIConstraintBridgePenalty::ScalePenaltyFluidContributions()
 {
-  if (Cff_->Scale(GetParams()->GetPenaltyParameter()) ||
-      ff_->Scale(GetParams()->GetPenaltyParameter()))
-    dserror("Scaling of the penalty force was unsuccessful!\n");
+  if (!fluid_scaled_)
+  {
+    if (Cff_->Scale(GetParams()->GetPenaltyParameter()) ||
+        ff_->Scale(GetParams()->GetPenaltyParameter()))
+      dserror("Scaling of the penalty force was unsuccessful!\n");
+    fluid_scaled_ = true;
+  }
 }
