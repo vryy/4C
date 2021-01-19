f689ca47ac1217b8609c7ebf0b819a08567d18f3
Matthias Mayr <matthias.mayr@unibw.de>
2021-01-19 14:40:36 +0100

    Str: add missing initializations of class members
    Fixes failure of convergence test of Lagrange multiplier increment in
    parallel in a debug build.
    
    Closes #590.

--- src/drt_structure/strtimint_impl.cpp
+++ src/drt_structure/strtimint_impl.cpp
@@ -106,6 +106,10 @@ STR::TimIntImpl::TimIntImpl(const Teuchos::ParameterList& timeparams,
       normpres_(0.0),
       normcontconstr_(0.0),  // < norm of contact constraints (saddlepoint formulation)
       normlagr_(0.0),        // < norm of lagrange multiplier increment (saddlepoint formulation)
+      normw_(0.0),
+      normwrhs_(0.0),
+      normwm_(0.0),
+      normwmrhs_(0.0),
       alpha_ls_(sdynparams.get<double>("ALPHA_LS")),
       sigma_ls_(sdynparams.get<double>("SIGMA_LS")),
       ls_maxiter_(sdynparams.get<int>("LSMAXITER")),
