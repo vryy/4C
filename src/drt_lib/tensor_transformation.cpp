3b742dad61e7a6b498a058c9f0ac90b109d9031d
Amadeus Gebauer <amadeus.gebauer@tum.de>
2022-02-04 14:54:50 +0100

    Implement utils for tensor transformation

---
+++ src/drt_lib/tensor_transformation.cpp
@@ -0,0 +1,11 @@
+/*! \file
+\level 1
+\brief Util functions for tensor transformations
+*/
+
+#include "tensor_transformation.H"
+
+template void UTILS::TENSOR::TensorRotation<3>(
+    const LINALG::Matrix<3, 3>&, const LINALG::Matrix<3, 3>&, LINALG::Matrix<3, 3>&);
+template void UTILS::TENSOR::InverseTensorRotation<3>(
+    const LINALG::Matrix<3, 3>&, const LINALG::Matrix<3, 3>&, LINALG::Matrix<3, 3>&);
