17ea5eaa816d1365ff68a14216149cb1e24eea77
Sebastian Proell <sebastian.proell@tum.de>
2022-06-06 12:01:08 +0200

    Clarify usage of BACI_EXPECT_NEAR and minor beauty

--- unittests/common/assertions.h
+++ unittests/common/assertions.h
@@ -13,18 +13,40 @@
 
 namespace TESTING::INTERNAL
 {
-
-  //! Determine a number of decimal digits for printing to a given tolerance.
-  template <typename T>
-  inline int PrecisionForPrinting(T tolerance)
+  namespace
   {
-    dsassert(tolerance > 0, "Tolerance must be positive.");
-    return tolerance > 1 ? 0 : static_cast<int>(std::ceil(-1.0 * std::log10(tolerance) + 1));
-  }
+    //! Determine a number of decimal digits for printing to a given tolerance.
+    template <typename T>
+    inline int PrecisionForPrinting(T tolerance)
+    {
+      dsassert(tolerance > 0, "Tolerance must be positive.");
+      return tolerance > 1 ? 0 : static_cast<int>(std::ceil(-1.0 * std::log10(tolerance) + 1));
+    }
+
+    //! If the @p nonMatchingEntries string is empty return success, otherwise return failure with a
+    //! descriptive message.
+    template <typename T>
+    inline ::testing::AssertionResult ResultBasedOnNonMatchingEntries(
+        const std::string& nonMatchingEntries, T tolerance, const char* expr1, const char* expr2)
+    {
+      if (nonMatchingEntries.empty())
+        return ::testing::AssertionSuccess();
+      else
+      {
+        return ::testing::AssertionFailure()
+               << "The following entries differ: absolute difference is not within tolerance "
+               << tolerance << "\n"
+               << "index: " << expr1 << " vs. " << expr2 << ":" << std::endl
+               << nonMatchingEntries;
+      }
+    }
+  }  // namespace
 
   /**
-   * Compare two std::vector<T> objects for double equality up to a tolerance. The  signature is
+   * Compare two std::vector<T> objects for double equality up to a tolerance. The signature is
    * mandated by GoogleTest's EXPECT_PRED_FORMAT3 macro.
+   *
+   * @note This function is not intended to be used directly. Use BACI_EXPECT_NEAR.
    */
   template <typename T>
   inline ::testing::AssertionResult AssertNear(const char* vec1Expr,
@@ -35,7 +57,6 @@ namespace TESTING::INTERNAL
     // argument is required for the EXPECT_PRED_FORMAT3 macro of GoogleTest for pretty printing
     (void)toleranceExpr;
 
-
     if (vec1.size() != vec2.size())
     {
       return ::testing::AssertionFailure()
@@ -43,33 +64,29 @@ namespace TESTING::INTERNAL
              << " has dimension " << vec2.size() << std::endl;
     }
 
-    bool allEntriesMatch = true;
-    std::stringstream nonMatchingEntries;
-    nonMatchingEntries << std::fixed << std::setprecision(PrecisionForPrinting(tolerance));
-    for (std::size_t i = 0; i < vec1.size(); ++i)
-    {
-      if (std::fabs(vec1[i] - vec2[i]) > tolerance)
-      {
-        allEntriesMatch = false;
-        nonMatchingEntries << "[" << i << "]: " << vec1[i] << " vs. " << vec2[i] << std::endl;
-      }
-    }
+    const std::string nonMatchingEntries = std::invoke(
+        [&]()
+        {
+          std::stringstream ss;
+          ss << std::fixed << std::setprecision(PrecisionForPrinting(tolerance));
+          for (std::size_t i = 0; i < vec1.size(); ++i)
+          {
+            if (std::fabs(vec1[i] - vec2[i]) > tolerance)
+            {
+              ss << "[" << i << "]: " << vec1[i] << " vs. " << vec2[i] << std::endl;
+            }
+          }
+          return ss.str();
+        });
 
-    if (allEntriesMatch)
-      return ::testing::AssertionSuccess();
-    else
-    {
-      return ::testing::AssertionFailure()
-             << "The following entries differ: absolute difference is not within tolerance "
-             << tolerance << "\n"
-             << "[i]: " << vec1Expr << " vs. " << vec2Expr << ":" << std::endl
-             << nonMatchingEntries.str();
-    }
+    return ResultBasedOnNonMatchingEntries(nonMatchingEntries, tolerance, vec1Expr, vec2Expr);
   }
 
   /**
    * Compare two LINALG::Matrix objects for double equality up to a tolerance. The signature is
    * mandated by GoogleTest's EXPECT_PRED_FORMAT3 macro.
+   *
+   * @note This function is not intended to be used directly. Use BACI_EXPECT_NEAR.
    */
   template <unsigned int M, unsigned int N, typename T>
   inline ::testing::AssertionResult AssertNear(const char* mat1Expr,
@@ -80,37 +97,33 @@ namespace TESTING::INTERNAL
     // argument is required for the EXPECT_PRED_FORMAT3 macro of GoogleTest for pretty printing
     (void)toleranceExpr;
 
-    bool allEntriesMatch = true;
-    std::stringstream nonMatchingEntries;
-    nonMatchingEntries << std::fixed << std::setprecision(PrecisionForPrinting(tolerance));
-    for (unsigned i = 0; i < M; ++i)
-    {
-      for (unsigned j = 0; j < N; ++j)
-      {
-        if (std::fabs(mat1(i, j) - mat2(i, j)) > tolerance)
+    const std::string nonMatchingEntries = std::invoke(
+        [&]()
         {
-          allEntriesMatch = false;
-          nonMatchingEntries << "(" << i << "," << j << "): " << mat1(i, j) << " vs. " << mat2(i, j)
-                             << std::endl;
-        }
-      }
-    }
+          std::stringstream ss;
+          ss << std::fixed << std::setprecision(PrecisionForPrinting(tolerance));
+          for (unsigned i = 0; i < M; ++i)
+          {
+            for (unsigned j = 0; j < N; ++j)
+            {
+              if (std::fabs(mat1(i, j) - mat2(i, j)) > tolerance)
+              {
+                ss << "(" << i << "," << j << "): " << mat1(i, j) << " vs. " << mat2(i, j)
+                   << std::endl;
+              }
+            }
+          }
+          return ss.str();
+        });
 
-    if (allEntriesMatch)
-      return ::testing::AssertionSuccess();
-    else
-    {
-      return ::testing::AssertionFailure()
-             << "The following entries differ: absolute difference is not within tolerance "
-             << tolerance << "\n"
-             << "(i,j): " << mat1Expr << " vs. " << mat2Expr << ":" << std::endl
-             << nonMatchingEntries.str();
-    }
+    return ResultBasedOnNonMatchingEntries(nonMatchingEntries, tolerance, mat1Expr, mat2Expr);
   }
 
   /**
    * Compare two LINALG::SerialDenseMatrix objects for double equality up to a tolerance. The
    * signature is mandated by GoogleTest's EXPECT_PRED_FORMAT3 macro.
+   *
+   * @note This function is not intended to be used directly. Use BACI_EXPECT_NEAR.
    */
   inline ::testing::AssertionResult AssertNear(const char* mat1Expr,
       const char* mat2Expr,  // NOLINT
@@ -129,47 +142,41 @@ namespace TESTING::INTERNAL
              << mat2.ColDim() << std::endl;
     }
 
-    bool allEntriesMatch = true;
-
-    std::stringstream nonMatchingEntries;
-    nonMatchingEntries << std::fixed << std::setprecision(PrecisionForPrinting(tolerance));
-    for (int i = 0; i < mat1.RowDim(); ++i)
-    {
-      for (int j = 0; j < mat1.ColDim(); ++j)
-      {
-        if (std::fabs(mat1(i, j) - mat2(i, j)) > tolerance)
+    const std::string nonMatchingEntries = std::invoke(
+        [&]()
         {
-          allEntriesMatch = false;
-          nonMatchingEntries << "(" << i << "," << j << "): " << mat1(i, j) << " vs. " << mat2(i, j)
-                             << std::endl;
-        }
-      }
-    }
+          std::stringstream ss;
+          ss << std::fixed << std::setprecision(PrecisionForPrinting(tolerance));
+          for (int i = 0; i < mat1.RowDim(); ++i)
+          {
+            for (int j = 0; j < mat1.ColDim(); ++j)
+            {
+              if (std::fabs(mat1(i, j) - mat2(i, j)) > tolerance)
+              {
+                ss << "(" << i << "," << j << "): " << mat1(i, j) << " vs. " << mat2(i, j)
+                   << std::endl;
+              }
+            }
+          }
+          return ss.str();
+        });
 
-    if (allEntriesMatch)
-      return ::testing::AssertionSuccess();
-    else
-    {
-      return ::testing::AssertionFailure()
-             << "The following entries differ: absolute difference is not within tolerance "
-             << tolerance << "\n"
-             << "(i,j): " << mat1Expr << " vs. " << mat2Expr << ":" << std::endl
-             << nonMatchingEntries.str();
-    }
+    return ResultBasedOnNonMatchingEntries(nonMatchingEntries, tolerance, mat1Expr, mat2Expr);
   }
 }  // namespace TESTING::INTERNAL
 
 /**
- * Custom assertion to test for equality up to a tolerance
+ * @brief Custom assertion to test for equality up to a tolerance.
  *
- * Overloads are provided for
+ * This macro tests two containers @p actual and @p expected for entry-wise equality up to an
+ * absolute @p tolerance. Overloads are provided for
  * - std::vector<T>
  * - LINALG::Matrix
  * - LINALG::SerialDenseMatrix
  *
- * @note This and similar macros are defined to avoid writing asserts in the unexpressive
- * EXPECT_PRED_FORMATn syntax by gtest. They are all prefixed with `BACI_` to easily distinguish
- * them from gtest asserts.
+ * @note Implementation details: this and similar macros are defined to avoid writing asserts in the
+ * unexpressive EXPECT_PRED_FORMATn syntax by gtest. They are all prefixed with `BACI_` to easily
+ * distinguish them from gtest asserts.
  */
 #define BACI_EXPECT_NEAR(actual, expected, tolerance) \
   EXPECT_PRED_FORMAT3(TESTING::INTERNAL::AssertNear, actual, expected, tolerance)
