a3da1cc57fe0650f159063785b67a6504204fcd8
Max Firmbach <max.firmbach@unibw.de>
2022-10-17 10:03:46 +0200

    Fix spelling

--- src/drt_geometric_search/geometric_search_params.cpp
+++ src/drt_geometric_search/geometric_search_params.cpp
@@ -23,11 +23,11 @@ GEOMETRICSEARCH::GeometricSearchParams::GeometricSearchParams()
 
   beam_radius_extension_factor_ = params_list.get<double>("BEAM_RADIUS_EXTENSION_FACTOR");
   dsassert(!std::signbit(beam_radius_extension_factor_),
-      "Beam radius extension factor needs to be positiv!");
+      "Beam radius extension factor needs to be positive!");
 
   sphere_radius_extension_factor_ = params_list.get<double>("SPHERE_RADIUS_EXTENSION_FACTOR");
   dsassert(!std::signbit(sphere_radius_extension_factor_),
-      "Sphere radius extension factor needs to be positiv!");
+      "Sphere radius extension factor needs to be positive!");
 
   Teuchos::ParameterList const& params_list_io = DRT::Problem::Instance()->IOParams();
   verbosity_ = DRT::INPUT::IntegralValue<IO::verbositylevel>(params_list_io, "VERBOSITY");
