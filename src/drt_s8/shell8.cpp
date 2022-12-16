ca0c6d27efa37e63ef0dd15a1b9fadadc3ea3855
Sebastian Proell <sebastian.proell@tum.de>
2022-12-16 17:13:07 +0100

    Linalg: Move Shell8 nullspace to shell8

--- src/drt_s8/shell8.cpp
+++ src/drt_s8/shell8.cpp
@@ -15,6 +15,98 @@
 
 #include "linalg_utils_nullspace.H"
 
+
+namespace
+{
+
+  /*!
+    \brief Helper function for the nodal nullspace of shell elements in 3D
+
+  \param node (in):    node to calculate the nullspace on
+    \param x0 (in):      center of discretization
+                     */
+  Teuchos::SerialDenseMatrix<int, double> ComputeShell3DNullSpace(DRT::Node& node, const double* x0)
+  {
+    /* the rigid body modes for structures are:
+
+    xtrans   ytrans  ztrans   xrot       yrot       zrot
+    mode[0]  mode[1] mode[2]  mode[3]    mode[4]    mode[5]
+    -----------------------------------------------------------
+     x   |    1       0       0       0          z-z0      -y+y0
+     y   |    0       1       0      -z+z0       0          x-x0
+     z   |    0       0       1       y-y0      -x+x0       0
+     dx  |    0       0       0       0          a3        -a2
+     dy  |    0       0       0      -a3         0          a1
+     dz  |    0       0       0       a2        -a1         0
+                       element types: shell8
+    */
+
+    LINALG::Matrix<1, 3> dir;
+
+    DRT::ELEMENTS::Shell8* s8 = dynamic_cast<DRT::ELEMENTS::Shell8*>(node.Elements()[0]);
+    if (!s8) dserror("Cannot cast to Shell8");
+    int j;
+    for (j = 0; j < s8->NumNode(); ++j)
+      if (s8->Nodes()[j]->Id() == node.Id()) break;
+    if (j == s8->NumNode()) dserror("Can't find matching node - weird!");
+    double h2 = (*s8->GetThickness())[j] / 2.0;
+
+    // get director
+    const Epetra_SerialDenseMatrix* a3ref = s8->GetDirectors();
+    dir(0, 0) = (*a3ref)(0, j) * h2;
+    dir(0, 1) = (*a3ref)(1, j) * h2;
+    dir(0, 2) = (*a3ref)(2, j) * h2;
+
+    const double* x = node.X();
+
+    Teuchos::SerialDenseMatrix<int, double> nullspace(6, 6);
+    // x-modes
+    nullspace(0, 0) = 1.0;
+    nullspace(0, 1) = 0.0;
+    nullspace(0, 2) = 0.0;
+    nullspace(0, 3) = 0.0;
+    nullspace(0, 4) = x[2] - x0[2];
+    nullspace(0, 5) = -x[1] + x0[1];
+    // y-modes
+    nullspace(1, 0) = 0.0;
+    nullspace(1, 1) = 1.0;
+    nullspace(1, 2) = 0.0;
+    nullspace(1, 3) = -x[2] + x0[2];
+    nullspace(1, 4) = 0.0;
+    nullspace(1, 5) = x[0] - x0[0];
+    // z-modes
+    nullspace(2, 0) = 0.0;
+    nullspace(2, 1) = 0.0;
+    nullspace(2, 2) = 1.0;
+    nullspace(2, 3) = x[1] - x0[1];
+    nullspace(2, 4) = -x[0] + x0[0];
+    nullspace(2, 5) = 0.0;
+    // dx-modes
+    nullspace(3, 0) = 0.0;
+    nullspace(3, 1) = 0.0;
+    nullspace(3, 2) = 0.0;
+    nullspace(3, 3) = 0.0;
+    nullspace(3, 4) = dir(0, 2);
+    nullspace(3, 5) = -dir(0, 1);
+    // dy-modes
+    nullspace(4, 0) = 0.0;
+    nullspace(4, 1) = 0.0;
+    nullspace(4, 2) = 0.0;
+    nullspace(4, 3) = -dir(0, 2);
+    nullspace(4, 4) = 0.0;
+    nullspace(4, 5) = dir(0, 0);
+    // dz-modes
+    nullspace(5, 0) = 0.0;
+    nullspace(5, 1) = 0.0;
+    nullspace(5, 2) = 0.0;
+    nullspace(5, 3) = dir(0, 1);
+    nullspace(5, 4) = -dir(0, 0);
+    nullspace(5, 5) = 0.0;
+
+    return nullspace;
+  }
+}  // namespace
+
 DRT::ELEMENTS::Shell8Type DRT::ELEMENTS::Shell8Type::instance_;
 
 
@@ -59,8 +151,7 @@ void DRT::ELEMENTS::Shell8Type::NodalBlockInformation(
 Teuchos::SerialDenseMatrix<int, double> DRT::ELEMENTS::Shell8Type::ComputeNullSpace(
     DRT::Node& node, const double* x0, const int numdof, const int dimnsp)
 {
-  return LINALG::ComputeShell3DNullSpace(node, x0);
-  ;
+  return ComputeShell3DNullSpace(node, x0);
 }
 
 void DRT::ELEMENTS::Shell8Type::SetupElementDefinition(
