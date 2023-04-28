0574f47a1fcb60bae80cc00576c029dceebd58bc
Max Firmbach <max.firmbach@unibw.de>
2023-04-28 12:33:19 +0200

    Move headers and update test

--- src/beaminteraction/beaminteraction_beam_to_solid_surface_meshtying_pair_mortar_FAD.cpp
+++ src/beaminteraction/beaminteraction_beam_to_solid_surface_meshtying_pair_mortar_FAD.cpp
@@ -7,11 +7,11 @@ evaluated with FAD.
 \level 3
 */
 
+#include "beaminteraction_beam_to_solid_surface_meshtying_pair_mortar_FAD.H"
+
 #include "beam3_reissner.H"
 #include "beam3_kirchhoff.H"
 #include "beam3_euler_bernoulli.H"
-
-#include "beaminteraction_beam_to_solid_surface_meshtying_pair_mortar_FAD.H"
 #include "beaminteraction_contact_params.H"
 #include "beaminteraction_beam_to_solid_surface_meshtying_params.H"
 #include "beaminteraction_calc_utils.H"
