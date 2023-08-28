cfdf9cdaf1bfae38f6e74158b7890fe48ddf7132
Amadeus Gebauer <amadeus.gebauer@tum.de>
2023-08-28 16:29:52 +0200

    Use IterateJacobianMappingAtGaussPoints in so-poro

--- src/solid_ele/baci_solid_ele_poro_calc_pressure_based.cpp
+++ src/solid_ele/baci_solid_ele_poro_calc_pressure_based.cpp
@@ -59,99 +59,94 @@ void DRT::ELEMENTS::SolidPoroPressureBasedEleCalc<distype>::EvaluateNonlinearFor
       EvaluateNodalCoordinates<distype>(ele, discretization, la[0].lm_);
 
   // Loop over all Gauss points
-  for (int gp = 0; gp < gauss_integration_.NumPoints(); ++gp)
-  {
-    const CORE::LINALG::Matrix<num_dim_, 1> xi =
-        EvaluateParameterCoordinate<distype>(gauss_integration_, gp);
+  IterateJacobianMappingAtGaussPoints(nodal_coordinates, gauss_integration_,
+      [&](const CORE::LINALG::Matrix<DETAIL::num_dim<distype>, 1>& xi,
+          const ShapeFunctionsAndDerivatives<distype>& shape_functions,
+          const JacobianMapping<distype>& jacobian_mapping, double integration_factor, int gp
 
-    const ShapeFunctionsAndDerivatives<distype> shape_functions =
-        EvaluateShapeFunctionsAndDerivs<distype>(xi);
+      )
+      {
+        const SpatialMaterialMapping<distype> spatial_material_mapping =
+            EvaluateSpatialMaterialMapping<distype>(
+                jacobian_mapping, nodal_coordinates, 1.0, kinematictype);
 
-    const JacobianMapping<distype> jacobian_mapping =
-        EvaluateJacobianMapping(shape_functions, nodal_coordinates);
+        const CauchyGreen<distype> cauchygreen = EvaluateCauchyGreen(spatial_material_mapping);
 
-    const SpatialMaterialMapping<distype> spatial_material_mapping =
-        EvaluateSpatialMaterialMapping<distype>(
-            jacobian_mapping, nodal_coordinates, 1.0, kinematictype);
+        CORE::LINALG::Matrix<num_str_, num_dof_per_ele_> Bop =
+            EvaluateStrainGradient(jacobian_mapping, spatial_material_mapping);
 
-    const CauchyGreen<distype> cauchygreen = EvaluateCauchyGreen(spatial_material_mapping);
+        CORE::LINALG::Matrix<num_str_, num_dof_per_ele_> dInverseRightCauchyGreen_dDisp =
+            EvaluateInverseCauchyGreenLinearization(
+                cauchygreen, jacobian_mapping, spatial_material_mapping);
 
-    CORE::LINALG::Matrix<num_str_, num_dof_per_ele_> Bop =
-        EvaluateStrainGradient(jacobian_mapping, spatial_material_mapping);
+        const double volchange = ComputeVolumeChange<distype>(spatial_material_mapping,
+            jacobian_mapping, ele, discretization, la[0].lm_, kinematictype);
 
-    CORE::LINALG::Matrix<num_str_, num_dof_per_ele_> dInverseRightCauchyGreen_dDisp =
-        EvaluateInverseCauchyGreenLinearization(
-            cauchygreen, jacobian_mapping, spatial_material_mapping);
+        CORE::LINALG::Matrix<1, num_dof_per_ele_> dDetDefGrad_dDisp =
+            ComputeLinearizationOfDetDefGradWrtDisp<distype>(
+                spatial_material_mapping, jacobian_mapping, kinematictype);
 
-    const double volchange = ComputeVolumeChange<distype>(
-        spatial_material_mapping, jacobian_mapping, ele, discretization, la[0].lm_, kinematictype);
+        const CORE::LINALG::Matrix<1, num_dof_per_ele_> dVolchange_dDisp =
+            ComputeLinearizationOfVolchangeWrtDisp<distype>(
+                dDetDefGrad_dDisp, jacobian_mapping, kinematictype);
 
-    CORE::LINALG::Matrix<1, num_dof_per_ele_> dDetDefGrad_dDisp =
-        ComputeLinearizationOfDetDefGradWrtDisp<distype>(
-            spatial_material_mapping, jacobian_mapping, kinematictype);
+        std::vector<double> fluidmultiphase_phiAtGP =
+            ComputeFluidMultiPhasePrimaryVariablesAtGP<distype>(
+                fluidmultiphase_ephi, nummultifluiddofpernode, shape_functions);
 
-    const CORE::LINALG::Matrix<1, num_dof_per_ele_> dVolchange_dDisp =
-        ComputeLinearizationOfVolchangeWrtDisp<distype>(
-            dDetDefGrad_dDisp, jacobian_mapping, kinematictype);
+        double solidpressure = ComputeSolPressureAtGP<distype>(
+            nummultifluiddofpernode, numfluidphases, fluidmultiphase_phiAtGP, porofluidmat);
+        // derivative of press w.r.t. displacements (only in case of volfracs)
+        CORE::LINALG::Matrix<1, num_dof_per_ele_> dSolidpressure_dDisp(true);
 
-    std::vector<double> fluidmultiphase_phiAtGP =
-        ComputeFluidMultiPhasePrimaryVariablesAtGP<distype>(
-            fluidmultiphase_ephi, nummultifluiddofpernode, shape_functions);
+        if (hasvolfracs)
+        {
+          CORE::LINALG::Matrix<1, num_dof_per_ele_> dPorosity_dDisp;
+          double porosity = 0.0;
 
-    double solidpressure = ComputeSolPressureAtGP<distype>(
-        nummultifluiddofpernode, numfluidphases, fluidmultiphase_phiAtGP, porofluidmat);
-    // derivative of press w.r.t. displacements (only in case of volfracs)
-    CORE::LINALG::Matrix<1, num_dof_per_ele_> dSolidpressure_dDisp(true);
+          ComputePorosityAndLinearization<distype>(porostructmat, params, solidpressure, gp,
+              volchange, porosity, dDetDefGrad_dDisp, dPorosity_dDisp);
 
-    if (hasvolfracs)
-    {
-      CORE::LINALG::Matrix<1, num_dof_per_ele_> dPorosity_dDisp;
-      double porosity = 0.0;
+          // save the pressure coming from the fluid S_i*p_i (old solidpressure, without accounting
+          // for volfracs)
+          const double fluidpress = solidpressure;
 
-      ComputePorosityAndLinearization<distype>(porostructmat, params, solidpressure, gp, volchange,
-          porosity, dDetDefGrad_dDisp, dPorosity_dDisp);
+          solidpressure = RecalculateSolPressureAtGP(fluidpress, porosity, nummultifluiddofpernode,
+              numfluidphases, numvolfrac, fluidmultiphase_phiAtGP);
 
-      // save the pressure coming from the fluid S_i*p_i (old solidpressure, without accounting for
-      // volfracs)
-      const double fluidpress = solidpressure;
+          RecalculateLinearizationOfSolPressWrtDisp<distype>(fluidpress, porosity,
+              nummultifluiddofpernode, numfluidphases, numvolfrac, fluidmultiphase_phiAtGP,
+              dPorosity_dDisp, dSolidpressure_dDisp);
+        }
 
-      solidpressure = RecalculateSolPressureAtGP(fluidpress, porosity, nummultifluiddofpernode,
-          numfluidphases, numvolfrac, fluidmultiphase_phiAtGP);
+        // inverse Right Cauchy-Green tensor as vector in voigt notation
+        CORE::LINALG::Matrix<num_str_, 1> C_inv_vec(false);
+        ::UTILS::VOIGT::Stresses::MatrixToVector(cauchygreen.inverse_right_cauchy_green, C_inv_vec);
 
-      RecalculateLinearizationOfSolPressWrtDisp<distype>(fluidpress, porosity,
-          nummultifluiddofpernode, numfluidphases, numvolfrac, fluidmultiphase_phiAtGP,
-          dPorosity_dDisp, dSolidpressure_dDisp);
-    }
+        // B^T . C^-1
+        CORE::LINALG::Matrix<num_dof_per_ele_, 1> BopCinv(true);
+        BopCinv.MultiplyTN(Bop, C_inv_vec);
 
-    const double detJ_w = jacobian_mapping.determinant_ * gauss_integration_.Weight(gp);
+        // update internal force vector
+        if (force.has_value())
+        {
+          UpdateInternalForceVectorMultiPhasePressureBased<distype>(integration_factor,
+              solidpressure, spatial_material_mapping.determinant_deformation_gradient_, BopCinv,
+              *force);
+        }
 
-    // inverse Right Cauchy-Green tensor as vector in voigt notation
-    CORE::LINALG::Matrix<num_str_, 1> C_inv_vec(false);
-    ::UTILS::VOIGT::Stresses::MatrixToVector(cauchygreen.inverse_right_cauchy_green, C_inv_vec);
+        // update stiffness matrix
+        if (stiff.has_value())
+        {
+          UpdateElasticStiffnessMatrixMultiPhasePressureBased<distype>(integration_factor,
+              solidpressure, spatial_material_mapping.determinant_deformation_gradient_, BopCinv,
+              Bop, dDetDefGrad_dDisp, dSolidpressure_dDisp, dInverseRightCauchyGreen_dDisp, *stiff);
 
-    // B^T . C^-1
-    CORE::LINALG::Matrix<num_dof_per_ele_, 1> BopCinv(true);
-    BopCinv.MultiplyTN(Bop, C_inv_vec);
-
-    // update internal force vector
-    if (force.has_value())
-    {
-      UpdateInternalForceVectorMultiPhasePressureBased<distype>(detJ_w, solidpressure,
-          spatial_material_mapping.determinant_deformation_gradient_, BopCinv, *force);
-    }
-
-    // update stiffness matrix
-    if (stiff.has_value())
-    {
-      UpdateElasticStiffnessMatrixMultiPhasePressureBased<distype>(detJ_w, solidpressure,
-          spatial_material_mapping.determinant_deformation_gradient_, BopCinv, Bop,
-          dDetDefGrad_dDisp, dSolidpressure_dDisp, dInverseRightCauchyGreen_dDisp, *stiff);
-
-      UpdateGeometricStiffnessMatrixMultiPhasePressureBased<distype>(detJ_w, solidpressure,
-          spatial_material_mapping.determinant_deformation_gradient_, C_inv_vec,
-          jacobian_mapping.N_XYZ_, *stiff);
-    }
-  }
+          UpdateGeometricStiffnessMatrixMultiPhasePressureBased<distype>(integration_factor,
+              solidpressure, spatial_material_mapping.determinant_deformation_gradient_, C_inv_vec,
+              jacobian_mapping.N_XYZ_, *stiff);
+        }
+      });
 }
 
 template <DRT::Element::DiscretizationType distype>
@@ -177,63 +172,59 @@ void DRT::ELEMENTS::SolidPoroPressureBasedEleCalc<distype>::CouplingPoroelast(
       EvaluateNodalCoordinates<distype>(ele, discretization, la[0].lm_);
 
   // Loop over all Gauss points
-  for (int gp = 0; gp < gauss_integration_.NumPoints(); ++gp)
-  {
-    const CORE::LINALG::Matrix<num_dim_, 1> xi =
-        EvaluateParameterCoordinate<distype>(gauss_integration_, gp);
+  IterateJacobianMappingAtGaussPoints(nodal_coordinates, gauss_integration_,
+      [&](const CORE::LINALG::Matrix<DETAIL::num_dim<distype>, 1>& xi,
+          const ShapeFunctionsAndDerivatives<distype>& shape_functions,
+          const JacobianMapping<distype>& jacobian_mapping, double integration_factor, int gp
 
-    const ShapeFunctionsAndDerivatives<distype> shape_functions =
-        EvaluateShapeFunctionsAndDerivs<distype>(xi);
+      )
+      {
+        const SpatialMaterialMapping<distype> spatial_material_mapping =
+            EvaluateSpatialMaterialMapping<distype>(
+                jacobian_mapping, nodal_coordinates, 1.0, kinematictype);
 
-    const JacobianMapping<distype> jacobian_mapping =
-        EvaluateJacobianMapping(shape_functions, nodal_coordinates);
+        const CauchyGreen<distype> cauchygreen = EvaluateCauchyGreen(spatial_material_mapping);
 
-    const SpatialMaterialMapping<distype> spatial_material_mapping =
-        EvaluateSpatialMaterialMapping<distype>(
-            jacobian_mapping, nodal_coordinates, 1.0, kinematictype);
+        CORE::LINALG::Matrix<num_str_, num_dof_per_ele_> Bop =
+            EvaluateStrainGradient(jacobian_mapping, spatial_material_mapping);
 
-    const CauchyGreen<distype> cauchygreen = EvaluateCauchyGreen(spatial_material_mapping);
+        // volume change (used for porosity law). Same as J in nonlinear theory.
+        const double volchange = ComputeVolumeChange<distype>(spatial_material_mapping,
+            jacobian_mapping, ele, discretization, la[0].lm_, kinematictype);
 
-    CORE::LINALG::Matrix<num_str_, num_dof_per_ele_> Bop =
-        EvaluateStrainGradient(jacobian_mapping, spatial_material_mapping);
+        std::vector<double> fluidmultiphase_phiAtGP =
+            ComputeFluidMultiPhasePrimaryVariablesAtGP<distype>(
+                fluidmultiphase_ephi, nummultifluiddofpernode, shape_functions);
 
-    // volume change (used for porosity law). Same as J in nonlinear theory.
-    const double volchange = ComputeVolumeChange<distype>(
-        spatial_material_mapping, jacobian_mapping, ele, discretization, la[0].lm_, kinematictype);
+        std::vector<double> solidpressurederiv = ComputeSolidPressureDeriv<distype>(
+            porofluidmat, fluidmultiphase_phiAtGP, numfluidphases);
 
-    std::vector<double> fluidmultiphase_phiAtGP =
-        ComputeFluidMultiPhasePrimaryVariablesAtGP<distype>(
-            fluidmultiphase_ephi, nummultifluiddofpernode, shape_functions);
+        if (hasvolfracs)
+        {
+          double solidpressure = ComputeSolPressureAtGP<distype>(
+              nummultifluiddofpernode, numfluidphases, fluidmultiphase_phiAtGP, porofluidmat);
 
-    std::vector<double> solidpressurederiv =
-        ComputeSolidPressureDeriv<distype>(porofluidmat, fluidmultiphase_phiAtGP, numfluidphases);
+          double porosity =
+              ComputePorosity<distype>(porostructmat, params, solidpressure, volchange, gp);
 
-    if (hasvolfracs)
-    {
-      double solidpressure = ComputeSolPressureAtGP<distype>(
-          nummultifluiddofpernode, numfluidphases, fluidmultiphase_phiAtGP, porofluidmat);
+          RecalculateSolPressureDeriv(fluidmultiphase_phiAtGP, nummultifluiddofpernode,
+              numfluidphases, numvolfrac, solidpressure, porosity, solidpressurederiv);
+        }
 
-      double porosity =
-          ComputePorosity<distype>(porostructmat, params, solidpressure, volchange, gp);
+        const double detJ_w = jacobian_mapping.determinant_ * gauss_integration_.Weight(gp);
 
-      RecalculateSolPressureDeriv(fluidmultiphase_phiAtGP, nummultifluiddofpernode, numfluidphases,
-          numvolfrac, solidpressure, porosity, solidpressurederiv);
-    }
+        // inverse Right Cauchy-Green tensor as vector in voigt notation
+        CORE::LINALG::Matrix<num_str_, 1> C_inv_vec(false);
+        ::UTILS::VOIGT::Stresses::MatrixToVector(cauchygreen.inverse_right_cauchy_green, C_inv_vec);
 
-    const double detJ_w = jacobian_mapping.determinant_ * gauss_integration_.Weight(gp);
+        // B^T . C^-1
+        CORE::LINALG::Matrix<num_dof_per_ele_, 1> BopCinv(true);
+        BopCinv.MultiplyTN(Bop, C_inv_vec);
 
-    // inverse Right Cauchy-Green tensor as vector in voigt notation
-    CORE::LINALG::Matrix<num_str_, 1> C_inv_vec(false);
-    ::UTILS::VOIGT::Stresses::MatrixToVector(cauchygreen.inverse_right_cauchy_green, C_inv_vec);
-
-    // B^T . C^-1
-    CORE::LINALG::Matrix<num_dof_per_ele_, 1> BopCinv(true);
-    BopCinv.MultiplyTN(Bop, C_inv_vec);
-
-    UpdateStiffnessMatrixCouplingMultiPhasePressureBased<distype>(detJ_w, solidpressurederiv,
-        BopCinv, shape_functions, spatial_material_mapping.determinant_deformation_gradient_,
-        nummultifluiddofpernode, stiffness_matrix);
-  }
+        UpdateStiffnessMatrixCouplingMultiPhasePressureBased<distype>(detJ_w, solidpressurederiv,
+            BopCinv, shape_functions, spatial_material_mapping.determinant_deformation_gradient_,
+            nummultifluiddofpernode, stiffness_matrix);
+      });
 }
 
 template <DRT::Element::DiscretizationType distype>
