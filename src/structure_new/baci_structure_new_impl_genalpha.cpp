3146d7d4bb3450544ee1313adeeae180779804a7
Hoang-Giang Bui <giang.bui@hereon.de>
2023-08-31 13:01:02 +0200

    Move SetInitialDisplacement to the base class

--- src/structure_new/baci_structure_new_impl_genalpha.cpp
+++ src/structure_new/baci_structure_new_impl_genalpha.cpp
@@ -41,7 +41,6 @@ STR::IMPLICIT::GenAlpha::GenAlpha()
       fviscon_ptr_(Teuchos::null),
       finertianp_ptr_(Teuchos::null),
       finertian_ptr_(Teuchos::null)
-
 {
   // empty constructor
 }
@@ -129,61 +128,7 @@ void STR::IMPLICIT::GenAlpha::Setup()
   // Has to be set before the PostSetup() routine is called!
   issetup_ = true;
 }
-/*----------------------------------------------------------------------------*
- *----------------------------------------------------------------------------*/
-void STR::IMPLICIT::GenAlpha::SetInitialDisplacement(
-    const INPAR::STR::InitialDisp init, const int startfuncno)
-{
-  switch (init)
-  {
-    case INPAR::STR::initdisp_zero_disp:
-    {
-      GlobalState().GetDisN()->PutScalar(0.0);
-      GlobalState().GetDisNp()->PutScalar(0.0);
 
-      break;
-    }
-    case INPAR::STR::initdisp_disp_by_function:
-    {
-      const Epetra_Map* dofrowmap = GlobalState().GetDiscret()->DofRowMap();
-
-      // loop all nodes on the processor
-      for (int lnodeid = 0; lnodeid < GlobalState().GetDiscret()->NumMyRowNodes(); lnodeid++)
-      {
-        // get the processor local node
-        DRT::Node* lnode = GlobalState().GetDiscret()->lRowNode(lnodeid);
-
-        // the set of degrees of freedom associated with the node
-        std::vector<int> nodedofset = GlobalState().GetDiscret()->Dof(0, lnode);
-
-        // loop nodal dofs
-        for (unsigned int d = 0; d < nodedofset.size(); ++d)
-        {
-          const int dofgid = nodedofset[d];
-          int doflid = dofrowmap->LID(dofgid);
-
-          // evaluate component k of spatial function
-          double initialval = DRT::Problem::Instance()
-                                  ->FunctionById<DRT::UTILS::FunctionOfSpaceTime>(startfuncno - 1)
-                                  .Evaluate(lnode->X(), GlobalState().GetTimeN(), d);
-
-          int err = GlobalState().GetDisN()->ReplaceMyValues(1, &initialval, &doflid);
-          if (err != 0) dserror("dof not on proc");
-        }
-      }
-
-      // initialize also the solution vector
-      GlobalState().GetDisNp()->Update(1.0, *GlobalState().GetDisN(), 0.0);
-
-      break;
-    }
-    default:
-      dserror("Unknown option for initial displacement: %d", init);
-      break;
-  }
-
-  return;
-}
 /*----------------------------------------------------------------------------*
  *----------------------------------------------------------------------------*/
 void STR::IMPLICIT::GenAlpha::PostSetup()
