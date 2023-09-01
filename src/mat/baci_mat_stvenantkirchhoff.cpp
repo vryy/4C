cb7a6d1002ab8ba8a7533efd1541288f47ef3055
Stephan Sinzig <stephan.sinzig@tum.de>
2023-09-01 13:37:35 +0200

    Remove SetupCmat2d from StVK material
    It was never used and therefore also not tested

--- src/mat/baci_mat_stvenantkirchhoff.cpp
+++ src/mat/baci_mat_stvenantkirchhoff.cpp
@@ -107,41 +107,6 @@ void MAT::StVenantKirchhoff::Unpack(const std::vector<char>& data)
 }
 
 /*----------------------------------------------------------------------*
-// computes isotropic eplane strain, rotational symmetry
-// plane strain, rotational symmetry
- *----------------------------------------------------------------------*/
-void MAT::StVenantKirchhoff::SetupCmat2d(CORE::LINALG::SerialDenseMatrix* cmat)
-{
-  const double ym = params_->youngs_;
-  const double pv = params_->poissonratio_;
-
-  // plane strain, rotational symmetry
-  const double c1 = ym / (1.0 + pv);
-  const double b1 = c1 * pv / (1.0 - 2.0 * pv);
-  const double a1 = b1 + c1;
-
-  (*cmat)(0, 0) = a1;
-  (*cmat)(0, 1) = b1;
-  (*cmat)(0, 2) = 0.;
-  (*cmat)(0, 3) = b1;
-
-  (*cmat)(1, 0) = b1;
-  (*cmat)(1, 1) = a1;
-  (*cmat)(1, 2) = 0.;
-  (*cmat)(1, 3) = b1;
-
-  (*cmat)(2, 0) = 0.;
-  (*cmat)(2, 1) = 0.;
-  (*cmat)(2, 2) = c1 / 2.;
-  (*cmat)(2, 3) = 0.;
-
-  (*cmat)(3, 0) = b1;
-  (*cmat)(3, 1) = b1;
-  (*cmat)(3, 2) = 0.;
-  (*cmat)(3, 3) = a1;
-}
-
-/*----------------------------------------------------------------------*
 // computes isotropic elasticity tensor in matrix notion for 3d
  *----------------------------------------------------------------------*/
 void MAT::StVenantKirchhoff::SetupCmat(CORE::LINALG::Matrix<6, 6>& cmat)
