5c14a0dd16a7de26473a5c6e4a26ba873cbd05f4
Amadeus Gebauer <amadeus.gebauer@tum.de>
2023-10-13 17:11:41 +0200

    Fix further tidy issues in beamcontact framework
    Follows !1755

--- src/beamcontact/baci_beamcontact_beam3contact.cpp
+++ src/beamcontact/baci_beamcontact_beam3contact.cpp
@@ -4521,16 +4521,7 @@ void CONTACT::Beam3contact<numnodes, numnodalvalues>::AssembleShapefunctions(
 {
   // assembly_N is just an array to help assemble the matrices of the shape functions
   // it determines, which shape function is used in which column of N
-  int assembly_N[3][3 * numnodes * numnodalvalues];
-
-  // Initialize to zero
-  for (int i = 0; i < 3 * numnodes * numnodalvalues; i++)
-  {
-    for (int j = 0; j < 3; j++)
-    {
-      assembly_N[j][i] = 0.0;
-    }
-  }
+  std::array<std::array<unsigned int, 3 * numnodes * numnodalvalues>, 3> assembly_N{};
 
   /*
   Set number of shape functions for each 3*3 block:
@@ -4585,16 +4576,7 @@ void CONTACT::Beam3contact<numnodes, numnodalvalues>::AssembleShapefunctions(
 {
   // assembly_N is just an array to help assemble the matrices of the shape functions
   // it determines, which shape function is used in which column of N
-  int assembly_N[3][3 * numnodes * numnodalvalues];
-
-  // Initialize to zero
-  for (int i = 0; i < 3 * numnodes * numnodalvalues; i++)
-  {
-    for (int j = 0; j < 3; j++)
-    {
-      assembly_N[j][i] = 0.0;
-    }
-  }
+  std::array<std::array<unsigned int, 3 * numnodes * numnodalvalues>, 3> assembly_N{};
 
   /*
   Set number of shape functions for each 3*3 block:
