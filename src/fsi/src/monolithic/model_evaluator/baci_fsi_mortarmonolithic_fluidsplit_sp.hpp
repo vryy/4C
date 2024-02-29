8d3ccc1f0059a598a1f5d3c21befca14706f59fc
Max Firmbach <max.firmbach@unibw.de>
2024-02-29 16:07:18 +0100

    Add Inf-norm scaling for saddlepoint fsi
    This adds an inf-norm scaling to the saddlepoint
    fsi system to improve the condition number.

--- src/fsi/src/monolithic/model_evaluator/baci_fsi_mortarmonolithic_fluidsplit_sp.hpp
+++ src/fsi/src/monolithic/model_evaluator/baci_fsi_mortarmonolithic_fluidsplit_sp.hpp
@@ -207,6 +207,18 @@ namespace FSI
     //! Set #notsetup_ = true after redistribution
     void SetNotSetup() override { notsetup_ = true; }
 
+    //! @name Methods for infnorm-scaling of the system
+    //!@{
+
+    /// apply infnorm scaling to linear block system
+    void ScaleSystem(CORE::LINALG::BlockSparseMatrixBase& mat, Epetra_Vector& b) override;
+
+    /// undo infnorm scaling from scaled solution
+    void UnscaleSolution(
+        CORE::LINALG::BlockSparseMatrixBase& mat, Epetra_Vector& x, Epetra_Vector& b) override;
+
+    //!@}
+
     /*! block system matrix
      *  System matrix has a 6x6-block structure corresponding to the vector of unknowns
      *
@@ -232,6 +244,16 @@ namespace FSI
     Teuchos::RCP<CORE::LINALG::MatrixColTransform> fluid_mesh_inner_inner_transform_;
 
     ///@}
+
+    /// @name infnorm scaling
+    //!@{
+
+    Teuchos::RCP<Epetra_Vector> srowsum_;
+    Teuchos::RCP<Epetra_Vector> scolsum_;
+    Teuchos::RCP<Epetra_Vector> arowsum_;
+    Teuchos::RCP<Epetra_Vector> acolsum_;
+
+    //!@}
 
     /// additional ale residual to avoid incremental ale errors
     Teuchos::RCP<Epetra_Vector> aleresidual_;
