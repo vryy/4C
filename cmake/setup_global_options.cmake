68081a8e9f555d79724346400ee8d6e64c839c04
Sebastian Proell <sebastian.proell@tum.de>
2024-03-06 17:23:24 +0100

    CMake: adapt flags to support clang builds

--- cmake/setup_global_options.cmake
+++ cmake/setup_global_options.cmake
@@ -2,17 +2,37 @@
 # Process global options
 #
 
+include(CheckCXXCompilerFlag)
+
+#
+# Add a compiler flag if the compiler understands it.
+#
+function(enable_flag_if_supported _flag)
+  check_cxx_compiler_flag(${_flag} BACI_COMPILER_HAS_FLAG_${_flag})
+  if(BACI_COMPILER_HAS_FLAG_${_flag})
+    target_compile_options(baci_global_compile_settings INTERFACE ${_flag})
+  endif()
+endfunction()
+
 # Define a target which pulls in all the global compiler settings and definitions
 add_library(baci_global_compile_settings INTERFACE)
 
-target_compile_options(baci_global_compile_settings INTERFACE "-Wall")
-target_compile_options(baci_global_compile_settings INTERFACE "-Wextra")
+enable_flag_if_supported("-Wall")
+enable_flag_if_supported("-Wextra")
 # Disable unused parameter detection since there would be too many hits to fix
-target_compile_options(baci_global_compile_settings INTERFACE "-Wno-unused-parameter")
+enable_flag_if_supported("-Wno-unused-parameter")
 # Disable maybe uninitialized detection since this can give false positives that are hard to circumvent.
 # Our address sanitizer checks will also find such errors, although only later.
-target_compile_options(baci_global_compile_settings INTERFACE "-Wno-maybe-uninitialized")
-target_compile_options(baci_global_compile_settings INTERFACE "-Wvla")
+enable_flag_if_supported("-Wno-maybe-uninitialized")
+enable_flag_if_supported("-Wvla")
+
+# For clang: do not error for a number of checks that are not yet fixed
+if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+  enable_flag_if_supported("-Wno-error=overloaded-virtual")
+  enable_flag_if_supported("-Wno-error=unused-variable")
+  enable_flag_if_supported("-Wno-error=undefined-var-template")
+  enable_flag_if_supported("-Wno-error=potentially-evaluated-expression")
+endif()
 
 baci_process_global_option(BACI_DSERROR_DUMP "dserror creates a core file" OFF)
 baci_process_global_option(BACI_TRAP_FE "Crash BACI if a nan or inf occurs" ON)
