1f9f9c6fcbb795c8da2638aad99fa4779dc9e7e2
Rishav Shaw <rishav.shaw@unibw.de>
2024-03-06 10:18:25 +0100

    Add RoughNode as a derived class of Node

---
+++ src/contact/baci_contact_rough_node.cpp
@@ -0,0 +1,69 @@
+/*-----------------------------------------------------------------------*/
+/*! \file
+\brief A class for a frictional contact node
+\level 2
+*/
+/*-----------------------------------------------------------------------*/
+
+#include "baci_contact_rough_node.hpp"
+
+#include "baci_contact_defines.hpp"
+#include "baci_contact_element.hpp"
+#include "baci_global_data.hpp"
+#include "baci_utils_function.hpp"
+
+#include <mirco_topology.h>
+#include <mirco_topologyutilities.h>
+
+BACI_NAMESPACE_OPEN
+
+CONTACT::RoughNodeType CONTACT::RoughNodeType::instance_;
+
+CORE::COMM::ParObject* CONTACT::RoughNodeType::Create(const std::vector<char>& data)
+{
+  // double x[3];
+  std::vector<double> x(3, 0.0);
+  std::vector<int> dofs(0);
+
+  CONTACT::RoughNode* node = new CONTACT::RoughNode(0, x, 0, dofs, false, false, 0, 0, 0, 0, 0, 0);
+  node->Unpack(data);
+
+  return node;
+}
+
+/*----------------------------------------------------------------------*
+ *----------------------------------------------------------------------*/
+CONTACT::RoughNode::RoughNode(int id, const std::vector<double>& coords, const int owner,
+    const std::vector<int>& dofs, const bool isslave, const bool initactive,
+    const int hurstexponentfunction, int initialtopologystddeviationfunction, int resolution,
+    int randomtopologyflag, int randomseedflag, int randomgeneratorseed)
+    : CONTACT::Node(id, coords, owner, dofs, isslave, initactive)
+{
+  if (isslave && hurstexponentfunction != 0)
+  {
+    hurstExponent_ = GLOBAL::Problem::Instance()
+                         ->FunctionById<CORE::UTILS::FunctionOfSpaceTime>(hurstexponentfunction - 1)
+                         .Evaluate(this->X().data(), 1, this->Dim());
+    initialTopologyStdDeviation_ = GLOBAL::Problem::Instance()
+                                       ->FunctionById<CORE::UTILS::FunctionOfSpaceTime>(
+                                           initialtopologystddeviationfunction - 1)
+                                       .Evaluate(this->X().data(), 1, this->Dim());
+
+    const int N = pow(2, resolution);
+    topology_ = Teuchos::rcp(new CORE::LINALG::SerialDenseMatrix(N + 1, N + 1));
+
+    std::string topologyFilePath = "";
+    Teuchos::RCP<MIRCO::TopologyGeneration> surfacegenerator;
+    // creating the correct surface object
+    MIRCO::CreateSurfaceObject(resolution, initialTopologyStdDeviation_, hurstExponent_,
+        randomseedflag, topologyFilePath, randomtopologyflag, randomgeneratorseed,
+        surfacegenerator);
+    surfacegenerator->GetSurface(*topology_);
+
+    auto max_and_mean = MIRCO::ComputeMaxAndMean(*topology_);
+    maxTopologyHeight_ = max_and_mean.max_;
+  }
+  return;
+}
+
+BACI_NAMESPACE_CLOSE
---
+++ src/contact/baci_contact_rough_node.hpp
@@ -0,0 +1,77 @@
+/*----------------------------------------------------------------------*/
+/*! \file
+\brief A class for a rough contact node
+\level 2
+*/
+/*----------------------------------------------------------------------*/
+#ifndef BACI_CONTACT_ROUGH_NODE_HPP
+#define BACI_CONTACT_ROUGH_NODE_HPP
+
+#include "baci_config.hpp"
+
+#include "baci_contact_node.hpp"
+
+BACI_NAMESPACE_OPEN
+
+namespace CONTACT
+{
+  class RoughNodeType : public CORE::COMM::ParObjectType
+  {
+   public:
+    std::string Name() const final { return "RoughNodeType"; }
+
+    static RoughNodeType& Instance() { return instance_; };
+
+    virtual CORE::COMM::ParObject* Create(const std::vector<char>& data);
+
+   private:
+    static RoughNodeType instance_;
+  };
+
+
+  class RoughNode : public Node
+  {
+   public:
+    //! @name Enums and Friends
+
+    /*!
+     \brief The Discretization is a friend of RoughNode
+     */
+    friend class DRT::Discretization;
+
+    //@}
+
+    //! @name Constructors and destructors and related methods
+
+    /*!
+     \brief Standard Constructor
+
+     \param id     (in): A globally unique node id
+     \param coords (in): vector of nodal coordinates, length 3
+     \param owner  (in): Owner of this node.
+     \param dofs   (in): list of global degrees of freedom
+     \param isslave(in): flag indicating whether node is slave or master
+     \param initactive (in): flag indicating whether initially set to active
+     \param hurstexponentfunction (in): function id for roughness parameter
+     \\ add remaining params here
+
+     */
+    RoughNode(int id, const std::vector<double>& coords, const int owner,
+        const std::vector<int>& dofs, const bool isslave, const bool initactive,
+        const int hurstexponentfunction, int initialtopologystddeviationfunction, int resolution,
+        int randomtopologyflag, int randomseedflag, int randomgeneratorseed);
+
+    Teuchos::RCP<CORE::LINALG::SerialDenseMatrix> GetTopology() const { return topology_; };
+    double GetMaxTopologyHeight() { return maxTopologyHeight_; };
+
+   protected:
+    double hurstExponent_ = 0;
+    double initialTopologyStdDeviation_ = 0;
+    Teuchos::RCP<CORE::LINALG::SerialDenseMatrix> topology_;
+    double maxTopologyHeight_;
+  };
+}  // namespace CONTACT
+
+BACI_NAMESPACE_CLOSE
+
+#endif  // CONTACT_ROUGH_NODE_HPP
