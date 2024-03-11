37761e2a4452ff1d7e5c35a4753c33d247e4d5a9
Amadeus Gebauer <amadeus.gebauer@tum.de>
2024-03-11 10:28:56 +0100

    Upgrade clang tidy and remove run-clang-tidy
    Our contribution to LLVM now enables us to remove our custom
    cun-clang-tidy script from our repo. Our code-check now works with
    the provided script in the LLVM repo.
    
    Closes #1125

--- dependencies/testing/clang-tidy/install.sh
+++ dependencies/testing/clang-tidy/install.sh
@@ -10,8 +10,8 @@ set -e
 INSTALL_DIR="$1"
 # Number of procs for building (default 4)
 NPROCS=${NPROCS=4}
-VERSION="17.0.2"
-CHECKSUM="dcba3eb486973dce45b6edfe618f3f29b703ae7e6ef9df65182fb50fb6fe4235"
+VERSION="18.1.1"
+CHECKSUM="62439f733311869dbbaf704ce2e02141d2a07092d952fc87ef52d1d636a9b1e4"
 
 wget --no-verbose https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-${VERSION}.tar.gz
 # Verify checksum
