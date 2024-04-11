e1ce33177780d56b6fea1f330a0392672f3acc5a
Rishav Shaw <rishav.shaw@unibw.de>
2024-04-11 12:05:28 +0200

    Add MIRCO unittests

---
+++ unittests/contact_constitutivelaw/baci_mirco_force_contactconstitutivelaw_test.cpp
@@ -0,0 +1,123 @@
+/*----------------------------------------------------------------------*/
+/*! \file
+
+\brief unit testing functionality for the mirco contact constitutivelaw with point-force-based Green
+function
+
+\level 2
+
+*/
+/*----------------------------------------------------------------------*/
+
+#include <gtest/gtest.h>
+
+#include "baci_contact_constitutivelaw_mirco_contactconstitutivelaw.hpp"
+#include "baci_global_data.hpp"
+#include "baci_mat_par_bundle.hpp"
+#include "baci_utils_function.hpp"
+
+#ifdef BACI_WITH_MIRCO
+
+namespace
+{
+  using namespace BACI;
+
+  class MircoConstitutiveLawForceTest : public ::testing::Test
+  {
+   public:
+    MircoConstitutiveLawForceTest()
+    {
+      const int problemid(0);
+      GLOBAL::Problem& problem = (*GLOBAL::Problem::Instance());
+      problem.Materials()->SetReadFromProblem(problemid);
+
+      Teuchos::RCP<CORE::UTILS::SymbolicFunctionOfSpaceTime<1>> FFUNCT1 =
+          Teuchos::rcp(new CORE::UTILS::SymbolicFunctionOfSpaceTime<1>({"0.7"}, {}));
+      Teuchos::RCP<CORE::UTILS::SymbolicFunctionOfSpaceTime<1>> FFUNCT2 =
+          Teuchos::rcp(new CORE::UTILS::SymbolicFunctionOfSpaceTime<1>({"20.0"}, {}));
+
+      Teuchos::RCP<CORE::UTILS::FunctionOfSpaceTime> FUNCT1 = FFUNCT1;
+      Teuchos::RCP<CORE::UTILS::FunctionOfSpaceTime> FUNCT2 = FFUNCT2;
+
+      CORE::UTILS::FunctionManager functionmanager_;
+      functionmanager_.SetFunctions<std::any>({FUNCT1, FUNCT2});
+      problem.SetFunctionManager(std::move(functionmanager_));
+
+      // set up material to be added to problem instance
+      const int matid(1);
+      Teuchos::RCP<MAT::PAR::Material> material =
+          Teuchos::rcp(new MAT::PAR::Material(matid, INPAR::MAT::m_stvenant, "first_material"));
+      material->Add("YOUNG", 1.0);
+      material->Add("NUE", 0.3);
+
+      // add material to problem instance
+      problem.Materials()->Insert(matid, material);
+
+      // initialize container for material parameters
+      const Teuchos::RCP<CONTACT::CONSTITUTIVELAW::Container> container =
+          Teuchos::rcp(new CONTACT::CONSTITUTIVELAW::Container(
+              1, INPAR::CONTACT::ConstitutiveLawType::colaw_mirco, "Mirco Constitutivelaw"));
+
+      // add parameters to container
+      container->Add("FirstMatID", 1);
+      container->Add("SecondMatID", 1);
+      container->Add("LateralLength", 1000.0);
+      container->Add("Resolution", 6);
+      container->Add("PressureGreenFunFlag", false);
+      container->Add("InitialTopologyStdDeviationFunct", 2);
+      container->Add("HurstExponentFunct", 1);
+      container->Add("RandomTopologyFlag", true);
+      container->Add("RandomSeedFlag", false);
+      container->Add("RandomGeneratorSeed", 95);
+      container->Add("Tolerance", 0.01);
+      container->Add("MaxIteration", 100);
+      container->Add("WarmStartingFlag", true);
+      container->Add("Offset", 2.0);
+      container->Add("FiniteDifferenceFraction", 0.001);
+      container->Add("ActiveGapTolerance", 1e-6);
+      container->Add("TopologyFilePath", std::string("sup6.dat"));
+
+      const Teuchos::RCP<CONTACT::CONSTITUTIVELAW::ConstitutiveLaw> mircococonstlaw =
+          CONTACT::CONSTITUTIVELAW::ConstitutiveLaw::Factory(container);
+      coconstlaw_ = mircococonstlaw;
+
+      std::vector<double> x(3, 0.0);
+      std::vector<int> dofs(3);
+
+      double hurstexponentfunct = *container->Get<int>("HurstExponentFunct");
+      double initialtopologystddevfunct = *container->Get<int>("InitialTopologyStdDeviationFunct");
+      int resolution = *container->Get<int>("Resolution");
+      bool randomtopologyflag = *container->Get<bool>("RandomTopologyFlag");
+      bool randomseedflag = *container->Get<bool>("RandomSeedFlag");
+      int randomgeneratorseed = *container->Get<int>("RandomGeneratorSeed");
+
+      cnode = Teuchos::rcp(new CONTACT::RoughNode(1, x, 1, dofs, true, true, hurstexponentfunct,
+          initialtopologystddevfunct, resolution, randomtopologyflag, randomseedflag,
+          randomgeneratorseed));
+    }
+    Teuchos::RCP<CONTACT::CONSTITUTIVELAW::ConstitutiveLaw> coconstlaw_;
+
+    Teuchos::RCP<CONTACT::RoughNode> cnode;
+  };  // namespace
+
+
+  //! test member function Evaluate
+  TEST_F(MircoConstitutiveLawForceTest, TestEvaluate)
+  {
+    // gap < 0
+    EXPECT_ANY_THROW(coconstlaw_->Evaluate(1.0, cnode.get()));
+    // 0< gap < offset
+    EXPECT_ANY_THROW(coconstlaw_->Evaluate(-0.25, cnode.get()));
+    // offset < gap
+    EXPECT_NEAR(coconstlaw_->Evaluate(-12.0, cnode.get()), -0.0005942101230076477, 1.e-10);
+  }
+
+  //! test member function EvaluateDeriv
+  TEST_F(MircoConstitutiveLawForceTest, TestEvaluateDeriv)
+  {
+    // EXPECT_NEAR(coconstlaw_->EvaluateDeriv(-12.0, cnode), 1.34284789678326e-04, 1.e-10);
+    EXPECT_ANY_THROW(coconstlaw_->EvaluateDeriv(-0.25, cnode.get()));
+  }
+}  // namespace
+
+#endif
