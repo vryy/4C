6b4b0951f3ff4fe350a160ff5019374353311e9b
Sebastian Proell <sebastian.proell@tum.de>
2024-05-03 16:57:37 +0200

    Cmake: pass test timeout as a cache variable

--- cmake/setup_tests.cmake
+++ cmake/setup_tests.cmake
@@ -1,44 +1,22 @@
-# Determine timeout for each test. Use default one, if it is not passed from the outside.
-if(DEFINED ENV{GLOBAL_TEST_TIMEOUT})
-  set(GLOBAL_TEST_TIMEOUT $ENV{GLOBAL_TEST_TIMEOUT})
-  message(STATUS "Global test timeout is $ENV{GLOBAL_TEST_TIMEOUT} s (before scaling).")
+if(${FOUR_C_BUILD_TYPE_UPPER} EQUAL "DEBUG")
+  set(FOUR_C_TEST_TIMEOUT_SCALE
+      4
+      CACHE STRING "Scale timeout of tests by this factor."
+      )
 else()
-  # default test timeout, if not passed as an environment variable
-  set(GLOBAL_TEST_TIMEOUT 120) # Default timeout
-
-  message(
-    STATUS
-      "Global test timeout is not passed as an environment variable. It is set to the default ${GLOBAL_TEST_TIMEOUT} s (before scaling)."
-    )
+  set(FOUR_C_TEST_TIMEOUT_SCALE
+      1
+      CACHE STRING "Scale timeout of tests by this factor."
+      )
 endif()
+message(STATUS "Global test timeout scale is ${FOUR_C_TEST_TIMEOUT_SCALE}.")
 
-# Determine timeout scale factor. Use default one, if it is not passed from the outside
-if(DEFINED ENV{GLOBAL_TEST_TIMEOUT_SCALE})
-  set(GLOBAL_TEST_TIMEOUT_SCALE $ENV{GLOBAL_TEST_TIMEOUT_SCALE})
-  message(STATUS "Global test timeout scale is $ENV{GLOBAL_TEST_TIMEOUT_SCALE}.")
-else()
-  # default test timeout scale, if not passed as an environment variable
-  if("${CMAKE_BUILD_TYPE}" STREQUAL "DEBUG")
-    set(GLOBAL_TEST_TIMEOUT_SCALE 4) # Default timeout scale for debug configuration
-  else()
-    set(GLOBAL_TEST_TIMEOUT_SCALE 1) # Default timeout scale
-  endif()
-
-  message(
-    STATUS
-      "Global test timeout scale is not passed as an environment variable. It is set to the default ${GLOBAL_TEST_TIMEOUT_SCALE} for this kind of build."
-    )
-endif()
-
-# Determine scaled global test timeout
-math(EXPR GLOBAL_TEST_TIMEOUT_SCALED "${GLOBAL_TEST_TIMEOUT}*${GLOBAL_TEST_TIMEOUT_SCALE}")
-message(STATUS "The scaled global test timeout is ${GLOBAL_TEST_TIMEOUT_SCALED} s.")
+math(EXPR FOUR_C_TEST_TIMEOUT "120*${FOUR_C_TEST_TIMEOUT_SCALE}")
+message(STATUS "The scaled global test timeout is ${FOUR_C_TEST_TIMEOUT} s.")
 
 # Fetch GoogleTest and setup the unit tests if option is enabled
-option(FOUR_C_WITH_GOOGLETEST "Use GoogleTest for unit testing" ON)
+four_c_process_global_option(FOUR_C_WITH_GOOGLETEST "Use GoogleTest for unit testing" ON)
 if(FOUR_C_WITH_GOOGLETEST)
-  message(STATUS "Unit tests with GoogleTest: enabled")
-
   # Define a convenience target for all unit tests and add it to 'full'
   # All unit test executables should add themselves as a dependency to 'unittests'
   add_custom_target(unittests)
@@ -58,23 +36,8 @@ if(FOUR_C_WITH_GOOGLETEST)
     )
   fetchcontent_makeavailable(googletest)
 
-  if(DEFINED ENV{UNITTEST_TIMEOUT})
-    set(UNITTEST_TIMEOUT $ENV{UNITTEST_TIMEOUT})
-    message(STATUS "GoogleTest: The unit test timeout is $ENV{UNITTEST_TIMEOUT} s.")
-  else()
-    # default unit test timeout, if not passed as an environment variable
-    set(UNITTEST_TIMEOUT 10) # Default unit test timeout
-
-    # Determine scaled global test timeout
-    math(EXPR UNITTEST_TIMEOUT "10*${GLOBAL_TEST_TIMEOUT_SCALE}")
-    message(STATUS "The scaled global test timeout is ${UNITTEST_TIMEOUT} s.")
-
-    message(
-      STATUS
-        "GoogleTest: The unit test timeout is not passed as an environment variable. "
-        "It is set to the default value 10 s * ${GLOBAL_TEST_TIMEOUT_SCALE} = ${UNITTEST_TIMEOUT} s."
-      )
-  endif()
+  math(EXPR UNITTEST_TIMEOUT "10*${FOUR_C_TEST_TIMEOUT_SCALE}")
+  message(STATUS "The scaled unit test timeout is ${UNITTEST_TIMEOUT} s.")
 
 else()
   message(STATUS "Unit tests with GoogleTest: disabled")
