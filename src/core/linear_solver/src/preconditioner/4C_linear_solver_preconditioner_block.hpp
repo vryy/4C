3200db582beca9a1dd44ffc8358872ab3aa4af98
Dharini Balachandran <dharini.balachandran@unibw.de>
2024-09-13 11:14:20 +0200

    Remove BGS class definitions

---
+++ src/core/linear_solver/src/preconditioner/4C_linear_solver_preconditioner_block.hpp
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------*/
+/*! \file
+
+\brief Declaration
+
+\level 1
+
+*/
+/*----------------------------------------------------------------------*/
+#ifndef FOUR_C_LINEAR_SOLVER_PRECONDITIONER_BLOCK_HPP
+#define FOUR_C_LINEAR_SOLVER_PRECONDITIONER_BLOCK_HPP
+
+#include "4C_config.hpp"
+
+#include "4C_linear_solver_preconditioner_type.hpp"
+
+FOUR_C_NAMESPACE_OPEN
+
+namespace Core::LinearSolver
+{
+  /// SIMPLE(R) block preconditioner
+  /*!
+    Block preconditioners assume the Epetra_Operator to be a
+    Core::LinAlg::BlockSparseMatrix.
+   */
+  class SimplePreconditioner : public PreconditionerTypeBase
+  {
+   public:
+    SimplePreconditioner(Teuchos::ParameterList& params);
+
+    void setup(bool create, Epetra_Operator* matrix, Epetra_MultiVector* x,
+        Epetra_MultiVector* b) override;
+
+    /// linear operator used for preconditioning
+    Teuchos::RCP<Epetra_Operator> prec_operator() const override { return p_; }
+
+   private:
+    Teuchos::ParameterList& params_;
+    Teuchos::RCP<Epetra_Operator> p_;
+  };
+
+  FOUR_C_NAMESPACE_CLOSE
+
+#endif
