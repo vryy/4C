89026ea5c73ffc9a4a574f379a17514ef151e1fd
Ana Dragos-Corneliu <dragos.ana@tum.de>
2024-10-10 16:25:07 +0200

    Fix Kronecker product
    - Fixed a bug in the component indices of the Kronecker
    product within the material service methods
    - Added a unit test to validate the correct behavior of the Kronecker
    product
    - Updated the framework tests impacted by this bug

--- unittests/mat/4C_material_service_test.cpp
+++ unittests/mat/4C_material_service_test.cpp
@@ -205,6 +205,76 @@ namespace
     FOUR_C_EXPECT_NEAR(s, s_ref, 1.0e-10);
   }
 
+  TEST(MaterialServiceTest, TestKroneckerProduct)
+  {
+    // test the calculation of the fourth order kronecker product of two second-order tensors
+    Core::LinAlg::Matrix<3, 3> a(false);
+    a(0, 0) = 1.0000000000;
+    a(0, 1) = 2.0000000000;
+    a(0, 2) = 3.0000000000;
+    a(1, 0) = 0.0000000000;
+    a(1, 1) = 5.0000000000;
+    a(1, 2) = 1.2000000000;
+    a(2, 0) = 1.0000000000;
+    a(2, 1) = 1.0000000000;
+    a(2, 2) = 1.0000000000;
+
+    Core::LinAlg::Matrix<3, 3> b(false);
+    b(0, 0) = 1.0000000000;
+    b(0, 1) = 0.0000000000;
+    b(0, 2) = 5.0000000000;
+    b(1, 0) = 0.0000000000;
+    b(1, 1) = 0.0000000000;
+    b(1, 2) = 7.0000000000;
+    b(2, 0) = 1.3000000000;
+    b(2, 1) = 1.2000000000;
+    b(2, 2) = 1.0000000000;
+
+    Core::LinAlg::Matrix<6, 6> a_kron_b_ref(true);
+    a_kron_b_ref(0, 0) = 1.0000000000;
+    a_kron_b_ref(0, 1) = 0.0000000000;
+    a_kron_b_ref(0, 2) = 15.0000000000;
+    a_kron_b_ref(0, 3) = 1.0000000000;
+    a_kron_b_ref(0, 4) = 5.0000000000;
+    a_kron_b_ref(0, 5) = 4.0000000000;
+    a_kron_b_ref(1, 0) = 0.0000000000;
+    a_kron_b_ref(1, 1) = 0.0000000000;
+    a_kron_b_ref(1, 2) = 8.4000000000;
+    a_kron_b_ref(1, 3) = 0.0000000000;
+    a_kron_b_ref(1, 4) = 17.5000000000;
+    a_kron_b_ref(1, 5) = 0.0000000000;
+    a_kron_b_ref(2, 0) = 1.3000000000;
+    a_kron_b_ref(2, 1) = 1.2000000000;
+    a_kron_b_ref(2, 2) = 1.0000000000;
+    a_kron_b_ref(2, 3) = 1.2500000000;
+    a_kron_b_ref(2, 4) = 1.1000000000;
+    a_kron_b_ref(2, 5) = 1.1500000000;
+    a_kron_b_ref(3, 0) = 0.0000000000;
+    a_kron_b_ref(3, 1) = 0.0000000000;
+    a_kron_b_ref(3, 2) = 21.0000000000;
+    a_kron_b_ref(3, 3) = 0.0000000000;
+    a_kron_b_ref(3, 4) = 7.0000000000;
+    a_kron_b_ref(3, 5) = 3.5000000000;
+    a_kron_b_ref(4, 0) = 0.0000000000;
+    a_kron_b_ref(4, 1) = 6.0000000000;
+    a_kron_b_ref(4, 2) = 1.2000000000;
+    a_kron_b_ref(4, 3) = 3.2500000000;
+    a_kron_b_ref(4, 4) = 3.2200000000;
+    a_kron_b_ref(4, 5) = 0.7800000000;
+    a_kron_b_ref(5, 0) = 1.3000000000;
+    a_kron_b_ref(5, 1) = 2.4000000000;
+    a_kron_b_ref(5, 2) = 3.0000000000;
+    a_kron_b_ref(5, 3) = 1.9000000000;
+    a_kron_b_ref(5, 4) = 2.8000000000;
+    a_kron_b_ref(5, 5) = 2.4500000000;
+
+    Core::LinAlg::Matrix<6, 6> a_kron_b(true);
+    Mat::add_kronecker_tensor_product(a_kron_b, 1.0, a, b, 0.0);
+
+    FOUR_C_EXPECT_NEAR(a_kron_b, a_kron_b_ref, 1.0e-10);
+  }
+
+
   TEST(MaterialServiceTest, TestDyadicProductMatrixMatrix)
   {
     // test the dyadic product of two Matrices
