7b9008f926703d875c24f494ac9f20941a38d83a
Hoang-Giang Bui <hgbk2008@gmail.com>
2026-09-15 19:16:38 +0100

    Fix the main installation script

--- dependencies/windows/compile_dependencies.ps1
+++ dependencies/windows/compile_dependencies.ps1
@@ -5,14 +5,9 @@
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-# helper function to run and return on error
-function Run($cmd, $args) {
-    & $cmd $args
-    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-}
-
 # Exit the script at the first failure
 $ErrorActionPreference = 'Stop'
+$PSNativeCommandUseErrorActionPreference = $true
 
 $env:DEP_DIR = "$env:USERPROFILE\opt"
 
@@ -20,40 +15,40 @@ $curDir = $PWD.Path
 
 cd $curDir/dependencies/windows/parmetis
 
-Run .\install.bat "$env:DEP_DIR\parmetis"
+.\install.bat "$env:DEP_DIR\parmetis"
 
 cd $curDir/dependencies/windows/lapack
 
-Run .\install.bat "$env:DEP_DIR\lapack"
+.\install.bat "$env:DEP_DIR\lapack"
 
 # cd $curDir/dependencies/windows/boost
 
-# Run .\install.bat "$env:DEP_DIR\boost"
+# .\install.bat "$env:DEP_DIR\boost"
 
 # cd $curDir/dependencies/windows/cln
 
-# Run .\install.bat "$env:DEP_DIR\cln"
+# .\install.bat "$env:DEP_DIR\cln"
 
-# cd $curDir/dependencies/windows/zlib
+cd $curDir/dependencies/windows/zlib
 
-# Run .\install.bat "$env:DEP_DIR\zlib"
+.\install.bat "$env:DEP_DIR\zlib"
 
-# cd $curDir/dependencies/windows/hdf5
+cd $curDir/dependencies/windows/hdf5
 
-# Run .\install.bat "$env:DEP_DIR\hdf5"
+.\install.bat "$env:DEP_DIR\hdf5"
 
-# cd $curDir/dependencies/windows/netcdf
+cd $curDir/dependencies/windows/netcdf
 
-# Run .\install.bat "$env:DEP_DIR\netcdf"
+.\install.bat "$env:DEP_DIR\netcdf"
 
-# cd $curDir/dependencies/windows/suitesparse
+cd $curDir/dependencies/windows/suitesparse
 
-# Run .\install.bat "$env:DEP_DIR\suitesparse"
+.\install.bat "$env:DEP_DIR\suitesparse"
 
-# cd $curDir/dependencies/windows/superlu_dist
+cd $curDir/dependencies/windows/superlu_dist
 
-# Run .\install.bat "$env:DEP_DIR\superlu_dist"
+.\install.bat "$env:DEP_DIR\superlu_dist"
 
-# cd $curDir/dependencies/windows/trilinos
+cd $curDir/dependencies/windows/trilinos
 
-# Run .\install.bat "$env:DEP_DIR\trilinos"
+.\install.bat "$env:DEP_DIR\trilinos"
